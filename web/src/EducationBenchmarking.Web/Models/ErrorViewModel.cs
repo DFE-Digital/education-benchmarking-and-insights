9b372941f5da3ee7615d88841b41764f29648b0f
J05h-L <josh.langford@hippodigital.co.uk>
2023-11-17 11:26:22 +0000

    basic scaffolding for web app created

---
+++ web/src/EducationBenchmarking.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationBenchmarking.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
