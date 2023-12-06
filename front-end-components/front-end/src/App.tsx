c1d018cf349a0dd0b91b10dd881e7c7d15643b01
BASRA <Shanice.BASRA@EDUCATION.GOV.UK>
2023-12-06 15:26:49 +0000

    typescript and unit tests

---
+++ front-end-components/front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header/Header'; // Assuming Header is also a TypeScript component
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
