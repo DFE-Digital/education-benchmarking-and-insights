f5709b85bc2265c556952c7d2e391122c1b663e0
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-06 15:14:20 +0000

    Adds terraform and pipeline for web app

---
+++ web/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {}
+variable "cip-environment" {}
+variable "environment-prefix" {}
+variable "location" {}
+
+variable "sizing" {
+  type = map(object({
+    sku = string
+  }))
+  default = {
+    development = {
+      sku = "B1"
+    }
+  }
+}
