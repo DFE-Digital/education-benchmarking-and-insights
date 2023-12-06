92903fb65a8e5ec728ecc960c2304276dfac5199
J05h-L <josh.langford@hippodigital.co.uk>
2023-12-06 14:41:21 +0000

    compile-sass and build scripts added to build front end - basic header added to _Layout page for testing

---
+++ web/src/EducationBenchmarking.Web/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var sass = require("gulp-dart-sass");
+var async = require("async");
+var rename = require("gulp-rename");
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
+
+const buildSass = () => gulp.src("wwwroot/css/*.scss")
+	.pipe(sass().on("error", sass.logError))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest("wwwroot/css"));
+
+
+const copyGovukAssets = () => gulp.src(["node_modules/govuk-frontend/govuk/assets/**/*"]).pipe(gulp.dest("wwwroot/css/assets")).on("end", () =>
+	gulp.src(["node_modules/govuk-frontend/govuk/all.js"]).pipe(rename("govuk.js")).pipe(gulp.dest("wwwroot/js/")));
+	gulp.src(["node_modules/govuk-frontend/govuk/all.js"]).pipe(uglify()).pipe(rename("govuk.js")).pipe(gulp.dest("wwwroot/js/"));
+
+
+gulp.task("build-fe", () => {
+	return async.series([
+		(next) => buildSass().on("end", next),
+		(next) => copyGovukAssets().on("end", next)
+	])
+}); 
