beb7d2817adc57e8f3836f4783401f47e77c4f2a
Jon Abbott <jon.r.abbott@gmail.com>
2023-12-06 15:17:25 +0000

    Adds terraform and pipeline for web app (#20)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/terraform/main.tf
@@ -0,0 +1,46 @@
+locals {
+  common-tags = {
+    "Environment"      = var.cip-environment
+    "Service Offering" = "DfE Financial Benchmarking service"
+    "Product"          = "DfE Financial Benchmarking service"
+    "Source"           = "terraform"
+  }
+}
+
+data "azurerm_application_insights" "application-insights" {
+  name                = "${var.environment-prefix}-ebis-ai"
+  resource_group_name = "${var.environment-prefix}-ebis-core"
+}
+
+resource "azurerm_resource_group" "resource-group" {
+  name     = "${var.environment-prefix}-ebis-web"
+  location = var.location
+  tags     = local.common-tags
+}
+
+resource "azurerm_app_service_plan" "education-benchmarking-asp" {
+  name                = "${var.environment-prefix}-education-benchmarking-asp"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  os_type             = "Windows"
+  sku_name            = var.sizing[var.environment].sku
+  tags                = local.common-tags
+}
+
+resource "azurerm_windows_web_app" "education-benchmarking-as" {
+  name                    = "${var.environment-prefix}-education-benchmarking"
+  location                = azurerm_resource_group.resource-group.location
+  resource_group_name     = azurerm_resource_group.resource-group.name
+  service_plan_id         = azurerm_app_service_plan.education-benchmarking-asp.id
+  client_affinity_enabled = false
+  https_only              = true
+
+  site_config {
+    http2_enabled = true
+  }
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = data.azurerm_application_insights.application-insights.instrumentation_key
+  }
+  tags = local.common-tags
+}
