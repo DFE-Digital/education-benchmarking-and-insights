598b9973e2a6961ec47fee401f3db8f879ce58a7
J05h-L <josh.langford@hippodigital.co.uk>
2023-12-11 10:46:47 +0000

    tests expanded to cover content

--- web/tests/EducationBenchmarking.Web.Tests/Infrastructure/GivenAHttpContent.cs
+++ web/tests/EducationBenchmarking.Web.Tests/Infrastructure/GivenAHttpContent.cs
@@ -6,141 +6,135 @@ namespace EducationBenchmarking.Web.Tests.Infrastructure;
 
 public class GivenAHttpContent
 {
-
-    //"application/json" => new JsonResponseBody(bytes),
     [Fact]
-    public async Task ShouldReturnJsonResponseBodyGivenMediaType()
+    public async Task ShouldReturnJsonResponseBody()
     {
-        // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
+        // arrange
+        var expected = Encoding.UTF8.GetBytes("test");
+        var testHttpContent = new ByteArrayContent(expected);
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<JsonResponseBody>(result);
+        Assert.True(result.HasContent);
+        Assert.Equal(result.Content, expected);
     }
 
 
-    //"application/json+paged" => new PagedJsonResponseBody(bytes),
     [Fact]
-    public async Task ShouldReturnPagedJsonResponseBodyGivenMediaType()
+    public async Task ShouldReturnPagedJsonResponseBody()
     {
         // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
+        var expected = Encoding.UTF8.GetBytes("test");
+        var testHttpContent = new ByteArrayContent(expected);
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json+paged");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<PagedJsonResponseBody>(result);
+        Assert.True(result.HasContent);
+        Assert.Equal(result.Content, expected);
     }
 
 
-    //"text/plain" => new TextResponseBody(bytes),
     [Fact]
-    public async Task ShouldReturnTextResponseBodyGivenMediaType()
+    public async Task ShouldReturnTextResponseBody()
     {
         // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
+        var expected = Encoding.UTF8.GetBytes("test");
+        var testHttpContent = new ByteArrayContent(expected);
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<TextResponseBody>(result);
+        Assert.True(result.HasContent);
+        Assert.Equal(result.Content, expected);
     }
 
 
-    //"text/html" => new EmptyResponseBody(),
     [Fact]
-    public async Task ShouldReturnEmptyResponseBodyGivenMediaType()
+    public async Task ShouldReturnEmptyResponseBody()
     {
-        // arrange       
+        // arrange
         var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/html");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<EmptyResponseBody>(result);
+        Assert.False(result.HasContent);
     }
 
 
-    //"text/csv" => new CsvResponseBody(bytes),
     [Fact]
-    public async Task ShouldReturnCsvResponseBodyGivenMediaType()
+    public async Task ShouldReturnCsvResponseBody()
     {
         // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
-        //testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/csv");
+        var expected = Encoding.UTF8.GetBytes("test");
+        var testHttpContent = new ByteArrayContent(expected);
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/csv");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<CsvResponseBody>(result);
+        Assert.True(result.HasContent);
+        Assert.Equal(result.Content, expected);
     }
 
 
-    //"application/pdf" => new PdfResponseBody(bytes),
     [Fact]
-    public async Task ShouldReturnPdfResponseBodyGivenMediaType()
+    public async Task ShouldReturnPdfResponseBody()
     {
         // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
+        var expected = Encoding.UTF8.GetBytes("test");
+        var testHttpContent = new ByteArrayContent(expected);
         testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<PdfResponseBody>(result);
+        Assert.True(result.HasContent);
+        Assert.Equal(result.Content, expected);
     }
 
 
-    //if (content is not { Headers.ContentLength: > 0 }) return new EmptyResponseBody();
     [Fact]
-    public async Task ShouldEmptyResponseBodyWithZeroContentLength()
+    public async Task ShouldReturnEmptyResponseBodyWhenContentLengthZero()
     {
         // arrange       
-        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
-        testHttpContent.Headers.ContentLength = 0;
+        var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes(""));
+        testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
 
         // act
         var result = await ApiResponseBody.FromHttpContent(testHttpContent);
 
-        //assert
+        // assert
         Assert.IsType<EmptyResponseBody>(result);
+        Assert.False(result.HasContent);
     }
 
 
-    //_ => throw new ArgumentException($"Unknown MIME type: {content.Headers.ContentType?.MediaType}")
     [Fact]
-    public async Task ShouldThrowExceptionWithUnknownMediaType()
+    public async Task ShouldThrowException()
     {
         // arrange       
         var testHttpContent = new ByteArrayContent(Encoding.UTF8.GetBytes("test"));
-        // testHttpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("");
 
         // act + assert
         await Assert.ThrowsAsync<ArgumentException>(() => ApiResponseBody.FromHttpContent(testHttpContent));
     }
-
-
-    // arrange 
-    // act
-    //assert
-
-
-
-
-    // arrange 
-    // act
-    //assert
 }
