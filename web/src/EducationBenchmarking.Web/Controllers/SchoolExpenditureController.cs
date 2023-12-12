410318b28fec929d760acd10c502d4d2bfb71138
J05h-L <josh.langford@hippodigital.co.uk>
2023-12-12 10:25:28 +0000

    ﻿expenditure page created

---
+++ web/src/EducationBenchmarking.Web/Controllers/SchoolExpenditureController.cs
@@ -0,0 +1,26 @@
+﻿using EducationBenchmarking.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationBenchmarking.Web.Controllers
+{
+    public class SchoolExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new SchoolExpenditureViewModel
+            {
+                SchoolName = "MySchool",
+                LastFinancialYear = "2022"
+            };
+
+            //    var query = HttpContext.Request.QueryString.Value;
+
+            //    var viewModel = new SchoolExpenditureViewModel
+            //    {
+            //        QueryString = string.IsNullOrEmpty(query) ? "" : query.Substring(1)
+            //};
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ web/src/EducationBenchmarking.Web/ViewModels/SchoolExpenditureViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationBenchmarking.Web.ViewModels
+{
+    public class SchoolExpenditureViewModel
+    {
+        public required string SchoolName { get; set; }
+
+        public required string LastFinancialYear { get; set; }
+    }
+}
