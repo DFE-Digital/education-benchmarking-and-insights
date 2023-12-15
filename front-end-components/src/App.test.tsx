8678d69e2d2b87615c3d546057c3f40413a104ac
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-15 16:19:26 +0000

    make test a standard “should render” for now

--- front-end-components/src/App.test.tsx
+++ front-end-components/src/App.test.tsx
@@ -6,7 +6,5 @@ import App from './App';
 describe('App component', () => {
   test('renders App component', () => {
     render(<App />);
-    const headerElement = screen.getByRole('banner');
-    expect(headerElement).toBeInTheDocument();
   });
 });
