9f73496516dc0e07addb1a63e3adf73d2478c2f6
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-18 19:39:08 +0000

    Use Vite lib mode rather than webpack

--- front-end-components/vite.config.ts
+++ front-end-components/vite.config.ts
@@ -2,14 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'Educational Benchmarking - Front-end components',
+      formats: ['es'],
+      fileName: `front-end.js`,
+    }
+  },
   resolve: {
     alias: {
       '@govuk-react/icon-crown': path.resolve(__dirname, 'node_modules/@govuk-react/icon-crown'),
     },
   },
-
 })
