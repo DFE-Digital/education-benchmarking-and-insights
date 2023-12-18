95e585d5b2a2dace8794d7b7423e9397f44c3a0c
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-18 21:27:14 +0000

    Add boilerplate pages

---
+++ web/src/EducationBenchmarking.Web/Controllers/SchoolDetailsController.cs
@@ -0,0 +1,34 @@
+﻿using EducationBenchmarking.Web.Infrastructure.Apis;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationBenchmarking.Web.Controllers;
+
+[Controller]
+[Route("school/{urn}/details")]
+public class SchoolDetailsController : Controller
+{
+    private readonly ILogger<SchoolDetailsController> _logger;
+
+    public SchoolDetailsController(ILogger<SchoolDetailsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index(string urn)
+    {
+        using (_logger.BeginScope(new {urn}))
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error displaying school details: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+}
