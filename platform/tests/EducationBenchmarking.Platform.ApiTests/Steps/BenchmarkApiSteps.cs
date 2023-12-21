816ab7b65e9741ebe04d73564ef49f77021be91b
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2023-12-21 11:50:35 +0000

    benchmark api tests

--- platform/tests/EducationBenchmarking.Platform.ApiTests/Steps/BenchmarkApiSteps.cs
+++ platform/tests/EducationBenchmarking.Platform.ApiTests/Steps/BenchmarkApiSteps.cs
@@ -23,18 +23,18 @@ namespace EducationBenchmarking.Platform.ApiTests.Steps
         [Given(@"the School Comparator Set API is running")]
         public void GivenTheSchoolComparatorSetApiIsRunning()
         {
-            // Additional setup code for initializing or checking the API's availability
         }
 
-        [When(@"I send a POST request to get comparators with the following data:")]
-        public async Task WhenISendAPostRequestToGetComparatorsWithTheFollowingData(Table table)
+        [When(@"I send a POST request to get comparators with the following payload:")]
+        public async Task WhenISendAPostRequestToGetComparatorsWithTheFollowingPayload(Table table)
         {
-            var requestData = table.Rows.ToDictionary(row => row["requestJson"], row => row["requestJson"]);
-            var jsonContent = JsonConvert.SerializeObject(requestData);
+            var requestData = table.Rows.ToDictionary(row => row["Type"], row => row["Body"]);
+            var jsonContent = requestData["requestJson"];
 
             var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
             _response = await _httpClient.PostAsync($"{Config.Apis.Benchmark.Host}/api/schools/comparator-set", content);
         }
+
 
         [Then(@"the response status code for benchmark api should be (.*)")]
         public void ThenTheResponseStatusCodeForBenchmarkApiShouldBe(int expectedStatusCode)
@@ -45,28 +45,56 @@ namespace EducationBenchmarking.Platform.ApiTests.Steps
         [Then(@"the response body should contain a valid School Comparator Set")]
         public async Task ThenTheResponseBodyShouldContainAValidSchoolComparatorSet()
         {
-            // Implement assertions for the structure and validity of the School Comparator Set in the response body
-            _response.EnsureSuccessStatusCode(); // Ensure the response is successful
-
+            _response.EnsureSuccessStatusCode();
             var responseBody = await _response.Content.ReadAsStringAsync();
             var schoolComparatorSet = JsonConvert.DeserializeObject<JObject>(responseBody);
-
             schoolComparatorSet.Should().NotBeNull();
             schoolComparatorSet["results"].Should().NotBeNull();
-            schoolComparatorSet["results"].Children().Should().HaveCountGreaterThan(0);
-            // Add more specific assertions based on the structure of your School Comparator Set
+            var schools = schoolComparatorSet["results"].ToObject<List<JObject>>();
+            foreach (var school in schools)
+            {
+                school.Should().ContainKey("urn");
+                school.Should().ContainKey("name");
+                school.Should().ContainKey("financeType");
+                school.Should().ContainKey("kind");
+            }
         }
 
         [Then(@"the response size should be '(.*)'")]
         public async Task ThenTheResponseSizeShouldBe(string expectedSize)
         {
-            // Implement assertions to check the size of the School Comparator Set in the response
             var responseBody = await _response.Content.ReadAsStringAsync();
             var schoolComparatorSet = JsonConvert.DeserializeObject<JObject>(responseBody);
-
             schoolComparatorSet.Should().NotBeNull();
             schoolComparatorSet["results"].Should().NotBeNull();
             schoolComparatorSet["results"].Children().Should().HaveCount(int.Parse(expectedSize));
         }
+        
+        [When(@"I send a POST request to get trusts comparators with the following payload:")]
+        public async Task WhenISendAPostRequestToGetTrustsComparatorsWithTheFollowingPayload(Table table)
+        {
+            var requestData = table.Rows.ToDictionary(row => row["Type"], row => row["Body"]);
+            var jsonContent = requestData["requestJson"];
+
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            _response = await _httpClient.PostAsync($"{Config.Apis.Benchmark.Host}/api/trusts/comparator-set", content);
+        }
+        [Then(@"the response body should contain a valid trusts Comparator Set")]
+        public async Task ThenTheResponseBodyShouldContainAValidTrustsComparatorSet()
+        {
+            _response.EnsureSuccessStatusCode();
+            var responseBody = await _response.Content.ReadAsStringAsync();
+            var academyComparatorSet = JsonConvert.DeserializeObject<JObject>(responseBody);
+            academyComparatorSet.Should().NotBeNull();
+            academyComparatorSet["results"].Should().NotBeNull();
+            var academies = academyComparatorSet["results"].ToObject<List<JObject>>();
+            foreach (var academy in academies)
+            {
+                academy.Should().ContainKey("companyNumber");
+                academy.Should().ContainKey("name");
+            }
+        }
+
+
     }
 }
