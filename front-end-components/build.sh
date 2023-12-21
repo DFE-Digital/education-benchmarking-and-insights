9351f702190f6b65cf483308daf63ca25a2df668
Joe El Kadi <joeekadi@gmail.com>
2023-12-21 10:34:33 +0000

    delete

--- front-end-components/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm install
-
-npm test
-npm run build
-
-mkdir -p ../web/src/EducationBenchmarking.Web/wwwroot/js
-cp dist/front-end.js ../web/src/EducationBenchmarking.Web/wwwroot/js
