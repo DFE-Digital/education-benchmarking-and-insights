7a69607c7a8e25ccb0bdb260f626611d4022db05
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2023-12-22 15:40:16 +0000

    added pages

---
+++ web/tests/EducationBenchmarking.Web.E2ETests/Steps/CompareYourCostsSteps.cs
@@ -0,0 +1,29 @@
+using Microsoft.Playwright;
+
+namespace EducationBenchmarking.Web.E2ETests.Steps;
+[Binding]
+public class CompareYourCostsSteps
+{
+     private readonly IPage _page;
+
+     public CompareYourCostsSteps(IPage page)
+     {
+          _page = page;
+     }
+
+     [Given(@"I search for a school on search page")]
+     public void GivenISearchForASchoolOnSearchPage()
+     {
+          
+     }
+
+     [Then(@"the expenditure benchmarking page is displayed")]
+     public void ThenTheExpenditureBenchmarkingPageIsDisplayed()
+     {
+          /*//add checks for the below 
+          And the common header and footer are used as per the design
+          And the URL is bookmarkable and contains the school's URN
+          And the school name is displayed at the top of the page*/
+          ScenarioContext.StepIsPending();
+     }
+}
--- web/tests/EducationBenchmarking.Web.E2ETests/Steps/SchoolHomePageSteps.cs
+++ web/tests/EducationBenchmarking.Web.E2ETests/Steps/SchoolHomePageSteps.cs
@@ -1,29 +1,6 @@
-using Microsoft.Playwright;
-
 namespace EducationBenchmarking.Web.E2ETests.Steps;
 [Binding]
 public class SchoolHomePageSteps
 {
-     private readonly IPage _page;
-
-     public SchoolHomePageSteps(IPage page)
-     {
-          _page = page;
-     }
-
-     [Given(@"I search for a school on search page")]
-     public void GivenISearchForASchoolOnSearchPage()
-     {
-          
-     }
-
-     [Then(@"the expenditure benchmarking page is displayed")]
-     public void ThenTheExpenditureBenchmarkingPageIsDisplayed()
-     {
-          /*//add checks for the below 
-          And the common header and footer are used as per the design
-          And the URL is bookmarkable and contains the school's URN
-          And the school name is displayed at the top of the page*/
-          ScenarioContext.StepIsPending();
-     }
+    
 }
