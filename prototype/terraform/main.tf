6a4b3830b2500853cd1a08650cd29ae4dacfac5d
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-22 09:49:26 +0000

    Add tags to prototype resource and change start up command

--- prototype/terraform/main.tf
+++ prototype/terraform/main.tf
@@ -1,16 +1,16 @@
 locals {
   common-tags = {
-    "Environment"               = var.cip-environment
-    "Service Offering"          = "DfE Financial Benchmarking service"
-    "Product"                   = "DfE Financial Benchmarking service"
-    "Source"                    = "terraform"
+    "Environment"      = var.cip-environment
+    "Service Offering" = "DfE Financial Benchmarking service"
+    "Product"          = "DfE Financial Benchmarking service"
+    "Source"           = "terraform"
   }
 }
 
 resource "azurerm_resource_group" "resource-group" {
-  name = "${var.environment-prefix}-ebis-prototype"
+  name     = "${var.environment-prefix}-ebis-prototype"
   location = var.location
-  tags = local.common-tags
+  tags     = local.common-tags
 }
 
 resource "azurerm_service_plan" "app-service-plan" {
@@ -19,14 +19,16 @@ resource "azurerm_service_plan" "app-service-plan" {
   resource_group_name = azurerm_resource_group.resource-group.name
   os_type             = "Linux"
   sku_name            = "B1"
+  tags                = local.common-tags
 }
 
 resource "azurerm_linux_web_app" "web-app" {
-  name                  = "${var.environment-prefix}-ebis-prototype"
-  location              = azurerm_resource_group.resource-group.location
-  resource_group_name   = azurerm_resource_group.resource-group.name
-  service_plan_id       = azurerm_service_plan.app-service-plan.id
-  https_only            = true
+  name                = "${var.environment-prefix}-ebis-prototype"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  service_plan_id     = azurerm_service_plan.app-service-plan.id
+  tags                = local.common-tags
+  https_only          = true
   site_config {
     minimum_tls_version = "1.2"
   }
