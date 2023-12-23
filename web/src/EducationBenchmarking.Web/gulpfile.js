9ac3d90031abdd026742c5c24d9bff93a5fe8dc6
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2023-12-23 14:09:28 +0000

    Bump front-end version and import css for front-end components

--- web/src/EducationBenchmarking.Web/gulpfile.js
+++ web/src/EducationBenchmarking.Web/gulpfile.js
@@ -14,6 +14,7 @@ const buildSass = () => gulp.src("AssetSrc/scss/*.scss")
 const copyStaticAssets = () => gulp.src(["node_modules/govuk-frontend/dist/govuk/assets/**/*"]).pipe(gulp.dest("wwwroot/assets")).on("end", () =>
 	gulp.src(["node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js"]).pipe(gulp.dest("wwwroot/js/"))).on("end", () =>
 	gulp.src(["node_modules/front-end/dist/front-end.js"]).pipe(gulp.dest("wwwroot/js/"))).on("end", () =>
+	gulp.src(["node_modules/front-end/dist/front-end.css"]).pipe(gulp.dest("wwwroot/css/"))).on("end", () =>
 	gulp.src(["AssetSrc/images/*"]).pipe(gulp.dest("wwwroot/assets/images")));
 
 
