a45a93d5059e7c7b9605aa95266830bfc3388ce6
Faizi52996 <111439003+Faizi52996@users.noreply.github.com>
2024-01-02 17:55:19 +0000

    Feature/185789/benchmark api tests contd (#53)
    * added tests for benchmark API
    
    * benchmark api tests
    
    * udpated steps and added tests for trusts 500 error
    
    * updated steps for readibility
    
    * -downgraded specflow for compactibility.
    -removed build warnings.
    
    * benchmark api tests
    
    * udpated steps and added tests for trusts 500 error
    
    * updated steps for readibility
    
    * -downgraded specflow for compactibility.
    -removed build warnings.
    
    * - updated scenarios and removed old ones
    - updated specflow version to latest
    
    ---------
    
    Co-authored-by: Faizan Ahmad <faizan.ahmad@education.gov.uk>

---
+++ platform/tests/EducationBenchmarking.Platform.ApiTests/Features/BenchmarkApi.feature
@@ -0,0 +1,11 @@
+﻿Feature: BenchmarkApi API Endpoint Testing
+        Scenario: Create a comparator set successfully
+            Given I have a valid comparator set request of size set to '7'
+            When I submit the request
+            Then a valid comparator set of size '7' should be returned
+            And the response status code api is 200
+
+    Scenario: Get error for Invalid School Comparator Set Request
+        Given I have a invalid comparator set request of size set to 'invalid'
+        When I submit the request
+        Then the response status code api is 500
---
+++ platform/tests/EducationBenchmarking.Platform.ApiTests/Steps/BenchmarkApiSteps.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Text;
+using EducationBenchmarking.Platform.ApiTests.TestSupport;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EducationBenchmarking.Platform.ApiTests.Steps
+{
+    [Binding]
+    public class BenchmarkApiSteps
+    {
+        private readonly HttpClient _httpClient;
+        private HttpResponseMessage _response;
+        private StringContent _content;
+
+        public BenchmarkApiSteps()
+        {
+            var httpClientHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(httpClientHandler);
+            _response = null!;
+            _content = null!;
+        }
+
+        [Then(@"the response status code api is (.*)")]
+        public void ThenTheResponseStatusCodeApiIs(int expectedStatusCode)
+        {
+            _response.StatusCode.Should().Be((HttpStatusCode)expectedStatusCode);
+        }
+
+        [Then(@"a valid comparator set of size '(.*)' should be returned")]
+        public async Task ThenAValidComparatorSetOfSizeShouldBeReturned(string expectedSize)
+        {
+            _response.EnsureSuccessStatusCode();
+            var responseBody = await _response.Content.ReadAsStringAsync();
+            var schoolComparatorSet = JsonConvert.DeserializeObject<JObject>(responseBody);
+            schoolComparatorSet.Should().NotBeNull();
+            schoolComparatorSet["results"].Should().NotBeNull();
+            var schools = schoolComparatorSet["results"].ToObject<List<JObject>>();
+            foreach (var school in schools)
+            {
+                school.Should().ContainKey("urn");
+                school.Should().ContainKey("name");
+                school.Should().ContainKey("financeType");
+                school.Should().ContainKey("kind");
+            }
+
+            schoolComparatorSet["results"].Children().Should().HaveCount(int.Parse(expectedSize));
+        }
+
+        
+        [When(@"I submit the request")]
+        public async Task WhenISubmitTheRequest()
+        {
+            _response = await _httpClient.PostAsync($"{Config.Apis.Benchmark.Host}/api/comparator-set", _content);
+        }
+
+
+        [Given(@"I have a valid comparator set request of size set to '(.*)'")]
+        public void GivenIHaveAValidComparatorSetRequestOfSizeSetTo(string size)
+        {
+            var jsonContent = "{\"includeSet\": true, \"size\": " + size + "}";
+            _content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        }
+
+        [Given(@"I have a invalid comparator set request of size set to '(.*)'")]
+        public void GivenIHaveAInvalidComparatorSetRequestOfSizeSetTo(string size)
+        {
+            var jsonContent = "{\"includeSet\": true, \"size\": " + size + "}";
+            _content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        }
+    }
+}
