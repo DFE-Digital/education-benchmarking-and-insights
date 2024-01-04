e19207f9cf6f2472f0f5c489632b4ec6bfe35346
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2024-01-04 15:08:22 +0000

    completed download expenditure chart

--- web/tests/EducationBenchmarking.Web.E2ETests/Pages/CompareYourCostsPage.cs
+++ web/tests/EducationBenchmarking.Web.E2ETests/Pages/CompareYourCostsPage.cs
@@ -6,6 +6,7 @@ namespace EducationBenchmarking.Web.E2ETests.Pages;
 public class CompareYourCostsPage
 {
     private IPage _page;
+    private Task<IDownload> _downloadEvent;
 
     public CompareYourCostsPage(IPage page)
     {
@@ -15,11 +16,15 @@ public class CompareYourCostsPage
     private ILocator PageH1Heading => _page.Locator("h1");
     private ILocator BreadCrumbs => _page.Locator(".govuk-breadcrumbs");
     private ILocator ChangeSchoolLink => _page.Locator("#change-school");
-    private ILocator SaveImageTotalExpenditure => _page.Locator("xpath=//*[@id='compare-your-school']/div[2]/div[2]/button");
+
+    private ILocator SaveImageTotalExpenditure =>
+        _page.Locator("xpath=//*[@id='compare-your-school']/div[2]/div[2]/button");
+
     private ILocator TotalExpenditureDimension => _page.Locator("#total-expenditure-dimension");
 
     private ILocator TotalExpenditureChart =>
         _page.Locator("xpath=//*[@id='compare-your-school']/div[3]/div/div/canvas");
+
     private ILocator ShowAllBtn => _page.Locator("span.govuk-accordion__show-all-text");
 
     private ILocator TeachingAndTeachingSupportStaffAccordion =>
@@ -27,33 +32,27 @@ public class CompareYourCostsPage
 
     public async Task AssertPage()
     {
-        
-      await PageH1Heading.IsVisibleAsync();
-      await BreadCrumbs.IsVisibleAsync();
-      await ChangeSchoolLink.IsVisibleAsync();
-      await  SaveImageTotalExpenditure.IsVisibleAsync();
-      await TotalExpenditureDimension.IsVisibleAsync();
-      await TotalExpenditureChart.IsVisibleAsync();
+        await PageH1Heading.IsVisibleAsync();
+        await BreadCrumbs.IsVisibleAsync();
+        await ChangeSchoolLink.IsVisibleAsync();
+        await SaveImageTotalExpenditure.IsVisibleAsync();
+        await TotalExpenditureDimension.IsVisibleAsync();
+        await TotalExpenditureChart.IsVisibleAsync();
         //check total dimesion defualt value
         await ShowAllBtn.IsVisibleAsync();
     }
 
     public async Task ClickOnSaveImg()
     {
-        await _page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
         await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
-       
-       // await Task.Delay(7000);
+         _downloadEvent = _page.WaitForDownloadAsync();
         await SaveImageTotalExpenditure.ClickAsync();
-        
     }
 
     public async Task AssertImageDownload()
     {
-            var downloadEvent = await _page.WaitForDownloadAsync();
-            await SaveImageTotalExpenditure.ClickAsync();
-            string downloadedFilePath = downloadEvent.SuggestedFilename;
-            Assert.EndsWith(".png", downloadedFilePath, StringComparison.OrdinalIgnoreCase);
+        var download =await _downloadEvent;
+        string downloadedFilePath = download.SuggestedFilename;
+        Assert.Equal("total-expenditure.png", downloadedFilePath);
     }
-
 }
