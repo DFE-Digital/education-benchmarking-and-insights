3939d567e83d9bc124eed559f32e3c34738315d9
Jon Abbott <jon.r.abbott@gmail.com>
2024-01-05 12:08:29 +0000

    Adds razor view, and controller, etc, for workforce data (#62)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/src/EducationBenchmarking.Web/ViewModels/SchoolWorkforceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using EducationBenchmarking.Web.Domain;
+
+namespace EducationBenchmarking.Web.ViewModels
+{
+    public class SchoolWorkforceViewModel
+    {
+        private readonly School _school;
+        private readonly FinanceYears _years;
+
+        public SchoolWorkforceViewModel(School school, FinanceYears years)
+        {
+            _school = school;
+            _years = years;
+        }
+
+        public string Urn => _school.Urn;
+        public string Name => _school.Name;
+        public string AcademiesFinancePeriod => $"{_years.Academies - 1} / {_years.Academies}";
+        public string MaintainedSchoolsFinancePeriod => $"{_years.MaintainedSchools - 1} - {_years.MaintainedSchools}";
+    }
+    
+}
