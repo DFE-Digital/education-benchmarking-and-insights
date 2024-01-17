b03c8b8d34a7dd141fff08fd257cdbfd8490d4f5
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2024-01-17 10:50:11 +0000

    added tests for insights miscellaneous

---
+++ platform/tests/EducationBenchmarking.Platform.ApiTests/Steps/InsightsFinanceYearSteps.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using EducationBenchmarking.Platform.ApiTests.Drivers;
+using EducationBenchmarking.Platform.ApiTests.TestSupport;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit.Sdk;
+
+namespace EducationBenchmarking.Platform.ApiTests.Steps;
+
+[Binding]
+public class InsightsFinanceYearSteps
+{
+    private const string GetFinanceYearKey = "get-finance-year";
+    private readonly ApiDriver _api = new(Config.Apis.Insight ?? throw new NullException(Config.Apis.Insight));
+    [Given(@"a valid finance year request")]
+    public void GivenAValidFinanceYearRequest()
+    {
+        _api.CreateRequest(GetFinanceYearKey, new HttpRequestMessage
+        {
+            RequestUri = new Uri($"/api/finance-years", UriKind.Relative),
+            Method = HttpMethod.Get
+        });
+    }
+
+    [When(@"I submit the finance year request")]
+    public async Task WhenISubmitTheFinanceYearRequest()
+    {
+        await _api.Send();
+    }
+
+    [Then(@"the finance year result should be ok")]
+    public async Task ThenTheFinanceYearResultShouldBeOk()
+    {
+        var response = _api[GetFinanceYearKey].Response ?? throw new NullException(_api[GetFinanceYearKey].Response);
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+        var jsonContent = JsonConvert.DeserializeObject<JObject>(content);
+        jsonContent.Should().ContainKey("academies");
+        jsonContent.Should().ContainKey("maintainedSchools");
+    }
+}
