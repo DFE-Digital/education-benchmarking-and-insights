a9990941f5b37ce09d826fefd54d420bb13e0dfc
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-01-19 15:46:43 +0000

    integration tests added (#80)
    integration tests added

--- web/tests/EducationBenchmarking.Web.Integration.Tests/Paths.cs
+++ web/tests/EducationBenchmarking.Web.Integration.Tests/Paths.cs
@@ -10,7 +10,9 @@ public static class Paths
     public static string SchoolHome(string urn) => $"/school/{urn}";
     public static string SchoolComparison(string urn) => $"/school/{urn}/comparison";
     public static string SchoolWorkforce(string urn) => $"/school/{urn}/workforce";
-    
+    public static string SchoolInvestigation(string urn) => $"/school/{urn}/investigation";
+    public static string SchoolCurriculumPlanning(string urn) => $"/school/{urn}/financial-planning";
+
     public static string ToAbsolute(this string path)
     {
         return $"https://localhost{path}";
