ec86aeb8bc43da8dd94d3bc6a19a450575d2c103
Josh Langford <josh.langford@hippodigital.co.uk>
2024-01-19 14:28:24 +0000

    WiP

--- web/src/EducationBenchmarking.Web/Controllers/SchoolPlanningController.cs
+++ web/src/EducationBenchmarking.Web/Controllers/SchoolPlanningController.cs
@@ -10,7 +10,6 @@ using EducationBenchmarking.Web.Infrastructure.Extensions;
 namespace EducationBenchmarking.Web.Controllers;
 
 [Controller]
-[Route("school/{urn}/financial-planning")]
 public class SchoolPlanningController : Controller
 {
     private readonly IEstablishmentApi _establishmentApi;
@@ -23,6 +22,7 @@ public class SchoolPlanningController : Controller
     }
 
     [HttpGet]
+    [Route("school/{urn}/financial-planning")]
     public async Task<IActionResult> Index(string urn)
     {
         using (_logger.BeginScope(new { urn }))
@@ -42,6 +42,33 @@ public class SchoolPlanningController : Controller
                 var viewModel = new SchoolPlanningViewModel(school);
 
                 return View(viewModel);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error displaying school details: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+
+    [HttpGet]
+    [Route("school/{urn}/financial-planning/help")]
+    public async Task<IActionResult> Help(string urn)
+    {
+        using (_logger.BeginScope(new { urn }))
+        {
+            try
+            {
+                var parentNode = new MvcBreadcrumbNode("Index", "School", "Your school") { RouteValues = new { urn } };
+                var childNode = new MvcBreadcrumbNode("Index", "SchoolPlanning", "Curriculum and financial planning")
+                {
+                    RouteValues = new { urn },
+                    Parent = parentNode
+                };
+
+                ViewData["BreadcrumbNode"] = childNode;
+
+                return View();
             }
             catch (Exception e)
             {
