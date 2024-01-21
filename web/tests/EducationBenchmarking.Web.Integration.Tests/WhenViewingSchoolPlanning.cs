92a41de70c13929ab63b8a401795503aca183135
J05h-L <josh.langford@hippodigital.co.uk>
2024-01-21 16:12:48 +0000

    WiP - int tests

---
+++ web/tests/EducationBenchmarking.Web.Integration.Tests/WhenViewingSchoolPlanning.cs
@@ -0,0 +1,62 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.XPath;
+using AutoFixture;
+using EducationBenchmarking.Web.Domain;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace EducationBenchmarking.Web.Integration.Tests;
+
+public class WhenViewingSchoolPlanning : BenchmarkingWebAppClient
+{
+    public WhenViewingSchoolPlanning(BenchmarkingWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Theory]
+    [InlineData(true)]
+    public async Task CanDisplaySchool(bool value)
+    {
+        Assert.True(value);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    public async Task CanNavigateToHelp(bool value)
+    {
+        Assert.True(value);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    public async Task CanNavigateToContinue(bool value)
+    {
+        Assert.True(value);
+    }
+
+
+    [Fact]
+    public async Task CanDisplayNotFound()
+    {
+        var page = await SetupEstablishmentWithNotFound()
+            .Navigate(Paths.SchoolComparison("12345"));
+
+        DocumentAssert.AssertPageUrl(page, Paths.StatusError(404).ToAbsolute());
+    }
+
+    [Fact]
+    public async Task CanDisplayProblemWithService()
+    {
+        var page = await SetupEstablishmentWithException()
+            .Navigate(Paths.SchoolComparison("12345"));
+
+        DocumentAssert.AssertPageUrl(page, Paths.StatusError(500).ToAbsolute());
+    }
+
+    private static void AssertPageLayout(IHtmlDocument page, School school)
+    {
+        Assert.True(true);
+        Assert.IsAssignableFrom<IHtmlDocument>(page);
+        Assert.IsType<School>(school);
+    }
+}
---
+++ web/tests/EducationBenchmarking.Web.Integration.Tests/WhenViewingSchoolPlanningHelp.cs
@@ -0,0 +1,36 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.XPath;
+using AutoFixture;
+using EducationBenchmarking.Web.Domain;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace EducationBenchmarking.Web.Integration.Tests
+{
+    public class WhenViewingSchoolPlanningHelp : BenchmarkingWebAppClient
+    {
+        public WhenViewingSchoolPlanningHelp(BenchmarkingWebAppFactory factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task CanNavigateBack()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task CanNavigateToSubmitEnquiry()
+        {
+            Assert.True(true);
+        }
+
+        private static void AssertPageLayout(IHtmlDocument page, School school)
+        {
+            Assert.True(true);
+            Assert.IsAssignableFrom<IHtmlDocument>(page);
+            Assert.IsType<School>(school);
+        }
+
+    }
+}
