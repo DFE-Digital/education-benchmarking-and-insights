113c58d838d0fee96f89524aac383accda4b11c4
Michael Fielding <michael.fielding@education.gov.uk>
2024-01-24 09:50:39 +0000

    Updates App Insights to use Log Analytics Workspace

--- core-infrastructure/terraform/main.tf
+++ core-infrastructure/terraform/main.tf
@@ -47,13 +47,20 @@ resource "azurerm_key_vault_access_policy" "terraform_sp_access" {
   key_permissions = ["Get", "List", "Create", "Purge", "Delete"]
 }
 
+resource "azurerm_log_analytics_workspace" "application-insights-workspace" {
+  name = "${var.environment-prefix}-ebis-aiw"
+  location = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  retention_in_days = 180
+  tags = local .common-tags
+}
 
 resource "azurerm_application_insights" "application-insights" {
   name                = "${var.environment-prefix}-ebis-ai"
   location            = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
   application_type    = "web"
-
+  workspace_id = azurerm_log_analytics_workspace.application-insights-workspace.id
   tags = local.common-tags
 }
 
