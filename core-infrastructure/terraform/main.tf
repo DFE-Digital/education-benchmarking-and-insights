6b7e87ef8b5e5f3a0eb68d67ece19f07aa7cf266
Michael Fielding <michael.fielding@education.gov.uk>
2024-01-24 10:42:34 +0000

    Story Id: 191512 Fixed erroneous space.

--- core-infrastructure/terraform/main.tf
+++ core-infrastructure/terraform/main.tf
@@ -52,7 +52,7 @@ resource "azurerm_log_analytics_workspace" "application-insights-workspace" {
   location = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
   retention_in_days = 180
-  tags = local .common-tags
+  tags = local.common-tags
 }
 
 resource "azurerm_application_insights" "application-insights" {
