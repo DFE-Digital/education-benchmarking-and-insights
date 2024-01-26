d22217038214d9b442a0ee5167837ed3528dcffb
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2024-01-26 13:53:24 +0000

    added E2E tests for ICFP Page one

---
+++ web/tests/EducationBenchmarking.Web.E2ETests/Features/CurriculumFinancialPlanning.feature
@@ -0,0 +1,12 @@
+﻿Feature: CurriculumFinancialPlanning
+Integrated Curriculum and financial planning feature
+
+    Scenario: see help page of curriculum and financial planning
+        Given I am on page 1 of the curriculum and financial planning journey for school with URN '139696'
+        When I click the can be found here in the content of page one
+        Then I am on the help page
+
+    Scenario: Go back from help page
+        Given I am on the help page  for school with URN '139696'
+        When I click back CTA on help page
+        Then I am on page 1 of the curriculum and financial planning journey
---
+++ web/tests/EducationBenchmarking.Web.E2ETests/Pages/CurriculumFinancialPlanning/ICFPPageOne.cs
@@ -0,0 +1,40 @@
+﻿using EducationBenchmarking.Web.E2ETests.Helpers;
+using EducationBenchmarking.Web.E2ETests.Hooks;
+using EducationBenchmarking.Web.E2ETests.TestSupport;
+using Microsoft.Playwright;
+
+namespace EducationBenchmarking.Web.E2ETests.Pages.CurriculumFinancialPlanning;
+
+public class ICFPPageOne(PageHook page)
+{
+    private readonly IPage _page = page.Current;
+    private ILocator PageH1Heading => _page.Locator("h1");
+    private ILocator BreadCrumbs => _page.Locator(".govuk-breadcrumbs");
+    private ILocator ContinueCta => _page.Locator(".govuk-button", new PageLocatorOptions { HasText = "Continue" });
+    private ILocator HelpPageLink => _page.Locator(".govuk-link", new PageLocatorOptions { HasText = "can be found here" });
+    
+    public async Task AssertPage()
+    {
+       await PageH1Heading.ShouldBeVisible();
+       await BreadCrumbs.ShouldBeVisible();
+       await ContinueCta.ShouldBeVisible();
+       await ContinueCta.ShouldBeEnabled();
+       await HelpPageLink.ShouldBeVisible();
+       
+       
+    }
+    public async Task GoToPage(string urn)
+    {
+        await _page.GotoAsync(PageUrl(urn));
+    }
+
+    private static string PageUrl(string urn)
+    {
+        return $"{Config.BaseUrl}/school/{urn}/financial-planning";
+    }
+
+    public async Task clickOnHelpBtn()
+    {
+        await HelpPageLink.Click();
+    }
+}
