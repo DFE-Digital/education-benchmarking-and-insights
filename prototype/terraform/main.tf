c2cb28799db8edba0285f6ecfc58551250058e01
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-02-01 15:38:07 +0000

    chore: resolve warnings on prototype tf

--- prototype/terraform/main.tf
+++ prototype/terraform/main.tf
@@ -23,16 +23,41 @@ resource "azurerm_service_plan" "app-service-plan" {
 }
 
 resource "azurerm_linux_web_app" "web-app" {
+  #checkov:skip=CKV_AZURE_88:Persistent storage not required
+  #checkov:skip=CKV_AZURE_222:Web app is public site
+  #checkov:skip=CKV_AZURE_17:Client cert no used
+  #checkov:skip=CKV_AZURE_213:Health end point not required
+  #checkov:skip=CKV_AZURE_212:Prototype site
+  #checkov:skip=CKV_AZURE_211:Prototype site
   name                = "${var.environment-prefix}-ebis-prototype"
   location            = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
   service_plan_id     = azurerm_service_plan.app-service-plan.id
   tags                = local.common-tags
   https_only          = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
+    http2_enabled       = true
     minimum_tls_version = "1.2"
     app_command_line    = "npm run start"
+    ftps_state          = "Disabled"
   }
+
+  logs {
+    failed_request_tracing  = true
+    detailed_error_messages = true
+    http_logs {
+      file_system {
+        retention_in_days = 4
+        retention_in_mb   = 25
+      }
+    }
+  }
+
   app_settings = {
     "NODE_ENV"                       = "production"
     "PASSWORD"                       = "q"
