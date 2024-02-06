b81299edacb3844770e1b6d45fdeaa7709c4b3fd
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-02-06 22:50:30 +0000

    display info for icfp prepopulated data page (#144)
    display school and finance info for icfp prepopulated data

--- web/src/EducationBenchmarking.Web/Controllers/SchoolPlanningYearController.cs
+++ web/src/EducationBenchmarking.Web/Controllers/SchoolPlanningYearController.cs
@@ -1,5 +1,7 @@
 using EducationBenchmarking.Web.Domain;
 using EducationBenchmarking.Web.Infrastructure.Apis;
+using EducationBenchmarking.Web.Infrastructure.Extensions;
+using EducationBenchmarking.Web.Services;
 using EducationBenchmarking.Web.TagHelpers;
 using EducationBenchmarking.Web.ViewModels;
 using Microsoft.AspNetCore.Http.Extensions;
@@ -9,22 +11,26 @@ namespace EducationBenchmarking.Web.Controllers;
 
 [Controller]
 [Route("school/{urn}/financial-planning/{year:int}")]
-public class SchoolPlanningYearController(ILogger<SchoolPlanningYearController> logger) : Controller
+public class SchoolPlanningYearController(
+    ILogger<SchoolPlanningYearController> logger,
+    IFinanceService financeService,
+    IEstablishmentApi establishmentApi) 
+    : Controller
 {
     [HttpGet]
-    public IActionResult Index(string urn, int year)
+    public async Task<IActionResult> Index(string urn, int year)
     {
         using (logger.BeginScope(new { urn, year }))
         {
             try
             {
-                //TODO: Get school and financial information
                 //TODO: Get if exists plan for school / year
                 //TODO: Display previous selection value if plan exists
-                //TODO: Conditionally display total education support staff costs fpr primary schools
                 ViewData[ViewDataConstants.Backlink] = new BacklinkInfo("SelectYear", "SchoolPlanning", new { urn });
-                var school = new School { Urn = urn };
-                var viewModel = new SchoolPlanViewModel(school, year);
+
+                var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
+                var finances = await financeService.GetFinances(school);
+                var viewModel = new SchoolPlanViewModel(school, finances, year);
                 return View(viewModel);
             }
             catch (Exception e)
@@ -36,7 +42,7 @@ public class SchoolPlanningYearController(ILogger<SchoolPlanningYearController> 
     }
     
     [HttpPost]
-    public IActionResult Index(string urn, int year, [FromForm]bool? useFigures)
+    public async Task<IActionResult> Index(string urn, int year, [FromForm]bool? useFigures)
     {
         using (logger.BeginScope(new { urn, year, useFigures }))
         {
@@ -54,8 +60,10 @@ public class SchoolPlanningYearController(ILogger<SchoolPlanningYearController> 
                         ? RedirectToAction("Timetable", new { urn, year })
                         : RedirectToAction("TotalIncome", new { urn, year });
                 }
-                var school = new School { Urn = urn };
-                var viewModel = new SchoolPlanViewModel(school, year);
+                // TODO: amend as required when working on post - below is here just to stop error with CTA
+                var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
+                var finances = await financeService.GetFinances(school);
+                var viewModel = new SchoolPlanViewModel(school, finances, year);
                 return View(viewModel);
             }
             catch (Exception e)
--- web/src/EducationBenchmarking.Web/ViewModels/SchoolPlanViewModel.cs
+++ web/src/EducationBenchmarking.Web/ViewModels/SchoolPlanViewModel.cs
@@ -4,11 +4,25 @@ namespace EducationBenchmarking.Web.ViewModels;
 
 public class SchoolPlanViewModel(School school)
 {
+    private readonly Finances? _finances;
     public SchoolPlanViewModel(School school, int? year) : this(school)
     {
         SelectedYear = year;
     }
+
+    public SchoolPlanViewModel(School school, Finances finances, int year) : this(school)
+    {
+        SelectedYear = year;
+        _finances = finances;
+    }
+
     public int? SelectedYear { get; set; }
     public string Name => school.Name;
     public string Urn => school.Urn;
+    public string CurrentTotalIncome => $"{_finances.TotalIncome:C}";
+    public string CurrentTotalExpenditure => $"{_finances.TotalExpenditure:C}";
+    public string CurrentTotalTeacherCosts => $"{_finances.TeachingStaffCosts:C}";
+    public string CurrentTotalNumberOfTeachersFte => $"{_finances.TotalNumberOfTeachersFte}";
+    public string CurrentEducationSupportStaffCosts => $"{_finances.EducationSupportStaffCosts:C}";
+    public bool IsPrimary => _finances.OverallPhase == "Primary";
 }
