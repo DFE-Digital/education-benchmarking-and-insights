c3f3d9f879ec062b0cc0853e89ba661d673d43ee
J05h-L <josh.langford@hippodigital.co.uk>
2024-02-08 10:18:55 +0000

    put and get for icfp prepopulate page

---
+++ web/src/EducationBenchmarking.Web/Domain/FinancialPlan.cs
@@ -0,0 +1,15 @@
+﻿namespace EducationBenchmarking.Web.Domain
+{
+    public class FinancialPlan
+    {
+        public int Year { get; set; }
+        public string? Urn { get; set; }
+        public bool UseFigures { get; set; }
+        public decimal? TotalIncome { get; set; }
+        public decimal? TotalExpenditure { get; set; }
+        public decimal? TotalTeacherCosts { get; set; }
+        public decimal? TotalNumberOfTeachersFte { get; set; }
+        public decimal? EducationSupportStaffCosts { get; set; }
+    }
+}
+
