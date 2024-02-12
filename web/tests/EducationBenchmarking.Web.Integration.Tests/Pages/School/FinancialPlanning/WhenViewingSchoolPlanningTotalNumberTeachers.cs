8c1489f9afdd716f7bf14a105356deb645a61755
spicy-rice <69350762+spicy-rice@users.noreply.github.com>
2024-02-12 17:58:49 +0000

    all but ShowsErrorOnInValidSubmit pass

--- web/tests/EducationBenchmarking.Web.Integration.Tests/Pages/School/FinancialPlanning/WhenViewingSchoolPlanningTotalNumberTeachers.cs
+++ web/tests/EducationBenchmarking.Web.Integration.Tests/Pages/School/FinancialPlanning/WhenViewingSchoolPlanningTotalNumberTeachers.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using AngleSharp.Html.Dom;
 using AutoFixture;
 using EducationBenchmarking.Web.Domain;
@@ -62,4 +63,77 @@ public class WhenViewingSchoolPlanningTotalNumberTeachers(BenchmarkingWebAppFact
         DocumentAssert.BackLink(page, "Back", Paths.SchoolCurriculumPlanningTotalTeacherCost(school.Urn, CurrentYear).ToAbsolute());
         DocumentAssert.TitleAndH1(page, "Total number of teachers", "Total number of teachers");
     }
+    
+    [Theory]
+    [InlineData(EstablishmentTypes.Academies)]
+    [InlineData(EstablishmentTypes.Maintained)]
+    public async Task CanSubmit(string financeType)
+    {
+        var (page, school) = await SetupNavigateInitPage(financeType);
+        AssertPageLayout(page, school);
+        var action = page.QuerySelector(".govuk-button");
+        Assert.NotNull(action);
+
+        page = await SubmitForm(page.Forms[0], action, f =>
+        {
+            f.SetFormValues(new Dictionary<string, string>
+            {
+                { "TotalNumberTeachers",  19.5.ToString()}
+            });
+        });
+        
+        DocumentAssert.AssertPageUrl(page, Paths.SchoolCurriculumPlanningTotalNumberTeachers(school.Urn, CurrentYear).ToAbsolute());
+    }
+    [Fact]
+    public async Task CanDisplayNotFound()
+    {
+        const string urn = "12345";
+        const int year = 2024;
+
+        var page = await SetupEstablishmentWithNotFound()
+            .Navigate(Paths.SchoolCurriculumPlanningTotalNumberTeachers(urn, year));
+
+        
+        var expectedUrl = Paths.SchoolCurriculumPlanningTotalNumberTeachers(urn, year).ToAbsolute();
+        DocumentAssert.AssertPageUrl(page, expectedUrl, HttpStatusCode.NotFound);
+    }
+
+    
+    [Fact]
+    public async Task CanDisplayProblemWithService()
+    {
+        const string urn = "12345";
+        const int year = 2024;
+        var page = await SetupEstablishmentWithException()
+            .Navigate(Paths.SchoolCurriculumPlanningTotalNumberTeachers(urn, year));
+
+        var expectedUrl = Paths.SchoolCurriculumPlanningTotalNumberTeachers(urn, year).ToAbsolute();
+        DocumentAssert.AssertPageUrl(page, expectedUrl, HttpStatusCode.InternalServerError);
+    }
+    
+
+    [Fact]
+    public async Task ShowsErrorOnInValidSubmit()
+    {
+        
+        var (page, school) = await SetupNavigateInitPage(EstablishmentTypes.Academies);
+        AssertPageLayout(page, school);
+        var action = page.QuerySelector(".govuk-button");
+        Assert.NotNull(action);
+        
+        
+        page = await SubmitForm(page.Forms[0], action, f =>
+        {
+            f.SetFormValues(new Dictionary<string, string>
+            {
+                { "TotalNumberOfTeachersFte",  ""}
+            });
+        });
+        
+        DocumentAssert.AssertPageUrl(page, Paths.SchoolCurriculumPlanningTotalNumberTeachers(school.Urn, CurrentYear).ToAbsolute());
+        DocumentAssert.FormErrors(page, ("total-number-teacher","Enter number of FTE teachers"));
+    }
+
+    
+    
 }
