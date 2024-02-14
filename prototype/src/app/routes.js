a0577d6a44e6294067e8d9f68c7fe23b8ebf5c4d
Pete Williams <pete.williams@hippodigital.co.uk>
2024-02-14 13:48:43 +0000

    Prototype: Setup (#172)
    * Update service name
    
    Mostly a test commit, but also updating prototype with proposed service name.
    
    * Prototype setup
    
    Adding some useful utilities to make the prototype kit easier to use.

--- prototype/src/app/routes.js
+++ prototype/src/app/routes.js
@@ -7,3 +7,34 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// viewing session data
+router.get('*/manage-prototype/view-data', function(req, res){
+
+    querystring = '';
+    for ( var key in req.session.data )
+    {
+        querystring += key +'=' + req.session.data[key] + '&';
+    }
+
+    res.render('manage-prototype/view-data', { data: JSON.stringify( req.session, null, 2), querystring: querystring } );
+})
+
+
+
+// Saving the session data to the clipboard using another page
+
+router.get('*/manage-prototype/update-session-data', function(req, res){
+
+    var querystringtemp = '';
+    for ( var key in req.session.data )
+    {
+        querystringtemp += key +'=' + req.session.data[key] + '&';
+    }
+
+    req.session.data['theoutputquerystring'] = "";
+    req.session.data['theoutputquerystring'] = "?" + querystringtemp;
+
+
+    res.redirect('../manage-prototype/copy-url-to-clipboard');
+})
