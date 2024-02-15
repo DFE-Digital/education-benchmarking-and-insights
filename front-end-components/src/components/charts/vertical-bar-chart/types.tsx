3e361bf65a3515e9e071af2d7aa8ee2bbe771300
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-02-15 10:33:28 +0000

    feat: Added grid and legend to vertical bar chart

--- front-end-components/src/components/charts/vertical-bar-chart/types.tsx
+++ front-end-components/src/components/charts/vertical-bar-chart/types.tsx
@@ -1,6 +1,7 @@
 export interface ChartProps<TData extends ChartDataSeries> {
   chartName: string;
   data: TData[];
+  gridEnabled?: boolean;
   highlightedItemKeys?: ChartSeriesValue[];
   keyField: keyof TData;
   margin?: number;
@@ -8,21 +9,26 @@ export interface ChartProps<TData extends ChartDataSeries> {
   seriesConfig?: ChartSeriesConfig<TData>;
   seriesLabel?: string;
   seriesLabelField: keyof TData;
+  seriesLabelRotate?: boolean;
+  legendEnabled?: boolean;
   valueLabel?: string;
+  valueUnit?: ChartSeriesValueUnit;
 }
 
 type ChartSeriesConfig<TData extends ChartDataSeries> = Partial<
   Record<
     keyof TData,
     {
-      visible: boolean;
       className?: string;
+      label?: string;
+      visible: boolean;
     }
   >
 >;
 
 type ChartSeriesName = string;
 type ChartSeriesValue = string | number;
+type ChartSeriesValueUnit = "%";
 export type ChartDataSeries = { [name: ChartSeriesName]: ChartSeriesValue };
 
 export type ChartHandler = {
