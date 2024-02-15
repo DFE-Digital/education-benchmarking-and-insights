6d27701f076c01788a2179ae8f439cc132046124
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-02-15 09:05:14 +0000

    feat: First steps for building vertical bar chart using Recharts

---
+++ front-end-components/src/components/charts/vertical-bar-chart/index.tsx
@@ -0,0 +1,3 @@
+/* eslint-disable react-refresh/only-export-components */
+export * from "src/components/charts/vertical-bar-chart/types";
+export * from "src/components/charts/vertical-bar-chart/component";
--- front-end-components/src/constants.tsx
+++ front-end-components/src/constants.tsx
@@ -1,2 +1,3 @@
 export const CompareYourSchoolElementId = "compare-your-school";
 export const CompareWorkforceElementId = "compare-workforce";
+export const VerticalChartElementId = "vertical-chart";
