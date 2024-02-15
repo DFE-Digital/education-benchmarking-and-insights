b25170ce336174df4ed05aaf73f1d6772f636ff9
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-02-15 14:36:32 +0000

    Formatting fix

--- web/src/EducationBenchmarking.Web/ViewModels/SchoolDetailsViewModel.cs
+++ web/src/EducationBenchmarking.Web/ViewModels/SchoolDetailsViewModel.cs
@@ -4,8 +4,8 @@ namespace EducationBenchmarking.Web.ViewModels;
 
 public class SchoolDetailsViewModel(School school) : SchoolViewModel(school)
 {
-    private readonly School _school = school
-        ;
+    private readonly School _school = school;
+    
     public string? Address => _school.Address;
     public string? Telephone => _school.Telephone;
     public string? LocalAuthorityName => _school.LocalAuthorityName;
