b7401407ea11521d4ec73dfb8351d9efbf94363b
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-02-22 08:34:14 +0000

    refactor: switch trcked anchor extension method to use tag builder rather than view, and add unit tests

--- web/src/EducationBenchmarking.Web/Extensions/HtmlExtensions.cs
+++ web/src/EducationBenchmarking.Web/Extensions/HtmlExtensions.cs
@@ -1,14 +1,37 @@
-using EducationBenchmarking.Web.ViewModels;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EducationBenchmarking.Web.Extensions;
 
 public static class HtmlHelperExtensions
 {
-    public static Task TrackedAnchorAsync(this IHtmlHelper htmlHelper, TrackedLinks link, string href, string content, string? hiddenContent = null, string? target = null, string[]? rel = null, params string[] classes)
+    public static IHtmlContent TrackedAnchor(this IHtmlHelper htmlHelper, TrackedLinks link, string href, string content,
+        string? hiddenContent = null, string? target = null, string[]? rel = null, params string[] classes)
     {
+        var anchorTag = new TagBuilder("a");
+
         classes = classes.Length == 0 ? ["govuk-link", "govuk-link--no-visited-state"] : classes;
-        var viewModel = new TrackedAnchorViewModel(link.GetStringValue(), href, content, hiddenContent, target, rel, classes);
-        return htmlHelper.RenderPartialAsync("_TrackedAnchor", viewModel);
+
+        anchorTag.AddCssClass(string.Join(" ", classes));
+        anchorTag.InnerHtml.Append(content);
+        anchorTag.MergeAttribute("href", href);
+        anchorTag.MergeAttribute("data-id", link.GetStringValue());
+
+        if (rel is not null && rel.Length > 0)
+        {
+            anchorTag.MergeAttribute("rel", string.Join(" ", rel));
+        }
+
+        if (!string.IsNullOrEmpty(target))
+        {
+            anchorTag.MergeAttribute("target", target);
+        }
+
+        if (!string.IsNullOrEmpty(hiddenContent))
+        {
+            anchorTag.InnerHtml.AppendHtml($"<span class=\"govuk-visually-hidden\"> {hiddenContent}</span>");
+        }
+
+        return anchorTag;
     }
 }
