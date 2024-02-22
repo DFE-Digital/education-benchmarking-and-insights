ccd6871e9611f3e9e67d911a2fb7bb76d9540d9b
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-02-22 08:56:24 +0000

    adds a11y tests for timetable cycle page (#314)

---
+++ web/tests/EducationBenchmarking.Web.A11yTests/Pages/Schools/FinancialPlanning/WhenViewingFinancialPlanningTimetableCycle.cs
@@ -0,0 +1,30 @@
+﻿using EducationBenchmarking.Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EducationBenchmarking.Web.A11yTests.Pages.Schools.FinancialPlanning;
+
+[Collection(nameof(FinancialPlanMinimalDataCollection))]
+public class WhenViewingFinancialPlanningTotalTimetableCycle(
+    ITestOutputHelper outputHelper,
+    WebDriver webDriver,
+    FinancialPlanMinimalDataFixture plan)
+    : PageBase(outputHelper, webDriver)
+{
+    protected override string PageUrl => $"/school/{plan.Urn}/financial-planning/steps/timetable-cycle?year={plan.Year}";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await EvaluatePage();
+    }
+
+    [Fact]
+    public async Task ValidationErrorThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await Page.Locator(":text('Continue')").ClickAsync();
+        await EvaluatePage();
+    }
+}
