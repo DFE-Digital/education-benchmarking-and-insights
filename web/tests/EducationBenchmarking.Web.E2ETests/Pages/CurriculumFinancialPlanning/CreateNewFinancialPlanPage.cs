d8901d5df312da45dc81738ad5ac3e055c71e863
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2024-02-23 15:00:02 +0000

    added new page for createNewFinancialPlan

---
+++ web/tests/EducationBenchmarking.Web.E2ETests/Pages/CurriculumFinancialPlanning/CreateNewFinancialPlanPage.cs
@@ -0,0 +1,32 @@
+﻿using EducationBenchmarking.Web.E2ETests.Helpers;
+using EducationBenchmarking.Web.E2ETests.Hooks;
+using Microsoft.Playwright;
+
+namespace EducationBenchmarking.Web.E2ETests.Pages.CurriculumFinancialPlanning;
+
+public class CreateNewFinancialPlanPage(PageHook page)
+{
+    private readonly IPage _page = page.Current;
+    private ILocator PageH1Heading => _page.Locator("h1");
+    private ILocator BreadCrumbs => _page.Locator(".govuk-breadcrumbs");
+    private ILocator ChangeSchoolLink => _page.Locator(":text('Change school')");
+
+    private async Task WaitForPage(string urn)
+    {
+        await _page.WaitForURLAsync($"{TestConfiguration.ServiceUrl}/school/{urn}/financial-planning");
+    }
+
+    private async Task AssertPage()
+    {
+        await PageH1Heading.ShouldBeVisible();
+        await BreadCrumbs.ShouldBeVisible();
+        await ChangeSchoolLink.ShouldBeVisible();
+    }
+
+    public async Task IsPageDisplayed(string urn)
+    {
+        await WaitForPage(urn);
+        await AssertPage();
+
+    }
+}
--- web/tests/EducationBenchmarking.Web.E2ETests/Pages/CurriculumFinancialPlanningPages.cs
+++ web/tests/EducationBenchmarking.Web.E2ETests/Pages/CurriculumFinancialPlanningPages.cs
@@ -6,10 +6,12 @@ public class CurriculumFinancialPlanningPages(
     StartPage start,
     HelpPage help,
     SelectYearPage selectYear,
-    PrePopulatedDataPage prePopulatedData)
+    PrePopulatedDataPage prePopulatedData,
+    CreateNewFinancialPlanPage createNewFinancialPlan)
 {
     public StartPage Start { get; } = start;
     public HelpPage Help { get; } = help;
     public SelectYearPage SelectYear { get; } = selectYear;
     public PrePopulatedDataPage PrePopulatedData { get; } = prePopulatedData;
+    public CreateNewFinancialPlanPage CreateNewFinancialPlan { get; } = createNewFinancialPlan;
 }
