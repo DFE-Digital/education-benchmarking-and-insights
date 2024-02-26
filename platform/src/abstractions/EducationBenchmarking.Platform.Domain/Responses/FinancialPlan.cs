6d0a95bfa22daff69ded7b1b2fdaae463b5e0781
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-02-26 14:37:38 +0000

    feat: update api end point with additional properties for pupil figures

--- platform/src/abstractions/EducationBenchmarking.Platform.Domain/Responses/FinancialPlan.cs
+++ platform/src/abstractions/EducationBenchmarking.Platform.Domain/Responses/FinancialPlan.cs
@@ -28,6 +28,28 @@ public record FinancialPlan
     public bool MixedAgeYear3Year4 { get; set; }
     public bool MixedAgeYear4Year5 { get; set; }
     public bool MixedAgeYear5Year6 { get; set; }
+    public int? PupilsYear7 { get; set; }
+    public int? PupilsYear8 { get; set; }
+    public int? PupilsYear9 { get; set; }
+    public int? PupilsYear10 { get; set; }
+    public int? PupilsYear11 { get; set; }
+    public decimal? PupilsYear12 { get; set; }
+    public decimal? PupilsYear13 { get; set; }
+    public decimal? PupilsNursery { get; set; }
+    public int? PupilsMixedReceptionYear1 { get; set; }
+    public int? PupilsMixedYear1Year2 { get; set; }
+    public int? PupilsMixedYear2Year3 { get; set; }
+    public int? PupilsMixedYear3Year4 { get; set; }
+    public int? PupilsMixedYear4Year5 { get; set; }
+    public int? PupilsMixedYear5Year6 { get; set; }
+    public int? PupilsReception { get; set; }
+    public int? PupilsYear1 { get; set; }
+    public int? PupilsYear2 { get; set; }
+    public int? PupilsYear3 { get; set; }
+    public int? PupilsYear4 { get; set; }
+    public int? PupilsYear5 { get; set; }
+    public int? PupilsYear6 { get; set; }
+    
     public static FinancialPlan Create(FinancialPlanDataObject dataObject)
     {
         return new FinancialPlan
@@ -52,7 +74,28 @@ public record FinancialPlan
             MixedAgeYear2Year3 = dataObject.MixedAgeYear2Year3,
             MixedAgeYear3Year4 = dataObject.MixedAgeYear3Year4,
             MixedAgeYear4Year5 = dataObject.MixedAgeYear4Year5,
-            MixedAgeYear5Year6 = dataObject.MixedAgeYear5Year6
+            MixedAgeYear5Year6 = dataObject.MixedAgeYear5Year6,
+            PupilsYear7 = dataObject.PupilsYear7,
+            PupilsYear8 = dataObject.PupilsYear8,
+            PupilsYear9 = dataObject.PupilsYear9,
+            PupilsYear10 = dataObject.PupilsYear10,
+            PupilsYear11 = dataObject.PupilsYear11,
+            PupilsYear12 = dataObject.PupilsYear12,
+            PupilsYear13 = dataObject.PupilsYear13,
+            PupilsNursery = dataObject.PupilsNursery,
+            PupilsReception = dataObject.PupilsReception,
+            PupilsYear1 = dataObject.PupilsYear1,
+            PupilsYear2 = dataObject.PupilsYear2,
+            PupilsYear3 = dataObject.PupilsYear3,
+            PupilsYear4 = dataObject.PupilsYear4,
+            PupilsYear5 = dataObject.PupilsYear5,
+            PupilsYear6 = dataObject.PupilsYear6,
+            PupilsMixedReceptionYear1 = dataObject.PupilsMixedReceptionYear1,
+            PupilsMixedYear1Year2 = dataObject.PupilsMixedYear1Year2,
+            PupilsMixedYear2Year3 = dataObject.PupilsMixedYear2Year3,
+            PupilsMixedYear3Year4 = dataObject.PupilsMixedYear3Year4, 
+            PupilsMixedYear4Year5 = dataObject.PupilsMixedYear4Year5, 
+            PupilsMixedYear5Year6 = dataObject.PupilsMixedYear5Year6
         };
     }
 }
