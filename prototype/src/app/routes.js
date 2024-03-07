a003755b95d999d16fadd299c545f85012eba79f
Pete Williams <me@petewilliams.info>
2024-03-07 17:35:49 +0000

    Prototype - View/create comparator list

--- prototype/src/app/routes.js
+++ prototype/src/app/routes.js
@@ -6,7 +6,61 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+
+router.get( '/add-comparator-school', (req, res) => {
+
+    var comparators = req.session.data.comparators || [];
+    var schoolName = req.session.data.school;
+    comparatorName = schoolName.substring( 0, schoolName.lastIndexOf(' (') );
+    comparatorLocation = schoolName.substring( schoolName.lastIndexOf(' (')+2, schoolName.lastIndexOf(',') );
+    comparatorPostcode = schoolName.substring( schoolName.lastIndexOf(',')+2, schoolName.length-1 );
+    comparatorPupils = Math.floor(Math.random() * (2782 - 438 + 1) ) + 438;
+    comparatorMeals = Math.floor( ( ( Math.random() * (18 - 4.3 + 1) ) + 4.3 )* 10 ) /10;
+
+    comparators.push({'comparatorName': comparatorName, 'comparatorLocation': comparatorLocation, 'comparatorPostcode': comparatorPostcode, 'comparatorPupils': comparatorPupils, 'comparatorMeals': comparatorMeals });
+ 
+    req.session.data['comparators'] = comparators;
+    
+    res.redirect( '/comparators/create' );
+
+})
+
+
+router.get( '/comparators/create', (req, res) => {
+
+    var rows = [];
+    var comparators = req.session.data.comparators || [];
+    
+    for ( i=0; i<comparators.length; i++) {
+        rows.push( [ {'text': comparators[i].comparatorName}, {'text': 'Secondary'}, {'text': comparators[i].comparatorPupils.toLocaleString()}, {'text': 'Good'}, {'text': comparators[i].comparatorMeals + '%'}, {'html': '<a href="/comparators/remove?id=' + i + '">Remove</a>' } ] );
+    }
+
+    res.render( '/comparators/create', { rows: rows } );
+
+})
+
+
+router.get( '/comparators/remove', (req, res) => {
+
+    req.session.data.comparators.splice( req.query.id, 1 );
+
+    res.redirect( '/comparators/create' );
+
+})
+
+
+router.get( '/set-school', (req, res) => {
+
+    var schoolName = req.session.data.school;
+
+    if (schoolName) {
+        req.session.data['school-name'] = schoolName.substring( 0, schoolName.lastIndexOf(' (') );
+    }
+
+    res.redirect( '/school-homepage' );
+
+})
+
 
 // viewing session data
 router.get('*/manage-prototype/view-data', function(req, res){
