a20ea1e2ecec967208a657dcc537f9343dee39e2
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-03-09 09:16:10 +0000

    Switch to run from package for prototype

--- prototype/terraform/README.md
+++ prototype/terraform/README.md
@@ -23,8 +23,6 @@ No modules.
 | [azurerm_linux_web_app.web-app](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_web_app) | resource |
 | [azurerm_resource_group.resource-group](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group) | resource |
 | [azurerm_service_plan.app-service-plan](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan) | resource |
-| [azurerm_service_plan.app-service-plan-b](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan) | resource |
-| [azurerm_windows_web_app.web-app-b](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/windows_web_app) | resource |
 
 ## Inputs
 
--- prototype/terraform/main.tf
+++ prototype/terraform/main.tf
@@ -14,9 +14,9 @@ resource "azurerm_resource_group" "resource-group" {
 }
 
 resource "azurerm_service_plan" "app-service-plan" {
-  #checkov:skip=CKV_AZURE_211:Non production - prototype toolkit
-  #checkov:skip=CKV_AZURE_212:Non production - prototype toolkit
-  #checkov:skip=CKV_AZURE_225:Non production - prototype toolkit
+  #checkov:skip=CKV_AZURE_211:Non production - prototype
+  #checkov:skip=CKV_AZURE_212:Non production - prototype
+  #checkov:skip=CKV_AZURE_225:Non production - prototype
   name                = "${var.environment-prefix}-ebis-asp-prototype"
   location            = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
@@ -26,11 +26,12 @@ resource "azurerm_service_plan" "app-service-plan" {
 }
 
 resource "azurerm_linux_web_app" "web-app" {
-  #checkov:skip=CKV_AZURE_13:Authentication not required
-  #checkov:skip=CKV_AZURE_88:Persistent storage not required
-  #checkov:skip=CKV_AZURE_222:Web app is public site
-  #checkov:skip=CKV_AZURE_17:Client cert no used
-  #checkov:skip=CKV_AZURE_213:Health end point not required
+  #checkov:skip=CKV_AZURE_13:Non production - prototype
+  #checkov:skip=CKV_AZURE_88:Non production - prototype
+  #checkov:skip=CKV_AZURE_222:Non production - prototype
+  #checkov:skip=CKV_AZURE_17:Non production - prototype
+  #checkov:skip=CKV_AZURE_213:Non production - prototype
+  #checkov:skip=CKV_AZURE_214:Non production - prototype
   name                = "${var.environment-prefix}-ebis-prototype"
   location            = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
@@ -68,48 +69,7 @@ resource "azurerm_linux_web_app" "web-app" {
     "NODE_ENV"                       = "production"
     "PASSWORD"                       = var.prototype-password
     "BUILD_FLAGS"                    = "Off"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
-    "KIT_PROJECT_DIR"                = "/home/site/wwwroot"
-  }
-}
-
-resource "azurerm_service_plan" "app-service-plan-b" {
-  name                = "${var.environment-prefix}-ebis-asp-prototype-b"
-  location            = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
-  os_type             = "Windows"
-  sku_name            = "F1"
-  tags                = local.common-tags
-}
-
-resource "azurerm_windows_web_app" "web-app-b" {
-  name                = "${var.environment-prefix}-ebis-prototype-b"
-  location            = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
-  service_plan_id     = azurerm_service_plan.app-service-plan-b.id
-  tags                = local.common-tags
-  https_only          = true
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  site_config {
-    always_on           = false
-    http2_enabled       = true
-    minimum_tls_version = "1.2"
-    app_command_line    = "npm run start"
-    ftps_state          = "Disabled"
-    application_stack {
-      current_stack = "node"
-      node_version  = "~18"
-    }
-  }
-
-  app_settings = {
-    "NODE_ENV"                       = "production"
-    "PASSWORD"                       = var.prototype-password
-    "BUILD_FLAGS"                    = "Off"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
+    "WEBSITE_RUN_FROM_PACKAGE"       = "1"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = false
   }
 }
