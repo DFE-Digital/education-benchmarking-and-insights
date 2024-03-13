5dbcb19a04e1de64434cb9aed45c1e88e7e398e2
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-03-13 17:14:52 +0000

    feat: Split `<Stat>` into container `<ResolvedStat>` and presentational `<Stat>` for better re-use
    Completed implementation of composed `<ComparisonChartSummary>` and added to demo with sample data

--- front-end-components/src/components/charts/stat/types.tsx
+++ front-end-components/src/components/charts/stat/types.tsx
@@ -1,11 +1,12 @@
-import { ChartDataSeries, ChartProps } from "src/components";
+import { ReactNode } from "react";
+import { ChartDataSeries, ChartProps, ChartSeriesValue } from "src/components";
 
 export interface StatProps<TData extends ChartDataSeries>
-  extends Pick<
-    ChartProps<TData>,
-    "chartName" | "data" | "seriesLabelField" | "valueUnit"
-  > {
+  extends Pick<ChartProps<TData>, "chartName" | "valueUnit"> {
   className?: string;
-  displayIndex: number;
-  valueField: keyof TData;
+  compactValue?: boolean;
+  label: string;
+  suffix?: string | ReactNode;
+  value: ChartSeriesValue;
+  valueSuffix?: string;
 }
