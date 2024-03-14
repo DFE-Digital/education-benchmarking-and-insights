9227d94ae2d7295591c0100198e8bbf09d5cbd83
Jon Abbott <jon.r.abbott@gmail.com>
2024-03-14 15:14:41 +0000

    feat: adds script for comparator sets table (#424)
    * feat: adds script for comparator sets table
    
    * linting
    
    * chore: set shared access key
    
    ---------
    
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

--- platform/terraform/storage.tf
+++ platform/terraform/storage.tf
@@ -8,6 +8,7 @@ resource "azurerm_storage_account" "platform-storage" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
   tags                            = local.common-tags
   min_tls_version                 = "TLS1_2"
 
@@ -46,6 +47,7 @@ resource "azurerm_storage_account" "audit-storage" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
   tags                            = local.common-tags
   min_tls_version                 = "TLS1_2"
 
@@ -71,6 +73,7 @@ resource "azurerm_storage_account" "threat-storage" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
   tags                            = local.common-tags
   min_tls_version                 = "TLS1_2"
 
