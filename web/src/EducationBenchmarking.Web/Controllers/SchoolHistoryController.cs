9841b60b7b2a38f55bc58d375a7aca324aabb3f6
spicy-rice <69350762+spicy-rice@users.noreply.github.com>
2024-03-15 09:08:12 +0000

    school history view model and controller

--- web/src/EducationBenchmarking.Web/Controllers/SchoolHistoryController.cs
+++ web/src/EducationBenchmarking.Web/Controllers/SchoolHistoryController.cs
@@ -3,41 +3,44 @@ using EducationBenchmarking.Web.Infrastructure.Apis;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using EducationBenchmarking.Web.Infrastructure.Extensions;
-using EducationBenchmarking.Web.Services;
 using EducationBenchmarking.Web.TagHelpers;
 using EducationBenchmarking.Web.ViewModels;
 
 
-namespace EducationBenchmarking.Web.Controllers;
-
-[Controller]
-[Route("school/{urn}/history")]
-public class SchoolHistoryController(
-    IEstablishmentApi establishmentApi,
-    ILogger<SchoolHistoryController> logger)
-    : Controller
+namespace EducationBenchmarking.Web.Controllers
 {
-    [HttpGet]
-    public async Task<IActionResult> Index(string urn)
+    [Route("school/{urn}/history")]
+    public class SchoolHistoryController : Controller
     {
-        using (logger.BeginScope(new { urn }))
+        private readonly IEstablishmentApi establishmentApi;
+        private readonly ILogger<SchoolHistoryController> logger;
+
+        public SchoolHistoryController(IEstablishmentApi establishmentApi, ILogger<SchoolHistoryController> logger)
         {
-            try
+            this.establishmentApi = establishmentApi;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string urn)
+        {
+            using (logger.BeginScope(new { urn }))
             {
-                ViewData[ViewDataKeys.Backlink] =
+                try
+                {
                     ViewData[ViewDataKeys.Backlink] = new BacklinkInfo(Url.Action("Index", "School", new { urn }));
 
-                var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
-                var viewModel = new SchoolHistoryViewModel(school);
-                return View(viewModel);
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "An error displaying school history: {DisplayUrl}", Request.GetDisplayUrl());
-                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+                    var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
+                    var viewModel = new SchoolHistoryViewModel(school, null); // Adjust according to actual data fetching
+                    return View(viewModel);
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "An error displaying school history: {DisplayUrl}", Request.GetDisplayUrl());
+                    return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+                }
             }
         }
     }
-
-
 }
+
--- web/src/EducationBenchmarking.Web/ViewModels/SchoolHistoryViewModel.cs
+++ web/src/EducationBenchmarking.Web/ViewModels/SchoolHistoryViewModel.cs
@@ -1,5 +1,4 @@
 using EducationBenchmarking.Web.Domain;
-using System.Collections.Generic;
 
 namespace EducationBenchmarking.Web.ViewModels;
 
@@ -7,35 +6,39 @@ public class SchoolHistoryViewModel
 {
     public string Name { get; private set; }
     public string Urn { get; private set; }
-    public List<SchoolYearHistoryViewModel> History { get; private set; }
+    public List<SchoolYearHistory> History { get; private set; }
+    
+    
+    public class SchoolYearHistory
+    {
+        public string Term { get; set; }
+        public double InYearBalance { get; set; }
+        public double RevenueReserve { get; set; }
+        public double TotalExpenditure { get; set; }
+        public double TotalIncome { get; set; }
+        public int NoPupils { get; set; }
+    }
 
-    public SchoolHistoryViewModel(School school, List<SchoolYearHistory> historyData)
+    public SchoolHistoryViewModel(School school, List<SchoolYearHistory> historyData = null)
     {
         Name = school.Name;
         Urn = school.Urn;
-        History = new List<SchoolYearHistoryViewModel>();
+        History = new List<SchoolYearHistory>();
 
-        foreach (var history in historyData)
+        if (historyData != null)
         {
-            History.Add(new SchoolYearHistoryViewModel
+            foreach (var history in historyData)
             {
-                Term = history.Term,
-                InYearBalance = history.InYearBalance,
-                RevenueReserve = history.RevenueReserve,
-                TotalExpenditure = history.TotalExpenditure,
-                TotalIncome = history.TotalIncome,
-                NoPupils = history.NoPupils
-            });
+                History.Add(new SchoolYearHistory
+                {
+                    Term = history.Term,
+                    InYearBalance = history.InYearBalance,
+                    RevenueReserve = history.RevenueReserve,
+                    TotalExpenditure = history.TotalExpenditure,
+                    TotalIncome = history.TotalIncome,
+                    NoPupils = history.NoPupils
+                });
+            }
         }
     }
-}
-
-public class SchoolYearHistoryViewModel
-{
-    public string Term { get; set; }
-    public double InYearBalance { get; set; }
-    public double RevenueReserve { get; set; }
-    public double TotalExpenditure { get; set; }
-    public double TotalIncome { get; set; }
-    public int NoPupils { get; set; }
 }
