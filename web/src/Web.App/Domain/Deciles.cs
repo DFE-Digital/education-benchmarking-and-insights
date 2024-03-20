efbe665f6ffcb83509a78d5bc799d22d43e3bc46
J05h-L <josh.langford@hippodigital.co.uk>
2024-03-20 10:40:20 +0000

    adds DecileHandler to calculate deciles for rag statuses

---
+++ web/src/Web.App/Domain/Deciles.cs
@@ -0,0 +1,67 @@
+﻿namespace Web.App.Domain;
+
+public interface IDecileHandler
+{
+    public int FindDecile();
+}
+
+public class DecileHandler : IDecileHandler
+{
+    private readonly decimal _value;
+    private readonly decimal[] _values;
+    private readonly bool _searchTopDown;
+
+    public DecileHandler(decimal value, decimal[] values, bool searchTopDown = false)
+    {
+        _value = value;
+        _values = values.Length >= 10
+            ? values
+            : throw new ArgumentException("There must be at least 10 values to calculate deciles.");
+        _searchTopDown = searchTopDown;
+    }
+
+    public int FindDecile()
+    {
+        var upperBounds = GetUpperBoundsForDeciles();
+
+        var matchingDeciles = upperBounds.Select((upperBound, index) =>
+        {
+            if (index == 0 && _value <= upperBound)
+                return index + 1;
+            if (index > 0 && _value > upperBounds[index - 1] && _value <= upperBound)
+                return index + 1;
+            if (index > 0 && _value == upperBounds[index])
+                return index + 1;
+            return -1;
+        })
+        .Where(decile => decile != -1);
+
+        return matchingDeciles.Any()
+            ? _searchTopDown
+                ? matchingDeciles.Max()
+                : matchingDeciles.Min()
+            : 10;
+    }
+
+    public decimal[] GetUpperBoundsForDeciles()
+    {
+        Array.Sort(_values);
+
+        var upperBounds = new decimal[9];
+
+        decimal decileSize = _values.Length / 10;
+
+        int sum = 0;
+        for (int i = 1; i <= 9; i++)
+        {
+            var currentDecileSize = (int)Math.Round((decileSize * i) / i);
+            
+            upperBounds[i - 1] = _values.Skip(sum).Take(currentDecileSize).Max();
+
+            sum += currentDecileSize;
+        }
+
+        return upperBounds;
+    }
+}
+
---
+++ web/tests/Web.Tests/Domain/GivenValuesDecileHandler.cs
@@ -0,0 +1,233 @@
+﻿using Web.App.Domain;
+using Xunit;
+
+namespace Web.Tests.Domain;
+
+public class GivenValuesDecileHandler
+{
+
+    [Theory]
+    [MemberData(nameof(DecilesTestvalues))]
+    public void GetUpperBoundsForDecilesCorrectly(decimal[] values, decimal[] expected)
+    {
+        var decileCalculator = new DecileHandler(1, values);
+
+        var result = decileCalculator.GetUpperBoundsForDeciles();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(-100, 1)]
+    [InlineData(-1, 1)]
+    [InlineData(2, 1)]
+    [InlineData(4, 2)]
+    [InlineData(9, 3)]
+    [InlineData(11, 4)]
+    [InlineData(12.01, 5)]
+    [InlineData(17.99, 6)]
+    [InlineData(20, 7)]
+    [InlineData(23, 8)]
+    [InlineData(27, 9)]
+    [InlineData(27.01, 10)]
+    [InlineData(31, 10)]
+    [InlineData(100, 10)]
+    public void FindsDecileCorrectly(decimal value, int expected)
+    {
+        var values = Enumerable.Range(1, 30).Select(x => (decimal)x).ToArray();
+        var decileCalculator = new DecileHandler(value, values);
+
+        var result = decileCalculator.FindDecile();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(-100, 1)]
+    [InlineData(-10, 1)]
+    [InlineData(-9, 1)]
+    [InlineData(-7, 2)]
+    [InlineData(-6.99, 3)]
+    [InlineData(-3.01, 4)]
+    [InlineData(-1.5, 5)]
+    [InlineData(0, 6)]
+    [InlineData(1.01, 7)]
+    [InlineData(3.1, 8)]
+    [InlineData(7, 9)]
+    [InlineData(7.01, 10)]
+    [InlineData(100, 10)]
+    public void FindsDecileCorrectlyWithNegativeValues(decimal value, int expected)
+    {
+        var values = Enumerable.Range(-10, 20).Select(x => (decimal)x).ToArray();
+        var decileCalculator = new DecileHandler(value, values);
+
+        var result = decileCalculator.FindDecile();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(-1500.0, 1)]
+    [InlineData(1976.35, 1)]
+    [InlineData(2000, 2)]
+    [InlineData(2035.20, 3)]
+    [InlineData(2077.45, 4)]
+    [InlineData(2078.35, 5)]
+    [InlineData(2090.89, 6)]
+    [InlineData(2154.56, 7)]
+    [InlineData(2187, 8)]
+    [InlineData(2188, 9)]
+    [InlineData(2200.85, 10)]
+    [InlineData(54325.45, 10)]
+    public void FindsDecileCorrectlyWithRealisticValues(decimal value, int expected)
+    {
+        var values = new decimal[]
+        {
+            2195.20m,
+            2121.21m,
+            2001.67m,
+            2067.92m,
+            2199.34m,
+            2218.46m,
+            2143.09m,
+            2090.88m,
+            2166.73m,
+            1976.35m,
+            2054.23m,
+            2088.66m,
+            2112.78m,
+            2187.91m,
+            2225.12m,
+            2154.56m,
+            1975.43m,
+            2022.89m,
+            2003.76m,
+            2220.37m,
+            2190.45m,
+            2078.34m,
+        };
+        var decileCalculator = new DecileHandler(value, values);
+
+        var result = decileCalculator.FindDecile();
+
+        Assert.Equal(expected, result);
+    }
+
+
+    [Theory]
+    [InlineData(0, true, 1)]
+    [InlineData(0, false, 1)]
+    [InlineData(1, true, 3)]
+    [InlineData(1, false, 1)]
+    [InlineData(2, true, 4)]
+    [InlineData(2, false, 4)]
+    [InlineData(3, true, 5)]
+    [InlineData(3, false, 5)]
+    [InlineData(4, true, 6)]
+    [InlineData(4, false, 6)]
+    [InlineData(5, true, 7)]
+    [InlineData(5, false, 7)]
+    [InlineData(6, true, 8)]
+    [InlineData(6, false, 8)]
+    [InlineData(7, true, 9)]
+    [InlineData(7, false, 9)]
+    [InlineData(8, true, 10)]
+    [InlineData(8, false, 10)]
+    [InlineData(9, true, 10)]
+    [InlineData(9, false, 10)]
+    public void FindsDecileCorrectlyWithSearchParam(int value, bool searchTopDown, int expected)
+    {
+        var decileCalculator = new DecileHandler(value, [1, 1, 1, 2, 3, 4, 5, 6, 7, 8], searchTopDown);
+
+        var result = decileCalculator.FindDecile();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(0, true, 1)]
+    [InlineData(0, false, 1)]
+    [InlineData(1, true, 1)]
+    [InlineData(1, false, 1)]
+    [InlineData(2, true, 2)]
+    [InlineData(2, false, 2)]
+    [InlineData(3, true, 3)]
+    [InlineData(3, false, 3)]
+    [InlineData(4, true, 6)]
+    [InlineData(4, false, 4)]
+    [InlineData(5, true, 7)]
+    [InlineData(5, false, 7)]
+    [InlineData(6, true, 8)]
+    [InlineData(6, false, 8)]
+    [InlineData(7, true, 9)]
+    [InlineData(7, false, 9)]
+    [InlineData(8, true, 10)]
+    [InlineData(8, false, 10)]
+    [InlineData(9, true, 10)]
+    [InlineData(9, false, 10)]
+    public void FindsDecileCorrectlyWithSearchParamDifferentValues(int value, bool searchTopDown, int expected)
+    {
+        var decileCalculator = new DecileHandler(value, [1, 2, 3, 4, 4, 4, 5, 6, 7, 8], searchTopDown);
+
+        var result = decileCalculator.FindDecile();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void DecileHandlerThrowsWithLessThanTenValues()
+    {
+        Assert.Throws<ArgumentException>(() => new DecileHandler(1, [1, 1, 1, 1, 1, 1, 1, 1, 1]));
+    }
+
+    public static IEnumerable<object[]> DecilesTestvalues()
+    {
+        yield return new object[]
+        {
+            new decimal[]
+            {
+                2195.20m,
+                2121.21m,
+                2001.67m,
+                2067.92m,
+                2199.34m,
+                2218.46m,
+                2143.09m,
+                2090.88m,
+                2166.73m,
+                1976.35m,
+                2054.23m,
+                2088.66m,
+                2112.78m,
+                2187.91m,
+                2225.12m,
+                2154.56m,
+                1975.43m,
+                2022.89m,
+                2003.76m,
+                2220.37m,
+                2190.45m,
+                2078.34m,
+            },
+            new decimal[] { 1976.35m, 2003.76m, 2054.23m, 2078.34m, 2090.88m, 2121.21m, 2154.56m, 2187.91m, 2195.20m },
+        };
+
+        yield return new object[]
+        {
+            Enumerable.Range(1, 10).Select(x => (decimal)x).ToArray(),
+            new decimal[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+        };
+
+        yield return new object[]
+        {
+            Enumerable.Range(1, 30).Select(x => (decimal)x).ToArray(),
+            new decimal[] { 3, 6, 9, 12, 15, 18, 21, 24, 27 },
+        };
+
+        yield return new object[]
+        {
+            Enumerable.Range(-10, 20).Select(x => (decimal)x).ToArray(),
+            new decimal[] { -9, -7, -5, -3, -1, 1, 3, 5, 7 },
+        };
+    }
+}
