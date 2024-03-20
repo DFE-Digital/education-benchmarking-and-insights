a5ce2873e8f0c15fa3c720526b8bd38da6f1e478
Jon Abbott <jon.r.abbott@gmail.com>
2024-03-20 17:16:22 +0000

    feat: update school name to be link to school home page (#471)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/src/Web.App/ViewModels/Components/SchoolHeadingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.App.ViewModels.Components;
+
+public class SchoolHeadingViewModel(string pageTitle, string schoolName, string urn)
+{
+    public string PageTitle => pageTitle;
+    public string SchoolName => schoolName;
+    public string Urn => urn;
+}
