a63fb0024d71437f81a1d617b4720485a3a257c1
J05h-L <josh.langford@hippodigital.co.uk>
2024-03-22 15:22:48 +0000

    linting and adds MidpointRounding.AwayFromZero

--- web/src/Web.App/Domain/Deciles.cs
+++ web/src/Web.App/Domain/Deciles.cs
@@ -17,7 +17,7 @@ public class DecileHandler : IDecileHandler
         {
             throw new ArgumentException("There must be at least 10 values to calculate deciles.");
         }
-        
+
         _value = value;
         _values = values.Order().ToArray();
         _searchTopDown = searchTopDown;
@@ -27,37 +27,37 @@ public class DecileHandler : IDecileHandler
     {
         return _searchTopDown ? TopDown() : BottomUp();
     }
-    
+
     private int TopDown()
     {
         for (var decile = 10; decile > 1; decile--)
         {
             var valueIndex = CountInDecile(decile) - 1;
             if (_value < _values[valueIndex]) continue;
-            
+
             return decile;
         }
 
         return 1;
     }
-    
+
     private int BottomUp()
     {
         for (var decile = 1; decile < 10; decile++)
         {
             var valueIndex = CountInDecile(decile) - 1;
             if (_value > _values[valueIndex]) continue;
-            
+
             return decile;
         }
 
         return 10;
     }
-    
+
     private int CountInDecile(int decile)
     {
         var perDecile = _values.Count / 10.0;
-        return (int)Math.Round(perDecile * decile);
+        return (int)Math.Round(perDecile * decile, MidpointRounding.AwayFromZero);
     }
 }
 
