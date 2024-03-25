37cca11e12eea71e68ef364038252a561f4b4f16
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-03-25 17:14:37 +0000

    fix: ensure chromium is install for a11y tests

---
+++ web/tests/Web.A11yTests/AssemblyFixture.cs
@@ -0,0 +1,23 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: Xunit.TestFramework("Web.A11yTests.AssemblyFixture", "Web.A11yTests")]
+
+namespace Web.A11yTests;
+
+public sealed class AssemblyFixture : XunitTestFramework, IDisposable
+{
+    public AssemblyFixture(IMessageSink messageSink) : base(messageSink)
+    {
+        var exitCode = Microsoft.Playwright.Program.Main(["install", "--with-deps", "chromium"]);
+        if (exitCode != 0)
+        {
+            throw new Exception($"Playwright exited with code {exitCode}");
+        }
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+    }
+}
