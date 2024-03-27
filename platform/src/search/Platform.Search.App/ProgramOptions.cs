004308fafb02461ddca6768ad81818ea9ed23e94
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-03-27 14:51:00 +0000

    chore: update platform to run search app for automated test env

--- platform/src/search/Platform.Search.App/ProgramOptions.cs
+++ platform/src/search/Platform.Search.App/ProgramOptions.cs
@@ -17,9 +17,6 @@ public class ProgramOptions
     [Option('d', "cosmosDatabaseId", Required = true)]
     public string? CosmosDatabaseId { get; set; }
 
-    [Option('l', "lookupCollectionName", Required = true)]
-    public string? LookupCollectionName { get; set; }
-
     [Option('p', "platformStorageConnectionString", Required = true)]
     public string? PlatformStorageConnectionString { get; set; }
 
