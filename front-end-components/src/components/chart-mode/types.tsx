850a1dfab7ff4144e8939a903a857218eb615e83
Jon Abbott <jon.r.abbott@gmail.com>
2024-03-27 16:22:39 +0000

    feat: adds initial layout for workforce and balance tabls (#515)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

--- front-end-components/src/components/chart-mode/types.tsx
+++ front-end-components/src/components/chart-mode/types.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export type ChartModeProps = {
   displayMode: string;
   handleChange: React.ChangeEventHandler<HTMLInputElement>;
+  prefix?: string;
 };
 
 export const ChartModeChart = "Chart";
---
+++ front-end-components/src/views/school-history/partials/index.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+export * from "src/views/school-history/partials/income";
+export * from "src/views/school-history/partials/workforce";
+export * from "src/views/school-history/partials/balance";
+export * from "src/views/school-history/partials/spending";
