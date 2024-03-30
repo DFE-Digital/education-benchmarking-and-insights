d9c533bfd7d98cafd6386eeff086545ccf625e6d
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-03-30 17:57:26 +0000

    chore: updates scripts to include map files in copy

--- front-end-components/copy-js.ps1
+++ front-end-components/copy-js.ps1
@@ -2,5 +2,5 @@ npm run lint
 npm test
 npm run build
 
-Copy-Item dist/front-end.js ../web/src/Web.App/wwwroot/js -Verbose
+Copy-Item dist/front-end.js* ../web/src/Web.App/wwwroot/js -Verbose
 Copy-Item dist/front-end.css ../web/src/Web.App/wwwroot/css -Verbose
