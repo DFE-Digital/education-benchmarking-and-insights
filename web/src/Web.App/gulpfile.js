74cebb074875914338e223b840bde61a660c8525
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-02 17:42:56 +0100

    fix: updates gulp task with corrected configuration for breaking change in v5

--- web/src/Web.App/gulpfile.js
+++ web/src/Web.App/gulpfile.js
@@ -8,12 +8,12 @@ const buildSass = () => gulp.src("AssetSrc/scss/*.scss")
 	.pipe(cleanCSS())
 	.pipe(gulp.dest("wwwroot/css"));
 
-const copyStaticAssets = () => gulp.src(["node_modules/govuk-frontend/dist/govuk/assets/**/*"]).pipe(gulp.dest("wwwroot/assets")).on("end", () =>
-	gulp.src(["node_modules/govuk-frontend/dist/govuk/assets/images/favicon.ico"]).pipe(gulp.dest("wwwroot/"))).on("end", () =>
+const copyStaticAssets = () => gulp.src(["node_modules/govuk-frontend/dist/govuk/assets/**/*"],{ encoding: false }).pipe(gulp.dest("wwwroot/assets")).on("end", () =>
+	gulp.src(["node_modules/govuk-frontend/dist/govuk/assets/images/favicon.ico"],{ encoding: false }).pipe(gulp.dest("wwwroot/"))).on("end", () =>
 	gulp.src(["node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js"]).pipe(gulp.dest("wwwroot/js/"))).on("end", () =>
 	gulp.src(["node_modules/front-end/dist/front-end.js"]).pipe(gulp.dest("wwwroot/js/"))).on("end", () =>
 	gulp.src(["node_modules/front-end/dist/front-end.css"]).pipe(gulp.dest("wwwroot/css/"))).on("end", () =>
-	gulp.src(["AssetSrc/images/*"]).pipe(gulp.dest("wwwroot/assets/images")));
+	gulp.src(["AssetSrc/images/*"],{ encoding: false }).pipe(gulp.dest("wwwroot/assets/images")));
 
 
 gulp.task("build-fe", () => {
