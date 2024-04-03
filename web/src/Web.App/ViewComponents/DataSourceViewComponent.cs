5fa56b3cc0f416ac060be7fa790355fdc38ee954
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-03 12:27:52 +0100

    chore: adds trust specific data source text

--- web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
+++ web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
@@ -6,12 +6,21 @@ namespace Web.App.ViewComponents;
 
 public class DataSourceViewComponent(IFinanceService financeService) : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(bool isPartOfTrust, string[]? additionText)
+    public async Task<IViewComponentResult> InvokeAsync(string kind, bool isPartOfTrust, string[]? additionText)
     {
         var years = await financeService.GetYears();
-        var dataSource = isPartOfTrust
-            ? $"Data for this school covers the financial year September {years.Aar - 1} to August {years.Aar}. It comes from the academies accounts return (AAR) return."
-            : $"Data for this school covers the financial year April {years.Cfr - 1} to {years.Cfr} 2023. It comes from the consistent financial reporting (CFR) return.";
+        var dataSource = kind switch
+        {
+            OrganisationTypes.School when isPartOfTrust =>
+                $"Data for this school covers the financial year September {years.Aar - 1} to August {years.Aar}. It comes from the academies accounts return (AAR) return.",
+            OrganisationTypes.School when !isPartOfTrust =>
+                $"Data for this school covers the financial year April {years.Cfr - 1} to {years.Cfr} 2023. It comes from the consistent financial reporting (CFR) return.",
+            OrganisationTypes.Trust =>
+                $"Data for this trust covers the financial year September {years.Aar - 1} to August {years.Aar}. It comes from the academies accounts return (AAR) return.",
+            _ => throw new ArgumentOutOfRangeException(nameof(kind))
+        };
+
+
         return View(new DataSourceViewModel(dataSource, additionText));
     }
 }
