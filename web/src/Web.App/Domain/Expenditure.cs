37858b8cd9e744d38a725d5fac700b049f12a047
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-03 17:05:33 +0100

    feat: updates api response to include additional props for expenditure history AB#185882 AB#202613

--- web/src/Web.App/Domain/Expenditure.cs
+++ web/src/Web.App/Domain/Expenditure.cs
@@ -16,4 +16,34 @@ public record Expenditure
     public decimal? AuditorsCosts { get; set; }
     public decimal? OtherStaffCosts { get; set; }
     public decimal? ProfessionalServicesNonCurriculumCosts { get; set; }
+    public decimal? TotalEducationalSuppliesCosts { get; set; }
+    public decimal? ExaminationFeesCosts { get; set; }
+    public decimal? LearningResourcesNonIctCosts { get; set; }
+    public decimal? LearningResourcesIctCosts { get; set; }
+    public decimal? TotalPremisesStaffServiceCosts { get; set; }
+    public decimal? CleaningCaretakingCosts { get; set; }
+    public decimal? MaintenancePremisesCosts { get; set; }
+    public decimal? OtherOccupationCosts { get; set; }
+    public decimal? PremisesStaffCosts { get; set; }
+    public decimal? TotalUtilitiesCosts { get; set; }
+    public decimal? EnergyCosts { get; set; }
+    public decimal? WaterSewerageCosts { get; set; }
+    public decimal? AdministrativeSuppliesCosts { get; set; }
+    public decimal? TotalGrossCateringCosts { get; set; }
+    public decimal? CateringStaffCosts { get; set; }
+    public decimal? CateringSuppliesCosts { get; set; }
+    public decimal? TotalOtherCosts { get; set; }
+    public decimal? DirectRevenueFinancingCosts { get; set; }
+    public decimal? GroundsMaintenanceCosts { get; set; }
+    public decimal? IndirectEmployeeExpenses { get; set; }
+    public decimal? InterestChargesLoanBank { get; set; }
+    public decimal? OtherInsurancePremiumsCosts { get; set; }
+    public decimal? PrivateFinanceInitiativeCharges { get; set; }
+    public decimal? RentRatesCosts { get; set; }
+    public decimal? SpecialFacilitiesCosts { get; set; }
+    public decimal? StaffDevelopmentTrainingCosts { get; set; }
+    public decimal? StaffRelatedInsuranceCosts { get; set; }
+    public decimal? SupplyTeacherInsurableCosts { get; set; }
+    public decimal? CommunityFocusedSchoolStaff { get; set; }
+    public decimal? CommunityFocusedSchoolCosts { get; set; }
 }
