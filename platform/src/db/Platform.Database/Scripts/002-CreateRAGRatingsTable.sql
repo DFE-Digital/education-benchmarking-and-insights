92554829d687d96734ceaf6e2b2bd7896d929b1d
Michael Fielding <98596456+michael-fielding@users.noreply.github.com>
2024-04-03 17:21:35 +0100

    Creates RAGRatings table
    Creates the actual table after I remember to save my changes before committing!

--- platform/src/db/Platform.Database/Scripts/002-CreateRAGRatingsTable.sql
+++ platform/src/db/Platform.Database/Scripts/002-CreateRAGRatingsTable.sql
@@ -1,30 +1,47 @@
 IF NOT EXISTS(SELECT *
               FROM INFORMATION_SCHEMA.TABLES
-              WHERE table_name = 'ComparatorSets')
+              WHERE table_name = 'RAGRatings')
     BEGIN
-        CREATE TABLE dbo.ComparatorSets
+        CREATE TABLE dbo.RAGRatings
         (
-            all_comparators_all_Id bigint       NOT NULL,
-            trust_academy_Id       bigint       NOT NULL,
-            URN                    int          NOT NULL,
-            UKPRN_URN1             nvarchar(50) NOT NULL,
-            UKPRN_URN2             nvarchar(50) NOT NULL,
-            UKPRN_URN_CG           nvarchar(50) NOT NULL,
-            PeerGroup              nvarchar(50) NOT NULL,
-            CostGroup              nvarchar(50) NOT NULL,
-            CompareNum             bit          NOT NULL,
-            compare                bit          NOT NULL,
-            RANK2                  int          NOT NULL,
-            Comparator_code        int          NOT NULL,
-            RANK3                  int          NOT NULL,
-            ReprocessFlag          int          NOT NULL,
-            UseAllCompFlag         bit          NOT NULL,
-            Range_flag             bit          NOT NULL,
-            DataReleaseId          int          NOT NULL,
-            PartYearDataFlag       int          NOT NULL,
-
-            CONSTRAINT PK_ComparatorSets PRIMARY KEY (all_comparators_all_Id)
+            [RAGRatingId] [int] NOT NULL,
+	        [academies_data_Id] [bigint] NULL,
+            [URN] [varchar](10) NULL,
+	        [UKPRN] [char](8) NOT NULL,
+	        [UKPRN_URN] [varchar](20) NOT NULL,
+	        [RAGAcademyName] [varchar](255) NULL,
+	        [Sector] [varchar](50) NOT NULL,
+	        [Cost Pool] [varchar](100) NOT NULL,
+	        [Cost Pool ID] [int] NOT NULL,
+	        [CostGroup] [varchar](5) NULL,
+	        [PeerGroup] [varchar](10) NOT NULL,
+	        [PerUnit] [decimal](38, 0) NULL,
+	        [5thDecile] [decimal](38, 5) NULL,
+	        [Decile] [int] NULL,
+	        [RAG] [int] NOT NULL,
+	        [Rank] [int] NULL,
+	        [PerUnitText] [varchar](100) NULL,
+	        [%ReductionText] [nvarchar](100) NULL,
+	        [RankAcademyLevel] [int] NULL,
+	        [RAGletter] [varchar](5) NULL,
+	        [RAGWord] [varchar](5) NULL,
+	        [RAGOrder] [int] NULL,
+	        [OverallAcademyRank] [int] NULL,
+	        [OverallCostPoolRank] [int] NULL,
+	        [PerUnitRank] [int] NULL,
+	        [%Comparators] [varchar](50) NULL,
+	        [£DiffText] [nvarchar](100) NULL,
+	        [%DiffText] [nvarchar](100) NULL,
+	        [HomepageText] [nvarchar](280) NULL,
+	        [Key] [varchar](18) NULL,
+	        [PerUnit_PreviousYear] [decimal](38, 0) NULL,
+	        [YOY%change] [decimal](38, 6) NULL,
+	        [YOY%changeText] [varchar](200) NULL,
+	        [PerUnit_PreviousYearText] [varchar](200) NULL,
+	        [DataReleaseId] [int] NULL,
+            
+            CONSTRAINT [PK_RAGRating] PRIMARY KEY (RAGRatingId) 
         );
 
-        CREATE INDEX ComparatorSets_URN_PeerGroup_CostGroup ON ComparatorSets ( URN, PeerGroup, CostGroup)
+        CREATE INDEX RAGRatings_URN_PeerGroup_CostGroup ON RAGRatings (URN, PeerGroup, CostGroup)
     END
