62447563339c2ce395fcb5e62346bbd0da2a8a32
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-08 11:37:48 +0100

    chore: adds azure front door and firewall policy

--- web/terraform/data.tf
+++ web/terraform/data.tf
@@ -3,3 +3,43 @@ data "azurerm_subnet" "web-app-subnet" {
   virtual_network_name = "${var.environment-prefix}-app-service-network"
   resource_group_name  = "${var.environment-prefix}-ebis-core"
 }
+
+data "azurerm_application_insights" "application-insights" {
+  name                = "${var.environment-prefix}-ebis-ai"
+  resource_group_name = "${var.environment-prefix}-ebis-core"
+}
+
+data "azurerm_key_vault" "key-vault" {
+  name                = "${var.environment-prefix}-ebis-keyvault"
+  resource_group_name = "${var.environment-prefix}-ebis-core"
+}
+
+data "azurerm_key_vault_secret" "insight-api-key" {
+  name         = "insight-host-key"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "insight-api-host" {
+  name         = "insight-host"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "benchmark-api-key" {
+  name         = "benchmark-host-key"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "benchmark-api-host" {
+  name         = "benchmark-host"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "establishment-api-host" {
+  name         = "establishment-host"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "establishment-api-key" {
+  name         = "establishment-host-key"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
