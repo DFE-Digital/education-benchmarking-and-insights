a3baf5a10e1978dad8f9301aca24677eca588c00
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-09 16:27:57 +0100

    feat: updates data source text

--- web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
+++ web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
@@ -9,14 +9,15 @@ public class DataSourceViewComponent(IFinanceService financeService) : ViewCompo
     public async Task<IViewComponentResult> InvokeAsync(string kind, bool isPartOfTrust, string[]? additionText)
     {
         var years = await financeService.GetYears();
+        
         var dataSource = kind switch
         {
             OrganisationTypes.School when isPartOfTrust =>
-                $"Data for this school covers the financial year September {years.Aar - 1} to August {years.Aar}. It comes from the academies accounts return (AAR) return.",
+                $"This school's data covers the financial year September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR).",
             OrganisationTypes.School when !isPartOfTrust =>
-                $"Data for this school covers the financial year April {years.Cfr - 1} to {years.Cfr} 2023. It comes from the consistent financial reporting (CFR) return.",
+                $"This school's data covers the financial year April {years.Cfr - 1} to March {years.Cfr} consistent financial reporting return (CFR).",
             OrganisationTypes.Trust =>
-                $"Data for this trust covers the financial year September {years.Aar - 1} to August {years.Aar}. It comes from the academies accounts return (AAR) return.",
+                $"This trust's data covers the financial September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR).",
             _ => throw new ArgumentOutOfRangeException(nameof(kind))
         };
 
