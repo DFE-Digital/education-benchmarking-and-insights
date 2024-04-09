de6b3f7a620fffe71e43cde248fba345f48fa7c3
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-04-09 10:52:17 +0100

    feat: Allow interactive `<SchoolTick>` to be tabbed-to and focusable for improved a11y support

--- front-end-components/src/components/charts/school-tick/types.tsx
+++ front-end-components/src/components/charts/school-tick/types.tsx
@@ -1,6 +1,9 @@
 import { BaseAxisProps, CartesianTickItem } from "recharts/types/util/types";
+import { TickProps } from "../types";
 
-export interface SchoolTickProps extends Omit<BaseAxisProps, "scale"> {
+export interface SchoolTickProps
+  extends Omit<BaseAxisProps, "scale">,
+    Omit<TickProps, "type" | "onClick" | "ref" | "textAnchor"> {
   highlightedItemKey?: string;
   linkToSchool?: boolean;
   onClick?: (urn: string) => void;
