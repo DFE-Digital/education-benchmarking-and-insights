d84f2ae1041f942559f1c2d0364464b086c627f9
Faizan Ahmad <faizan.ahmad@education.gov.uk>
2024-04-15 11:45:17 +0100

    added assertion to wait for dropdown to appear

--- web/tests/Web.E2ETests/Pages/FindOrganisationPage.cs
+++ web/tests/Web.E2ETests/Pages/FindOrganisationPage.cs
@@ -8,6 +8,7 @@ public enum OrganisationTypes
     School,
     Trust
 }
+
 public class FindOrganisationPage(IPage page)
 {
     private const string ArrowDownKey = "ArrowDown";
@@ -16,8 +17,12 @@ public class FindOrganisationPage(IPage page)
     private ILocator Breadcrumbs => page.Locator(Selectors.GovBreadcrumbs);
     private ILocator OrganisationsTypeRadios => page.Locator(Selectors.GovRadios);
     private ILocator SchoolSearchInputField => page.Locator(Selectors.SchoolSearchInput);
-    private ILocator ContinueButton => page.Locator(Selectors.GovButton, new PageLocatorOptions { HasText = "Continue" });
+
+    private ILocator ContinueButton =>
+        page.Locator(Selectors.GovButton, new PageLocatorOptions { HasText = "Continue" });
+
     private ILocator SchoolRadioButton => page.Locator(Selectors.SchoolRadio);
+    private ILocator SchoolSuggestionsDropdown => page.Locator(Selectors.SchoolSuggestDropdown);
 
     public async Task IsDisplayed()
     {
@@ -29,7 +34,9 @@ public class FindOrganisationPage(IPage page)
 
     public async Task<HomePage> SelectSchoolFromSuggester(string text)
     {
-        await SchoolSearchInputField.PressSequentially(text).Press(ArrowDownKey).Press(EnterKey);
+        await SchoolSearchInputField.PressSequentially(text);
+        await SchoolSuggestionsDropdown.ShouldBeVisible();
+        await SchoolSearchInputField.Press(ArrowDownKey).Press(EnterKey);
         await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
         return new HomePage(page);
     }
