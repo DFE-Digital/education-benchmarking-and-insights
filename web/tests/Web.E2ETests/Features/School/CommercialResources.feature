0b45444a494a459856593ce5a53d7a9723d1f872
Faizi52996 <111439003+Faizi52996@users.noreply.github.com>
2024-04-19 16:06:11 +0100

    Feature/185942/e2e tests (#632)
    * WIP initial implementation of e2e
    
    * completed tests
    
    * formatted code
    
    ---------
    
    Co-authored-by: Faizan Ahmad <faizan.ahmad@education.gov.uk>

---
+++ web/tests/Web.E2ETests/Features/School/CommercialResources.feature
@@ -0,0 +1,23 @@
+﻿Feature: School find ways to spend less
+
+    Scenario: View all resources
+        Given I am on 'recommended' resources page for school with URN '101241'
+        And the following priority categories are shown on the page
+          | Name                                | Priority        |
+          | Catering staff and services         | High priority   |
+          | Utilities                           | High priority   |
+          | Premises and services               | High priority   |
+          | Educational ICT                     | Medium priority |
+          | Teaching and teaching support staff | Medium priority |
+          | Other                               | Medium priority |
+          | Administrative supplies             | Medium priority |
+        When I click on all resources
+        Then all resources tab is displayed
+
+    Scenario: Show all should expand all sections
+        Given I am on 'all' resources page for school with URN '101241'
+        When I click on show all sections
+        Then all sections on the page are expanded
+        And the show all text changes to hide all sections
+        And all resources sub categories are displayed on the page
+        And all sub categories have correct link
---
+++ web/tests/Web.E2ETests/Steps/School/CommercialResourcesSteps.cs
@@ -0,0 +1,95 @@
+﻿using Web.E2ETests.Drivers;
+using Web.E2ETests.Pages.School;
+using Xunit;
+
+namespace Web.E2ETests.Steps.School;
+[Binding]
+[Scope(Feature = "School find ways to spend less")]
+public class CommercialResourcesSteps(PageDriver driver)
+{
+    private CommercialResourcesPage? _commercialResourcesPage;
+
+    [Given("I am on '(.*)' resources page for school with URN '(.*)'")]
+    public async Task GivenIAmOnResourcesPageForSchoolWithUrn(string tab, string urn)
+    {
+        var url = CommercialResourcesUrl(tab, urn);
+        var page = await driver.Current;
+        await page.GotoAndWaitForLoadAsync(url);
+
+        _commercialResourcesPage = new CommercialResourcesPage(page);
+        await _commercialResourcesPage.IsDisplayed();
+    }
+
+    [Given("the following priority categories are shown on the page")]
+    public async Task GivenTheFollowingPriorityCategoriesAreShownOnThePage(Table table)
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        var expectedOrder = new List<string[]>();
+        foreach (var row in table.Rows)
+        {
+            string[] chartPriorityArray = {
+                row["Name"],
+                row["Priority"]
+            };
+            expectedOrder.Add(chartPriorityArray);
+        }
+
+        await _commercialResourcesPage.CheckPriorityOrderOfResources(expectedOrder);
+    }
+
+    [When("I click on all resources")]
+    public async Task WhenIClickOnAllResources()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.ClickAllResources();
+    }
+
+    [Then("all resources tab is displayed")]
+    public async Task ThenAllResourcesTabIsDisplayed()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.IsAllResourcesDisplayed();
+    }
+
+    [When("I click on show all sections")]
+    [Given("I click on show all sections")]
+    public async Task WhenIClickOnShowAllSections()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.ClickShowAllSections();
+    }
+
+
+    [Then("all sections on the page are expanded")]
+    public async Task ThenAllSectionsOnThePageAreExpanded()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.AreSectionsExpanded();
+    }
+
+    [Then("the show all text changes to hide all sections")]
+    public async Task ThenTheShowAllTextChangesToHideAllSections()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.IsShowHideAllSectionsText("Hide all sections");
+    }
+
+    [Then("all resources sub categories are displayed on the page")]
+    public async Task ThenAllResourcesSubCategoriesAreDisplayedOnThePage()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.AreAllResourcesVisible();
+
+    }
+
+    [Then("all sub categories have correct link")]
+    public async Task ThenAllSubCategoriesHaveCorrectLink()
+    {
+        Assert.NotNull(_commercialResourcesPage);
+        await _commercialResourcesPage.AreCorrectLinksDisplayed();
+    }
+
+    private static string CommercialResourcesUrl(string tab, string urn) => $"{TestConfiguration.ServiceUrl}/school/{urn}/find-ways-to-spend-less#{tab}";
+
+
+}
