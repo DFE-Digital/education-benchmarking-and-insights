50031867372bb380ca9607f5c7c1dc9c74445dea
Colin <colinbul@gmail.com>
2024-04-26 10:10:39 +0100

    stash: stash commit

--- pipeline/src/main.py
+++ pipeline/src/main.py
@@ -1,10 +1,35 @@
+import logging
 import os
+import sys
+import time
 
-from fastapi import FastAPI
+from azure.storage.queue import QueueClient
 
-app = FastAPI()
+logger = logging.getLogger('DocumentParser')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-@app.get("/")
-async def main_route():
-    return {"message": f'Hello World! Environment - {os.environ.get("FASTAPI_ENV")}'}
+def receive_messages():
+
+    conn_str = os.getenv('QUEUE_CONNECTION_STRING')
+    queue_name = os.getenv('QUEUE_NAME')
+    if not conn_str:
+        logger.error("queue connection string not provided!")
+        return
+
+    if not queue_name:
+        logger.error("queue name not provided!")
+        return
+
+    queue_client = QueueClient.from_connection_string(conn_str=conn_str, queue_name=queue_name)
+    msg = queue_client.receive_message()
+
+    logger.info(f'received message {msg}')
+
+    time.sleep(45)
+
+    logger.info(f'processing complete')
+
+
+if __name__ == '__main__':
+    receive_messages()
