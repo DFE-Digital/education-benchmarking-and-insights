ac85a14becb7aa2017aaa5a698835620c20af779
Colin <colinbul@gmail.com>
2024-04-27 10:04:12 +0100

    feat: adding acr to core infrastructure

---
+++ core-infrastructure/terraform/acr.tf
@@ -0,0 +1,20 @@
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.environment-prefix}acr"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  sku                 = "Basic"
+  # TODO: Need to review as needed for container apps I believe, unless I can allow the SystemAssignedIdentity
+  admin_enabled       = true 
+  public_network_access_enabled = false 
+
+  retention_policy {
+    days = 30
+    enabled = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
+  tags = local.common-tags
+}
