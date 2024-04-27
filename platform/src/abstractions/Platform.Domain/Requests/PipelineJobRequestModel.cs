c1c361b845a243cc1316bec2f7369706c52a744e
Jon Abbott <jon.r.abbott@gmail.com>
2024-04-27 18:56:18 +0100

    fix: typo in default job id

--- platform/src/abstractions/Platform.Domain/Requests/PipelineJobRequestModel.cs
+++ platform/src/abstractions/Platform.Domain/Requests/PipelineJobRequestModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Platform.Domain;
 
@@ -10,7 +10,7 @@ public enum JobKind
 
 public class PipelineJobRequestModel
 {
-    public string JobId => Kind == JobKind.Custom ? $"{Kind.ToString().ToLower()}-{Year}-{RequestId}" : $"{Kind.ToString().ToLower()}-{Year}-";
+    public string JobId => Kind == JobKind.Custom ? $"{Kind.ToString().ToLower()}-{Year}-{RequestId}" : $"{Kind.ToString().ToLower()}-{Year}";
     public JobKind? Kind { get; set; }
     public int Year { get; set; }
     public Guid? RequestId { get; set; }
