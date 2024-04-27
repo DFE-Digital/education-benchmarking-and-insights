fc18841b9ff3028b399298e9c2e433ae5e18139c
Colin <colinbul@gmail.com>
2024-04-27 13:40:57 +0100

    chore: Skipping retention policy as not available on Standard tier

--- core-infrastructure/terraform/acr.tf
+++ core-infrastructure/terraform/acr.tf
@@ -2,13 +2,14 @@ resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_233:See ADO backlog AB#206776
   #checkov:skip=CKV_AZURE_165:See ADO backlog AB#206776
   #checkov:skip=CKV_AZURE_166:See ADO backlog AB#206776
+  #checkov:skip=CKV_AZURE_167:See ADO backlog AB#206776
   name                          = "${var.environment-prefix}acr"
   resource_group_name           = azurerm_resource_group.resource-group.name
   location                      = azurerm_resource_group.resource-group.location
   sku                           = "Standard"
   admin_enabled                 = false
   public_network_access_enabled = false
-  
+
   #TODO: Review as premium is required for reention policy
   retention_policy {}
 
