f716ad74aaa5ca3a78a8d71e11c43c93819e0a1c
Colin <colinbul@gmail.com>
2024-04-27 10:58:52 +0100

    chore: Fixing checkov advice inconsistency

--- core-infrastructure/terraform/acr.tf
+++ core-infrastructure/terraform/acr.tf
@@ -1,13 +1,13 @@
 resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_233:See ADO backlog AB#206776
   #checkov:skip=CKV_AZURE_165:See ADO backlog AB#206776
+  #checkov:skip=CKV_AZURE_166:See ADO backlog AB#206776
   name                          = "${var.environment-prefix}acr"
   resource_group_name           = azurerm_resource_group.resource-group.name
   location                      = azurerm_resource_group.resource-group.location
   sku                           = "Standard"
   admin_enabled                 = false
   public_network_access_enabled = false
-  quarantine_policy_enabled     = true
   retention_policy {
     days    = 30
     enabled = true
