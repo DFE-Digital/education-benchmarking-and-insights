b0320faf984dee0c50fcc6e201a896b810f6886f
Colin <colinbul@gmail.com>
2024-04-27 16:11:50 +0100

    chore: re-enabling public access as not supported on SKU

--- core-infrastructure/terraform/acr.tf
+++ core-infrastructure/terraform/acr.tf
@@ -4,12 +4,15 @@ resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_166:See ADO backlog AB#206776
   #checkov:skip=CKV_AZURE_167:See ADO backlog AB#206776
   #checkov:skip=CKV_AZURE_164:See ADO backlog AB#206776
+  #checkov:skip=CKV_AZURE_139:See ADO backlog AB#206776
   name                          = "${var.environment-prefix}acr"
   resource_group_name           = azurerm_resource_group.resource-group.name
   location                      = azurerm_resource_group.resource-group.location
   sku                           = "Standard"
   admin_enabled                 = false
-  public_network_access_enabled = false
+
+  #TODO: Review as premium is required to limit publis access
+  public_network_access_enabled = true 
 
   #TODO: Review as premium is required for reention policy
   retention_policy {}
