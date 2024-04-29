d7ebb982a23a1faed22807029862e423df694f2b
Jon Abbott <jon.r.abbott@gmail.com>
2024-04-29 11:25:00 +0100

    chore: adds school and trust authorization attributes (#660)
    * chore: adds school and trust authorization attributes
    
    * fix: corrects variable name
    
    ---------
    
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/src/Web.App/Attributes/TrustAuthorizationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web.App.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class TrustAuthorizationAttribute : AuthorizeAttribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var companyNumber = context.RouteData.Values["companyNumber"]?.ToString();
+
+        var isValid = context.HttpContext.User.Claims.Any(c => companyNumber != null && c.Type == "Trusts" && c.Value.Contains(companyNumber));
+        if (!isValid)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
--- web/src/Web.App/Constants/BreadcrumbNodes.cs
+++ web/src/Web.App/Constants/BreadcrumbNodes.cs
@@ -13,4 +13,5 @@ public static class BreadcrumbNodes
     public static MvcBreadcrumbNode TrustHome(string companyNumber) => new("Index", "Trust", PageTitles.TrustHome) { RouteValues = new { companyNumber } };
     public static MvcBreadcrumbNode TrustComparison(string companyNumber) => new("Index", "TrustComparison", PageTitles.Comparison) { RouteValues = new { companyNumber }, Parent = TrustHome(companyNumber) };
     public static MvcBreadcrumbNode TrustWorkforce(string companyNumber) => new("Index", "TrustWorkforce", PageTitles.Workforce) { RouteValues = new { companyNumber }, Parent = TrustHome(companyNumber) };
+    public static MvcBreadcrumbNode TrustPlanning(string companyNumber) => new("Index", "TrustPlanning", "Curriculum and financial planning") { RouteValues = new { companyNumber }, Parent = TrustHome(companyNumber) };
 }
