f84fb191f2286d4bc27f7ebdc9853e52ef353014
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-04-30 19:03:41 +0100

    chore: remove margin on warning banner for incomplete data

--- front-end-components/src/components/warning-banner/component.tsx
+++ front-end-components/src/components/warning-banner/component.tsx
@@ -3,5 +3,5 @@ import { WarningBannerProps } from "src/components/warning-banner";
 export const WarningBanner: React.FC<WarningBannerProps> = (props) => {
   const { isRendered, message } = props;
 
-  return isRendered ? <div className="govuk-inset-text">{message}</div> : null;
+  return isRendered ? <div className="govuk-inset-text govuk-body govuk-!-margin-0">{message}</div> : null;
 };
