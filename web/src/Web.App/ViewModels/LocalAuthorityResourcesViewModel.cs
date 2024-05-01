02ed251e9a3b4c4693d1b8948e9e2ea4eede9c4e
J05h-L <josh.langford@hippodigital.co.uk>
2024-05-01 20:44:29 +0100

    adds controller, view model and view for local authority resources page

---
+++ web/src/Web.App/ViewModels/LocalAuthorityResourcesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Web.App.Domain;
+
+namespace Web.App.ViewModels;
+
+public class LocalAuthorityResourcesViewModel(LocalAuthority localAuthority)
+{
+    public string? Code => localAuthority.Code;
+    public string? Name => localAuthority.Name;
+}
