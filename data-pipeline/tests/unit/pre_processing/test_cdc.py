fc86e4481eaaa194fd6f381c381446c6de39519b
Roger G. Coram <roger.coram@hippodigital.co.uk>
2024-05-02 14:51:47 +0100

    test: move data to fixture
    - add `conftest.py` for pre-processing fixtures.
    - add test for initial preparation.

--- data-pipeline/tests/unit/pre_processing/test_cdc.py
+++ data-pipeline/tests/unit/pre_processing/test_cdc.py
@@ -1,28 +1,26 @@
 from io import StringIO
 
-import pandas as pd
 import pytest
 
-from src.pipeline.pre_processing import prepare_cdc_data
-
-data = pd.DataFrame({
-        'Site': ['Site A', 'Site A', 'Site A', 'Site A', 'Site B'],
-        'URN': [100150, 100150, 100150, 100150, 100162],
-        'SurveySectionSectionType': ['ANCILLARY', 'BLOCK', 'BLOCK', 'BLOCK', 'ANCILLARY'],
-        'GIFA': [89, 2221, 436, 57, 131],
-        'Block Age': ['NULL', '1961-1970', '2001-2010', '1991-2000', 'NULL']
-    })
-
-actual = prepare_cdc_data(StringIO(data.to_csv()), 2022).reset_index().to_dict('records')[0]
+from pipeline.pre_processing import prepare_cdc_data
 
 
-def test_cdc_has_correct_index():
-    assert actual['URN'] == 100150
+def test_prepare_cdc_data(cdc_data):
+    prepared_cdc_data = prepare_cdc_data(StringIO(cdc_data.to_csv()), 2022)
+
+    assert list(prepared_cdc_data.columns) == [
+        "Total Internal Floor Area",
+        "Age Average Score",
+    ]
 
 
-def test_cdc_has_correct_total_internal_floor_area():
-    assert actual['Total Internal Floor Area'] == 2803.0
+def test_cdc_has_correct_index(prepared_cdc_data: dict):
+    assert prepared_cdc_data["URN"] == 100150
 
 
-def test_cdc_has_correct_age_average_score():
-    assert pytest.approx(actual['Age Average Score'], 0.001) == 48.358
+def test_cdc_has_correct_total_internal_floor_area(prepared_cdc_data: dict):
+    assert prepared_cdc_data["Total Internal Floor Area"] == 2803.0
+
+
+def test_cdc_has_correct_age_average_score(prepared_cdc_data: dict):
+    assert pytest.approx(prepared_cdc_data["Age Average Score"], 0.001) == 48.358
