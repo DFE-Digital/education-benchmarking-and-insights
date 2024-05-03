91c5c481f66097c2e2a0bd22bcd147d5bae75413
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-05-03 08:03:36 +0100

    adds controller, view model and view for local authority resources page (#686)

---
+++ web/src/Web.App/Controllers/LocalAuthorityResourcesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Web.App.Infrastructure.Apis;
+
+namespace Web.App.Controllers;
+
+[Controller]
+[Route("local-authority/{code}/find-ways-to-spend-less")]
+public class LocalAuthorityResourcesController(
+    ILogger<LocalAuthorityResourcesController> logger) : Controller
+{
+    [HttpGet]
+    public IActionResult Index(string code)
+    {
+
+        using (logger.BeginScope(new { code }))
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error displaying local authority resources: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+}
