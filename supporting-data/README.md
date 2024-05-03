39fcf9e93d6e26af5efde4fc3b65add689a8805d
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-05-03 16:08:29 +0100

    feat: Supporting data terraform and pipeline

---
+++ supporting-data/README.md
@@ -0,0 +1,5 @@
+# Outline
+
+This directory contains the IaC to manage the supporting data resources as part of the FIDUS application.
+
+It also contains the source files as managed by Azure Data Factory.
---
+++ supporting-data/terraform/data.tf
@@ -0,0 +1,14 @@
+data "azurerm_key_vault" "key-vault" {
+  name                = "${var.dev-environment-prefix}-ebis-keyvault"
+  resource_group_name = "${var.dev-environment-prefix}-ebis-core"
+}
+
+data "azurerm_key_vault_secret" "platform-cosmos-readwrite-connection-string" {
+  name         = "ebis-cdb-connection-string-rw"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
+
+data "azurerm_key_vault_secret" "platform-sql-connection-string" {
+  name         = "ebis-sql-connection-string"
+  key_vault_id = data.azurerm_key_vault.key-vault.id
+}
