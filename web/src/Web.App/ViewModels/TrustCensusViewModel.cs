21b308b7b065a755a587f89115e6ffa57f679425
J05h-L <josh.langford@hippodigital.co.uk>
2024-05-14 13:42:55 +0100

    adds phase filter for trust benchmarking

--- web/src/Web.App/ViewModels/TrustCensusViewModel.cs
+++ web/src/Web.App/ViewModels/TrustCensusViewModel.cs
@@ -2,8 +2,9 @@
 
 namespace Web.App.ViewModels;
 
-public class TrustCensusViewModel(Trust trust)
+public class TrustCensusViewModel(Trust trust, string[] phases)
 {
     public string? CompanyNumber => trust.CompanyNumber;
     public string? Name => trust.Name;
+    public string[] Phases => phases;
 }
