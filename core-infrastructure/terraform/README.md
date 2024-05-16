8464731695f793646888b6ec49a883e398a17223
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-05-16 19:24:22 +0100

    chore: adds core database and initial scripts

--- core-infrastructure/terraform/README.md
+++ core-infrastructure/terraform/README.md
@@ -11,6 +11,7 @@
 | Name | Version |
 |------|---------|
 | <a name="provider_azurerm"></a> [azurerm](#provider\_azurerm) | 3.98.0 |
+| <a name="provider_random"></a> [random](#provider\_random) | 3.6.1 |
 
 ## Modules
 
@@ -25,13 +26,28 @@ No modules.
 | [azurerm_key_vault.key-vault](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault) | resource |
 | [azurerm_key_vault_access_policy.terraform_sp_access](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_access_policy) | resource |
 | [azurerm_key_vault_secret.data-storage-connection-string](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_secret) | resource |
+| [azurerm_key_vault_secret.sql-connection-string](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_secret) | resource |
 | [azurerm_log_analytics_workspace.application-insights-workspace](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/log_analytics_workspace) | resource |
 | [azurerm_monitor_action_group.action-group](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_action_group) | resource |
 | [azurerm_monitor_smart_detector_alert_rule.failure-anomalies-detector](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_smart_detector_alert_rule) | resource |
+| [azurerm_mssql_database.sql-db](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_database) | resource |
+| [azurerm_mssql_database_extended_auditing_policy.db-audit-policy](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_database_extended_auditing_policy) | resource |
+| [azurerm_mssql_firewall_rule.sql-server-fw-azure-services](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_firewall_rule) | resource |
+| [azurerm_mssql_server.sql-server](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server) | resource |
+| [azurerm_mssql_server_extended_auditing_policy.sql-server-audit-policy](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server_extended_auditing_policy) | resource |
+| [azurerm_mssql_server_security_alert_policy.sql-security-alert-policy](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server_security_alert_policy) | resource |
+| [azurerm_mssql_server_vulnerability_assessment.sql-server-vulnerability](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server_vulnerability_assessment) | resource |
 | [azurerm_network_security_group.network-security-group](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group) | resource |
 | [azurerm_resource_group.resource-group](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group) | resource |
+| [azurerm_role_assignment.sql-audit-storage-role-blob](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_role_assignment.sql-threat-storage-role-blob](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
+| [azurerm_role_assignment.sql-vulnerability-storage-role-blob](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment) | resource |
 | [azurerm_storage_account.data](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account) | resource |
+| [azurerm_storage_account.sql-audit-storage](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account) | resource |
+| [azurerm_storage_account.sql-threat-storage](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account) | resource |
+| [azurerm_storage_account.sql-vulnerability-storage](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account) | resource |
 | [azurerm_storage_container.pipeline-raw-data](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container) | resource |
+| [azurerm_storage_container.sql-vulnerability-container](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container) | resource |
 | [azurerm_storage_queue.pipeline-message-finished-queue](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_queue) | resource |
 | [azurerm_storage_queue.pipeline-message-start-queue](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_queue) | resource |
 | [azurerm_subnet.platform-subnet](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet) | resource |
@@ -39,6 +55,7 @@ No modules.
 | [azurerm_subnet_network_security_group_association.platform-subnet-nsg-association](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet_network_security_group_association) | resource |
 | [azurerm_subnet_network_security_group_association.web-app-subnet-nsg-association](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet_network_security_group_association) | resource |
 | [azurerm_virtual_network.app-service-network](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network) | resource |
+| [random_password.sql-admin-password](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password) | resource |
 | [azurerm_client_config.client](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/client_config) | data source |
 
 ## Inputs
---
+++ core-infrastructure/terraform/databases.tf
@@ -0,0 +1,233 @@
+resource "random_password" "sql-admin-password" {
+  length           = 16
+  special          = true
+  override_special = "!@#$*()-_=+"
+}
+
+locals {
+  sql-admin-login = "sql-admin"
+}
+
+resource "azurerm_key_vault_secret" "sql-connection-string" {
+  #checkov:skip=CKV_AZURE_41:See ADO backlog AB#206511
+  name         = "core-sql-connection-string"
+  value        = "Server=tcp:${azurerm_mssql_server.sql-server.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.sql-db.name};User ID=${local.sql-admin-login};Password=${random_password.sql-admin-password.result};Trusted_Connection=False;Encrypt=True;"
+  key_vault_id = azurerm_key_vault.key-vault.id
+  content_type = "connection-string"
+}
+
+resource "azurerm_role_assignment" "sql-audit-storage-role-blob" {
+  scope                = azurerm_storage_account.sql-audit-storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_mssql_server.sql-server.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "sql-threat-storage-role-blob" {
+  scope                = azurerm_storage_account.sql-threat-storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_mssql_server.sql-server.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_role_assignment" "sql-vulnerability-storage-role-blob" {
+  scope                = azurerm_storage_account.sql-vulnerability-storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_mssql_server.sql-server.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_mssql_server" "sql-server" {
+  #checkov:skip=CKV_AZURE_113:See ADO backlog AB#206493
+  #checkov:skip=CKV2_AZURE_45:See ADO backlog AB#206493
+  name                         = "${var.environment-prefix}-sql"
+  version                      = "12.0"
+  resource_group_name          = azurerm_resource_group.resource-group.name
+  location                     = azurerm_resource_group.resource-group.location
+  administrator_login          = local.sql-admin-login
+  administrator_login_password = random_password.sql-admin-password.result
+  tags                         = local.common-tags
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "michael.fielding@education.gov.uk"
+    object_id      = "42665fd4-ab1f-4192-9033-bfc059c6ea9a"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_mssql_server_extended_auditing_policy" "sql-server-audit-policy" {
+  server_id              = azurerm_mssql_server.sql-server.id
+  storage_endpoint       = azurerm_storage_account.sql-audit-storage.primary_blob_endpoint
+  retention_in_days      = 120
+  log_monitoring_enabled = true
+}
+
+resource "azurerm_mssql_database" "sql-db" {
+  #checkov:skip=CKV_AZURE_224:See ADO backlog AB#206493
+  #checkov:skip=CKV_AZURE_229:See ADO backlog AB#206493
+  name        = "data"
+  server_id   = azurerm_mssql_server.sql-server.id
+  tags        = local.common-tags
+  sku_name    = var.configuration[var.environment].sql-db.sku_name
+  max_size_gb = var.configuration[var.environment].sql-db.max_size_gb
+
+  threat_detection_policy {
+    state            = "Enabled"
+    storage_endpoint = azurerm_storage_account.sql-threat-storage.primary_blob_endpoint
+    retention_days   = 120
+  }
+}
+
+resource "azurerm_mssql_database_extended_auditing_policy" "db-audit-policy" {
+  database_id            = azurerm_mssql_database.sql-db.id
+  storage_endpoint       = azurerm_storage_account.sql-audit-storage.primary_blob_endpoint
+  retention_in_days      = 120
+  log_monitoring_enabled = true
+}
+
+resource "azurerm_mssql_firewall_rule" "sql-server-fw-azure-services" {
+  name             = "allow_azure_services_fw"
+  server_id        = azurerm_mssql_server.sql-server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "sql-security-alert-policy" {
+  #checkov:skip=CKV_AZURE_26:See ADO backlog AB#206493
+  #checkov:skip=CKV_AZURE_27:See ADO backlog AB#206493
+  resource_group_name = azurerm_resource_group.resource-group.name
+  server_name         = azurerm_mssql_server.sql-server.name
+  state               = "Enabled"
+}
+
+resource "azurerm_mssql_server_vulnerability_assessment" "sql-server-vulnerability" {
+  #checkov:skip=CKV2_AZURE_4:See ADO backlog AB#206493
+  #checkov:skip=CKV2_AZURE_5:See ADO backlog AB#206493
+  server_security_alert_policy_id = azurerm_mssql_server_security_alert_policy.sql-security-alert-policy.id
+  storage_container_path          = "${azurerm_storage_account.sql-vulnerability-storage.primary_blob_endpoint}${azurerm_storage_container.sql-vulnerability-container.name}/"
+
+  recurring_scans {
+    enabled = true
+  }
+}
+
+resource "azurerm_storage_account" "sql-audit-storage" {
+  #checkov:skip=CKV_AZURE_43:False positive on storage account adhering to the naming rules
+  #checkov:skip=CKV2_AZURE_33:See ADO backlog AB#206389
+  #checkov:skip=CKV2_AZURE_1:See ADO backlog AB#206389
+  #checkov:skip=CKV_AZURE_59:See ADO backlog AB#206389
+  name                            = "${var.environment-prefix}audit"
+  location                        = azurerm_resource_group.resource-group.location
+  resource_group_name             = azurerm_resource_group.resource-group.name
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  allow_nested_items_to_be_public = false
+  tags                            = local.common-tags
+  min_tls_version                 = "TLS1_2"
+  shared_access_key_enabled       = false
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
+  network_rules {
+    default_action = "Deny"
+    bypass = [
+      "AzureServices"
+    ]
+  }
+}
+
+resource "azurerm_storage_account" "sql-threat-storage" {
+  #checkov:skip=CKV_AZURE_43:False positive on storage account adhering to the naming rules
+  #checkov:skip=CKV2_AZURE_33:See ADO backlog AB#206389
+  #checkov:skip=CKV2_AZURE_1:See ADO backlog AB#206389
+  #checkov:skip=CKV_AZURE_59:See ADO backlog AB#206389
+  name                            = "${var.environment-prefix}threat"
+  location                        = azurerm_resource_group.resource-group.location
+  resource_group_name             = azurerm_resource_group.resource-group.name
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  allow_nested_items_to_be_public = false
+  tags                            = local.common-tags
+  min_tls_version                 = "TLS1_2"
+  shared_access_key_enabled       = false
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
+  network_rules {
+    default_action = "Deny"
+    bypass = [
+      "AzureServices"
+    ]
+  }
+}
+
+resource "azurerm_storage_account" "sql-vulnerability-storage" {
+  #checkov:skip=CKV_AZURE_43:False positive on storage account adhering to the naming rules
+  #checkov:skip=CKV2_AZURE_33:See ADO backlog AB#206389
+  #checkov:skip=CKV2_AZURE_1:See ADO backlog AB#206389
+  #checkov:skip=CKV_AZURE_59:See ADO backlog AB#206389
+  name                            = "${var.environment-prefix}vulnerability"
+  location                        = azurerm_resource_group.resource-group.location
+  resource_group_name             = azurerm_resource_group.resource-group.name
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  allow_nested_items_to_be_public = false
+  tags                            = local.common-tags
+  min_tls_version                 = "TLS1_2"
+  shared_access_key_enabled       = false
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+}
+
+resource "azurerm_storage_container" "sql-vulnerability-container" {
+  #checkov:skip=CKV2_AZURE_21:See ADO backlog AB#206507
+  name                  = "vulnerability-assessment"
+  storage_account_name  = azurerm_storage_account.sql-vulnerability-storage.name
+  container_access_type = "private"
+}
