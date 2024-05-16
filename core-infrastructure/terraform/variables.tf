f4e2b840bd75a8fc1e0c6427738e9549d0d7bb87
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-05-16 19:31:15 +0100

    fix path names

--- core-infrastructure/terraform/variables.tf
+++ core-infrastructure/terraform/variables.tf
@@ -8,44 +8,44 @@ variable "disable-purge-protection" {}
 variable "configuration" {
   type = map(object({
     sql-db = object({
-      sku_name = string
+      sku_name    = string
       max_size_gb = number
     })
   }))
   default = {
     development = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
     automated-test = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
     test = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
     feature = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
     pre-production = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
     production = {
       sql-db = {
-        sku_name = "S0"
+        sku_name    = "S0"
         max_size_gb = 5
       }
     }
