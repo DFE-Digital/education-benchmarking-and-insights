e0c29eb39f809866c90fabf4b0d85c900133690e
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-05-22 22:27:06 +0100

    adds year information to homepages (#783)

--- web/tests/Web.Integration.Tests/Pages/Trusts/WhenViewingHome.cs
+++ web/tests/Web.Integration.Tests/Pages/Trusts/WhenViewingHome.cs
@@ -62,6 +62,11 @@ public class WhenViewingHome(SchoolBenchmarkingWebAppClient client) : PageBase<S
         DocumentAssert.AssertPageUrl(page, Paths.TrustHome(trust.CompanyNumber).ToAbsolute());
         DocumentAssert.Breadcrumbs(page, expectedBreadcrumbs);
 
+        var dataSourceElement = page.QuerySelector("main > div > div:nth-child(2) > div > p");
+        Assert.NotNull(dataSourceElement);
+
+        DocumentAssert.TextEqual(dataSourceElement, "This trust's data covers the financial year September 2021 to August 2022 academies accounts return (AAR).");
+
         Assert.NotNull(trust.Name);
         DocumentAssert.TitleAndH1(page, "Your trust - Financial Benchmarking and Insights Tool - GOV.UK", trust.Name);
     }
