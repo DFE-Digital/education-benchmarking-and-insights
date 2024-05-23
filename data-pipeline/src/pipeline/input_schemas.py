4a0355c451339c857e65cddd805058f000d9bb88
Roger G. Coram <roger.coram@hippodigital.co.uk>
2024-05-23 09:31:48 +0100

    chore: run linting

--- data-pipeline/src/pipeline/input_schemas.py
+++ data-pipeline/src/pipeline/input_schemas.py
@@ -46,13 +46,13 @@ gias = {
     "OfstedRating (name)": "string",
     "MSOA (code)": "string",
     "LSOA (code)": "string",
-    "StatutoryLowAge":"Int64",
-    "StatutoryHighAge":"Int64",
-    "Street":"string",
-    "Locality":"string",
-    "Address3":"string",
-    "Town":"string",
-    "County (name)":"string",
+    "StatutoryLowAge": "Int64",
+    "StatutoryHighAge": "Int64",
+    "Street": "string",
+    "Locality": "string",
+    "Address3": "string",
+    "Town": "string",
+    "County (name)": "string",
 }
 
 gias_links_index_col = "URN"
@@ -263,9 +263,9 @@ pupil_census = {
     "% of pupils known to be eligible for free school meals (Performa": "float",
     "number of pupils whose first language is known or believed to be other than English": "float",
     "full time pupils": "float",
-    "ward_name":"string",
-    "district_administrative_name":"string",
-    "region_name":"string",
+    "ward_name": "string",
+    "district_administrative_name": "string",
+    "region_name": "string",
 }
 
 cfr_index_col = "URN"
@@ -400,7 +400,7 @@ aar_academies = {
     "Costs of Brought in Professional Services": "float",
     "Total Expenditure": "float",
     "Share of Revenue Reserve, distributed on per pupil basis\n": "float",
-    "London Weighting":"string",
+    "London Weighting": "string",
 }
 
 aar_central_services_index_col = "Lead UPIN"
