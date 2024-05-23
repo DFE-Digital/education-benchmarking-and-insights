c8696ed782762aca9b20db6b45f566dc35e31125
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-05-23 14:18:08 +0100

    chore: Integration tests for Trust homepage

--- web/tests/Web.Integration.Tests/Pages/Trusts/WhenViewingHome.cs
+++ web/tests/Web.Integration.Tests/Pages/Trusts/WhenViewingHome.cs
@@ -1,19 +1,50 @@
 ﻿using System.Net;
+using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using AutoFixture;
 using Web.App.Domain;
 using Xunit;
-
 namespace Web.Integration.Tests.Pages.Trusts;
 
 public class WhenViewingHome(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
 {
+    private static readonly Dictionary<int, string> AllCostCategories = new()
+    {
+        {
+            1, "Teaching and teaching support staff"
+        },
+        {
+            2, "Non-educational support staff"
+        },
+        {
+            3, "Educational supplies"
+        },
+        {
+            4, "Educational ICT"
+        },
+        {
+            5, "Premises and services"
+        },
+        {
+            6, "Utilities"
+        },
+        {
+            7, "Administrative supplies"
+        },
+        {
+            8, "Catering staff and services"
+        },
+        {
+            9, "Other"
+        }
+    };
+
     [Fact]
     public async Task CanDisplay()
     {
-        var (page, trust) = await SetupNavigateInitPage();
+        var (page, trust, ratings, schools) = await SetupNavigateInitPage();
 
-        AssertPageLayout(page, trust);
+        AssertPageLayout(page, trust, ratings, schools);
     }
 
     [Fact]
@@ -38,20 +69,37 @@ public class WhenViewingHome(SchoolBenchmarkingWebAppClient client) : PageBase<S
         DocumentAssert.AssertPageUrl(page, Paths.TrustHome(companyName).ToAbsolute(), HttpStatusCode.InternalServerError);
     }
 
-    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage()
+    private async Task<(IHtmlDocument page, Trust trust, RagRating[] ratings, School[] schools)> SetupNavigateInitPage()
     {
+        var random = new Random();
+
         var trust = Fixture.Build<Trust>()
             .Create();
 
+        var schools = Fixture.Build<School>()
+            .With(x => x.OverallPhase, () => OverallPhaseTypes.All.ElementAt(random.Next(0, OverallPhaseTypes.All.Length - 1)))
+            .With(x => x.CompanyNumber, trust.CompanyNumber)
+            .CreateMany(20).ToArray();
 
-        var page = await Client.SetupEstablishment(trust)
-            .SetupInsights()
+        var ratings = Fixture.Build<RagRating>()
+            .OmitAutoProperties()
+            .With(x => x.CostCategoryId, () => AllCostCategories.Keys.ElementAt(random.Next(0, AllCostCategories.Keys.Count - 1)))
+            .With(x => x.Status, () => Lookups.StatusPriorityMap.Keys.ElementAt(random.Next(0, Lookups.StatusPriorityMap.Keys.Count - 1)))
+            .CreateMany(50).ToArray();
+        foreach (var rating in ratings)
+        {
+            rating.CostCategory = AllCostCategories[rating.CostCategoryId];
+            rating.Urn = schools.ElementAt(random.Next(0, schools.Length - 1)).Urn;
+        }
+
+        var page = await Client.SetupEstablishment(trust, schools)
+            .SetupInsights(ratings)
             .Navigate(Paths.TrustHome(trust.CompanyNumber));
 
-        return (page, trust);
+        return (page, trust, ratings, schools);
     }
 
-    private static void AssertPageLayout(IHtmlDocument page, Trust trust)
+    private static void AssertPageLayout(IHtmlDocument page, Trust trust, RagRating[] ratings, School[] schools)
     {
         var expectedBreadcrumbs = new[]
         {
@@ -69,5 +117,41 @@ public class WhenViewingHome(SchoolBenchmarkingWebAppClient client) : PageBase<S
 
         Assert.NotNull(trust.Name);
         DocumentAssert.TitleAndH1(page, "Your trust - Financial Benchmarking and Insights Tool - GOV.UK", trust.Name);
+
+        // headlines
+        var highPriorityHeadline = page.QuerySelector("li.app-headline-high") as IHtmlListItemElement;
+        Assert.Equal($"{ratings.Count(r => r.CostCategory != "Other" && r.Status == "Red")}\n\nHigh priority costs", highPriorityHeadline.GetInnerText());
+
+        var mediumPriorityHeadline = page.QuerySelector("li.app-headline-medium") as IHtmlListItemElement;
+        Assert.Equal($"{ratings.Count(r => r.CostCategory != "Other" && r.Status == "Amber")}\n\nMedium priority costs", mediumPriorityHeadline.GetInnerText());
+
+        var lowPriorityHeadline = page.QuerySelector("li.app-headline-low") as IHtmlListItemElement;
+        Assert.Equal($"{ratings.Count(r => r.CostCategory != "Other" && r.Status == "Green")}\n\nLow priority costs", lowPriorityHeadline.GetInnerText());
+
+        // cost categories
+        var costCategoryRag = page.QuerySelector("table.table-cost-category-rag") as IHtmlTableElement;
+        var costCategoryRagRows = costCategoryRag?.Bodies.First().Rows;
+        Assert.Equal(AllCostCategories.Count - 1, costCategoryRagRows?.Length);
+
+        // school phases
+        var nurserySchoolRag = page.QuerySelector("#school-rag-nursery-schools") as IHtmlTableElement;
+        var nurserySchoolRagRows = nurserySchoolRag?.Bodies.First().Rows;
+        Assert.Equal(schools.Count(s => s.OverallPhase == OverallPhaseTypes.Nursery), nurserySchoolRagRows?.Length ?? 0);
+
+        var primarySchoolRag = page.QuerySelector("#school-rag-primary-schools") as IHtmlTableElement;
+        var primarySchoolRagRows = primarySchoolRag?.Bodies.First().Rows;
+        Assert.Equal(schools.Count(s => s.OverallPhase == OverallPhaseTypes.Primary), primarySchoolRagRows?.Length ?? 0);
+
+        var secondarySchoolRag = page.QuerySelector("#school-rag-secondary-schools") as IHtmlTableElement;
+        var secondarySchoolRagRows = secondarySchoolRag?.Bodies.First().Rows;
+        Assert.Equal(schools.Count(s => s.OverallPhase == OverallPhaseTypes.Secondary), secondarySchoolRagRows?.Length ?? 0);
+
+        var specialSchoolRag = page.QuerySelector("#school-rag-specials-and-pupil-referrals-units-prus") as IHtmlTableElement;
+        var specialSchoolRagRows = specialSchoolRag?.Bodies.First().Rows;
+        Assert.Equal(schools.Count(s => s.OverallPhase is OverallPhaseTypes.Special or OverallPhaseTypes.PupilReferralUnit), specialSchoolRagRows?.Length ?? 0);
+
+        var allThroughSchoolRag = page.QuerySelector("#school-rag-all-through") as IHtmlTableElement;
+        var allThroughSchoolRagRows = allThroughSchoolRag?.Bodies.First().Rows;
+        Assert.Equal(schools.Count(s => s.OverallPhase == OverallPhaseTypes.AllThrough), allThroughSchoolRagRows?.Length ?? 0);
     }
 }
