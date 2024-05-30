6d06664a2415dddf2fa50847272fc00d42545e16
Colin <colinbul@gmail.com>
2024-05-30 08:54:28 +0100

    hotfix: Removing migration script

--- core-infrastructure/src/db/Core.Database/Scripts/003-AddSetTypeToRAGTable.sql
+++
@@ -1,7 +0,0 @@
-IF EXISTS(SELECT *
-          FROM INFORMATION_SCHEMA.COLUMNS
-          WHERE TABLE_NAME = 'School'
-            AND COLUMN_NAME = 'OfstedRating')
-    BEGIN
-        ALTER TABLE MetricRAG ADD SetType nvarchar(50) NOT NULL
-    END
