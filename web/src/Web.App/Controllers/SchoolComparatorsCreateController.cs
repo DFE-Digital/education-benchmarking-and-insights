b7e5cd99ecfd5fcec49039ec573d636a427826c9
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-03 11:14:09 +0100

    feat: User defined comparator sets landing page, with auth-toggled CTA

---
+++ web/src/Web.App/Controllers/SchoolComparatorsCreateController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using Web.App.Domain;
+using Web.App.Infrastructure.Apis;
+using Web.App.Infrastructure.Extensions;
+using Web.App.ViewModels;
+namespace Web.App.Controllers;
+
+[Controller]
+[FeatureGate(FeatureFlags.UserDefinedComparators)]
+[Route("school/{urn}/comparators/create")]
+public class SchoolComparatorsCreateController(ILogger<SchoolComparatorsCreateController> logger, IEstablishmentApi establishmentApi) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index(string urn, string referrer)
+    {
+        using (logger.BeginScope(new
+        {
+            urn,
+            referrer
+        }))
+        {
+            try
+            {
+                ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolComparators(urn);
+
+                var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
+                var viewModel = new SchoolComparatorsViewModel(school);
+                return View(viewModel);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error displaying create school comparators: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+
+    // todo: next up
+    [HttpGet]
+    [Route("by")]
+    public async Task<IActionResult> By(string urn, string referrer)
+    {
+        using (logger.BeginScope(new
+        {
+            urn,
+            referrer
+        }))
+        {
+            try
+            {
+                ViewData[ViewDataKeys.BreadcrumbNode] = BreadcrumbNodes.SchoolComparators(urn);
+
+                var school = await establishmentApi.GetSchool(urn).GetResultOrThrow<School>();
+                var viewModel = new SchoolComparatorsViewModel(school);
+                return View(viewModel);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error displaying create school comparators by: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+}
