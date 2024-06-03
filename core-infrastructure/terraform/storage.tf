18ccc25694cdded07d057289af21edda5f67b7e3
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-06-03 08:12:56 +0100

    hotfix: false positive in checkov

--- core-infrastructure/terraform/storage.tf
+++ core-infrastructure/terraform/storage.tf
@@ -5,6 +5,7 @@ resource "azurerm_storage_account" "data" {
   #checkov:skip=CKV2_AZURE_40:See ADO backlog AB#206389
   #checkov:skip=CKV2_AZURE_41:See ADO backlog AB#206389
   #checkov:skip=CKV_AZURE_59:See ADO backlog AB#206389
+  #checkov:skip=CKV2_AZURE_50:potential false positive https://github.com/bridgecrewio/checkov/issues/6388
   name                            = "${var.environment-prefix}data"
   location                        = azurerm_resource_group.resource-group.location
   resource_group_name             = azurerm_resource_group.resource-group.name
