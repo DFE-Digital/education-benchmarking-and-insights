e0d135d22b7322b0a6184004f43273a4bfc0ad6e
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-05 10:04:18 +0100

    feat: Added `<LaInput>` front end component and wired up to Model validation

---
+++ web/src/Web.App/Attributes/RequiredDependsAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Web.App.Extensions;
+namespace Web.App.Attributes;
+
+public class RequiredDependsAttribute(string otherProperty, string? otherValue = null) : CompareAttribute(otherProperty)
+{
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var otherPropertyValue = validationContext.GetOtherPropertyValueOrThrow<string>(OtherProperty);
+        var propertyValue = value as string;
+
+        // other property is not set or not the otherValue, so ignore this value
+        var otherPropertyValueIsSetOrMatches = string.IsNullOrWhiteSpace(otherValue)
+            ? !string.IsNullOrWhiteSpace(propertyValue)
+            : otherPropertyValue == otherValue;
+        if (!otherPropertyValueIsSetOrMatches)
+        {
+            return null;
+        }
+
+        // other property is set, but so is this one
+        if (!string.IsNullOrWhiteSpace(propertyValue))
+        {
+            return null;
+        }
+
+        var memberNames = validationContext.MemberName != null
+            ? new[]
+            {
+                validationContext.MemberName
+            }
+            : null;
+        return new ValidationResult(FormatErrorMessage(validationContext.DisplayName), memberNames);
+    }
+}
