ca464a4c844d9065c786b7aea3f474736b809916
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-06-05 07:42:53 +0100

    feat: adds user data clean up func app

---
+++ platform/src/apis/Platform.UserDataCleanUp/CleanUpFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Platform.UserDataCleanUp;
+
+public class CleanUpFunctions
+{
+    private readonly ILogger<CleanUpFunctions> _logger;
+    private readonly IPlatformDb _db;
+
+    public CleanUpFunctions(ILogger<CleanUpFunctions> logger, IPlatformDb db)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+    [FunctionName("CleanUpFunction")]
+    public async Task RunAsync([TimerTrigger("0 0 12 * * *")] TimerInfo timer)
+    {
+        using (_logger.BeginScope(new Dictionary<string, object>
+               {
+                   { "Application", Constants.ApplicationName }
+               }))
+        {
+            try
+            {
+                var records = await _db.GetUserDataForDeletion();
+
+                foreach (var record in records)
+                {
+                    _logger.LogInformation($"Removing data : {record.Id} ({record.Type})");
+                    switch (record.Type)
+                    {
+                        case "comparator-set" when record.OrganisationType == "school":
+                            await _db.RemoveSchoolComparatorSet(record.Id);
+                            break;
+                        case "comparator-set" when record.OrganisationType == "trust":
+                            await _db.RemoveTrustComparatorSet(record.Id);
+                            break;
+                        case "custom-data":
+                            await _db.RemoveCustomData(record.Id);
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to clean up user data");
+            }
+        }
+    }
+}
---
+++ platform/src/apis/Platform.UserDataCleanUp/PipelineDb.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Dapper;
+using Platform.Infrastructure.Sql;
+
+namespace Platform.UserDataCleanUp;
+
+public record UserData
+{
+    public string Id { get; set; }
+    public string Type { get; set; }
+    public string OrganisationType { get; set; }
+}
+
+public interface IPlatformDb
+{
+    Task<IEnumerable<UserData>> GetUserDataForDeletion();
+    Task RemoveSchoolComparatorSet(string id);
+    Task RemoveTrustComparatorSet(string id);
+    Task RemoveCustomData(string id);
+}
+
+[ExcludeFromCodeCoverage]
+public class PlatformDb : IPlatformDb
+{
+    private readonly IDatabaseFactory _dbFactory;
+
+    public PlatformDb(IDatabaseFactory dbFactory)
+    {
+        _dbFactory = dbFactory;
+    }
+
+    public async Task<IEnumerable<UserData>> GetUserDataForDeletion()
+    {
+        const string sql = "SELECT * FROM UserData WHERE Status = 'removed' OR Expiry < GETUTCDATE()";
+        using var conn = await _dbFactory.GetConnection();
+        return await conn.QueryAsync<UserData>(sql);
+    }
+
+    public async Task RemoveSchoolComparatorSet(string id)
+    {
+        const string comparatorSetSql = "DELETE FROM UserDefinedSchoolComparatorSet WHERE RunId = @Id";
+        const string metricRAGSql = "DELETE FROM MetricRAG WHERE RunId = @Id AND RunType = 'default'";
+        const string userDataSql = "DELETE FROM UserData WHERE Id = @Id AND Type = 'comparator-set' AND OrganisationType = 'school'";
+
+        var parameters = new { Id = id };
+
+        using var conn = await _dbFactory.GetConnection();
+        using var transaction = conn.BeginTransaction();
+
+        await conn.ExecuteAsync(comparatorSetSql, parameters, transaction);
+        await conn.ExecuteAsync(metricRAGSql, parameters, transaction);
+        await conn.ExecuteAsync(userDataSql, parameters, transaction);
+
+        transaction.Commit();
+
+    }
+
+    public async Task RemoveTrustComparatorSet(string id)
+    {
+        const string comparatorSetSql = "DELETE FROM UserDefinedTrustComparatorSet WHERE RunId = @Id";
+        const string userDataSql = "DELETE FROM UserData WHERE Id = @Id AND Type = 'comparator-set' AND OrganisationType = 'trust'";
+
+        var parameters = new { Id = id };
+
+        using var conn = await _dbFactory.GetConnection();
+        using var transaction = conn.BeginTransaction();
+
+        await conn.ExecuteAsync(comparatorSetSql, parameters, transaction);
+        await conn.ExecuteAsync(userDataSql, parameters, transaction);
+
+        transaction.Commit();
+
+    }
+
+    public async Task RemoveCustomData(string id)
+    {
+        const string comparatorSetSql = "DELETE FROM ComparatorSet WHERE RunId = @Id AND RunType = 'custom'";
+        const string financialSql = "DELETE FROM Financial WHERE RunId = @Id AND RunType = 'custom'";
+        const string metricRAGSql = "DELETE FROM MetricRAG WHERE RunId = @Id AND RunType = 'custom'";
+        const string nonFinancialSql = "DELETE FROM NonFinancial WHERE RunId = @Id AND RunType = 'custom'";
+        const string userDataSql = "DELETE FROM UserData WHERE Id = @Id AND Type = 'custom-data' AND OrganisationType = 'school'";
+
+        var parameters = new { Id = id };
+
+        using var conn = await _dbFactory.GetConnection();
+        using var transaction = conn.BeginTransaction();
+
+        await conn.ExecuteAsync(comparatorSetSql, parameters, transaction);
+        await conn.ExecuteAsync(financialSql, parameters, transaction);
+        await conn.ExecuteAsync(metricRAGSql, parameters, transaction);
+        await conn.ExecuteAsync(nonFinancialSql, parameters, transaction);
+        await conn.ExecuteAsync(userDataSql, parameters, transaction);
+
+        transaction.Commit();
+    }
+}
