106b9218412d08fc60395a2426619cea01bbf74b
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-05 16:21:56 +0100

    feat: Added final additional characteristics to user defined comparators

--- web/src/Web.App/ViewModels/SchoolComparatorsByCharacteristicViewModel.cs
+++ web/src/Web.App/ViewModels/SchoolComparatorsByCharacteristicViewModel.cs
@@ -116,9 +116,71 @@ public record UserDefinedCharacteristicViewModel
     [RequiredDepends(nameof(OfstedRating), "true", ErrorMessage = "Select one or more Ofsted ratings")]
     public string[] OfstedRatings { get; init; } = [];
 
+    // number of pupils
+    public string? SchoolsInTrust { get; init; }
+
+    [Display(Name = "Number of schools within trust from")]
+    [RequiredDepends(nameof(SchoolsInTrust), "true", ErrorMessage = "Enter the number of schools within trust from")]
+    [Range(1, 1_000, ErrorMessage = "Enter number of schools within trust from between 1 and 1,000")]
+    public int? SchoolsInTrustFrom { get; init; }
+
+    [Display(Name = "Number of schools within trust to")]
+    [RequiredDepends(nameof(SchoolsInTrust), "true", ErrorMessage = "Enter the number of schools within trust to")]
+    [Range(1, 1_000, ErrorMessage = "Enter number of schools within trust to between 1 and 1,000")]
+    [CompareIntValue(nameof(SchoolsInTrustFrom), Operator.GreaterThanOrEqualTo)]
+    public int? SchoolsInTrustTo { get; init; }
+
+    // deficit
+    public string? Deficit { get; init; }
+
+    [RequiredDepends(nameof(Deficit), "true", ErrorMessage = "Select whether in deficit")]
+    public string[] Deficits { get; init; } = [];
+
     // pfi
     public string? PrivateFinanceInitiative { get; init; }
 
     [RequiredDepends(nameof(PrivateFinanceInitiative), "true", ErrorMessage = "Select whether part of PFI")]
     public string[] PrivateFinanceInitiatives { get; init; } = [];
+
+    // number of sixth form pupils
+    public string? TotalPupilsSixthForm { get; init; }
+
+    [Display(Name = "Number of sixth form pupils from")]
+    [RequiredDepends(nameof(TotalPupilsSixthForm), "true", ErrorMessage = "Enter the number of sixth form pupils from")]
+    [Range(1, 10_000, ErrorMessage = "Enter number of pupils from between 1 and 10,000")]
+    public int? TotalPupilsSixthFormFrom { get; init; }
+
+    [Display(Name = "Number of sixth form pupils to")]
+    [RequiredDepends(nameof(TotalPupilsSixthForm), "true", ErrorMessage = "Enter the number of sixth form pupils to")]
+    [Range(1, 10_000, ErrorMessage = "Enter number of sixth form pupils to between 1 and 10,000")]
+    [CompareIntValue(nameof(TotalPupilsSixthFormFrom), Operator.GreaterThanOrEqualTo)]
+    public int? TotalPupilsSixthFormTo { get; init; }
+
+    // ks2
+    public string? KeyStage2Progress { get; init; }
+
+    [Display(Name = "Key stage 2 progress from")]
+    [RequiredDepends(nameof(KeyStage2Progress), "true", ErrorMessage = "Enter the key stage 2 progress from")]
+    [Range(0, 1, ErrorMessage = "Enter key stage 2 progress from between 0 and 1")]
+    public decimal? KeyStage2ProgressFrom { get; init; }
+
+    [Display(Name = "Key stage 2 progress to")]
+    [RequiredDepends(nameof(KeyStage2Progress), "true", ErrorMessage = "Enter the key stage 2 progress to")]
+    [Range(0, 1, ErrorMessage = "Enter key stage 2 progress to between 0 and 1")]
+    [CompareDecimalValue(nameof(KeyStage2ProgressFrom), Operator.GreaterThanOrEqualTo)]
+    public decimal? KeyStage2ProgressTo { get; init; }
+
+    // ks4
+    public string? KeyStage4Progress { get; init; }
+
+    [Display(Name = "Key stage 4 progress from")]
+    [RequiredDepends(nameof(KeyStage4Progress), "true", ErrorMessage = "Enter the key stage 4 progress from")]
+    [Range(0, 1, ErrorMessage = "Enter key stage 4 progress from between 0 and 1")]
+    public decimal? KeyStage4ProgressFrom { get; init; }
+
+    [Display(Name = "Key stage 4 progress to")]
+    [RequiredDepends(nameof(KeyStage4Progress), "true", ErrorMessage = "Enter the key stage 4 progress to")]
+    [Range(0, 1, ErrorMessage = "Enter key stage 4 progress to between 0 and 1")]
+    [CompareDecimalValue(nameof(KeyStage4ProgressFrom), Operator.GreaterThanOrEqualTo)]
+    public decimal? KeyStage4ProgressTo { get; init; }
 }
