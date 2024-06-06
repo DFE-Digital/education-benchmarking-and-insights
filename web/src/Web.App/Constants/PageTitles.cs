f274832f6b9cc407f05182f9e14ef3cd4f4eef2c
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-06 08:25:01 +0100

    feat: Comparator set result preview

--- web/src/Web.App/Constants/PageTitles.cs
+++ web/src/Web.App/Constants/PageTitles.cs
@@ -30,6 +30,7 @@ public static class PageTitles
     public const string SchoolComparatorsCreateBy = "How do you want to choose your set of schools?";
     public const string SchoolComparatorsCreateByCharacteristic = "Choose characteristics to find matching schools";
     public const string SchoolComparatorsCreateByName = "Choose schools to benchmark against";
+    public const string SchoolComparatorsPreview = "Schools successfully matched";
     public const string SchoolComparatorsSubmit = "Updating benchmarking data";
     public const string TrustHome = "Your trust";
     public const string HistoricData = "Historic data";
