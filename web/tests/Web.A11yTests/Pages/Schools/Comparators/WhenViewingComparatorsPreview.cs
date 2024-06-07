8eebb22c50cc2c0010325090459df22be417c886
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-07 13:27:26 +0100

    chore: A11y tests for User Defined comparator sets

---
+++ web/tests/Web.A11yTests/Pages/Schools/Comparators/WhenViewingComparatorsPreview.cs
@@ -0,0 +1,30 @@
+﻿using Deque.AxeCore.Commons;
+using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+namespace Web.A11yTests.Pages.Schools.Comparators;
+
+[Trait("Category", "Comparators")]
+public class WhenViewingComparatorsPreview(ITestOutputHelper testOutputHelper, WebDriver webDriver) : AuthPageBase(testOutputHelper, webDriver)
+{
+    private readonly AxeRunContext _context = new()
+    {
+        // known issues
+        Exclude =
+        [
+            // Ensures an element's role supports its ARIA attributes
+            new AxeSelector("#LaSelection-Choose")
+        ]
+    };
+
+    protected override string PageUrl => $"/school/{TestConfiguration.School}/comparators/create/by/characteristic";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await Page.Locator("button[value='continue']").ClickAsync();
+        await Page.WaitForURLAsync("**/preview");
+        await EvaluatePage(_context);
+    }
+}
