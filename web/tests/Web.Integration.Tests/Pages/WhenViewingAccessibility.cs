ae611dc767338953baf36fec2801276285b0d2c2
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-06-11 14:21:55 +0100

    removes static content pages no longer required from service (#929)

--- web/tests/Web.Integration.Tests/Pages/WhenViewingAccessibility.cs
+++ web/tests/Web.Integration.Tests/Pages/WhenViewingAccessibility.cs
@@ -1,16 +1,17 @@
-using Xunit;
+// TODO: review for public beta
+//using Xunit;
 
-namespace Web.Integration.Tests.Pages;
+//namespace Web.Integration.Tests.Pages;
 
-public class WhenViewingAccessibility(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
-{
-    [Fact]
-    public async Task CanDisplay()
-    {
-        var page = await Client.Navigate(Paths.Accessibility);
+//public class WhenViewingAccessibility(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
+//{
+//    [Fact]
+//    public async Task CanDisplay()
+//    {
+//        var page = await Client.Navigate(Paths.Accessibility);
 
-        DocumentAssert.AssertPageUrl(page, Paths.Accessibility.ToAbsolute());
-        DocumentAssert.BackLink(page, "Back", $"{Paths.Accessibility.ToAbsolute()}#");
-        DocumentAssert.TitleAndH1(page, "Financial Benchmarking and Insights Tool - GOV.UK", "Accessibility");
-    }
-}
+//        DocumentAssert.AssertPageUrl(page, Paths.Accessibility.ToAbsolute());
+//        DocumentAssert.BackLink(page, "Back", $"{Paths.Accessibility.ToAbsolute()}#");
+//        DocumentAssert.TitleAndH1(page, "Financial Benchmarking and Insights Tool - GOV.UK", "Accessibility");
+//    }
+//}
