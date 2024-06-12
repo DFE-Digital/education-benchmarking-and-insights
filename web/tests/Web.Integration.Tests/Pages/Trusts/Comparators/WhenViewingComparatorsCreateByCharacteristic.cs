feac65cbad69d4efdd731f9a677be502d48967db
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-12 09:42:37 +0100

    chore: Trust characteristics form and result preview integration tests

---
+++ web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsCreateByCharacteristic.cs
@@ -0,0 +1,82 @@
+﻿using AngleSharp.Html.Dom;
+using AutoFixture;
+using Web.App.Domain;
+using Xunit;
+namespace Web.Integration.Tests.Pages.Trusts.Comparators;
+
+public class WhenViewingComparatorsCreateByCharacteristic(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
+{
+    [Fact]
+    public async Task CanDisplay()
+    {
+        var (page, trust) = await SetupNavigateInitPage();
+        AssertPageLayout(page, trust);
+    }
+
+    [Fact]
+    public async Task CanSubmitCharacteristics()
+    {
+        var (page, trust) = await SetupNavigateInitPage();
+        var action = page.QuerySelector("#submit-characteristic");
+        Assert.NotNull(action);
+
+        page = await Client.SubmitForm(page.Forms[0], action, f =>
+        {
+            f.SetFormValues(new Dictionary<string, string>
+            {
+                {
+                    "TotalPupils", "true"
+                },
+                {
+                    "TotalPupilsFrom", "10000"
+                },
+                {
+                    "TotalPupilsTo", "10000"
+                }
+            });
+        });
+
+        DocumentAssert.AssertPageUrl(page, Paths.TrustComparatorsCreatePreview(trust.CompanyNumber).ToAbsolute());
+    }
+
+    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage()
+    {
+        var trust = Fixture.Build<Trust>()
+            .With(x => x.CompanyNumber, "12345")
+            .Create();
+
+        var page = await Client.SetupEstablishment(trust)
+            .SetupTrustInsightApi(new[]
+            {
+                new TrustCharacteristic
+                {
+                    CompanyNumber = trust.CompanyNumber,
+                    TrustName = trust.TrustName
+                }
+            })
+            .SetupComparatorApi(null, new ComparatorTrusts
+            {
+                Trusts = new[]
+                {
+                    "1",
+                    "2",
+                    "3"
+                },
+                TotalTrusts = 123
+            })
+            .SetupHttpContextAccessor()
+            .Navigate(Paths.TrustComparatorsCreateByCharacteristic(trust.CompanyNumber));
+
+        return (page, trust);
+    }
+
+    private static void AssertPageLayout(IHtmlDocument page, Trust trust)
+    {
+        DocumentAssert.BackLink(page, "Back", Paths.TrustComparatorsCreateBy(trust.CompanyNumber).ToAbsolute());
+        DocumentAssert.TitleAndH1(page,
+            "Choose characteristics to find matching trusts - Financial Benchmarking and Insights Tool - GOV.UK",
+            "Choose characteristics to find matching trusts");
+        var cta = page.QuerySelector("#submit-characteristic");
+        DocumentAssert.PrimaryCta(cta, "Continue", Paths.TrustComparatorsCreateByCharacteristic(trust.CompanyNumber));
+    }
+}
---
+++ web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsCreatePreview.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using AngleSharp.Html.Dom;
+using AutoFixture;
+using Web.App;
+using Web.App.Domain;
+using Web.App.Extensions;
+using Xunit;
+namespace Web.Integration.Tests.Pages.Trusts.Comparators;
+
+public class WhenViewingComparatorsCreatePreview(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
+{
+    [Fact]
+    public async Task CanDisplay()
+    {
+        var (page, trust) = await SetupNavigateInitPage();
+        AssertPageLayout(page, trust);
+    }
+
+    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage()
+    {
+        var trust = Fixture.Build<Trust>()
+            .With(x => x.CompanyNumber, "12345")
+            .Create();
+
+        var key = SessionKeys.TrustComparatorSetUserDefined(trust.CompanyNumber!);
+        var set = new UserDefinedTrustComparatorSet
+        {
+            Set = ["1", "2", "3"],
+            TotalTrusts = 123
+        };
+        var sessionState = new ConcurrentDictionary<string, byte[]>
+        {
+            [key] = Encoding.ASCII.GetBytes(set.ToJson())
+        };
+
+        var page = await Client.SetupEstablishment(trust)
+            .SetupTrustInsightApi(new[]
+            {
+                new TrustCharacteristic
+                {
+                    CompanyNumber = "114504",
+                    TrustName = "Abbey Academies Trust"
+                }
+            })
+            .SetupHttpContextAccessor(sessionState)
+            .Navigate(Paths.TrustComparatorsCreatePreview(trust.CompanyNumber));
+
+        return (page, trust);
+    }
+
+    private static void AssertPageLayout(IHtmlDocument page, Trust trust)
+    {
+        DocumentAssert.BackLink(page, "Back", Paths.TrustComparatorsCreateBy(trust.CompanyNumber).ToAbsolute());
+        DocumentAssert.TitleAndH1(page,
+            "Trusts successfully matched - Financial Benchmarking and Insights Tool - GOV.UK",
+            "Trusts successfully matched");
+        var cta = page.QuerySelector(".govuk-button");
+        DocumentAssert.PrimaryCta(cta, "Create a set using these trusts", Paths.TrustComparatorsCreateSubmit(trust.CompanyNumber));
+        var change = page.QuerySelector("#change-set");
+        DocumentAssert.Link(change, "Change characteristics and create a new set", Paths.TrustComparatorsCreateByCharacteristic(trust.CompanyNumber).ToAbsolute());
+    }
+}
