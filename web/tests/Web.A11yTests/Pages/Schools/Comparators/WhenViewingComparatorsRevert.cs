e828cd41c0e31dd75b31c77c0e6c6174fbc84ba8
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-13 11:28:42 +0100

    chore: Axe tests for user defined Trust comparator sets

---
+++ web/tests/Web.A11yTests/Pages/Schools/Comparators/WhenViewingComparatorsRevert.cs
@@ -0,0 +1,28 @@
+﻿using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+namespace Web.A11yTests.Pages.Schools.Comparators;
+
+[Trait("Category", "Comparators")]
+public class WhenViewingComparatorsRevert(ITestOutputHelper testOutputHelper, WebDriver webDriver) : AuthPageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/school/{TestConfiguration.School}/comparators/create/by/name";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await Page.Locator("#school-input").FillAsync("school");
+        await Page.Locator("#school-input__listbox.autocomplete__menu--visible").WaitForAsync();
+        await Page.Keyboard.DownAsync("ArrowDown");
+        await Page.Keyboard.DownAsync("Enter");
+        await Page.Locator("button[type='submit']").ClickAsync();
+        await Page.Locator("#create-set").WaitForAsync();
+        await Page.Locator("#create-set").ClickAsync();
+        await Page.WaitForURLAsync("**/submit");
+        await Page.Locator("#revert-set").WaitForAsync();
+        await Page.Locator("#revert-set").ClickAsync();
+        await Page.WaitForURLAsync("**/revert");
+        await EvaluatePage();
+    }
+}
---
+++ web/tests/Web.A11yTests/Pages/Trust/Comparators/WhenViewingComparatorsRevert.cs
@@ -0,0 +1,28 @@
+﻿using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+namespace Web.A11yTests.Pages.Trust.Comparators;
+
+[Trait("Category", "Comparators")]
+public class WhenViewingComparatorsRevert(ITestOutputHelper testOutputHelper, WebDriver webDriver) : AuthPageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/trust/{TestConfiguration.Trust}/comparators/create/by/name";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await Page.Locator("#trust-input").FillAsync("trust");
+        await Page.Locator("#trust-input__listbox.autocomplete__menu--visible").WaitForAsync();
+        await Page.Keyboard.DownAsync("ArrowDown");
+        await Page.Keyboard.DownAsync("Enter");
+        await Page.Locator("button[type='submit']").ClickAsync();
+        await Page.Locator("#create-set").WaitForAsync();
+        await Page.Locator("#create-set").ClickAsync();
+        await Page.WaitForURLAsync("**/submit");
+        await Page.Locator("#revert-set").WaitForAsync();
+        await Page.Locator("#revert-set").ClickAsync();
+        await Page.WaitForURLAsync("**/revert");
+        await EvaluatePage();
+    }
+}
