da5d5c389e8ecd61859f0a976d28210821dfda24
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-14 08:19:52 +0100

    chore: OpenIdConnect logging

--- web/src/Web.App/Identity/Models/DfeSignInEvents.cs
+++ web/src/Web.App/Identity/Models/DfeSignInEvents.cs
@@ -2,17 +2,18 @@ using System.Diagnostics.CodeAnalysis;
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-
 namespace Web.App.Identity.Models;
 
 [ExcludeFromCodeCoverage]
 public class DfeSignInEvents
 {
-    public Action<TokenResponse> OnRejectPrincipal { get; set; } = response => { };
+    public Action<TokenResponse> OnRejectPrincipal { get; set; } = response => {};
 
-    public Action<MessageReceivedContext> OnSpuriousAuthenticationRequest { get; set; } = context => { };
+    public Action<MessageReceivedContext> OnSpuriousAuthenticationRequest { get; set; } = context => {};
 
-    public Action<RemoteFailureContext> OnRemoteFailure { get; set; } = ctx => { };
+    public Action<RemoteFailureContext> OnRemoteFailure { get; set; } = ctx => {};
 
-    public Action<TokenValidatedContext> OnValidatedPrincipal { get; set; } = ctx => { };
+    public Action<TokenValidatedContext> OnValidatedPrincipal { get; set; } = ctx => {};
+
+    public Action<TokenValidatedContext, Exception> OnNotValidatedPrincipal { get; set; } = (ctx, ex) => {};
 }
