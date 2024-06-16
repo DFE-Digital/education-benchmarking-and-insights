4b430c3d4b141ea690dccd4250ee5031bb89b674
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-06-16 16:13:00 +0100

    chore: ensure 5 year data history

---
+++ core-infrastructure/src/db/Core.Database/Scripts/038-CreateYearsViews.sql
@@ -0,0 +1,42 @@
+IF EXISTS(SELECT 1
+          FROM sys.views
+          WHERE name = 'AARHistoryYears')
+    BEGIN
+        DROP VIEW AARHistoryYears
+    END
+GO
+
+CREATE VIEW AARHistoryYears
+AS
+SELECT Value 'Year' FROM Parameters WHERE Name = 'LatestAARYear'
+UNION
+SELECT Value-1 'Year' FROM Parameters WHERE Name = 'LatestAARYear'
+UNION
+SELECT Value-2 'Year' FROM Parameters WHERE Name = 'LatestAARYear'
+UNION
+SELECT Value-3 'Year' FROM Parameters WHERE Name = 'LatestAARYear'
+UNION
+SELECT Value-4 'Year' FROM Parameters WHERE Name = 'LatestAARYear'
+GO
+
+
+IF EXISTS(SELECT 1
+          FROM sys.views
+          WHERE name = 'CFRHistoryYears')
+    BEGIN
+        DROP VIEW AARHistoryYears
+    END
+GO
+
+CREATE VIEW AARHistoryYears
+AS
+SELECT Value 'Year' FROM Parameters WHERE Name = 'LatestCFRYear'
+UNION
+SELECT Value-1 'Year' FROM Parameters WHERE Name = 'LatestCFRYear'
+UNION
+SELECT Value-2 'Year' FROM Parameters WHERE Name = 'LatestCFRYear'
+UNION
+SELECT Value-3 'Year' FROM Parameters WHERE Name = 'LatestCFRYear'
+UNION
+SELECT Value-4 'Year' FROM Parameters WHERE Name = 'LatestCFRYear'
+GO
