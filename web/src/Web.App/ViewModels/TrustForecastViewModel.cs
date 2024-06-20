9429fb5d261948a5cd134bf2ab9e3a2af90782e1
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-06-20 15:29:31 +0100

    feat: Added priority guidance panel to Trust forecast and risks page, pending data to set various states

--- web/src/Web.App/ViewModels/TrustForecastViewModel.cs
+++ web/src/Web.App/ViewModels/TrustForecastViewModel.cs
@@ -1,8 +1,36 @@
 ﻿using Web.App.Domain;
 namespace Web.App.ViewModels;
 
-public class TrustForecastViewModel(Trust trust)
+public class TrustForecastViewModel(Trust trust, TrustBalance balance)
 {
     public string? CompanyNumber => trust.CompanyNumber;
     public string? Name => trust.TrustName;
+
+    // red
+    public bool BalancesInDeficit => balance.InYearBalance < 0;
+    public bool BalancesInDeficitOrForecastingDeficit => false;
+    public bool SteepDeclineInBalances => false;
+    public bool SteepDeclineInBalancesAndHighProportionStaffCosts => false;
+    public bool IsRed => BalancesInDeficit
+                         || BalancesInDeficitOrForecastingDeficit
+                         || SteepDeclineInBalancesAndHighProportionStaffCosts
+                         || SteepDeclineInBalances;
+
+    // amber
+    public bool DeclineInBalancesButNoForecastDecline => false;
+    public bool DeclineInBalancesButAboveForecastHistory => false;
+    public bool SteepInclineInBalancesForecastButBelowForecastHistory => false;
+    public bool IsAmber => DeclineInBalancesButNoForecastDecline
+                           || DeclineInBalancesButAboveForecastHistory
+                           || SteepInclineInBalancesForecastButBelowForecastHistory;
+
+    // green
+    public bool BalancesStableAndPositive => false;
+    public bool BalancesIncreasingSteadily => false;
+    public bool BalancesIncreasingSteeply => false;
+    public bool IsGreen => BalancesStableAndPositive
+                           || BalancesIncreasingSteadily
+                           || BalancesIncreasingSteeply;
+
+    public bool HasGuidance => IsRed || IsAmber || IsGreen;
 }
