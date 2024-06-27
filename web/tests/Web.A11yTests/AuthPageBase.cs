0d9ac9a994473451fbb297e8d1095027bf1986dd
Faizi52996 <111439003+Faizi52996@users.noreply.github.com>
2024-06-27 08:55:11 +0100

    removed saving of auth state (#1012)
    Co-authored-by: Faizan Ahmad <faizan.ahmad@education.gov.uk>

--- web/tests/Web.A11yTests/AuthPageBase.cs
+++ web/tests/Web.A11yTests/AuthPageBase.cs
@@ -18,21 +18,6 @@ public abstract partial class AuthPageBase(ITestOutputHelper testOutputHelper, W
         await page.Locator("#organisation").Locator("input[type='radio']").First.ClickAsync();
         await page.GetByText("Continue").ClickAsync();
         await page.WaitForURLAsync($"{ServiceUrl.TrimEnd('/')}/");
-
-        // Save storage state into the file.
-        // Tests are executed in <TestProject>\bin\Debug\netX.0\ therefore relative
-        // path is used to reference playwright/.auth created in project root.
-        const string statePath = "../../../playwright/.auth/state.json";
-        if (!File.Exists(statePath))
-        {
-            await File.Create(statePath).DisposeAsync();
-        }
-
-        await page.Context.StorageStateAsync(new BrowserContextStorageStateOptions
-        {
-            Path = statePath
-        });
-
         await base.GoToPage(statusCode, page);
     }
 
