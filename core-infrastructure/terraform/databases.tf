bd9ec1f055904ebcb0e98ec655f8012511485d03
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-06-29 16:58:58 +0100

    chore: add recommended indexes

--- core-infrastructure/terraform/databases.tf
+++ core-infrastructure/terraform/databases.tf
@@ -207,6 +207,7 @@ resource "azurerm_storage_container" "sql-vulnerability-container" {
 
 # https://learn.microsoft.com/en-us/rest/api/sql/server-automatic-tuning/update?view=rest-sql-2021-11-01
 resource "azapi_resource_action" "sql-server-auto-tuning" {
+  count       = lower(var.cip-environment) == "dev" ? 1 : 0
   resource_id = "${azurerm_mssql_server.sql-server.id}/automaticTuning/current"
   type        = "Microsoft.Sql/servers/automaticTuning@2021-11-01"
   method      = "PATCH"
