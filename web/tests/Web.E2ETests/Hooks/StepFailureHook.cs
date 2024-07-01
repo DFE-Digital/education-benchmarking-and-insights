b40d08049ce374108448b13e91344bf51f246d9d
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-07-01 11:26:37 +0100

    hotfix: use feature context for page driver

--- web/tests/Web.E2ETests/Hooks/StepFailureHook.cs
+++ web/tests/Web.E2ETests/Hooks/StepFailureHook.cs
@@ -5,7 +5,7 @@ using Web.E2ETests.Drivers;
 namespace Web.E2ETests.Hooks;
 
 [Binding]
-public class StepFailureHook(ISpecFlowOutputHelper outputHelper, PageDriver driver, ScenarioContext scenarioContext)
+public class StepFailureHook(ISpecFlowOutputHelper outputHelper, ScenarioContext scenarioContext, FeatureContext featureContext)
 {
     [AfterStep]
     public async Task AfterStep()
@@ -20,7 +20,7 @@ public class StepFailureHook(ISpecFlowOutputHelper outputHelper, PageDriver driv
     {
         var file = $"{Guid.NewGuid()}.png";
         var path = Path.Combine("screenshots", file);
-        var page = await driver.Current;
+        var page = await featureContext.FeatureContainer.Resolve<PageDriver>().Current;
 
         await page.ScreenshotAsync(new PageScreenshotOptions { Path = path });
 
