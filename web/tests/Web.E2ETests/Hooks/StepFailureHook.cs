a8a57fb49b9d8cd3562e4ba7f26db9728fb649e5
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-07-01 09:10:41 +0100

    chore: capture screenshot for test failures

---
+++ web/tests/Web.E2ETests/Hooks/StepFailureHook.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+using TechTalk.SpecFlow.Infrastructure;
+using Web.E2ETests.Drivers;
+
+namespace Web.E2ETests.Hooks;
+
+[Binding]
+public class StepFailureHook(ISpecFlowOutputHelper outputHelper, PageDriver driver, ScenarioContext scenarioContext)
+{
+    [AfterStep]
+    public async Task AfterStep()
+    {
+        if (scenarioContext.TestError != null)
+        {
+            await ScreenShot(scenarioContext.ScenarioInfo.Title);
+        }
+    }
+
+    private async Task ScreenShot(string title)
+    {
+        var file = $"{Guid.NewGuid()}.png" ;
+        var path = Path.Combine("screenshots",file );
+        var page = await driver.Current;
+        
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = path });
+        
+        outputHelper.WriteLine($"Scenario '{title}' failed. Page screenshot: {file}");
+    }
+}
