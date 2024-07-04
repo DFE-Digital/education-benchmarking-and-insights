1e96816a2ea4c2b35d97f357c4d6549b20647da0
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-04 08:17:50 +0100

    feat: Log analytics query pack

--- core-infrastructure/terraform/ai.tf
+++ core-infrastructure/terraform/ai.tf
@@ -36,3 +36,19 @@ resource "azurerm_monitor_smart_detector_alert_rule" "failure-anomalies-detector
     ids = [azurerm_monitor_action_group.action-group.id]
   }
 }
+
+resource "azurerm_log_analytics_query_pack" "query-pack" {
+  name                = "${var.environment-prefix}-ebis-query-pack"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  tags                = local.common-tags
+}
+
+resource "azurerm_log_analytics_query_pack_query" "establishment-requests" {
+  name          = "5fd0997f-94e2-481f-a390-3ebedf324ca1"
+  query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
+  body          = "AppRequests\n| extend Urn = tostring(Properties[\"Urn\"]), CompanyNumber = tostring(Properties[\"CompanyNumber\"]), Code = tostring(Properties[\"Code\"]), Establishment = tostring(Properties[\"Establishment\"]), Feature = tostring(Properties[\"Feature\"])\n| where isnotempty(Establishment)\n| project TimeGenerated, Name, ResultCode, OperationId, UserId, Establishment, Feature, Identifier = iff(Establishment == \"school\", Urn, iff(Establishment == \"trust\", CompanyNumber, iff(Establishment == \"local-authority\", Code, \"\")))"
+  display_name  = "Requests - Establishments"
+  categories    = ["applications"]
+  tags          = local.common-tags
+}
