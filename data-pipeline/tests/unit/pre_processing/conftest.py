2264ed40fdf532d4ceb236941f632f26bf01f18f
Colin <colinbul@gmail.com>
2024-07-04 16:22:36 +0100

    fix: for bug AB#214975 (ID-6)

--- data-pipeline/tests/unit/pre_processing/conftest.py
+++ data-pipeline/tests/unit/pre_processing/conftest.py
@@ -153,6 +153,7 @@ def aar_data() -> pd.DataFrame:
             "LA": [865, 845, 929],
             "Estab": [5411, 4026, 4002],
             "ACADEMYUPIN": [111443, 111451, 111453],
+            "ACADEMYTRUSTSTATUS": ["Existing", "Closed", "Existing"],
             "Company_Number": ["8146633", "8075785", "8146633"],
             "Date joined or opened if in period:": [None, "01/05/2023", None],
             "Date left or closed if in period:": [None, "01/05/2023", None],
