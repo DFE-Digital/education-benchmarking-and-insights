8cbad015a9be54ec5038e3cd9196f0d92d4cd9a7
J05h-L <117924208+J05h-L@users.noreply.github.com>
2024-07-05 13:54:19 +0100

    feat: adds comparator page for workforce only (#1088)

--- web/src/Web.App/ViewModels/Components/ComparatorSetDetailsViewModel.cs
+++ web/src/Web.App/ViewModels/Components/ComparatorSetDetailsViewModel.cs
@@ -1,8 +1,16 @@
 ﻿namespace Web.App.ViewModels.Components;
 
-public class ComparatorSetDetailsViewModel(string identifier, bool hasUserDefinedSet, bool hasCustomData)
+public class ComparatorSetDetailsViewModel(string identifier, bool hasUserDefinedSet, bool hasCustomData, ComparatorSetType type)
 {
     public string Identifier => identifier;
     public bool HasUserDefinedSet => hasUserDefinedSet;
     public bool HasCustomData => hasCustomData;
+    public ComparatorSetType Type => type;
 }
+
+public enum ComparatorSetType
+{
+    Costs,
+    Workforce
+}
+
