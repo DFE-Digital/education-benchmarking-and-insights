441ad22ed2e954e61a8163b08214dd5263b82bf8
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-05 08:42:09 +0100

    feat: Starter MI dashboard

--- core-infrastructure/terraform/queries.tf
+++ core-infrastructure/terraform/queries.tf
@@ -1,160 +1,72 @@
+locals {
+  query-tags = {
+    "Type"   = "MI"
+    "Source" = "terraform"
+  }
+}
+
 resource "azurerm_log_analytics_saved_search" "get-establishment-requests" {
   name                       = "GetEstablishmentRequests"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.application-insights-workspace.id
+  category                   = "Function"
+  display_name               = "GetEstablishmentRequests"
+  function_alias             = "GetEstablishmentRequests"
+  tags                       = local.query-tags
 
-  category       = "Function"
-  display_name   = "GetEstablishmentRequests"
-  function_alias = "GetEstablishmentRequests"
-  query          = <<-EOT
-    AppRequests
-    | extend
-        Urn = tostring(Properties["Urn"]),
-        CompanyNumber = tostring(Properties["CompanyNumber"]),
-        Code = tostring(Properties["Code"]),
-        Establishment = tostring(Properties["Establishment"]),
-        Feature = tostring(Properties["Feature"])
-    | where isnotempty(Establishment)
-    | project
-        TimeGenerated,
-        Name,
-        ResultCode,
-        OperationId,
-        UserId,
-        Establishment,
-        Feature,
-        Identifier = iff(Establishment == "school", Urn, iff(Establishment == "trust", CompanyNumber, iff(Establishment == "local-authority", Code, "")))
-    | order by
-        TimeGenerated desc
-  EOT
-  tags           = local.common-tags
+  query = file("${path.module}/queries/get-establishment-requests.kql")
 }
 
 resource "azurerm_log_analytics_query_pack_query" "popular-school-requests-chart" {
   name          = "5fd0997f-94e2-481f-a390-3ebedf324ca2"
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    GetEstablishmentRequests
-    | where 
-        Establishment == "school" and 
-        ResultCode == 200
-    | project 
-        TimeGenerated, 
-        Feature, 
-        Urn = Identifier
-    | summarize 
-        Count=count() by Urn, 
-        Feature
-    | sort by 
-        Count desc
-    | take(50)
-    | render 
-        columnchart with(title="50 most popular school requests")
-  EOT
   display_name  = "Popular Requests – School"
   description   = "Chart of the most popular School requests, split by feature"
   categories    = ["applications"]
-  tags          = local.common-tags
+  tags          = local.query-tags
+
+  body = file("${path.module}/queries/popular-school-requests-chart.kql")
 }
 
 resource "azurerm_log_analytics_query_pack_query" "popular-trust-requests-chart" {
   name          = "5fd0997f-94e2-481f-a390-3ebedf324ca3"
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    GetEstablishmentRequests
-    | where 
-        Establishment == "trust" and 
-        ResultCode == 200
-    | project 
-        TimeGenerated, 
-        Feature, 
-        CompanyNumber = Identifier
-    | summarize 
-        Count=count() by CompanyNumber, 
-        Feature
-    | sort by 
-        Count desc
-    | take(50)
-    | render 
-        columnchart with(title="50 most popular trust requests")
-  EOT
   display_name  = "Popular Requests – Trust"
   description   = "Chart of the most popular Trust requests, split by feature"
   categories    = ["applications"]
-  tags          = local.common-tags
+  tags          = local.query-tags
+
+  body = file("${path.module}/queries/popular-trust-requests-chart.kql")
 }
 
 resource "azurerm_log_analytics_query_pack_query" "popular-local-authority-requests-chart" {
   name          = "5fd0997f-94e2-481f-a390-3ebedf324ca4"
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    GetEstablishmentRequests
-    | where 
-        Establishment == "local-authority" and 
-        ResultCode == 200
-    | project 
-        TimeGenerated, 
-        Feature, 
-        Code = Identifier
-    | summarize 
-        Count=count() by Code, 
-        Feature
-    | sort by 
-        Count desc
-    | take(50)
-    | render 
-        columnchart with(title="50 most popular local authority requests")
-  EOT
+  tags          = local.query-tags
   display_name  = "Popular Requests – Local Authority"
   description   = "Chart of the most popular Local Authority requests, split by feature"
   categories    = ["applications"]
-  tags          = local.common-tags
+
+  body = file("${path.module}/queries/popular-local-authority-requests-chart.kql")
 }
 
 resource "azurerm_log_analytics_saved_search" "get-tracked-links" {
   name                       = "GetTrackedLinks"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.application-insights-workspace.id
+  category                   = "Function"
+  display_name               = "GetTrackedLinks"
+  function_alias             = "GetTrackedLinks"
+  tags                       = local.query-tags
 
-  category       = "Function"
-  display_name   = "GetTrackedLinks"
-  function_alias = "GetTrackedLinks"
-  query          = <<-EOT
-    AppEvents
-    | where
-        Properties["baseTypeSource"] == "ClickEvent" and
-        Name in (
-            %{for index, trackedEvent in var.trackedEvents~}
-            "${trackedEvent}"
-            %{if index < length(var.trackedEvents) - 1}
-            ,
-            %{endif}
-            %{endfor~}
-        )
-    | extend
-        Source = tostring(Properties["uri"]),
-        Target = tostring(Properties["targetUri"])
-    | join kind=leftouter
-        (
-        AppRequests
-        | extend
-            Urn = tostring(Properties["Urn"]),
-            CompanyNumber = tostring(Properties["CompanyNumber"]),
-            Code = tostring(Properties["Code"]),
-            Establishment = tostring(Properties["Establishment"]),
-            Feature = tostring(Properties["Feature"])
-        | where isnotempty(Establishment))
-        on $left.ParentId == $right.OperationId
-    | project
-        TimeGenerated,
-        Name,
-        Source,
-        Target,
-        OperationId,
-        UserId,
-        Establishment,
-        Feature,
-        Identifier = iff(Establishment == "school", Urn, iff(Establishment == "trust", CompanyNumber, iff(Establishment == "local-authority", Code, "")))
-  EOT
-  tags           = local.common-tags
+  query = templatefile("${path.module}/queries/get-tracked-links.kql", {
+    trackedEvents = <<EOT
+      %{for index, trackedEvent in var.trackedEvents~}
+      "${trackedEvent}"
+      %{if index < length(var.trackedEvents) - 1}
+      ,
+      %{endif}
+      %{endfor~}
+    EOT
+  })
 }
 
 resource "random_uuid" "tracked-links-id" {
@@ -171,95 +83,47 @@ resource "azurerm_log_analytics_query_pack_query" "tracked-links" {
   for_each      = local.trackedEventUuids
   name          = each.value
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    GetTrackedLinks
-    | where
-        Name == "${each.key}"
-    | project
-        TimeGenerated,
-        Source,
-        Target,
-        Establishment,
-        Feature,
-        Identifier
-  EOT
   display_name  = "Tracked Links – ${each.key}"
   description   = "Table of ${each.key} clicks"
   categories    = ["applications"]
-  tags          = local.common-tags
+  tags          = local.query-tags
+
+  body = templatefile("${path.module}/queries/tracked-links.kql", {
+    name = each.key
+  })
 }
 
 resource "azurerm_log_analytics_query_pack_query" "pipeline-runs" {
   name          = "5fd0997f-94e2-481f-a390-3ebedf324ca0"
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    ContainerAppConsoleLogs_CL
-    | where 
-        RevisionName_s startswith "${var.environment-prefix}-ebis-data-pipeline"
-    | where 
-        TimeGenerated between (ago(1d)..now())
-    | project 
-        TimeGenerated, 
-        ContainerId_s, 
-        Log_s
-    | order by 
-        TimeGenerated desc 
-  EOT
   display_name  = "Recent pipeline runs"
   description   = "Logs from the most recent data pipeline runs"
   categories    = ["applications"]
-  tags          = local.common-tags
+  tags          = local.query-tags
+
+  body = templatefile("${path.module}/queries/pipeline-runs.kql", {
+    name = "${var.environment-prefix}-ebis-data-pipeline"
+  })
 }
 
 resource "azurerm_log_analytics_saved_search" "get-feature-requests" {
   name                       = "GetFeatureRequests"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.application-insights-workspace.id
+  category                   = "Function"
+  display_name               = "GetFeatureRequests"
+  function_alias             = "GetFeatureRequests"
+  tags                       = local.query-tags
 
-  category       = "Function"
-  display_name   = "GetFeatureRequests"
-  function_alias = "GetFeatureRequests"
-  query          = <<-EOT
-    AppRequests
-    | extend
-        Establishment = tostring(Properties["Establishment"]),
-        Feature = tostring(Properties["Feature"])
-    | where isnotempty(Feature)
-    | project
-        TimeGenerated,
-        Name,
-        ResultCode,
-        OperationId,
-        UserId,
-        UserAuthenticatedId,
-        Establishment,
-        Feature
-    | order by
-        TimeGenerated desc
-  EOT
-  tags           = local.common-tags
+  query = file("${path.module}/queries/get-feature-requests.kql")
 }
 
 resource "azurerm_log_analytics_query_pack_query" "feature-requests" {
   name          = "5fd0997f-94e2-481f-a390-3ebedf324ca5"
   query_pack_id = azurerm_log_analytics_query_pack.query-pack.id
-  body          = <<-EOT
-    GetFeatureRequests
-    | where 
-        ResultCode == 200
-    | project 
-        TimeGenerated, 
-        Establishment,
-        Feature, 
-        IsAuthenticated = isnotempty(UserAuthenticatedId)
-    | summarize 
-        Count=count() by Establishment, 
-        Feature,
-        IsAuthenticated
-    | sort by 
-        Count desc
-  EOT
   display_name  = "Feature Requests"
   description   = "Table of the most popular Feature requests, split by authenticated state"
   categories    = ["applications"]
-  tags          = local.common-tags
+  tags          = local.query-tags
+
+  body = file("${path.module}/queries/feature-requests.kql")
 }
