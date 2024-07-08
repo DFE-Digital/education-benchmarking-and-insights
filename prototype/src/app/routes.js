1e0f85607cf2c9e5eaae35099fafbd6a7e9c4714
Pete Williams <me@petewilliams.info>
2024-07-08 10:35:41 +0100

    Prototype/Versioning structure setup

--- prototype/src/app/routes.js
+++ prototype/src/app/routes.js
@@ -8,3 +8,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 require('./routes/latest.js');
 require('./routes/v2.js');
+
+router.all( /^(?!((\/latest|\/v\d+))).*$/, (req, res) => {
+    res.redirect('/latest' + req.url );
+})
