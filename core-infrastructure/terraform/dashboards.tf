1a17fb6529522e3a8579e982f775a4de38af6d57
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-10 16:03:54 +0100

    feat: Commercial Resources dashboard part

--- core-infrastructure/terraform/dashboards.tf
+++ core-infrastructure/terraform/dashboards.tf
@@ -5,6 +5,7 @@ locals {
   user-counts-table-query                      = replace(replace(file("${path.module}/queries/user-counts.kql"), "/[\r\n]+/", "\\n"), "\"", "\\\"")
   user-sessions-table-query                    = replace(replace(file("${path.module}/queries/user-sessions.kql"), "/[\r\n]+/", "\\n"), "\"", "\\\"")
   custom-data-funnel-query                     = replace(replace(file("${path.module}/queries/custom-data-funnel.kql"), "/[\r\n]+/", "\\n"), "\"", "\\\"")
+  popular-commercial-resources-query           = replace(replace(file("${path.module}/queries/popular-commercial-resources.kql"), "/[\r\n]+/", "\\n"), "\"", "\\\"")
 }
 
 resource "azurerm_portal_dashboard" "mi-dashboard" {
@@ -19,14 +20,14 @@ resource "azurerm_portal_dashboard" "mi-dashboard" {
 
   dashboard_properties = templatefile("${path.module}/dashboards/mi.tpl",
     {
-      workspace_id    = azurerm_log_analytics_workspace.application-insights-workspace.id,
-      app_insights_id = azurerm_application_insights.application-insights.id,
+      workspace_id    = azurerm_log_analytics_workspace.application-insights-workspace.id
+      app_insights_id = azurerm_application_insights.application-insights.id
 
-      popular_school_requests_chart_id    = azurerm_log_analytics_query_pack_query.popular-school-requests-chart.name,
+      popular_school_requests_chart_id    = azurerm_log_analytics_query_pack_query.popular-school-requests-chart.name
       popular_school_requests_chart_query = local.popular-school-requests-chart-query,
       popular_school_requests_chart_title = azurerm_log_analytics_query_pack_query.popular-school-requests-chart.description
 
-      popular_trust_requests_chart_id    = azurerm_log_analytics_query_pack_query.popular-trust-requests-chart.name,
+      popular_trust_requests_chart_id    = azurerm_log_analytics_query_pack_query.popular-trust-requests-chart.name
       popular_trust_requests_chart_query = local.popular-trust-requests-chart-query,
       popular_trust_requests_chart_title = azurerm_log_analytics_query_pack_query.popular-trust-requests-chart.description
 
@@ -34,17 +35,21 @@ resource "azurerm_portal_dashboard" "mi-dashboard" {
       popular_local_authority_requests_chart_query = local.popular-local-authority-requests-chart-query,
       popular_local_authority_requests_chart_title = azurerm_log_analytics_query_pack_query.popular-local-authority-requests-chart.description
 
-      user_counts_table_id    = azurerm_log_analytics_query_pack_query.user-counts-table.name,
+      user_counts_table_id    = azurerm_log_analytics_query_pack_query.user-counts-table.name
       user_counts_table_query = local.user-counts-table-query,
       user_counts_table_title = azurerm_log_analytics_query_pack_query.user-counts-table.description
 
-      user_sessions_table_id    = azurerm_log_analytics_query_pack_query.user-sessions-table.name,
+      user_sessions_table_id    = azurerm_log_analytics_query_pack_query.user-sessions-table.name
       user_sessions_table_query = local.user-sessions-table-query,
       user_sessions_table_title = azurerm_log_analytics_query_pack_query.user-sessions-table.description
 
-      custom_data_funnel_id    = azurerm_log_analytics_query_pack_query.custom-data-funnel.name,
+      custom_data_funnel_id    = azurerm_log_analytics_query_pack_query.custom-data-funnel.name
       custom_data_funnel_query = local.custom-data-funnel-query,
       custom_data_funnel_title = azurerm_log_analytics_query_pack_query.custom-data-funnel.description
+
+      popular_commercial_resources_id    = azurerm_log_analytics_query_pack_query.popular-commercial-resources.name,
+      popular_commercial_resources_query = local.popular-commercial-resources-query,
+      popular_commercial_resources_title = azurerm_log_analytics_query_pack_query.popular-commercial-resources.description
   })
 }
 
