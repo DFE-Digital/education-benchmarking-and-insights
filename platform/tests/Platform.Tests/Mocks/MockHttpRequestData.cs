9d0d9d7ae11f8ad7facaf1aff80d6f74db8ff696
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-22 09:10:40 +0100

    chore: Updated Benchmark API unit tests

---
+++ platform/tests/Platform.Tests/Mocks/MockHttpRequestData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+namespace Platform.Tests.Mocks;
+
+public abstract class MockHttpRequestData
+{
+    public static HttpRequestData Create(Dictionary<string, StringValues>? query = null) => Create<string>("", query);
+
+    public static HttpRequestData Create<T>(T requestData, Dictionary<string, StringValues>? query = null) where T : class
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddFunctionsWorkerDefaults();
+
+        var serializedData = JsonSerializer.Serialize(requestData);
+        var bodyDataStream = new MemoryStream(Encoding.UTF8.GetBytes(serializedData));
+
+        var context = new Mock<FunctionContext>();
+        context.SetupProperty(functionContext => functionContext.InstanceServices, serviceCollection.BuildServiceProvider());
+
+        var queryCollection = new NameValueCollection();
+        if (query != null)
+        {
+            foreach (var key in query.Keys)
+            {
+                foreach (var value in query[key])
+                {
+                    queryCollection.Add(key, value);
+                }
+            }
+        }
+
+        var request = new Mock<HttpRequestData>(context.Object);
+        request.Setup(r => r.Body).Returns(bodyDataStream);
+        request.Setup(r => r.Headers).Returns([]);
+        request.Setup(r => r.Query).Returns(queryCollection);
+        request.Setup(r => r.CreateResponse()).Returns(new MockHttpResponseData(context.Object));
+
+        return request.Object;
+    }
+}
