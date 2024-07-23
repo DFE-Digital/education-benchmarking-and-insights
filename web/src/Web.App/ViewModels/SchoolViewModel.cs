a1bef1433e8fe362aebd01e28860ec245cdaf286
Josh Langford <josh.langford@hippodigital.co.uk>
2024-07-23 10:15:37 +0100

    fix: filter Category.Other for homepage only

--- web/src/Web.App/ViewModels/SchoolViewModel.cs
+++ web/src/Web.App/ViewModels/SchoolViewModel.cs
@@ -40,7 +40,7 @@ public class SchoolViewModel(School school)
 
         HasMetricRag = ratingsArray.Length != 0;
         Ratings = ratingsArray
-            .Where(x => x.RAG is "red" or "amber" && x.Category is not Category.Other)
+            .Where(x => x.RAG is "red" or "amber")
             .OrderBy(x => Lookups.StatusOrderMap[x.RAG ?? string.Empty])
             .ThenByDescending(x => x.Decile)
             .ThenByDescending(x => x.Value);
