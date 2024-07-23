bed64a959faf77e794036e2244c973e32d390eba
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-23 13:42:06 +0100

    chore: Fixup Establishment API tests

---
+++ platform/tests/Platform.Tests/Extensions/HttpResponseDataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using Microsoft.Azure.Functions.Worker.Http;
+namespace Platform.Tests.Extensions;
+
+public static class HttpResponseDataExtensions
+{
+    public static async Task<T?> ReadAsJsonAsync<T>(this HttpResponseData response)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+        response.Body.Position = 0;
+        return await JsonSerializer.DeserializeAsync<T>(response.Body);
+    }
+}
