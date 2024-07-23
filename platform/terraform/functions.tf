66e1f1092a7a46596e2de944e17ed43c29dd9857
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-07-23 10:47:48 +0100

    chore: Build and deploy Insight API using .NET 8

--- platform/terraform/functions.tf
+++ platform/terraform/functions.tf
@@ -30,25 +30,30 @@ module "benchmark-fa" {
 }
 
 module "insight-fa" {
-  source               = "./modules/functions"
-  function-name        = "insight"
-  common-tags          = local.common-tags
-  environment-prefix   = var.environment-prefix
-  resource-group-name  = azurerm_resource_group.resource-group.name
-  storage-account-name = azurerm_storage_account.platform-storage.name
-  storage-account-id   = azurerm_storage_account.platform-storage.id
-  storage-account-key  = azurerm_storage_account.platform-storage.primary_access_key
-  key-vault-id         = data.azurerm_key_vault.key-vault.id
-  location             = var.location
-  enable-restrictions  = lower(var.cip-environment) != "dev"
-  instrumentation-key  = data.azurerm_application_insights.application-insights.instrumentation_key
+  source                      = "./modules/functions"
+  function-name               = "insight"
+  common-tags                 = local.common-tags
+  environment-prefix          = var.environment-prefix
+  resource-group-name         = azurerm_resource_group.resource-group.name
+  storage-account-name        = azurerm_storage_account.platform-storage.name
+  storage-account-id          = azurerm_storage_account.platform-storage.id
+  storage-account-key         = azurerm_storage_account.platform-storage.primary_access_key
+  key-vault-id                = data.azurerm_key_vault.key-vault.id
+  location                    = var.location
+  enable-restrictions         = lower(var.cip-environment) != "dev"
+  instrumentation-conn-string = data.azurerm_application_insights.application-insights.connection_string
   app-settings = merge(local.default_app_settings, {
-    "Sql__ConnectionString" = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.core-sql-connection-string.versionless_id})"
+    "Sql__ConnectionString"                  = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.core-sql-connection-string.versionless_id})"
+    "WEBSITE_USE_PLACEHOLDER_DOTNETISOLATED" = 1
   })
-  subnet_id           = data.azurerm_subnet.web-app-subnet.id
-  sql-server-fqdn     = data.azurerm_mssql_server.sql-server.fully_qualified_domain_name
-  sql-server-username = data.azurerm_key_vault_secret.sql-user-name.value
-  sql-server-password = data.azurerm_key_vault_secret.sql-password.value
+  subnet_id            = data.azurerm_subnet.web-app-subnet.id
+  sql-server-fqdn      = data.azurerm_mssql_server.sql-server.fully_qualified_domain_name
+  sql-server-username  = data.azurerm_key_vault_secret.sql-user-name.value
+  sql-server-password  = data.azurerm_key_vault_secret.sql-password.value
+  dotnet-version       = "v8.0"
+  use-32-bit-worker    = false
+  use-isolated-runtime = true
+  worker-runtime       = "dotnet-isolated"
 }
 
 module "establishment-fa" {
