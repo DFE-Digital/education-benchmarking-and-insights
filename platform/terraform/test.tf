ca31bd514f0d84cdf69aaa04a450c2e18f72a41e
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-08-06 06:23:29 +0100

    spike: Sample load test during CI

---
+++ platform/terraform/test.tf
@@ -0,0 +1,17 @@
+resource "azurerm_load_test" "platform-load-test" {
+  name                = "${var.environment-prefix}-platform-load-test"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  tags                = local.common-tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "keyvault_load_test_policy" {
+  key_vault_id       = data.azurerm_key_vault.key-vault.id
+  tenant_id          = azurerm_load_test.platform-load-test.identity[0].tenant_id
+  object_id          = azurerm_load_test.platform-load-test.identity[0].principal_id
+  secret_permissions = ["Get"]
+}
