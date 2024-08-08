a69b587ae411329d8dd67d987ca1deb18ff661dd
Josh Langford <josh.langford@hippodigital.co.uk>
2024-08-08 15:20:58 +0100

    fix: changes to school user defined comparator set edit journey. On edit button text amended. On submit title and heading updated.

--- web/src/Web.App/ViewModels/SchoolComparatorsChosenSchoolsViewModel.cs
+++ web/src/Web.App/ViewModels/SchoolComparatorsChosenSchoolsViewModel.cs
@@ -2,8 +2,9 @@ using Web.App.Domain;
 
 namespace Web.App.ViewModels;
 
-public class SchoolComparatorsChosenSchoolsViewModel(string? urn, SchoolCharacteristicUserDefined[]? schoolCharacteristics)
+public class SchoolComparatorsChosenSchoolsViewModel(string? urn, SchoolCharacteristicUserDefined[]? schoolCharacteristics, bool isEdit)
 {
     public string? Urn => urn;
     public IOrderedEnumerable<SchoolCharacteristicUserDefined>? Schools => schoolCharacteristics?.OrderBy(c => c.SchoolName);
+    public bool IsEdit => isEdit;
 }
