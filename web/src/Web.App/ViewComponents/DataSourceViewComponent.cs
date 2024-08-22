de670ce2b875e37b16ebea65290fda32bfb6b276
Josh Langford <josh.langford@hippodigital.co.uk>
2024-08-22 09:08:55 +0100

    fix: data source for benchmark census DataSource component updated to use sourceType, all usage updated

--- web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
+++ web/src/Web.App/ViewComponents/DataSourceViewComponent.cs
@@ -7,27 +7,51 @@ namespace Web.App.ViewComponents;
 public class DataSourceViewComponent(IFinanceService financeService) : ViewComponent
 {
     public async Task<IViewComponentResult> InvokeAsync(
-        string kind,
+        string organisationType,
+        string sourceType,
         bool isPartOfTrust,
         string[]? additionText,
         string wrapperClassName = "govuk-grid-row",
         string className = "govuk-grid-column-full")
     {
-        var years = await financeService.GetYears();
-
-        var dataSource = kind switch
+        var dataSource = sourceType switch
         {
-            OrganisationTypes.School when isPartOfTrust =>
-                $"This school's data covers the financial year September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR).",
-            OrganisationTypes.School when !isPartOfTrust =>
-                $"This school's data covers the financial year April {years.Cfr - 1} to March {years.Cfr} consistent financial reporting return (CFR).",
-            OrganisationTypes.Trust =>
-                $"This trust's data covers the financial year September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR).",
-            OrganisationTypes.LocalAuthority =>
-                $"This data covers the financial year April {years.Cfr - 1} to March {years.Cfr} consistent financial reporting return (CFR).",
-            _ => throw new ArgumentOutOfRangeException(nameof(kind))
+            DataSourceTypes.Spending =>
+                await GetSpendingDataSource(organisationType, isPartOfTrust),
+            DataSourceTypes.Census =>
+            [
+                "Workforce data is taken from the workforce census.",
+                "Pupil data is taken from the school census data set in January."
+            ],
+            _ => throw new ArgumentOutOfRangeException(nameof(sourceType))
         };
 
         return View(new DataSourceViewModel(dataSource, additionText, wrapperClassName, className));
+    }
+
+    private async Task<string[]> GetSpendingDataSource(string organisationType, bool isPartOfTrust)
+    {
+        var years = await financeService.GetYears();
+
+        return organisationType switch
+        {
+            OrganisationTypes.School when isPartOfTrust =>
+            [
+                $"This school's data covers the financial year September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR)."
+            ],
+            OrganisationTypes.School when !isPartOfTrust =>
+            [
+                $"This school's data covers the financial year April {years.Cfr - 1} to March {years.Cfr} consistent financial reporting return (CFR)."
+            ],
+            OrganisationTypes.Trust =>
+            [
+                $"This trust's data covers the financial year September {years.Aar - 1} to August {years.Aar} academies accounts return (AAR)."
+            ],
+            OrganisationTypes.LocalAuthority =>
+            [
+                $"This data covers the financial year April {years.Cfr - 1} to March {years.Cfr} consistent financial reporting return (CFR)."
+            ],
+            _ => throw new ArgumentOutOfRangeException(nameof(organisationType))
+        };
     }
 }
