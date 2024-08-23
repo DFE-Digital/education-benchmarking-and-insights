9d282046d9b8085992b74c6bf30f66c34d8b4158
Chris Lorch <123955539+Chris-Lorch@users.noreply.github.com>
2024-08-23 14:37:52 +0100

    Bug fix/bfr testing errors (#1234)
    * shortened code for testing, and fixed error in metrics calculation
    
    * corrected BFR 2023 vs AAR table, still need to test 2022, 2021 for BFR following change
    
    * run check over historic BFR
    
    * uncommented full process following testing
    
    * added comment to reflect Y2P1 + Y2P2 logic

--- data-pipeline/src/pipeline/bfr.py
+++ data-pipeline/src/pipeline/bfr.py
@@ -3,8 +3,16 @@ import pandas as pd
 
 
 def calculate_metrics(bfr: pd.DataFrame) -> pd.DataFrame:
-    df = bfr[["Company Registration Number", "Category", "Y1P2"]].pivot_table(
-        index=["Company Registration Number"], columns="Category", values="Y1P2"
+    
+    # based on the data, balance values should be the position at the end of Y2P2.
+    # while costs and incomes should be the sum of Y2P1 and Y2P2 since they represent
+    # expenditure or income over those periods.
+    costs_list = ['Total income','Staff costs','Total expenditure', "Self-generated income"]
+    bfr['metric'] = bfr.apply(lambda row: row['Y2P1'] + row['Y2P2'] if row['Category'] in costs_list else row['Y2P2'], axis=1)
+
+
+    df = bfr[["Company Registration Number", "Category", "metric"]].pivot_table(
+        index=["Company Registration Number"], columns="Category", values="metric"
     )
 
     df["Revenue reserve as percentage of income"] = (
