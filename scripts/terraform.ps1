45bda099502593b4fdd35b743a0912349c4ae0fb
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-08-23 10:42:06 +0100

    chore: setup support analytics pipeline

--- scripts/terraform.ps1
+++ scripts/terraform.ps1
@@ -9,6 +9,7 @@ terraform -chdir='../platform/terraform' fmt -recursive
 terraform -chdir='../web/terraform' fmt -recursive
 terraform -chdir='../prototype/terraform' fmt -recursive
 terraform -chdir='../performance-tests/terraform' fmt -recursive
+terraform -chdir='../support-analytics/terraform' fmt -recursive
 
 terraform -chdir='../core-infrastructure/terraform' init -backend=false -upgrade
 terraform -chdir='../core-infrastructure/terraform' validate
@@ -28,9 +29,13 @@ terraform -chdir='../prototype/terraform' validate
 terraform -chdir='../performance-tests/terraform' init -backend=false -upgrade
 terraform -chdir='../performance-tests/terraform' validate
 
+terraform -chdir='../support-analytics/terraform' init -backend=false -upgrade
+terraform -chdir='../support-analytics/terraform' validate
+
 terraform-docs markdown table --output-file README.md ../core-infrastructure/terraform
 terraform-docs markdown table --output-file README.md ../data-pipeline/terraform
 terraform-docs markdown table --recursive --output-file README.md ../platform/terraform
 terraform-docs markdown table --output-file README.md ../web/terraform
 terraform-docs markdown table --output-file README.md ../prototype/terraform
 terraform-docs markdown table --output-file README.md ../performance-tests/terraform
+terraform-docs markdown table --output-file README.md ../support-analytics/terraform
