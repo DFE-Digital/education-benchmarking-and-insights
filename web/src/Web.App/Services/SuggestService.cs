4fe0d01dd09b78974924b4f6043e77f3a023f78c
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-08-27 11:27:14 +0100

    feat: updates suggest service response for schools

--- web/src/Web.App/Services/SuggestService.cs
+++ web/src/Web.App/Services/SuggestService.cs
@@ -26,30 +26,7 @@ public class SuggestService(IEstablishmentApi establishmentApi) : ISuggestServic
         }
 
         var suggestions = await establishmentApi.SuggestSchools(search, query).GetResultOrThrow<SuggestOutput<School>>();
-        return suggestions.Results.Select(value =>
-        {
-            var text = value.Text?.Replace("*", "");
-
-            var additionalDetails = new List<string?>();
-
-            if (!string.IsNullOrWhiteSpace(value.Document?.AddressTown))
-                additionalDetails.Add(text == value.Document.AddressTown ? value.Text : value.Document.AddressTown);
-            if (!string.IsNullOrWhiteSpace(value.Document?.AddressPostcode))
-                additionalDetails.Add(text == value.Document.AddressPostcode ? value.Text : value.Document.AddressPostcode);
-
-            if (text != value.Document?.SchoolName)
-            {
-                value.Text = value.Document?.SchoolName;
-            }
-
-            var additionalText = additionalDetails.Count > 0
-                ? $" ({string.Join(", ", additionalDetails.Select(a => a))})"
-                : "";
-
-            value.Text = $"{value.Text}{additionalText}";
-
-            return value;
-        });
+        return suggestions.Results.Select(SchoolSuggestValue);
     }
 
     public async Task<IEnumerable<SuggestValue<Trust>>> TrustSuggestions(string search, string[]? excludeTrusts = null)
@@ -64,22 +41,24 @@ public class SuggestService(IEstablishmentApi establishmentApi) : ISuggestServic
         }
 
         var suggestions = await establishmentApi.SuggestTrusts(search, query).GetResultOrThrow<SuggestOutput<Trust>>();
-        return suggestions.Results.Select(value =>
-        {
-            var text = value.Text?.Replace("*", "");
+        return suggestions.Results.Select(TrustSuggestValue);
+    }
 
-            var additionalText = "";
+    private static SuggestValue<Trust> TrustSuggestValue(SuggestValue<Trust> value)
+    {
+        var text = value.Text?.Replace("*", "");
 
-            if (!string.IsNullOrWhiteSpace(value.Document?.CompanyNumber))
-                additionalText = text == value.Document.CompanyNumber ? $" ({value.Text})" : $" ({value.Document.CompanyNumber})";
+        var additionalText = "";
 
-            if (text != value.Document?.TrustName)
-                value.Text = value.Document?.TrustName;
+        if (!string.IsNullOrWhiteSpace(value.Document?.CompanyNumber))
+            additionalText = text == value.Document.CompanyNumber ? $" ({value.Text})" : $" ({value.Document.CompanyNumber})";
 
-            value.Text = $"{value.Text}{additionalText}";
+        if (text != value.Document?.TrustName)
+            value.Text = value.Document?.TrustName;
 
-            return value;
-        });
+        value.Text = $"{value.Text}{additionalText}";
+
+        return value;
     }
 
     public async Task<IEnumerable<SuggestValue<LocalAuthority>>> LocalAuthoritySuggestions(string search, string[]? excludeLas = null)
@@ -94,16 +73,84 @@ public class SuggestService(IEstablishmentApi establishmentApi) : ISuggestServic
         }
 
         var suggestions = await establishmentApi.SuggestLocalAuthorities(search, query).GetResultOrThrow<SuggestOutput<LocalAuthority>>();
-        return suggestions.Results.Select(value =>
+        return suggestions.Results.Select(LocalAuthoritySuggestValue);
+    }
+
+    private static SuggestValue<LocalAuthority> LocalAuthoritySuggestValue(SuggestValue<LocalAuthority> value)
+    {
+        var text = value.Text?.Replace("*", "");
+
+        if (text != value.Document?.Name)
         {
-            var text = value.Text?.Replace("*", "");
+            value.Text = value.Document?.Name;
+        }
 
-            if (text != value.Document?.Name)
-            {
-                value.Text = value.Document?.Name;
-            }
+        return value;
+    }
 
-            return value;
-        });
+
+    private static SuggestValue<School> SchoolSuggestValue(SuggestValue<School> value)
+    {
+        var text = value.Text?.Replace("*", "");
+
+        var additionalDetails = SchoolAdditionalDetails(value, text);
+
+        if (text != value.Document?.SchoolName)
+        {
+            value.Text = value.Document?.SchoolName;
+        }
+
+        var additionalText = additionalDetails.Count > 0
+            ? $" ({string.Join(", ", additionalDetails.Select(a => a))})"
+            : "";
+
+        value.Text = $"{value.Text}{additionalText}";
+
+        return value;
+    }
+
+    private static List<string?> SchoolAdditionalDetails(SuggestValue<School> value, string? text)
+    {
+
+        return text == value.Document?.URN
+            ? [value.Text]
+            : SchoolAddressAdditionalDetails(value, text);
+    }
+
+    private static List<string?> SchoolAddressAdditionalDetails(SuggestValue<School> value, string? text)
+    {
+        var additionalDetails = new List<string?>();
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressStreet) && text == value.Document.AddressStreet)
+        {
+            additionalDetails.Add(value.Text);
+        }
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressLocality) && text == value.Document.AddressLocality)
+        {
+            additionalDetails.Add(value.Text);
+        }
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressLine3) && text == value.Document.AddressLine3)
+        {
+            additionalDetails.Add(value.Text);
+        }
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressTown))
+        {
+            additionalDetails.Add(text == value.Document.AddressTown ? value.Text : value.Document.AddressTown);
+        }
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressCounty) && text == value.Document.AddressCounty)
+        {
+            additionalDetails.Add(value.Text);
+        }
+
+        if (!string.IsNullOrWhiteSpace(value.Document?.AddressPostcode))
+        {
+            additionalDetails.Add(text == value.Document.AddressPostcode ? value.Text : value.Document.AddressPostcode);
+        }
+
+        return additionalDetails;
     }
 }
