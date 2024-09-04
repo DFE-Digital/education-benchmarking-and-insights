4a602737a35095f1888701820dea20fccf2dece0
ChrisLorch <chris.lorch@hippodigital.co.uk>
2024-09-04 13:32:37 +0100

    updated order of mappings to prioritise GIAS codes

--- data-pipeline/src/pipeline/mappings.py
+++ data-pipeline/src/pipeline/mappings.py
@@ -15,22 +15,34 @@ def map_ofsted_rating(rating: str):
 
 def map_phase_type(code: int, provision: str):
     if not (pd.isna(code) or pd.isna(provision)):
-        if provision.lower() =='primary' or code == 2 or code == 3:
+        if code == 2 or code == 3:
             return 'Primary'
-        elif provision.lower() == 'nursery' or code == 1:
+        elif code == 1:
             return 'Nursery'
-        elif provision.lower() == 'secondary' or code == 4 or code == 5:
+        elif code == 4 or code == 5:
             return 'Secondary'
-        elif (provision.lower() == 'all through' or provision.lower() == 'all-through' or code == 7) and code != 40:
+        elif code == 7 and code != 40:
             return 'All-through'
-        elif provision.lower() == 'post-16' or provision.lower() == '16 plus' or code == 39 or code == 45 or code == 46 or code == 6:
+        elif code == 39 or code == 45 or code == 46 or code == 6:
             return 'Post-16'
-        elif provision.lower() == "special" or code == 33 or code == 36 or code == 44:
+        elif code == 33 or code == 36 or code == 44:
             return 'Special'
         elif code == 38 or code == 42 or code == 43:
             return 'Alternative Provision'
         elif code == 40:
             return 'University Technical College'
+        elif provision.lower() =='primary':
+            return 'Primary'
+        elif provision.lower() == 'nursery':
+            return 'Nursery'
+        elif provision.lower() == 'secondary':
+            return 'Secondary'
+        elif provision.lower() == 'all through' or provision.lower() == 'all-through':
+            return 'All-through'
+        elif provision.lower() == 'post-16' or provision.lower() == '16 plus':
+            return 'Post-16'
+        elif provision.lower() == "special":
+            return 'Special'
         elif provision.lower() == 'pupil referral unit':
             return 'Pupil Referral Unit'
         else:
