2ceae0a66e0c2f6aec22209645efcf087ad17b86
Ṃarc Woolfson <3224486+WolfyUK@users.noreply.github.com>
2024-09-19 16:40:04 +0100

    feat: Show warning message in School table if part year data present (#1367)

---
+++ front-end-components/src/components/charts/part-year-data-warning/index.tsx
@@ -0,0 +1,3 @@
+/* eslint-disable react-refresh/only-export-components */
+export * from "src/components/charts/part-year-data-warning/types";
+export * from "src/components/charts/part-year-data-warning/component";
