b243b3858f14ceb02633eb6dee273c2e0b10ec46
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2024-09-25 16:16:32 +0100

    chore: adds alert descriptions [skip ci]

--- support-analytics/terraform/smart-alerts.tf
+++ support-analytics/terraform/smart-alerts.tf
@@ -24,6 +24,7 @@ resource "azurerm_monitor_smart_detector_alert_rule" "failure-anomalies-detector
 
 resource "azurerm_monitor_smart_detector_alert_rule" "request-performance-degradation-detector" {
   name                = "request-performance-degradation"
+  description         = "Alerts if responses to requests appear more slowly than they used to"
   resource_group_name = azurerm_resource_group.resource-group.name
   severity            = local.smart_alert.severity
   scope_resource_ids  = [data.azurerm_application_insights.application-insights.id]
@@ -39,6 +40,7 @@ resource "azurerm_monitor_smart_detector_alert_rule" "request-performance-degrad
 
 resource "azurerm_monitor_smart_detector_alert_rule" "dependency-performance-degradation-detector" {
   name                = "dependency-performance-degradation"
+  description         = "Alerts if a dependency call has started responding to requests more slowly than it used to"
   resource_group_name = azurerm_resource_group.resource-group.name
   severity            = local.smart_alert.severity
   scope_resource_ids  = [data.azurerm_application_insights.application-insights.id]
@@ -54,6 +56,7 @@ resource "azurerm_monitor_smart_detector_alert_rule" "dependency-performance-deg
 
 resource "azurerm_monitor_smart_detector_alert_rule" "exception-volume-changed-detector" {
   name                = "exception-volume-changed"
+  description         = "Alerts if exhibiting an abnormal rise in the number of exceptions"
   resource_group_name = azurerm_resource_group.resource-group.name
   severity            = local.smart_alert.severity
   scope_resource_ids  = [data.azurerm_application_insights.application-insights.id]
@@ -69,6 +72,7 @@ resource "azurerm_monitor_smart_detector_alert_rule" "exception-volume-changed-d
 
 resource "azurerm_monitor_smart_detector_alert_rule" "trace-severity-detector" {
   name                = "trace-severity"
+  description         = "Alerts if the percentage of “bad” traces (logged with a level of Warning, Error, or Fatal) out of all traces is degrading"
   resource_group_name = azurerm_resource_group.resource-group.name
   severity            = local.smart_alert.severity
   scope_resource_ids  = [data.azurerm_application_insights.application-insights.id]
@@ -84,6 +88,7 @@ resource "azurerm_monitor_smart_detector_alert_rule" "trace-severity-detector" {
 
 resource "azurerm_monitor_smart_detector_alert_rule" "memory-leak-detector" {
   name                = "memory-leak"
+  description         = "Alerts on a consistent increase in memory consumption over a long period of time"
   resource_group_name = azurerm_resource_group.resource-group.name
   severity            = local.smart_alert.severity
   scope_resource_ids  = [data.azurerm_application_insights.application-insights.id]
