e9a395d171bbc295aae3db5210a9d66446645d62
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-09-27 11:48:11 +0100

    chore: Modified get single `LocalAuthority` API response to include `Schools` collection

--- platform/tests/Platform.Tests/Establishment/LocalAuthoritiesFunctionsTestBase.cs
+++ platform/tests/Platform.Tests/Establishment/LocalAuthoritiesFunctionsTestBase.cs
@@ -2,20 +2,22 @@ using FluentValidation;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Platform.Api.Establishment.LocalAuthorities;
+using Platform.Api.Establishment.Schools;
 using Platform.Search;
-
 namespace Platform.Tests.Establishment;
 
 public class LocalAuthoritiesFunctionsTestBase : FunctionsTestBase
 {
     protected readonly LocalAuthoritiesFunctions Functions;
-    protected readonly Mock<ILocalAuthoritiesService> Service;
+    protected readonly Mock<ILocalAuthoritiesService> LocalAuthoritiesService;
+    protected readonly Mock<ISchoolsService> SchoolsService;
     protected readonly Mock<IValidator<SuggestRequest>> Validator;
 
     protected LocalAuthoritiesFunctionsTestBase()
     {
-        Service = new Mock<ILocalAuthoritiesService>();
+        LocalAuthoritiesService = new Mock<ILocalAuthoritiesService>();
+        SchoolsService = new Mock<ISchoolsService>();
         Validator = new Mock<IValidator<SuggestRequest>>();
-        Functions = new LocalAuthoritiesFunctions(new NullLogger<LocalAuthoritiesFunctions>(), Service.Object, Validator.Object);
+        Functions = new LocalAuthoritiesFunctions(new NullLogger<LocalAuthoritiesFunctions>(), LocalAuthoritiesService.Object, SchoolsService.Object, Validator.Object);
     }
 }
