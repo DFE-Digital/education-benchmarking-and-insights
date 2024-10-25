4add5f282dbabb334870860fc3060b10b511c236
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-10-25 15:38:27 +0100

    feat: Post alerts to Teams channel

---
+++ support-analytics/terraform/action-group.tf
@@ -0,0 +1,18 @@
+resource "azurerm_monitor_action_group" "service-support-action" {
+  name                = "Service support"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  short_name          = "Support"
+  tags                = local.common-tags
+
+  email_receiver {
+    name                    = "send-to-support"
+    email_address           = var.support-alert-email
+    use_common_alert_schema = true
+  }
+
+  webhook_receiver {
+    name                    = "send-to-teams"
+    service_uri             = azurerm_logic_app_trigger_http_request.http-trigger.callback_url
+    use_common_alert_schema = true
+  }
+}
--- support-analytics/terraform/terraform.tfvars
+++ support-analytics/terraform/terraform.tfvars
@@ -3,3 +3,9 @@ environment         = "__environment__"
 location            = "__location__"
 cip-environment     = "__cipEnvironment__"
 support-alert-email = "__support-alert-email__"
+
+# To obtain the Team ID and (must be URL decoded) Channel ID, find the target Channel in Teams  
+# and select 'Get link to channel' from the context menu. From the URL, obtain the IDs as:
+# https://teams.microsoft.com/l/channel/<Channel ID>/FBIT%20Alerts?groupId=<Team ID>&tenantId=<UNUSED>
+teams-team-id    = "__support-alert-teams-team-id__"
+teams-channel-id = "__support-alert-teams-channel-id__"
