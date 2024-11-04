6ab6f768b96edb758c8f74e10d7a80e96183c2f9
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-11-04 14:21:34 +0000

    chore: Bumped `azurerm` Terraform provider and GDS packages in Prototype

--- prototype/terraform/provider.tf
+++ prototype/terraform/provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.9.8"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.3.0"
+      version = "~> 4.8.0"
     }
   }
   backend "azurerm" {}
