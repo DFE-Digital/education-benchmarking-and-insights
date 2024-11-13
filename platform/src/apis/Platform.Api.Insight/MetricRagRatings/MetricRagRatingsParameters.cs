c24e9b35d4876ce6556bff6e096c0145119ce142
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-11-13 11:19:59 +0000

    feat: Modified MetricRagRatings function to abstract validation away from parameters record in order to support more complex rules

--- platform/src/apis/Platform.Api.Insight/MetricRagRatings/MetricRagRatingsParameters.cs
+++ platform/src/apis/Platform.Api.Insight/MetricRagRatings/MetricRagRatingsParameters.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Platform.Functions;
 using Platform.Functions.Extensions;
-
 namespace Platform.Api.Insight.MetricRagRatings;
 
 public record MetricRagRatingsParameters : QueryParameters
 {
-    public string[] Categories { get; private set; } = Array.Empty<string>();
-    public string[] Schools { get; private set; } = Array.Empty<string>();
-    public string[] Statuses { get; private set; } = Array.Empty<string>();
+    public string[] Categories { get; internal set; } = [];
+    public string[] Statuses { get; internal set; } = [];
+    public string[] Urns { get; internal set; } = [];
+    public string? Phase { get; internal set; }
+    public string? CompanyNumber { get; internal set; }
+    public string? LaCode { get; internal set; }
 
     public override void SetValues(IQueryCollection query)
     {
         Statuses = query.ToStringArray("statuses");
         Categories = query.ToStringArray("categories");
-        Schools = query.ToStringArray("urns");
+        Urns = query.ToStringArray("urns");
+        CompanyNumber = query["companyNumber"].ToString();
+        Phase = query["phase"].ToString();
+        LaCode = query["laCode"].ToString();
     }
 }
--- platform/tests/Platform.Tests/Insight/MetricRagRatings/WhenMetricRagRatingsParametersSetsValues.cs
+++ platform/tests/Platform.Tests/Insight/MetricRagRatings/WhenMetricRagRatingsParametersSetsValues.cs
@@ -7,9 +7,21 @@ namespace Platform.Tests.Insight.MetricRagRatings;
 public class WhenMetricRagRatingsParametersSetsValues
 {
     [Theory]
-    [InlineData("1,2,3", "4,5,6", "7,8,9", "1|2|3", "4|5|6", "7|8|9")]
-    [InlineData(null, null, null, "", "", "")]
-    public void ShouldSetValuesFromIQueryCollection(string? statuses, string? categories, string? urns, string expectedStatuses, string expectedCategories, string expectedSchools)
+    [InlineData("1,2,3", "4,5,6", "7,8,9", "companyNumber", "phase", "laCode", "1|2|3", "4|5|6", "7|8|9", "companyNumber", "phase", "laCode")]
+    [InlineData(null, null, null, null, null, null, "", "", "", "", "", "")]
+    public void ShouldSetValuesFromIQueryCollection(
+        string? statuses,
+        string? categories,
+        string? urns,
+        string? companyNumber,
+        string? phase,
+        string? laCode,
+        string expectedStatuses,
+        string expectedCategories,
+        string expectedUrns,
+        string expectedCompanyNumber,
+        string expectedPhase,
+        string expectedLaCode)
     {
         var parameters = new MetricRagRatingsParameters();
         var query = new QueryCollection(new Dictionary<string, StringValues>
@@ -22,6 +34,15 @@ public class WhenMetricRagRatingsParametersSetsValues
             },
             {
                 "urns", urns
+            },
+            {
+                "companyNumber", companyNumber
+            },
+            {
+                "phase", phase
+            },
+            {
+                "laCode", laCode
             }
         });
 
@@ -29,6 +50,9 @@ public class WhenMetricRagRatingsParametersSetsValues
 
         Assert.Equal(expectedStatuses, string.Join("|", parameters.Statuses));
         Assert.Equal(expectedCategories, string.Join("|", parameters.Categories));
-        Assert.Equal(expectedSchools, string.Join("|", parameters.Schools));
+        Assert.Equal(expectedUrns, string.Join("|", parameters.Urns));
+        Assert.Equal(expectedCompanyNumber, parameters.CompanyNumber);
+        Assert.Equal(expectedPhase, parameters.Phase);
+        Assert.Equal(expectedLaCode, parameters.LaCode);
     }
 }
