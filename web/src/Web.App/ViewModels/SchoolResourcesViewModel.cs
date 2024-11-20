b06a25053192d1713992b649db96a335111fe757
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-11-20 10:37:17 +0000

    feat: Remove commercial resources from Find Ways to Spend Less if red/amber RAG is less than average

---
+++ web/src/Web.App/ViewModels/SchoolResourcesViewModel.cs
@@ -0,0 +1,17 @@
+using Web.App.Domain;
+namespace Web.App.ViewModels;
+
+public class SchoolResourcesViewModel(School school, IEnumerable<RagRating> ratings)
+{
+    private readonly CostCategory[] _categories = CategoryBuilder.Build(ratings, Array.Empty<SchoolExpenditure>(), Array.Empty<SchoolExpenditure>()).ToArray();
+
+    public string? Name => school.SchoolName;
+    public string? Urn => school.URN;
+    public bool HasMetricRag => ratings.Any();
+
+    public IEnumerable<CostCategory> CostCategories => _categories
+        .Where(x => x.Rating.RAG is "red" or "amber" && x.Rating.Category is not Category.Other)
+        .OrderBy(x => Lookups.StatusOrderMap[x.Rating.RAG ?? string.Empty])
+        .ThenByDescending(x => x.Rating.Decile)
+        .ThenByDescending(x => x.Rating.Value);
+}
--- web/src/Web.App/ViewModels/SchoolViewModel.cs
+++ web/src/Web.App/ViewModels/SchoolViewModel.cs
@@ -34,21 +34,6 @@ public class SchoolViewModel(School school) : ISchoolKeyInformationViewModel
 
     public SchoolViewModel(
         School school,
-        IEnumerable<RagRating> ratings)
-        : this(school)
-    {
-        var ratingsArray = ratings.ToArray();
-
-        HasMetricRag = ratingsArray.Length != 0;
-        Ratings = ratingsArray
-            .Where(x => x.RAG is "red" or "amber" && x.Category is not Category.Other)
-            .OrderBy(x => Lookups.StatusOrderMap[x.RAG ?? string.Empty])
-            .ThenByDescending(x => x.Decile)
-            .ThenByDescending(x => x.Value);
-    }
-
-    public SchoolViewModel(
-        School school,
         bool? customDataGenerated = false)
         : this(school)
     {
@@ -65,8 +50,6 @@ public class SchoolViewModel(School school) : ISchoolKeyInformationViewModel
 
     public string? Name => school.SchoolName;
     public string? Urn => school.URN;
-    public string? OverallPhase => school.OverallPhase;
-    public string? OfstedRating => school.OfstedDescription;
     public bool IsPartOfTrust => school.IsPartOfTrust;
     public string? Address => school.Address;
     public string? Telephone => school.Telephone;
@@ -99,11 +82,13 @@ public class SchoolViewModel(School school) : ISchoolKeyInformationViewModel
 
     public string? UserDefinedSetId { get; }
     public string? CustomDataId { get; }
-    public decimal? InYearBalance { get; }
-    public decimal? RevenueReserve { get; }
     public bool HasMetricRag { get; }
     public IEnumerable<RagRating> Ratings { get; } = [];
     public bool? ComparatorGenerated { get; }
     public bool? ComparatorReverted { get; }
     public bool? CustomDataGenerated { get; }
+    public string? OverallPhase => school.OverallPhase;
+    public string? OfstedRating => school.OfstedDescription;
+    public decimal? InYearBalance { get; }
+    public decimal? RevenueReserve { get; }
 }
