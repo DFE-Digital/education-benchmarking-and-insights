753a035584e77fc8bb74258c6543a36f06095997
Josh Langford <josh.langford@hippodigital.co.uk>
2024-11-25 10:47:00 +0000

    chore: adds E2E tests for additional dsi claims to la users for their schools and academies for their parent trust

---
+++ web/tests/Web.E2ETests/Pages/Trust/TrustForecastPage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+using Xunit;
+namespace Web.E2ETests.Pages.Trust;
+
+public class TrustForecastPage(IPage page)
+{
+    private ILocator PageH1Heading => page.Locator(Selectors.H1);
+
+    public async Task IsDisplayed()
+    {
+        await PageH1Heading.ShouldBeVisible().ShouldHaveText("Forecast and risks"); ;
+    }
+
+    public async Task IsForbidden()
+    {
+        await PageH1Heading.ShouldBeVisible().ShouldHaveText("Access denied");
+    }
+}
---
+++ web/tests/Web.E2ETests/Steps/LocalAuthority/AccessFinancialPlanningForSchool.cs
@@ -0,0 +1,48 @@
+﻿using Web.E2ETests.Drivers;
+using Web.E2ETests.Pages.School;
+using Xunit;
+using HomePage = Web.E2ETests.Pages.School.HomePage;
+
+namespace Web.E2ETests.Steps.LocalAuthority;
+
+[Binding]
+[Scope(Feature = "Local Authority access to School curriculum and financial planning for its schools.")]
+public class AccessFinancialPlanningForSchools(PageDriver driver)
+{
+    private CurriculumFinancialPlanningPage? _curriculumAndFinancialPlanningPage;
+    private HomePage? _schoolHomePage;
+
+    private static string SchoolHomeUrl(string urn) => $"{TestConfiguration.ServiceUrl}/school/{urn}";
+
+    [Given("I am on school homepage for school with urn '(.*)'")]
+    public async Task GivenIAmOnSchoolHomepageForSchoolWithUrn(string urn)
+    {
+        var url = SchoolHomeUrl(urn);
+        var page = await driver.Current;
+        await page.GotoAndWaitForLoadAsync(url);
+
+        _schoolHomePage = new HomePage(page);
+        await _schoolHomePage.IsDisplayed();
+    }
+
+    [When("I click on curriculum and financial planning")]
+    public async Task WhenIClickOnCurriculumAndFinancialPlanning()
+    {
+        Assert.NotNull(_schoolHomePage);
+        _curriculumAndFinancialPlanningPage = await _schoolHomePage.ClickFinancialPlanning();
+    }
+
+    [Then("the curriculum and financial planning page is displayed")]
+    public async Task ThenTheCurriculumAndFinancialPlanningPageIsDisplayed()
+    {
+        Assert.NotNull(_curriculumAndFinancialPlanningPage);
+        await _curriculumAndFinancialPlanningPage.IsDisplayed();
+    }
+
+    [Then("the forbidden page is displayed")]
+    public async Task ThenTheCurriculumAndFinancialPlanningPageIsForbidden()
+    {
+        Assert.NotNull(_curriculumAndFinancialPlanningPage);
+        await _curriculumAndFinancialPlanningPage.IsForbidden();
+    }
+}
