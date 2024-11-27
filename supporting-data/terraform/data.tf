19433fb822444830e15fcc2e97bbf0eecf655c48
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-11-27 11:27:24 +0000

    chore: Delete `supporting-data` files

--- supporting-data/terraform/data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_key_vault" "key-vault" {
-  name                = "${var.dev-environment-prefix}-ebis-keyvault"
-  resource_group_name = "${var.dev-environment-prefix}-ebis-core"
-}
-
-data "azurerm_key_vault_secret" "platform-sql-connection-string" {
-  name         = "core-sql-connection-string"
-  key_vault_id = data.azurerm_key_vault.key-vault.id
-}
