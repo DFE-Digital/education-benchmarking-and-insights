0dd50bd8a5d4518a4347514c2e06964acf78f0c3
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-12-02 15:26:41 +0000

    chore: `core-infrastructure` dependency updates

--- core-infrastructure/terraform/provider.tf
+++ core-infrastructure/terraform/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.8.0"
+      version = "~> 4.12.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 2.0.1"
+      version = "~> 2.1.0"
     }
     mssql = {
       source  = "betr-io/mssql"
