fad46c8adca56a32ce947406e7ed76563ea4c57b
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-12-04 14:16:07 +0000

    feat: Added additional series to historic comparison data table

--- front-end-components/src/composed/historic-chart-2-composed/component.tsx
+++ front-end-components/src/composed/historic-chart-2-composed/component.tsx
@@ -9,7 +9,7 @@ import {
   shortValueFormatter,
   fullValueFormatter,
 } from "src/components/charts/utils.ts";
-import { ChartProps, ValueFormatterValue } from "src/components/charts/types";
+import { ChartProps } from "src/components/charts/types";
 import { ChartDimensionContext, useChartModeContext } from "src/contexts";
 import { SchoolHistoryBase } from "src/services";
 import { HistoricDataTooltip } from "src/components/charts/historic-data-tooltip";
@@ -47,13 +47,22 @@ export function HistoricChart2<TData extends SchoolHistoryBase>({
       result.push({
         year,
         term: s.term,
-        school: !schoolValue || isNaN(schoolValue) ? undefined : schoolValue,
+        school:
+          schoolValue === undefined ||
+          schoolValue === null ||
+          isNaN(schoolValue)
+            ? undefined
+            : schoolValue,
         comparatorSetAverage:
-          !comparatorSetAverageValue || isNaN(comparatorSetAverageValue)
+          comparatorSetAverageValue === undefined ||
+          comparatorSetAverageValue === null ||
+          isNaN(comparatorSetAverageValue)
             ? undefined
             : comparatorSetAverageValue,
         nationalAverage:
-          !nationalAverageValue || isNaN(nationalAverageValue)
+          nationalAverageValue === undefined ||
+          nationalAverageValue === null ||
+          isNaN(nationalAverageValue)
             ? undefined
             : nationalAverageValue,
       });
@@ -120,28 +129,77 @@ export function HistoricChart2<TData extends SchoolHistoryBase>({
             <table className="govuk-table">
               <thead className="govuk-table__head">
                 <tr className="govuk-table__row">
-                  <th className="govuk-table__header govuk-!-width-one-half">
-                    Year
-                  </th>
+                  <th className="govuk-table__header">Year</th>
                   <th className="govuk-table__header">
                     {columnHeading ?? dimension.heading}
+                  </th>
+                  <th className="govuk-table__header">
+                    Average across comparator set
+                  </th>
+                  <th className="govuk-table__header">
+                    National average across phase type
                   </th>
                 </tr>
               </thead>
               <tbody className="govuk-table__body">
-                {data.school?.map((item) => (
-                  <tr className="govuk-table__row" key={item.year}>
-                    <td className="govuk-table__cell">{String(item.term)}</td>
-                    <td className="govuk-table__cell">
-                      {fullValueFormatter(
-                        item[valueField] as ValueFormatterValue,
-                        {
-                          valueUnit: valueUnit ?? dimension.unit,
-                        }
-                      )}
-                    </td>
-                  </tr>
-                ))}
+                {data.school?.map((item) => {
+                  const year = item.year;
+                  const schoolValue = item[valueField] as number;
+                  const comparatorSetAverage = data.comparatorSetAverage?.find(
+                    (c) => c.year == year
+                  );
+                  const comparatorSetAverageValue = comparatorSetAverage
+                    ? (comparatorSetAverage[valueField] as number)
+                    : undefined;
+                  const nationalAverage = data.nationalAverage?.find(
+                    (c) => c.year == year
+                  );
+                  const nationalAverageValue = nationalAverage
+                    ? (nationalAverage[valueField] as number)
+                    : undefined;
+
+                  return (
+                    <tr className="govuk-table__row" key={year}>
+                      <td className="govuk-table__cell">{String(item.term)}</td>
+                      <td className="govuk-table__cell">
+                        {fullValueFormatter(
+                          schoolValue === undefined ||
+                            schoolValue === null ||
+                            isNaN(schoolValue)
+                            ? undefined
+                            : schoolValue,
+                          {
+                            valueUnit: valueUnit ?? dimension.unit,
+                          }
+                        )}
+                      </td>
+                      <td className="govuk-table__cell">
+                        {fullValueFormatter(
+                          comparatorSetAverageValue === undefined ||
+                            comparatorSetAverageValue === null ||
+                            isNaN(comparatorSetAverageValue)
+                            ? undefined
+                            : comparatorSetAverageValue,
+                          {
+                            valueUnit: valueUnit ?? dimension.unit,
+                          }
+                        )}
+                      </td>
+                      <td className="govuk-table__cell">
+                        {fullValueFormatter(
+                          nationalAverageValue === undefined ||
+                            nationalAverageValue === null ||
+                            isNaN(nationalAverageValue)
+                            ? undefined
+                            : nationalAverageValue,
+                          {
+                            valueUnit: valueUnit ?? dimension.unit,
+                          }
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
