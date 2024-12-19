125f212913684bf4d367f107373231a28035caa4
Marc Woolfson <marc.woolfson@education.gov.uk>
2024-12-19 10:19:30 +0000

    feat: Moved Redis to its own project

---
+++ platform/src/abstractions/Platform.Cache/Configuration/Services.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace Platform.Cache.Configuration;
+
+public static class Services
+{
+    public static void AddRedis(this IServiceCollection serviceCollection)
+    {
+        var cacheHost = Environment.GetEnvironmentVariable("Cache__Host");
+        var cachePort = Environment.GetEnvironmentVariable("Cache__Port");
+        var cachePassword = Environment.GetEnvironmentVariable("Cache__Password");
+        ArgumentNullException.ThrowIfNull(cacheHost);
+        ArgumentNullException.ThrowIfNull(cachePort);
+
+        serviceCollection.AddOptions<RedisCacheOptions>().Configure(x =>
+        {
+            x.Host = cacheHost;
+            x.Port = cachePort;
+            x.Password = cachePassword;
+        });
+
+        serviceCollection
+            .AddSingleton<IRedisConnectionMultiplexerFactory, RedisConnectionMultiplexerFactory>()
+            .AddSingleton<IDistributedCache, RedisDistributedCache>();
+    }
+
+    public static void AddRedis(this IHealthChecksBuilder healthChecks)
+    {
+        healthChecks.AddRedis(p =>
+        {
+            var cache = p.GetService<IDistributedCache>();
+            return cache!.Connection.Value.GetAwaiter().GetResult();
+        });
+    }
+}
---
+++ platform/src/abstractions/Platform.Cache/RedisConnectionMultiplexerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Platform.Cache.Configuration;
+using StackExchange.Redis;
+namespace Platform.Cache;
+
+[ExcludeFromCodeCoverage]
+public class RedisConnectionMultiplexerFactory(ILogger<RedisConnectionMultiplexerFactory> logger, IOptions<RedisCacheOptions> options) : IRedisConnectionMultiplexerFactory
+{
+    public async Task<IConnectionMultiplexer> CreateAsync()
+    {
+        var configurationOptions = ConfigurationOptions.Parse(options.Value.ConnectionString);
+        if (string.IsNullOrWhiteSpace(options.Value.Password))
+        {
+            logger.LogDebug("Password is empty - configuring Redis with system assigned managed identity");
+            await configurationOptions.ConfigureForAzureWithSystemAssignedManagedIdentityAsync();
+        }
+
+        logger.LogInformation("Establishing connection to Redis cache at {EndPoint}", configurationOptions.EndPoints.First());
+        return await ConnectionMultiplexer.ConnectAsync(configurationOptions);
+    }
+}
+
+public interface IRedisConnectionMultiplexerFactory
+{
+    Task<IConnectionMultiplexer> CreateAsync();
+}
