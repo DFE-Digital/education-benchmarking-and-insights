6c9c7f0fe7380460989ac666bf081bdeaaa19aa5
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-01-13 06:19:46 +0000

    feat: Added Redis delete-by-key-pattern implementation

--- platform/src/abstractions/tests/Platform.Cache.Tests/Cache/RedisDistributedCacheTestBase.cs
+++ platform/src/abstractions/tests/Platform.Cache.Tests/Cache/RedisDistributedCacheTestBase.cs
@@ -7,25 +7,27 @@ namespace Platform.Cache.Tests.Cache;
 
 public abstract class RedisDistributedCacheTestBase
 {
+
+    private static readonly RedisCacheOptions CacheOptions = new()
+    {
+        Host = "host",
+        Port = "port"
+    };
     private readonly Mock<IConnectionMultiplexer> _connectionMultiplexer = new();
     private readonly Mock<IRedisConnectionMultiplexerFactory> _factory = new();
     protected readonly RedisDistributedCache Cache;
     protected readonly Mock<IDatabase> Database = new();
     protected readonly Mock<IServer> Server = new();
 
-    protected RedisDistributedCacheTestBase(ITestOutputHelper testOutputHelper, bool allowAdmin = false)
+    protected RedisDistributedCacheTestBase(ITestOutputHelper testOutputHelper, RedisCacheOptions? options = null)
     {
         _connectionMultiplexer.Setup(c => c.GetDatabase(It.IsAny<int>(), It.IsAny<object?>())).Returns(Database.Object);
         _factory.Setup(f => f.CreateAsync()).ReturnsAsync(_connectionMultiplexer.Object);
         var logger = MockLogger.Create<RedisDistributedCache>(testOutputHelper);
 
-        var options = new RedisCacheOptions
-        {
-            Host = "host",
-            AllowAdmin = allowAdmin
-        };
+        options ??= CacheOptions;
         _factory.SetupGet(f => f.Options).Returns(options);
-        _connectionMultiplexer.Setup(c => c.GetServer(options.Host, It.IsAny<object?>())).Returns(Server.Object);
+        _connectionMultiplexer.Setup(c => c.GetServer(options.Server, It.IsAny<object?>())).Returns(Server.Object);
 
         Cache = new RedisDistributedCache(logger.Object, _factory.Object);
     }
--- platform/src/abstractions/tests/Platform.Cache.Tests/Cache/WhenRedisDistributedCacheFlushes.cs
+++ platform/src/abstractions/tests/Platform.Cache.Tests/Cache/WhenRedisDistributedCacheFlushes.cs
@@ -1,11 +1,19 @@
 ﻿using Moq;
+using Platform.Cache.Configuration;
 using StackExchange.Redis;
 using Xunit;
 using Xunit.Abstractions;
 namespace Platform.Cache.Tests.Cache;
 
-public class WhenRedisDistributedCacheFlushesWithAdminMode(ITestOutputHelper testOutputHelper) : RedisDistributedCacheTestBase(testOutputHelper, true)
+public class WhenRedisDistributedCacheFlushesWithAdminMode(ITestOutputHelper testOutputHelper) : RedisDistributedCacheTestBase(testOutputHelper, CacheOptions)
 {
+
+    private static readonly RedisCacheOptions CacheOptions = new()
+    {
+        Host = "host",
+        Port = "port",
+        AllowAdmin = true
+    };
     [Fact]
     public async Task ShouldPerformFlush()
     {
@@ -29,6 +37,28 @@ public class WhenRedisDistributedCacheFlushesWithoutAdminMode(ITestOutputHelper 
             .Verifiable(Times.Never);
 
         var exception = await Assert.ThrowsAsync<UnauthorizedAccessException>(() => Cache.FlushAsync());
+
+        Server.Verify();
+        Assert.NotNull(exception);
+    }
+}
+
+public class WhenRedisDistributedCacheFlushesWithoutHostNameInConfig(ITestOutputHelper testOutputHelper) : RedisDistributedCacheTestBase(testOutputHelper, CacheOptions)
+{
+
+    private static readonly RedisCacheOptions CacheOptions = new()
+    {
+        Port = "port",
+        AllowAdmin = true
+    };
+    [Fact]
+    public async Task ShouldNotPerformFlush()
+    {
+        Server
+            .Setup(d => d.FlushDatabaseAsync(-1, CommandFlags.None))
+            .Verifiable(Times.Never);
+
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => Cache.FlushAsync());
 
         Server.Verify();
         Assert.NotNull(exception);
