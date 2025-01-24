fe19bdc6a4c4c01c730cefc0d58cd58509d79d2f
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-01-24 12:12:07 +0000

    feat: Added scheduled query alert for excessive failed pipeline finished messages

--- support-analytics/terraform/alerts.tf
+++ support-analytics/terraform/alerts.tf
@@ -1,3 +1,7 @@
+locals {
+  pipeline-messages-finished-query = file("${path.module}/queries/pipeline-messages-finished.kql")
+}
+
 resource "azurerm_monitor_metric_alert" "availability-alert" {
   name                = "availability-alert"
   resource_group_name = azurerm_resource_group.resource-group.name
@@ -163,5 +167,43 @@ resource "azurerm_monitor_metric_alert" "insight_api_error_alert" {
 
   action {
     action_group_id = azurerm_monitor_action_group.service-support-action.id
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "failed-finished-pipeline-messages" {
+  name                    = "failed-finished-pipeline-messages-alert"
+  resource_group_name     = azurerm_resource_group.resource-group.name
+  scopes                  = [data.azurerm_application_insights.application-insights.id]
+  location                = azurerm_resource_group.resource-group.location
+  display_name            = "Failed finished pipeline messages alert"
+  description             = "Alert if number of failed finished pipeline messages exceeds ${var.configuration[var.environment].thresholds.error}"
+  severity                = 2
+  evaluation_frequency    = "PT10M"
+  window_duration         = "PT30M"
+  auto_mitigation_enabled = true
+  enabled                 = var.configuration[var.environment].alerts_enabled
+  tags                    = local.common-tags
+
+  criteria {
+    query                   = local.pipeline-messages-finished-query
+    time_aggregation_method = "Total"
+    metric_measure_column   = "Events"
+    operator                = "GreaterThan"
+    threshold               = var.configuration[var.environment].thresholds.error
+
+    dimension {
+      name     = "dimension"
+      operator = "Include"
+      values   = ["False", "True"]
+    }
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  action {
+    action_groups = [azurerm_monitor_action_group.service-support-action.id]
   }
 }
