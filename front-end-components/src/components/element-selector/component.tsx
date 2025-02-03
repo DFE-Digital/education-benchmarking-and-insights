8986bb10d0833f1436349ecad6737bab31254e40
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-03 10:38:30 +0000

    feat: Validation on minimum number of selected items

--- front-end-components/src/components/element-selector/component.tsx
+++ front-end-components/src/components/element-selector/component.tsx
@@ -1,11 +1,13 @@
 import { ElementAndTitle } from "src/hooks/useDownloadImage";
 import { ElementSelectorProps } from "./types";
 import "./styles.scss";
+import classNames from "classnames";
 
 export function ElementSelector({
   elements,
   onChange,
   selected,
+  showValidationError,
 }: ElementSelectorProps) {
   const handleChange = (element: ElementAndTitle, checked: boolean) => {
     const selectedElements = [...selected.filter((e) => e !== element)];
@@ -17,30 +19,57 @@ export function ElementSelector({
   };
 
   return (
-    <div
-      className="govuk-checkboxes govuk-checkboxes--elements"
-      data-module="govuk-checkboxes"
-    >
-      {elements.map((element, i) => {
-        const id = `elements-${i}`;
-
-        return (
-          <div key={i} className="govuk-checkboxes__item">
-            <input
-              checked={selected.includes(element)}
-              className="govuk-checkboxes__input"
-              id={id}
-              name={id}
-              onChange={(e) => handleChange(element, e.target.checked)}
-              type="checkbox"
-              value={id}
-            />
-            <label className="govuk-label govuk-checkboxes__label" htmlFor={id}>
-              {element.title || `Item ${i + 1}`}
-            </label>
+    <>
+      {showValidationError && (
+        <div className="govuk-error-summary" data-module="govuk-error-summary">
+          <div role="alert">
+            <h2 className="govuk-error-summary__title">There is a problem</h2>
+            <div className="govuk-error-summary__body">
+              <ul className="govuk-list govuk-error-summary__list">
+                <li>
+                  <a href="#elements-0">Select one or more items</a>
+                </li>
+              </ul>
+            </div>
           </div>
-        );
-      })}
-    </div>
+        </div>
+      )}
+      <div
+        className={classNames("govuk-form-group", {
+          "govuk-form-group--error": showValidationError,
+        })}
+      >
+        <fieldset className="govuk-fieldset">
+          <div
+            className="govuk-checkboxes govuk-checkboxes--elements"
+            data-module="govuk-checkboxes"
+          >
+            {elements.map((element, i) => {
+              const id = `elements-${i}`;
+
+              return (
+                <div key={i} className="govuk-checkboxes__item">
+                  <input
+                    checked={selected.includes(element)}
+                    className="govuk-checkboxes__input"
+                    id={id}
+                    name={id}
+                    onChange={(e) => handleChange(element, e.target.checked)}
+                    type="checkbox"
+                    value={id}
+                  />
+                  <label
+                    className="govuk-label govuk-checkboxes__label"
+                    htmlFor={id}
+                  >
+                    {element.title || `Item ${i + 1}`}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+        </fieldset>
+      </div>
+    </>
   );
 }
