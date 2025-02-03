84dab93dd6759792842135a3020546fbfcba4bec
Roger G. Coram <roger.coram@hippodigital.co.uk>
2025-02-03 12:22:38 +0000

    feat: make temp. table names unique
    - derive a unique temp. table name from the target-table name, the
      `RunId` value and the current epoch time
    - amend the `to_sql` parameter `if_exists` to `fail` if the temp. table
      already exists: this cannot reasonably occur, given the above

---
+++ data-pipeline/tests/unit/database.py
@@ -0,0 +1,31 @@
+import uuid
+
+from pipeline import database
+
+
+def test_unique_temp_table_names():
+    random_run_id = str(uuid.uuid4())
+    table_names = [
+        "ComparatorSet",
+        "MetricRAG",
+        "School",
+        "LocalAuthority",
+        "Trust",
+        "NonFinancial",
+        "Financial",
+        "TrustFinancial",
+        "BudgetForecastReturnMetric",
+        "BudgetForecastReturn",
+    ]
+
+    temp_tables = [
+        temp_table
+        for table in table_names
+        for temp_table in [
+            database._get_temp_table(table, "2021"),
+            database._get_temp_table(table, random_run_id),
+            database._get_temp_table(table, str(uuid.uuid4())),
+        ]
+    ]
+
+    assert len(temp_tables) == len(set(temp_tables))
