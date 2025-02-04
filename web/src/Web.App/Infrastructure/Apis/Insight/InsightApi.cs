f5fa513c2c5181bafefa2207ce04f9d219ef074d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-04 12:28:15 +0000

    chore: Revert test endpoints for `429` retry logic

--- web/src/Web.App/Infrastructure/Apis/Insight/InsightApi.cs
+++ web/src/Web.App/Infrastructure/Apis/Insight/InsightApi.cs
@@ -2,13 +2,13 @@
 
 public class InsightApi(HttpClient httpClient, string? key = default) : ApiBase(httpClient, key), IInsightApi
 {
-    public async Task<ApiResult> GetCurrentReturnYears() => await GetAsync(Api.Insight.CurrentReturnYears);
-
-    public async Task<ApiResult> Test429() => await GetAsync("api/429");
+    public async Task<ApiResult> GetCurrentReturnYears()
+    {
+        return await GetAsync(Api.Insight.CurrentReturnYears);
+    }
 }
 
 public interface IInsightApi
 {
     Task<ApiResult> GetCurrentReturnYears();
-    Task<ApiResult> Test429();
 }
