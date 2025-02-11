ae5c57290d9f671e93fb77fa11f56d9ebaee618d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-11 16:36:13 +0000

    chore: Converted `_SaveChartsButton` partial view to view component

---
+++ web/src/Web.App/ViewComponents/PageActionsViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.App.ViewModels.Components;
+
+namespace Web.App.ViewComponents;
+
+public class PageActionsViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(string? title, string? name, string? id, string? kind)
+        => View(new PageActionsViewModel(title, name, id, kind));
+}
