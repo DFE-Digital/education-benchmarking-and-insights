009216eef88285e473176fa0c7c0f849c113ba18
Roger G. Coram <roger.coram@hippodigital.co.uk>
2025-02-13 12:12:45 +0000

    feat: use JSON for logging
    use JSON-formatted log messages throughout, courtesy of
    [Python JSON Logger](https://nhairs.github.io/python-json-logger/).

--- data-pipeline/src/pipeline/log.py
+++ data-pipeline/src/pipeline/log.py
@@ -1,16 +1,17 @@
 import logging
 import os
 
+from pythonjsonlogger.json import JsonFormatter
 
-def setup_logger(name):
-    formatter = logging.Formatter(
-        fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s"
-    )
+
+def setup_logger(name: str):
+    logger = logging.getLogger(name)
+
+    logger.setLevel(os.getenv("LOG_LEVEL", logging.DEBUG))
 
     handler = logging.StreamHandler()
+    formatter = JsonFormatter(fmt="%(asctime)s %(levelname)s %(module)s %(message)s")
     handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
+
     return logger
