ce9d9464ee6ba77d2b81ae0bf490d4fdf81db461
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-14 06:11:23 +0000

    chore: `TelemetryService` unit test coverage

---
+++ platform/src/abstractions/Platform.Test/Mocks/MockTelemetryClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Platform.Test.Mocks;
+
+public static class MockTelemetryClient
+{
+    public static TelemetryClient Create()
+    {
+        var mockTelemetryChannel = new MockTelemetryChannel();
+        var mockTelemetryConfig = new TelemetryConfiguration
+        {
+            TelemetryChannel = mockTelemetryChannel,
+            InstrumentationKey = Guid.NewGuid().ToString()
+        };
+
+        var mockTelemetryClient = new TelemetryClient(mockTelemetryConfig);
+        return mockTelemetryClient;
+    }
+}
+
+public class MockTelemetryChannel : ITelemetryChannel
+{
+    private readonly ConcurrentBag<ITelemetry> _sentTelemetries = [];
+
+    public bool? DeveloperMode { get; set; }
+    public string? EndpointAddress { get; set; }
+
+    public void Send(ITelemetry item)
+    {
+        _sentTelemetries.Add(item);
+    }
+
+    public void Flush()
+    {
+    }
+
+    [SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize")]
+    public void Dispose()
+    {
+    }
+
+    public T? GetLastTelemetryItemOfType<T>() => _sentTelemetries.Cast<T>().LastOrDefault();
+}
