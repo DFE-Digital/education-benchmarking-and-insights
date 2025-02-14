37f1ed03e8e827fd10f18207aa2ad0269a9c3ff6
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-14 06:11:23 +0000

    chore: `TelemetryService` unit test coverage

--- platform/src/apis/Platform.Orchestrator/Extensions/LoggerExtensions.cs
+++ platform/src/apis/Platform.Orchestrator/Extensions/LoggerExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 
 namespace Platform.Orchestrator.Extensions;
 
+[ExcludeFromCodeCoverage]
 public static class LoggerExtensions
 {
     public static IDisposable? BeginApplicationScope(this ILogger? logger, string? jobId = null)
---
+++ platform/tests/Platform.Orchestrator.Tests/Telemetry/WhenTelemetryServiceTracksEvent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Platform.Orchestrator.Telemetry;
+using Platform.Test.Mocks;
+using Xunit;
+
+namespace Platform.Orchestrator.Tests.Telemetry;
+
+public class WhenTelemetryServiceTracksEvent
+{
+    private readonly TelemetryService _service;
+    private readonly TelemetryClient _telemetryClient;
+
+    public WhenTelemetryServiceTracksEvent()
+    {
+        _telemetryClient = MockTelemetryClient.Create();
+        _service = new TelemetryService(_telemetryClient);
+    }
+
+    [Fact]
+    public void ShouldTrackEventInTelemetryClient()
+    {
+        const string eventName = nameof(eventName);
+        const string jobId = nameof(jobId);
+        var props = new Dictionary<string, string?>
+        {
+            { "Key", "Value" }
+        };
+
+        _service.TrackEvent(eventName, jobId, props);
+
+        var expectedProps = new Dictionary<string, string>
+        {
+            { "JobId", jobId },
+            { "Key", "Value" }
+        };
+        var channel = _telemetryClient.TelemetryConfiguration.TelemetryChannel as MockTelemetryChannel;
+        var telemetry = channel?.GetLastTelemetryItemOfType<EventTelemetry>();
+        Assert.NotNull(telemetry);
+        Assert.Equal(eventName, telemetry.Name);
+        Assert.Equal(expectedProps, telemetry.Properties);
+    }
+}
