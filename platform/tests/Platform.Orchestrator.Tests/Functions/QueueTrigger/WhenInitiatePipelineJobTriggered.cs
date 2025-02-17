bb1a2d209d23a40db0a70ee750a164ca1d775743
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-17 11:14:08 +0000

    feat: Unit tests for QueueTrigger and TimerTrigger functions

---
+++ platform/tests/Platform.Orchestrator.Tests/Functions/QueueTrigger/WhenInitiatePipelineJobTriggered.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Moq;
+using Platform.Domain.Messages;
+using Platform.Orchestrator.Functions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Platform.Orchestrator.Tests.Functions.QueueTrigger;
+
+public class WhenInitiatePipelineJobTriggered(ITestOutputHelper testOutputHelper) : QueueTriggerFunctionTest(testOutputHelper)
+{
+    [Theory]
+    [InlineData(OrchestrationRuntimeStatus.Running, false)]
+    [InlineData(OrchestrationRuntimeStatus.Completed, true)]
+    [InlineData(OrchestrationRuntimeStatus.Pending, false)]
+    [InlineData(OrchestrationRuntimeStatus.Suspended, true)]
+    public async Task ShouldScheduleNewOrchestrationInstanceIfNotPendingRunning(OrchestrationRuntimeStatus runtimeStatus, bool shouldSchedule)
+    {
+        var message = new PipelinePending
+        {
+            JobId = "jobId"
+        };
+
+        var status = new OrchestrationMetadata("name", "instanceId")
+        {
+            RuntimeStatus = runtimeStatus
+        };
+
+        var client = new Mock<DurableTaskClient>("name");
+        client
+            .Setup(c => c.GetInstanceAsync(message.JobId, It.IsAny<bool>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(status);
+
+        client
+            .Setup(c => c.ScheduleNewOrchestrationInstanceAsync(nameof(OrchestratorFunctions.PipelineJobOrchestrator), message, It.Is<StartOrchestrationOptions>(o => o.InstanceId == message.JobId), It.IsAny<CancellationToken>()))
+            .Verifiable(shouldSchedule ? Times.Once() : Times.Never());
+
+        await Functions.InitiatePipelineJob(message, client.Object);
+
+        client.Verify();
+    }
+
+    [Fact]
+    public void ShouldReceiveMessagesFromPendingQueueFromConfiguration()
+    {
+        var method = typeof(PipelineQueueTriggerFunctions).GetMethod(nameof(PipelineQueueTriggerFunctions.InitiatePipelineJob));
+        var parameters = method?.GetParameters();
+        var messageParam = parameters?.SingleOrDefault(p => p.Name == "message");
+        var attribute = messageParam?.GetCustomAttributes(typeof(QueueTriggerAttribute), false).FirstOrDefault() as QueueTriggerAttribute;
+
+        Assert.NotNull(attribute);
+        Assert.Equal("PipelineMessageHub:ConnectionString", attribute.Connection);
+        Assert.Equal("%PipelineMessageHub:JobPendingQueue%", attribute.QueueName);
+    }
+}
---
+++ platform/tests/Platform.Orchestrator.Tests/Functions/TimerTrigger/WhenPipelineJobPurgeHistoryTriggered.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Platform.Orchestrator.Functions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Platform.Orchestrator.Tests.Functions.TimerTrigger;
+
+public class WhenPipelineJobPurgeHistoryTriggered
+{
+    private readonly Mock<FunctionContext> _context;
+    private readonly TimerInfo _timer;
+
+    public WhenPipelineJobPurgeHistoryTriggered(ITestOutputHelper testOutputHelper)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+
+        _context = new Mock<FunctionContext>();
+        _context.SetupProperty(functionContext => functionContext.InstanceServices, serviceCollection.BuildServiceProvider());
+
+        _timer = new TimerInfo();
+    }
+
+    [Fact]
+    public async Task ShouldPurgeAllInstances()
+    {
+        PurgeInstancesFilter? actualFilter = null;
+
+        var client = new Mock<DurableTaskClient>("name");
+        client
+            .Setup(c => c.PurgeAllInstancesAsync(It.IsAny<PurgeInstancesFilter>(), It.IsAny<PurgeInstanceOptions?>(), It.IsAny<CancellationToken>()))
+            .Callback<PurgeInstancesFilter, PurgeInstanceOptions?, CancellationToken>((filter, _, _) =>
+            {
+                actualFilter = filter;
+            });
+
+        await TimerTriggerFunctions.PipelineJobPurgeHistory(client.Object, _timer, _context.Object);
+
+        client.Verify();
+        Assert.NotNull(actualFilter);
+        Assert.Equal(DateTime.MinValue, actualFilter.CreatedFrom);
+        Assert.InRange((DateTime)actualFilter.CreatedTo?.DateTime!, DateTime.UtcNow.AddDays(-7).AddSeconds(-1), DateTime.UtcNow.AddDays(-7).AddSeconds(1));
+        Assert.Contains(OrchestrationRuntimeStatus.Completed, actualFilter.Statuses!);
+        Assert.Contains(OrchestrationRuntimeStatus.Failed, actualFilter.Statuses!);
+        Assert.Contains(OrchestrationRuntimeStatus.Terminated, actualFilter.Statuses!);
+        Assert.Contains(OrchestrationRuntimeStatus.Suspended, actualFilter.Statuses!);
+    }
+
+    [Fact]
+    public void ShouldSetCronJobToDailyAtNoon()
+    {
+        var method = typeof(TimerTriggerFunctions).GetMethod(nameof(TimerTriggerFunctions.PipelineJobPurgeHistory));
+        var parameters = method?.GetParameters();
+        var messageParam = parameters?.SingleOrDefault(p => p.Name == "timer");
+        var attribute = messageParam?.GetCustomAttributes(typeof(TimerTriggerAttribute), false).FirstOrDefault() as TimerTriggerAttribute;
+
+        Assert.NotNull(attribute);
+        Assert.Equal("0 0 12 * * *", attribute.Schedule);
+    }
+}
