d35b75aa4f759e2383656717e048bec399ad5548
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-20 18:25:30 +0000

    feat: Reverted changes to `LocalAuthorityFinanceToolsViewComponent` and managed via view instead

--- web/src/Web.App/ViewComponents/LocalAuthorityToolsViewComponent.cs
+++ web/src/Web.App/ViewComponents/LocalAuthorityToolsViewComponent.cs
@@ -1,19 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.FeatureManagement;
 using Web.App.ViewModels.Components;
 
 namespace Web.App.ViewComponents;
 
-public class LocalAuthorityFinanceToolsViewComponent(IFeatureManager featureManager) : ViewComponent
+public class LocalAuthorityFinanceToolsViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(string identifier, FinanceTools[] tools)
+    public IViewComponentResult Invoke(string identifier, FinanceTools[] tools)
     {
-        var highNeedsEnabled = await featureManager.IsEnabledAsync(FeatureFlags.HighNeeds);
-        if (!highNeedsEnabled)
-        {
-            tools = tools.Where(t => t != FinanceTools.HighNeeds).ToArray();
-        }
-
         return View(new FinanceToolsViewModel(identifier, tools));
     }
 }
