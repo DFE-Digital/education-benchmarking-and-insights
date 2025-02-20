468c6949c86cc482005cb9669436310a029b50e1
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-20 07:24:30 +0000

    feat: New High needs target pages and links from High needs landing page

--- web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsBenchmarkingPage.cs
+++ web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsBenchmarkingPage.cs
@@ -5,9 +5,39 @@ namespace Web.E2ETests.Pages.LocalAuthority;
 public class HighNeedsBenchmarkingPage(IPage page)
 {
     private ILocator PageH1Heading => page.Locator($"main {Selectors.H1}");
+    private ILocator StartBenchmarkingButton => page.Locator(Selectors.CtaButton, new PageLocatorOptions
+    {
+        HasText = "Start benchmarking"
+    });
+    private ILocator ViewNationalRankingsButton => page.Locator(Selectors.CtaButton, new PageLocatorOptions
+    {
+        HasText = "View national rankings"
+    });
+    private ILocator ViewHistoricDataButton => page.Locator(Selectors.CtaButton, new PageLocatorOptions
+    {
+        HasText = "View historic data"
+    });
 
     public async Task IsDisplayed()
     {
         await PageH1Heading.ShouldBeVisible();
+    }
+
+    public async Task<HighNeedsStartBenchmarkingPage> ClickStartBenchmarking()
+    {
+        await StartBenchmarkingButton.Click();
+        return new HighNeedsStartBenchmarkingPage(page);
+    }
+
+    public async Task<HighNeedsNationalRankingsPage> ClickViewNationalRankings()
+    {
+        await ViewNationalRankingsButton.Click();
+        return new HighNeedsNationalRankingsPage(page);
+    }
+
+    public async Task<HighNeedsHistoricDataPage> ClickViewHistoricData()
+    {
+        await ViewHistoricDataButton.Click();
+        return new HighNeedsHistoricDataPage(page);
     }
 }
