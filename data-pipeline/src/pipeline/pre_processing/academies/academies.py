816b450669325642d9c3faa0e6efb11a699094e8
Roger G. Coram <roger.coram@hippodigital.co.uk>
2025-02-24 11:10:36 +0000

    refactor: move generic GIAS-links function

--- data-pipeline/src/pipeline/pre_processing/academies/academies.py
+++ data-pipeline/src/pipeline/pre_processing/academies/academies.py
@@ -186,60 +186,6 @@ def prepare_aar_data(aar_path, year: int):
     return aar.set_index(input_schemas.aar_academies_index_col)
 
 
-def _link_data(
-    df: pd.DataFrame,
-    linkable: pd.DataFrame,
-    gias_links: pd.DataFrame,
-) -> pd.DataFrame:
-    """
-    Extend a dataset via GIAS-links.
-
-    - `df`: point of reference, must have `URN`
-    - `linkable`: must have `URN`, an extended version of this
-      `DataFrame` will be returned
-    - `gias_links`: must have `URN` and `LinkURN`, will be used to
-      extend `linkable` with records missing from `df`.
-
-    The data present in `linkable` may be missing URNs present in the
-    `df` data. Where this is the case, we extend those data with
-    additional records referencing the GIAS-link `LinkURN` (i.e. the
-    data will then contain a record for both the GIAS-link `URN` and
-    `LinkURN`).
-
-    - determine which `df` records are missing from the `linkable`
-    - of those missing records, determine which have GIAS-link records
-    - of those GIAS-links, determine which have `linkable` records
-    - supplement the `linkable` records, adding records with the
-      mapped GIAS-links
-    """
-    _df = df.reset_index()[["URN"]]
-    _linkable = linkable.reset_index()
-    _gias_links = gias_links.reset_index()[["URN", "LinkURN"]]
-
-    aar_missing = _df[~_df["URN"].isin(_linkable["URN"])][["URN"]]
-
-    link_urns = _gias_links[
-        (_gias_links["URN"].isin(aar_missing["URN"]))
-        & (_gias_links["LinkURN"].isin(_linkable["URN"]))
-    ][["URN", "LinkURN"]]
-
-    linked = (
-        _linkable[_linkable["URN"].isin(link_urns["LinkURN"])]
-        .merge(
-            link_urns,
-            how="inner",
-            left_on="URN",
-            right_on="LinkURN",
-            suffixes=("_census", "_link"),
-        )
-        .drop(columns=["URN_census", "LinkURN"])
-        .rename(columns={"URN_link": "URN"})
-        .set_index("URN")
-    )
-
-    return pd.concat([linkable, linked])
-
-
 def build_academy_data(
     schools: pd.DataFrame,
     census: pd.DataFrame,
@@ -286,13 +232,13 @@ def build_academy_data(
         aar.reset_index()
         .merge(schools, on="URN")
         .merge(
-            _link_data(aar, census, gias_links),
+            gias.link_data(aar, census, gias_links),
             on="URN",
             how="left",
         )
         .merge(sen, on="URN", how="left")
         .merge(
-            _link_data(aar, cdc, gias_links),
+            gias.link_data(aar, cdc, gias_links),
             on="URN",
             how="left",
         )
