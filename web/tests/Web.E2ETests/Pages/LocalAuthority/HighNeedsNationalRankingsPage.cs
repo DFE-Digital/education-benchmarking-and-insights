8b4b70d4a5822abe2742123a4e07effb17d749d5
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-24 10:49:32 +0000

    feat: E2E for LA national rankings page

--- web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsNationalRankingsPage.cs
+++ web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsNationalRankingsPage.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.Playwright;
+using Web.E2ETests.Assist;
 
 namespace Web.E2ETests.Pages.LocalAuthority;
 
 public class HighNeedsNationalRankingsPage(IPage page)
 {
     private ILocator PageH1Heading => page.Locator($"main {Selectors.H1}");
+    private ILocator ViewAsTableRadio => page.Locator(Selectors.ModeTable);
+    private ILocator Table => page.Locator($"#la-national-rank {Selectors.GovTable}");
+    private ILocator SaveImageButton => page.Locator("xpath=//*[@data-custom-event-chart-name='National ranking'][@data-custom-event-id='save-chart-as-image']");
+    private ILocator CopyImageButton => page.Locator("xpath=//*[@data-custom-event-chart-name='National ranking'][@data-custom-event-id='copy-chart-as-image']");
 
     public async Task IsDisplayed()
     {
         await PageH1Heading.ShouldBeVisible();
+    }
+
+    public async Task ClickViewAsTable()
+    {
+        await ViewAsTableRadio.ShouldBeVisible();
+        await ViewAsTableRadio.ClickAsync();
+    }
+
+    public async Task TableContainsValues(DataTable table)
+    {
+        await Table.ShouldBeVisible();
+        var rows = await Table.Locator("> tbody > tr").AllAsync();
+        var set = new List<dynamic>();
+
+        foreach (var row in rows)
+        {
+            set.Add(new
+            {
+                Name = await row.Locator("> td").Nth(0).InnerTextAsync(),
+                Value = await row.Locator("> td").Nth(1).InnerTextAsync()
+            });
+        }
+
+        table.CompareToDynamicSet(set, false);
+    }
+
+    public async Task ClickSaveAsImage()
+    {
+        await SaveImageButton.Click();
+    }
+
+    public async Task ClickCopyImage()
+    {
+        await CopyImageButton.Click();
     }
 }
---
+++ web/tests/Web.E2ETests/Steps/LocalAuthority/HighNeedsNationalRankingsSteps.cs
@@ -0,0 +1,79 @@
+using Microsoft.Playwright;
+using Web.E2ETests.Drivers;
+using Web.E2ETests.Pages.LocalAuthority;
+using Xunit;
+
+namespace Web.E2ETests.Steps.LocalAuthority;
+
+[Binding]
+[Scope(Feature = "Local Authority high needs national rankings")]
+public class HighNeedsNationalRankingsSteps(PageDriver driver)
+{
+    private HighNeedsNationalRankingsPage? _highNeedsNationalRankingsPage;
+    private IDownload? _download;
+
+    [Given("I am on local authority high needs national rankings for local authority with code '(.*)'")]
+    public async Task GivenIAmOnLocalAuthorityHighNeedsNationalRankingsForLocalAuthorityWithCode(string laCode)
+    {
+        var url = LocalAuthorityHighNeedsNationalRankingsUrl(laCode);
+        var page = await driver.Current;
+        await page.GotoAndWaitForLoadAsync(url);
+
+        _highNeedsNationalRankingsPage = new HighNeedsNationalRankingsPage(page);
+        await _highNeedsNationalRankingsPage.IsDisplayed();
+    }
+
+    [When("I click on table view")]
+    public async Task WhenIClickOnTableView()
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        await _highNeedsNationalRankingsPage.ClickViewAsTable();
+    }
+
+    [Then("the national rankings table is displayed with the following values:")]
+    public async Task ThenTheNationalRankingsTableIsDisplayedWithTheFollowingValues(DataTable table)
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        await _highNeedsNationalRankingsPage.TableContainsValues(table);
+    }
+
+    [When("I click on save as image")]
+    public async Task WhenIClickOnSaveAsImage()
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        var page = await driver.Current;
+        _download = await page.RunAndWaitForDownloadAsync(() => _highNeedsNationalRankingsPage.ClickSaveAsImage());
+    }
+
+    [Then("the chart image is downloaded")]
+    public void ThenTheChartImageIsDownloaded()
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        Assert.NotNull(_download);
+        var downloadedFilePath = _download.SuggestedFilename;
+        Assert.Equal("national-ranking.png", downloadedFilePath);
+    }
+
+    [When("I click on copy image")]
+    public async Task WhenIClickOnCopyImage()
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        await _highNeedsNationalRankingsPage.ClickCopyImage();
+    }
+
+    [Then("the chart image is copied")]
+    public async Task ThenTheChartImageIsCopied()
+    {
+        Assert.NotNull(_highNeedsNationalRankingsPage);
+        const string exp = "navigator.clipboard.read()";
+        var page = await driver.Current;
+        var actual = await page.EvaluateAsync<object[]>(exp);
+        Assert.NotNull(actual);
+        Assert.Single(actual);
+    }
+
+    private static string LocalAuthorityHighNeedsNationalRankingsUrl(string laCode)
+    {
+        return $"{TestConfiguration.ServiceUrl}/local-authority/{laCode}/high-needs/national-rank";
+    }
+}
