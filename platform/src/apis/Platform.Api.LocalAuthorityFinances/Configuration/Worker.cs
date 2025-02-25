8692f6a292586ddb47c71bcffdef6c23e291714b
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-25 09:04:02 +0000

    feat: Added new Function app projects to solution

---
+++ platform/src/apis/Platform.Api.LocalAuthorityFinances/Configuration/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+using Platform.Functions.Middleware;
+
+namespace Platform.Api.LocalAuthorityFinances.Configuration;
+
+[ExcludeFromCodeCoverage]
+internal static class Worker
+{
+    internal static void Configure(IFunctionsWorkerApplicationBuilder builder)
+    {
+        builder.UseWhen<CorrelationHeaderMiddleware>(context =>
+        {
+            // We want to use this middleware only for http trigger invocations.
+            return context.FunctionDefinition.InputBindings.Values.First(a => a.Type.EndsWith("Trigger")).Type == "httpTrigger";
+        });
+
+        builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
+    internal static void Options(WorkerOptions options)
+    {
+    }
+}
---
+++ platform/src/apis/Platform.Api.NonFinancial/Program.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.Hosting;
+using Platform.Api.NonFinancial.Configuration;
+
+[assembly: InternalsVisibleTo("Platform.Tests")]
+
+var hostBuilder = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(Worker.Configure, Worker.Options)
+    .ConfigureServices(Services.Configure)
+    .ConfigureLogging(Logging.Configure)
+    .ConfigureOpenApi();
+
+hostBuilder.Build().Run();
+
+[ExcludeFromCodeCoverage]
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class Program;
