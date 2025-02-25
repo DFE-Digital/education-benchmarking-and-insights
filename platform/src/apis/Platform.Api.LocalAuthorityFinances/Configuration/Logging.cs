c6f4a975978bdafd250c485174deab80afaf51c8
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-25 09:04:02 +0000

    feat: Added new Function app projects to solution

---
+++ platform/src/apis/Platform.Api.LocalAuthorityFinances/Configuration/Logging.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Platform.Api.LocalAuthorityFinances.Configuration;
+
+[ExcludeFromCodeCoverage]
+internal static class Logging
+{
+    internal static void Configure(ILoggingBuilder builder)
+    {
+        builder.Services.Configure<LoggerFilterOptions>(_ => { });
+    }
+}
---
+++ platform/src/apis/Platform.Api.NonFinancial/Configuration/Services.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Functions;
+using Platform.Json;
+using Platform.Sql;
+
+// ReSharper disable UnusedMethodReturnValue.Local
+
+namespace Platform.Api.NonFinancial.Configuration;
+
+[ExcludeFromCodeCoverage]
+internal static class Services
+{
+    internal static void Configure(IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddSingleton<IFunctionContextDataProvider, FunctionContextDataProvider>();
+
+        serviceCollection
+            .AddTelemetry()
+            .AddHealthCheckServices()
+            .AddPlatformServices()
+            .AddFeatures();
+
+        serviceCollection.Configure<JsonSerializerOptions>(SystemTextJsonExtensions.Options);
+    }
+
+    private static IServiceCollection AddHealthCheckServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddHealthChecks()
+            .AddPlatformSql();
+
+        return serviceCollection;
+    }
+
+    private static IServiceCollection AddTelemetry(this IServiceCollection serviceCollection)
+    {
+        //TODO: Add serilog configuration AB#227696
+        // App Insights must be BEFORE any keyed services:
+        // • https://github.com/microsoft/ApplicationInsights-dotnet/issues/2828
+        // • https://github.com/microsoft/ApplicationInsights-dotnet/issues/2879
+        var sqlTelemetryEnabled = Environment.GetEnvironmentVariable("Sql__TelemetryEnabled");
+        serviceCollection
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, _) =>
+            {
+                module.EnableSqlCommandTextInstrumentation = bool.TrueString.Equals(sqlTelemetryEnabled, StringComparison.OrdinalIgnoreCase);
+            });
+
+        return serviceCollection;
+    }
+
+    private static IServiceCollection AddPlatformServices(this IServiceCollection serviceCollection) => serviceCollection
+        .AddPlatformSql();
+
+    private static IServiceCollection AddFeatures(this IServiceCollection serviceCollection) => serviceCollection;
+}
