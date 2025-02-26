19ea752e3b5ee935a8201e302e9f41b071e10041
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-02-26 09:59:36 +0000

    feat: API tests over High needs history endpoint

---
+++ platform/tests/Platform.ApiTests/Features/LocalAuthoritiesHighNeeds.feature
@@ -0,0 +1,119 @@
+Feature: Local authorities high needs endpoints
+# todo: replace stubbed values with real
+
+    Scenario: Sending a valid high needs request returns the expected outturn values
+        Given a valid high needs request with LA codes '201'
+        When I submit the high needs request
+        Then the high needs result should be ok and have the following values:
+          | Start year | End year |
+          | 2021       | 2024     |
+        And the high needs result should contain the following outturn values for '2021':
+          | Field | Value |
+          | Year  | 2021  |
+          | Code  | 201   |
+        And the high needs result should contain the following outturn values for '2022':
+          | Field | Value |
+          | Year  | 2022  |
+          | Code  | 201   |
+        And the high needs result should contain the following outturn values for '2023':
+          | Field | Value |
+          | Year  | 2023  |
+          | Code  | 201   |
+        And the high needs result should contain the following outturn values for '2024':
+          | Field | Value |
+          | Year  | 2024  |
+          | Code  | 201   |
+        And the high needs result should contain the following outturn high needs amount values for '2021':
+          | Field                        | Value   |
+          | TotalPlaceFunding            | 1002022 |
+          | TopUpFundingMaintained       | 1002023 |
+          | TopUpFundingNonMaintained    | 1002024 |
+          | SenServices                  | 1002025 |
+          | AlternativeProvisionServices | 1002026 |
+          | HospitalServices             | 1002027 |
+          | OtherHealthServices          | 1002028 |
+        And the high needs result should contain the following outturn maintained values for '2021':
+          | Field                | Value   |
+          | EarlyYears           | 1002029 |
+          | Primary              | 1002030 |
+          | Secondary            | 1002031 |
+          | Special              | 1002032 |
+          | AlternativeProvision | 1002033 |
+          | PostSchool           | 1002034 |
+          | Income               | 1002035 |
+        And the high needs result should contain the following outturn non maintained values for '2021':
+          | Field                | Value   |
+          | EarlyYears           | 1002036 |
+          | Primary              | 1002037 |
+          | Secondary            | 1002038 |
+          | Special              | 1002039 |
+          | AlternativeProvision | 1002040 |
+          | PostSchool           | 1002041 |
+          | Income               | 1002042 |
+        And the high needs result should contain the following outturn place funding values for '2021':
+          | Field                | Value   |
+          | Primary              | 1002043 |
+          | Secondary            | 1002044 |
+          | Special              | 1002045 |
+          | AlternativeProvision | 1002046 |
+
+    Scenario: Sending a valid high needs request returns the expected budget values
+        Given a valid high needs request with LA codes '201'
+        When I submit the high needs request
+        Then the high needs result should be ok and have the following values:
+          | Start year | End year |
+          | 2021       | 2024     |
+        And the high needs result should contain the following budget values for '2021':
+          | Field | Value |
+          | Year  | 2021  |
+          | Code  | 201   |
+        And the high needs result should contain the following budget values for '2022':
+          | Field | Value |
+          | Year  | 2022  |
+          | Code  | 201   |
+        And the high needs result should contain the following budget values for '2023':
+          | Field | Value |
+          | Year  | 2023  |
+          | Code  | 201   |
+        And the high needs result should contain the following budget values for '2024':
+          | Field | Value |
+          | Year  | 2024  |
+          | Code  | 201   |
+        And the high needs result should contain the following budget high needs amount values for '2021':
+          | Field                        | Value   |
+          | TotalPlaceFunding            | 1102022 |
+          | TopUpFundingMaintained       | 1102023 |
+          | TopUpFundingNonMaintained    | 1102024 |
+          | SenServices                  | 1102025 |
+          | AlternativeProvisionServices | 1102026 |
+          | HospitalServices             | 1102027 |
+          | OtherHealthServices          | 1102028 |
+        And the high needs result should contain the following budget maintained values for '2021':
+          | Field                | Value   |
+          | EarlyYears           | 1102029 |
+          | Primary              | 1102030 |
+          | Secondary            | 1102031 |
+          | Special              | 1102032 |
+          | AlternativeProvision | 1102033 |
+          | PostSchool           | 1102034 |
+          | Income               | 1102035 |
+        And the high needs result should contain the following budget non maintained values for '2021':
+          | Field                | Value   |
+          | EarlyYears           | 1102036 |
+          | Primary              | 1102037 |
+          | Secondary            | 1102038 |
+          | Special              | 1102039 |
+          | AlternativeProvision | 1102040 |
+          | PostSchool           | 1102041 |
+          | Income               | 1102042 |
+        And the high needs result should contain the following budget place funding values for '2021':
+          | Field                | Value   |
+          | Primary              | 1102043 |
+          | Secondary            | 1102044 |
+          | Special              | 1102045 |
+          | AlternativeProvision | 1102046 |
+
+    Scenario: Sending an invalid high needs request
+        Given an invalid high needs request
+        When I submit the high needs request
+        Then the high needs result should be bad request
---
+++ platform/tests/Platform.ApiTests/Steps/LocalAuthoritiesHighNeedsSteps.cs
@@ -0,0 +1,157 @@
+﻿using System.Net;
+using FluentAssertions;
+using Platform.Api.LocalAuthorityFinances.Features.HighNeeds.Models;
+using Platform.ApiTests.Drivers;
+using Platform.Json;
+using Xunit;
+
+namespace Platform.ApiTests.Steps;
+
+[Binding]
+[Scope(Feature = "Local authorities high needs endpoints")]
+public class LocalAuthoritiesHighNeedsSteps(LocalAuthorityFinancesApiDriver api)
+{
+    private const string HistoryKey = "history";
+    private History<LocalAuthorityHighNeedsYear>? _result;
+
+    [Given("a valid high needs request with LA codes '(.*)'")]
+    public void GivenAValidHighNeedsRequestWithLaCodes(string codes)
+    {
+        api.CreateRequest(HistoryKey, new HttpRequestMessage
+        {
+            RequestUri = new Uri($"/api/high-needs/history?code={string.Join("&code=", codes.Split(","))}", UriKind.Relative),
+            Method = HttpMethod.Get
+        });
+    }
+
+    [Given("an invalid high needs request")]
+    public void GivenAnInvalidHighNeedsRequest()
+    {
+        api.CreateRequest(HistoryKey, new HttpRequestMessage
+        {
+            RequestUri = new Uri("/api/high-needs/history", UriKind.Relative),
+            Method = HttpMethod.Get
+        });
+    }
+
+    [When("I submit the high needs request")]
+    public async Task WhenISubmitTheHighNeedsRequest()
+    {
+        await api.Send();
+    }
+
+    [Then("the high needs result should be ok and have the following values:")]
+    public async Task ThenTheHighNeedsResultShouldBeOkAndHaveTheFollowingValues(DataTable table)
+    {
+        var response = api[HistoryKey].Response;
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var content = await response.Content.ReadAsByteArrayAsync();
+        _result = content.FromJson<History<LocalAuthorityHighNeedsYear>>();
+        var startYear = table.Rows.First()["Start year"];
+        var endYear = table.Rows.First()["End year"];
+
+        Assert.Equal(startYear, _result.StartYear.ToString());
+        Assert.Equal(endYear, _result.EndYear.ToString());
+    }
+
+    [Then("the high needs result should contain the following outturn values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingOutturnValuesFor(string year, DataTable table)
+    {
+        var outturn = OutturnResultForYear(year);
+        Assert.NotNull(outturn);
+        table.CompareToInstance(outturn);
+    }
+
+    [Then("the high needs result should contain the following outturn high needs amount values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingOutturnHighNeedsAmountValuesFor(string year, DataTable table)
+    {
+        var highNeedsAmount = OutturnResultForYear(year)?.HighNeedsAmount;
+        Assert.NotNull(highNeedsAmount);
+        table.CompareToInstance(highNeedsAmount);
+    }
+
+    [Then("the high needs result should contain the following outturn maintained values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingOutturnMaintainedValuesFor(string year, DataTable table)
+    {
+        var maintained = OutturnResultForYear(year)?.Maintained;
+        Assert.NotNull(maintained);
+        table.CompareToInstance(maintained);
+    }
+
+    [Then("the high needs result should contain the following outturn non maintained values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingOutturnNonMaintainedValuesFor(string year, DataTable table)
+    {
+        var nonMaintained = OutturnResultForYear(year)?.NonMaintained;
+        Assert.NotNull(nonMaintained);
+        table.CompareToInstance(nonMaintained);
+    }
+
+    [Then("the high needs result should contain the following outturn place funding values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingOutturnPlaceFundingValuesFor(string year, DataTable table)
+    {
+        var placeFunding = OutturnResultForYear(year)?.PlaceFunding;
+        Assert.NotNull(placeFunding);
+        table.CompareToInstance(placeFunding);
+    }
+
+    [Then("the high needs result should contain the following budget values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingBudgetValuesFor(string year, DataTable table)
+    {
+        var budget = BudgetResultForYear(year);
+        Assert.NotNull(budget);
+        table.CompareToInstance(budget);
+    }
+
+    [Then("the high needs result should contain the following budget high needs amount values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingBudgetHighNeedsAmountValuesFor(string year, DataTable table)
+    {
+        var highNeedsAmount = BudgetResultForYear(year)?.HighNeedsAmount;
+        Assert.NotNull(highNeedsAmount);
+        table.CompareToInstance(highNeedsAmount);
+    }
+
+    [Then("the high needs result should contain the following budget maintained values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingBudgetMaintainedValuesFor(string year, DataTable table)
+    {
+        var maintained = BudgetResultForYear(year)?.Maintained;
+        Assert.NotNull(maintained);
+        table.CompareToInstance(maintained);
+    }
+
+    [Then("the high needs result should contain the following budget non maintained values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingBudgetNonMaintainedValuesFor(string year, DataTable table)
+    {
+        var nonMaintained = BudgetResultForYear(year)?.NonMaintained;
+        Assert.NotNull(nonMaintained);
+        table.CompareToInstance(nonMaintained);
+    }
+
+    [Then("the high needs result should contain the following budget place funding values for '(.*)':")]
+    public void ThenTheHighNeedsResultShouldContainTheFollowingBudgetPlaceFundingValuesFor(string year, DataTable table)
+    {
+        var placeFunding = BudgetResultForYear(year)?.PlaceFunding;
+        Assert.NotNull(placeFunding);
+        table.CompareToInstance(placeFunding);
+    }
+
+    [Then("the high needs result should be bad request")]
+    public void ThenTheHighNeedsResultShouldBeBadRequest()
+    {
+        var response = api[HistoryKey].Response;
+
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    private LocalAuthorityHighNeedsYear? OutturnResultForYear(string year)
+    {
+        return _result?.Outturn?.SingleOrDefault(o => o.Year.ToString() == year);
+    }
+
+    private LocalAuthorityHighNeedsYear? BudgetResultForYear(string year)
+    {
+        return _result?.Budget?.SingleOrDefault(o => o.Year.ToString() == year);
+    }
+}
