bead9badd83b26833c633dd665402dd13f34f68b
Jon Abbott <jon.r.abbott@gmail.com>
2025-03-03 09:57:22 +0000

    feat: adds boilerplate view component for hign needs help and support section (#2063)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/src/Web.App/ViewComponents/LocalAuthorityHighNeedsHelpViewComponent.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.App.ViewComponents;
+
+public class LocalAuthorityHighNeedsHelpViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
