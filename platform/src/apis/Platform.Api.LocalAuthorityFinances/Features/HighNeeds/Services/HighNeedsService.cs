76c01c28aaa147ada2082aaf1638ceabe7f13ceb
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-14 10:53:58 +0000

    feat: Unit tests for `HighNeedsService` so far, plus coverage for `LocalAuthorityCurrentFinancialQuery`

--- platform/src/apis/Platform.Api.LocalAuthorityFinances/Features/HighNeeds/Services/HighNeedsService.cs
+++ platform/src/apis/Platform.Api.LocalAuthorityFinances/Features/HighNeeds/Services/HighNeedsService.cs
@@ -21,70 +21,70 @@ public class HighNeedsService(IDatabaseFactory dbFactory) : HighNeedsStubService
         string[] fields =
         [
             // LocalAuthorityBase
-            "LaCode AS Code",
-            "Name",
+            "LaCode AS [Code]",
+            "[Name]",
             // HighNeedsBase
-            "OutturnTotalHighNeeds AS Total",
+            "OutturnTotalHighNeeds AS [Total]",
             // HighNeedsAmount
-            "OutturnTotalPlaceFunding AS TotalPlaceFunding",
-            "OutturnTotalTopUpFundingMaintained AS TopUpFundingMaintained",
-            "OutturnTotalTopUpFundingNonMaintained AS TopUpFundingNonMaintained",
-            "OutturnTotalSenServices AS SenServices",
-            "OutturnTotalAlternativeProvisionServices AS AlternativeProvisionServices",
-            "OutturnTotalHospitalServices AS HospitalServices",
-            "OutturnTotalOtherHealthServices AS OtherHealthServices",
+            "OutturnTotalPlaceFunding AS [TotalPlaceFunding]",
+            "OutturnTotalTopUpFundingMaintained AS [TopUpFundingMaintained]",
+            "OutturnTotalTopUpFundingNonMaintained AS [TopUpFundingNonMaintained]",
+            "OutturnTotalSenServices AS [SenServices]",
+            "OutturnTotalAlternativeProvisionServices AS [AlternativeProvisionServices]",
+            "OutturnTotalHospitalServices AS [HospitalServices]",
+            "OutturnTotalOtherHealthServices AS [OtherHealthServices]",
             // TopFunding
-            "OutturnTopFundingMaintainedEarlyYears AS EarlyYears",
+            "OutturnTopFundingMaintainedEarlyYears AS [EarlyYears]",
             "OutturnTopFundingMaintainedPrimary AS [Primary]",
-            "OutturnTopFundingMaintainedSecondary AS Secondary",
-            "OutturnTopFundingMaintainedSpecial AS Special",
-            "OutturnTopFundingMaintainedAlternativeProvision AS AlternativeProvision",
-            "OutturnTopFundingMaintainedPostSchool AS PostSchool",
-            "OutturnTopFundingMaintainedIncome AS Income",
+            "OutturnTopFundingMaintainedSecondary AS [Secondary]",
+            "OutturnTopFundingMaintainedSpecial AS [Special]",
+            "OutturnTopFundingMaintainedAlternativeProvision AS [AlternativeProvision]",
+            "OutturnTopFundingMaintainedPostSchool AS [PostSchool]",
+            "OutturnTopFundingMaintainedIncome AS [Income]",
             // TopFunding
-            "OutturnTopFundingNonMaintainedEarlyYears AS EarlyYears",
+            "OutturnTopFundingNonMaintainedEarlyYears AS [EarlyYears]",
             "OutturnTopFundingNonMaintainedPrimary AS [Primary]",
-            "OutturnTopFundingNonMaintainedSecondary AS Secondary",
-            "OutturnTopFundingNonMaintainedSpecial AS Special",
-            "OutturnTopFundingNonMaintainedAlternativeProvision AS AlternativeProvision",
-            "OutturnTopFundingNonMaintainedPostSchool AS PostSchool",
-            "OutturnTopFundingNonMaintainedIncome AS Income",
+            "OutturnTopFundingNonMaintainedSecondary AS [Secondary]",
+            "OutturnTopFundingNonMaintainedSpecial AS [Special]",
+            "OutturnTopFundingNonMaintainedAlternativeProvision AS [AlternativeProvision]",
+            "OutturnTopFundingNonMaintainedPostSchool AS [PostSchool]",
+            "OutturnTopFundingNonMaintainedIncome AS [Income]",
             // PlaceFunding
             "OutturnPlaceFundingPrimary AS [Primary]",
-            "OutturnPlaceFundingSecondary AS Secondary",
-            "OutturnPlaceFundingSpecial AS Special",
-            "OutturnPlaceFundingAlternativeProvision AS AlternativeProvision",
+            "OutturnPlaceFundingSecondary AS [Secondary]",
+            "OutturnPlaceFundingSpecial AS [Special]",
+            "OutturnPlaceFundingAlternativeProvision AS [AlternativeProvision]",
             // HighNeedsBase
-            "BudgetTotalHighNeeds AS Total",
+            "BudgetTotalHighNeeds AS [Total]",
             // HighNeedsAmount
-            "BudgetTotalPlaceFunding AS TotalPlaceFunding",
-            "BudgetTotalTopUpFundingMaintained AS TopUpFundingMaintained",
-            "BudgetTotalTopUpFundingNonMaintained AS TopUpFundingNonMaintained",
-            "BudgetTotalSenServices AS SenServices",
-            "BudgetTotalAlternativeProvisionServices AS AlternativeProvisionServices",
-            "BudgetTotalHospitalServices AS HospitalServices",
-            "BudgetTotalOtherHealthServices AS OtherHealthServices",
+            "BudgetTotalPlaceFunding AS [TotalPlaceFunding]",
+            "BudgetTotalTopUpFundingMaintained AS [TopUpFundingMaintained]",
+            "BudgetTotalTopUpFundingNonMaintained AS [TopUpFundingNonMaintained]",
+            "BudgetTotalSenServices AS [SenServices]",
+            "BudgetTotalAlternativeProvisionServices AS [AlternativeProvisionServices]",
+            "BudgetTotalHospitalServices AS [HospitalServices]",
+            "BudgetTotalOtherHealthServices AS [OtherHealthServices]",
             // TopFunding
-            "BudgetTopFundingMaintainedEarlyYears AS EarlyYears",
+            "BudgetTopFundingMaintainedEarlyYears AS [EarlyYears]",
             "BudgetTopFundingMaintainedPrimary AS [Primary]",
-            "BudgetTopFundingMaintainedSecondary AS Secondary",
-            "BudgetTopFundingMaintainedSpecial AS Special",
-            "BudgetTopFundingMaintainedAlternativeProvision AS AlternativeProvision",
-            "BudgetTopFundingMaintainedPostSchool AS PostSchool",
-            "BudgetTopFundingMaintainedIncome AS Income",
+            "BudgetTopFundingMaintainedSecondary AS [Secondary]",
+            "BudgetTopFundingMaintainedSpecial AS [Special]",
+            "BudgetTopFundingMaintainedAlternativeProvision AS [AlternativeProvision]",
+            "BudgetTopFundingMaintainedPostSchool AS [PostSchool]",
+            "BudgetTopFundingMaintainedIncome AS [Income]",
             // TopFunding
-            "BudgetTopFundingNonMaintainedEarlyYears AS EarlyYears",
+            "BudgetTopFundingNonMaintainedEarlyYears AS [EarlyYears]",
             "BudgetTopFundingNonMaintainedPrimary AS [Primary]",
-            "BudgetTopFundingNonMaintainedSecondary AS Secondary",
-            "BudgetTopFundingNonMaintainedSpecial AS Special",
-            "BudgetTopFundingNonMaintainedAlternativeProvision AS AlternativeProvision",
-            "BudgetTopFundingNonMaintainedPostSchool AS PostSchool",
-            "BudgetTopFundingNonMaintainedIncome AS Income",
+            "BudgetTopFundingNonMaintainedSecondary AS [Secondary]",
+            "BudgetTopFundingNonMaintainedSpecial AS [Special]",
+            "BudgetTopFundingNonMaintainedAlternativeProvision AS [AlternativeProvision]",
+            "BudgetTopFundingNonMaintainedPostSchool AS [PostSchool]",
+            "BudgetTopFundingNonMaintainedIncome AS [Income]",
             // PlaceFunding
             "BudgetPlaceFundingPrimary AS [Primary]",
-            "BudgetPlaceFundingSecondary AS Secondary",
-            "BudgetPlaceFundingSpecial AS Special",
-            "BudgetPlaceFundingAlternativeProvision AS AlternativeProvision"
+            "BudgetPlaceFundingSecondary AS [Secondary]",
+            "BudgetPlaceFundingSpecial AS [Special]",
+            "BudgetPlaceFundingAlternativeProvision AS [AlternativeProvision]"
         ];
 
         Type[] types =
@@ -120,44 +120,44 @@ public class HighNeedsService(IDatabaseFactory dbFactory) : HighNeedsStubService
         var laBuilder = new LocalAuthorityCurrentFinancialQuery(dimension, fields)
             .WhereLaCodesIn(codes);
 
-        var results = await conn.QueryAsync(laBuilder, types, Map, splitOn);
+        var results = await conn.QueryAsync(laBuilder, types, MultiMapToHighNeeds, splitOn);
         return results.ToArray();
+    }
 
-        LocalAuthority<Models.HighNeeds> Map(object[] objects)
+    internal static LocalAuthority<Models.HighNeeds> MultiMapToHighNeeds(object[] objects)
+    {
+        var localAuthority = objects[0] as LocalAuthorityBase;
+        var outturn = objects[1] as HighNeedsBase;
+        var outturnHighNeedsAmount = objects[2] as HighNeedsAmount;
+        var outturnTopFundingMaintained = objects[3] as TopFunding;
+        var outturnTopFundingNonMaintained = objects[4] as TopFunding;
+        var outturnPlaceFunding = objects[5] as PlaceFunding;
+        var budget = objects[6] as HighNeedsBase;
+        var budgetHighNeedsAmount = objects[7] as HighNeedsAmount;
+        var budgetTopFundingMaintained = objects[8] as TopFunding;
+        var budgetTopFundingNonMaintained = objects[9] as TopFunding;
+        var budgetPlaceFunding = objects[10] as PlaceFunding;
+
+        return new LocalAuthority<Models.HighNeeds>
         {
-            var localAuthority = objects[0] as LocalAuthorityBase;
-            var outturn = objects[1] as HighNeedsBase;
-            var outturnHighNeedsAmount = objects[2] as HighNeedsAmount;
-            var outturnTopFundingMaintained = objects[3] as TopFunding;
-            var outturnTopFundingNonMaintained = objects[4] as TopFunding;
-            var outturnPlaceFunding = objects[5] as PlaceFunding;
-            var budget = objects[6] as HighNeedsBase;
-            var budgetHighNeedsAmount = objects[7] as HighNeedsAmount;
-            var budgeTopFundingMaintained = objects[8] as TopFunding;
-            var budgetTopFundingNonMaintained = objects[9] as TopFunding;
-            var budgetPlaceFunding = objects[10] as PlaceFunding;
-
-            return new LocalAuthority<Models.HighNeeds>
+            Code = localAuthority?.Code,
+            Name = localAuthority?.Name,
+            Outturn = new Models.HighNeeds
             {
-                Code = localAuthority?.Code,
-                Name = localAuthority?.Name,
-                Outturn = new Models.HighNeeds
-                {
-                    Total = outturn?.Total,
-                    HighNeedsAmount = outturnHighNeedsAmount,
-                    Maintained = outturnTopFundingMaintained,
-                    NonMaintained = outturnTopFundingNonMaintained,
-                    PlaceFunding = outturnPlaceFunding
-                },
-                Budget = new Models.HighNeeds
-                {
-                    Total = budget?.Total,
-                    HighNeedsAmount = budgetHighNeedsAmount,
-                    Maintained = budgeTopFundingMaintained,
-                    NonMaintained = budgetTopFundingNonMaintained,
-                    PlaceFunding = budgetPlaceFunding
-                }
-            };
-        }
+                Total = outturn?.Total,
+                HighNeedsAmount = outturnHighNeedsAmount,
+                Maintained = outturnTopFundingMaintained,
+                NonMaintained = outturnTopFundingNonMaintained,
+                PlaceFunding = outturnPlaceFunding
+            },
+            Budget = new Models.HighNeeds
+            {
+                Total = budget?.Total,
+                HighNeedsAmount = budgetHighNeedsAmount,
+                Maintained = budgetTopFundingMaintained,
+                NonMaintained = budgetTopFundingNonMaintained,
+                PlaceFunding = budgetPlaceFunding
+            }
+        };
     }
 }
