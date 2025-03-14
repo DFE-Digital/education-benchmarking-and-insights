41273e5c4af8ec37a01568a68749992a0f2bd697
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-14 12:52:57 +0000

    chore: Moved `MultiMapToHighNeeds` function to its own `Mapper` class

--- platform/src/apis/Platform.Api.LocalAuthorityFinances/Features/HighNeeds/Services/HighNeedsService.cs
+++ platform/src/apis/Platform.Api.LocalAuthorityFinances/Features/HighNeeds/Services/HighNeedsService.cs
@@ -120,44 +120,7 @@ public class HighNeedsService(IDatabaseFactory dbFactory) : HighNeedsStubService
         var laBuilder = new LocalAuthorityCurrentFinancialQuery(dimension, fields)
             .WhereLaCodesIn(codes);
 
-        var results = await conn.QueryAsync(laBuilder, types, MultiMapToHighNeeds, splitOn);
+        var results = await conn.QueryAsync(laBuilder, types, Mapper.MultiMapToHighNeeds, splitOn);
         return results.ToArray();
-    }
-
-    internal static LocalAuthority<Models.HighNeeds> MultiMapToHighNeeds(object[] objects)
-    {
-        var localAuthority = objects[0] as LocalAuthorityBase;
-        var outturn = objects[1] as HighNeedsBase;
-        var outturnHighNeedsAmount = objects[2] as HighNeedsAmount;
-        var outturnTopFundingMaintained = objects[3] as TopFunding;
-        var outturnTopFundingNonMaintained = objects[4] as TopFunding;
-        var outturnPlaceFunding = objects[5] as PlaceFunding;
-        var budget = objects[6] as HighNeedsBase;
-        var budgetHighNeedsAmount = objects[7] as HighNeedsAmount;
-        var budgetTopFundingMaintained = objects[8] as TopFunding;
-        var budgetTopFundingNonMaintained = objects[9] as TopFunding;
-        var budgetPlaceFunding = objects[10] as PlaceFunding;
-
-        return new LocalAuthority<Models.HighNeeds>
-        {
-            Code = localAuthority?.Code,
-            Name = localAuthority?.Name,
-            Outturn = new Models.HighNeeds
-            {
-                Total = outturn?.Total,
-                HighNeedsAmount = outturnHighNeedsAmount,
-                Maintained = outturnTopFundingMaintained,
-                NonMaintained = outturnTopFundingNonMaintained,
-                PlaceFunding = outturnPlaceFunding
-            },
-            Budget = new Models.HighNeeds
-            {
-                Total = budget?.Total,
-                HighNeedsAmount = budgetHighNeedsAmount,
-                Maintained = budgetTopFundingMaintained,
-                NonMaintained = budgetTopFundingNonMaintained,
-                PlaceFunding = budgetPlaceFunding
-            }
-        };
     }
 }
---
+++ platform/tests/Platform.LocalAuthorityFinances.Tests/HighNeeds/WhenHighNeedsMapperMaps.cs
@@ -0,0 +1,59 @@
+﻿using AutoFixture;
+using Platform.Api.LocalAuthorityFinances.Features.HighNeeds;
+using Platform.Api.LocalAuthorityFinances.Features.HighNeeds.Models;
+using Xunit;
+
+namespace Platform.LocalAuthorityFinances.Tests.HighNeeds;
+
+public class WhenHighNeedsMapperMaps
+{
+    private readonly Fixture _fixture = new();
+
+    [Fact]
+    public void ShouldMapWhenMultiMapToHighNeeds()
+    {
+        // arrange
+        var localAuthority = _fixture.Create<LocalAuthorityBase>();
+        var outturn = _fixture.Create<HighNeedsBase>();
+        var outturnHighNeedsAmount = _fixture.Create<HighNeedsAmount>();
+        var outturnTopFundingMaintained = _fixture.Create<TopFunding>();
+        var outturnTopFundingNonMaintained = _fixture.Create<TopFunding>();
+        var outturnPlaceFunding = _fixture.Create<PlaceFunding>();
+        var budget = _fixture.Create<HighNeedsBase>();
+        var budgetHighNeedsAmount = _fixture.Create<HighNeedsAmount>();
+        var budgetTopFundingMaintained = _fixture.Create<TopFunding>();
+        var budgetTopFundingNonMaintained = _fixture.Create<TopFunding>();
+        var budgetPlaceFunding = _fixture.Create<PlaceFunding>();
+        object[] objects =
+        [
+            localAuthority,
+            outturn,
+            outturnHighNeedsAmount,
+            outturnTopFundingMaintained,
+            outturnTopFundingNonMaintained,
+            outturnPlaceFunding,
+            budget,
+            budgetHighNeedsAmount,
+            budgetTopFundingMaintained,
+            budgetTopFundingNonMaintained,
+            budgetPlaceFunding
+        ];
+
+        // act
+        var actual = Mapper.MultiMapToHighNeeds(objects);
+
+        // assert
+        Assert.Equal(localAuthority.Code, actual.Code);
+        Assert.Equal(localAuthority.Name, actual.Name);
+        Assert.Equal(outturn.Total, actual.Outturn?.Total);
+        Assert.Equal(outturnHighNeedsAmount, actual.Outturn?.HighNeedsAmount);
+        Assert.Equal(outturnTopFundingMaintained, actual.Outturn?.Maintained);
+        Assert.Equal(outturnTopFundingNonMaintained, actual.Outturn?.NonMaintained);
+        Assert.Equal(outturnPlaceFunding, actual.Outturn?.PlaceFunding);
+        Assert.Equal(budget.Total, actual.Budget?.Total);
+        Assert.Equal(budgetHighNeedsAmount, actual.Budget?.HighNeedsAmount);
+        Assert.Equal(budgetTopFundingMaintained, actual.Budget?.Maintained);
+        Assert.Equal(budgetTopFundingNonMaintained, actual.Budget?.NonMaintained);
+        Assert.Equal(budgetPlaceFunding, actual.Budget?.PlaceFunding);
+    }
+}
