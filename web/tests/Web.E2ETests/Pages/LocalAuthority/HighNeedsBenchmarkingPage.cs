ab01972e2daaa68a1ff9b61601a7e42df852efc7
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-19 10:16:26 +0000

    fix: Set correct `valueUnit` for SEND2 benchmarking charts

--- web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsBenchmarkingPage.cs
+++ web/tests/Web.E2ETests/Pages/LocalAuthority/HighNeedsBenchmarkingPage.cs
@@ -79,6 +79,6 @@ public class HighNeedsBenchmarkingPage(IPage page)
             });
         }
 
-        expected.CompareToDynamicSet(set);
+        expected.CompareToDynamicSet(set, false);
     }
 }
