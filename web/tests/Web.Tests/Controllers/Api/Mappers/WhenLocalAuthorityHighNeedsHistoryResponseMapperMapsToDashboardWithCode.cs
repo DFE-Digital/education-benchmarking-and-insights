ae6e011703704c678618a70cab0f81338af257f4
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-21 10:26:44 +0000

    fix: Exclude historical data from dashboard start/end years if they are missing budget or outturn

--- web/tests/Web.Tests/Controllers/Api/Mappers/WhenLocalAuthorityHighNeedsHistoryResponseMapperMapsToDashboardWithCode.cs
+++ web/tests/Web.Tests/Controllers/Api/Mappers/WhenLocalAuthorityHighNeedsHistoryResponseMapperMapsToDashboardWithCode.cs
@@ -56,18 +56,46 @@ public class WhenLocalAuthorityHighNeedsHistoryResponseMapperMapsToDashboardWith
             {
                 case 2021:
                     AssertFieldsMapped(Outturn2021, actual.Outturn);
+                    AssertFieldsMapped(Budget2021, actual.Budget);
                     break;
                 case 2022:
                     AssertFieldsMapped(null, actual.Outturn);
+                    AssertFieldsMapped(null, actual.Budget);
                     break;
                 case 2023:
                     AssertFieldsMapped(Outturn2023, actual.Outturn);
+                    AssertFieldsMapped(Budget2023, actual.Budget);
                     break;
                 default:
                     throw new IndexOutOfRangeException();
             }
+        }
+    }
 
-            AssertFieldsMapped(null, actual.Budget);
+    [Fact]
+    public void ShouldMapToLocalAuthorityHighNeedsHistoryResponsesWithPartialData()
+    {
+        var result = PartialHistory.MapToDashboardResponse(Code).ToArray();
+
+        Assert.Equal(EndYear - StartYear - 2, result.Length);
+
+        foreach (var actual in result)
+        {
+            Assert.NotNull(actual.Year);
+
+            switch (actual.Year)
+            {
+                case 2022:
+                    AssertFieldsMapped(Outturn2022, actual.Outturn);
+                    AssertFieldsMapped(Budget2022, actual.Budget);
+                    break;
+                case 2023:
+                    AssertFieldsMapped(Outturn2023, actual.Outturn);
+                    AssertFieldsMapped(Budget2023, actual.Budget);
+                    break;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
         }
     }
 
