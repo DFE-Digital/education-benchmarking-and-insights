4859e216704143444143a5927cb125b7e9208488
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-24 10:32:10 +0000

    feat: Provision `ChartRendering` function app

---
+++ platform/src/apis/Platform.Api.ChartRendering/Constants.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Platform.Api.ChartRendering;
+
+[ExcludeFromCodeCoverage]
+public static class Constants
+{
+    public const string ApplicationName = "chart-rendering-api";
+
+    public static class Features
+    {
+        public const string HealthCheck = "Health Check";
+    }
+}
