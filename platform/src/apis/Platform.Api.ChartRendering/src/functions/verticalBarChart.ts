851faa79e887f35b55f33ba116833157c810cdc2
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-25 15:31:14 +0000

    feat: Wired up sample spending and priorities chart data

--- platform/src/apis/Platform.Api.ChartRendering/src/functions/verticalBarChart.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/functions/verticalBarChart.ts
@@ -6,6 +6,7 @@ import {
 } from "@azure/functions";
 import * as d3 from "d3";
 import { JSDOM } from "jsdom";
+import classnames from "classnames";
 
 export async function verticalBarChart(
   request: HttpRequest,
@@ -13,43 +14,35 @@ export async function verticalBarChart(
 ): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
-  let data = (await request.json()) as {
-    letter: string;
-    frequency: number;
-  }[];
-
-  if (!data || !Array.isArray(data)) {
-    data = [
-      { letter: "A", frequency: 0.08167 },
-      { letter: "B", frequency: 0.01492 },
-      { letter: "C", frequency: 0.02782 },
-      { letter: "D", frequency: 0.04253 },
-      { letter: "E", frequency: 0.12702 },
-      { letter: "F", frequency: 0.02288 },
-      { letter: "G", frequency: 0.02015 },
-      { letter: "H", frequency: 0.06094 },
-      { letter: "I", frequency: 0.06966 },
-      { letter: "J", frequency: 0.00153 },
-      { letter: "K", frequency: 0.00772 },
-      { letter: "L", frequency: 0.04025 },
-      { letter: "M", frequency: 0.02406 },
-      { letter: "N", frequency: 0.06749 },
-      { letter: "O", frequency: 0.07507 },
-      { letter: "P", frequency: 0.01929 },
-      { letter: "Q", frequency: 0.00095 },
-      { letter: "R", frequency: 0.05987 },
-      { letter: "S", frequency: 0.06327 },
-      { letter: "T", frequency: 0.09056 },
-      { letter: "U", frequency: 0.02758 },
-      { letter: "V", frequency: 0.00978 },
-      { letter: "W", frequency: 0.0236 },
-      { letter: "X", frequency: 0.0015 },
-      { letter: "Y", frequency: 0.01974 },
-      { letter: "Z", frequency: 0.00074 },
-    ];
+  const payload = (await request.json()) as VerticalBarChartPayload;
+  if (!payload || !payload.data) {
+    // todo: more validation
+    return {
+      body: "Bad request",
+      status: 400,
+    };
   }
 
-  const chart = buildChart(data);
+  let chart: SVGSVGElement | undefined;
+  const { data, height, keyField, valueField, width, ...rest } = payload;
+
+  try {
+    chart = buildChart({
+      data,
+      height: height || 500,
+      keyField: keyField as never,
+      valueField: valueField as never,
+      width: width || 928,
+      ...rest,
+    });
+  } catch (e) {
+    context.error(e);
+
+    return {
+      body: e,
+      status: 500,
+    };
+  }
 
   return {
     body: chart?.outerHTML,
@@ -60,7 +53,15 @@ export async function verticalBarChart(
 }
 
 // https://observablehq.com/@d3/bar-chart/2
-function buildChart(data: { letter: string; frequency: number }[]) {
+function buildChart<T>({
+  data,
+  height,
+  highlightKey,
+  keyField,
+  sort,
+  valueField,
+  width,
+}: ChartOptions<T>) {
   const window = new JSDOM(`<html><head></head><body></body></html>`, {
     pretendToBeVisual: true,
   }).window;
@@ -68,8 +69,6 @@ function buildChart(data: { letter: string; frequency: number }[]) {
   window.d3 = d3.select(window.document); // get d3 into the dom
 
   // Declare the chart dimensions and margins.
-  const width = 928;
-  const height = 500;
   const marginTop = 30;
   const marginRight = 0;
   const marginBottom = 30;
@@ -79,23 +78,27 @@ function buildChart(data: { letter: string; frequency: number }[]) {
   const x = d3
     .scaleBand()
     .domain(
-      d3.groupSort(
-        data,
-        ([d]) => -d.frequency,
-        (d) => d.letter,
-      ),
-    ) // descending frequency
+      sort === undefined
+        ? data.map((d) => d[keyField] as string)
+        : d3.groupSort(
+            data,
+            ([d]) => (d[valueField] as number) * (sort === "asc" ? 1 : -1),
+            (d) => d[keyField] as string,
+          ),
+    )
     .range([marginLeft, width - marginRight])
-    .padding(0.1);
+    .padding(0.2);
 
   // Declare the y (vertical position) scale.
   const y = d3
     .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.frequency)])
+    .domain([0, d3.max(data, (d) => d[valueField] as number)])
     .range([height - marginBottom, marginTop]);
 
   // Create the SVG container.
-  const svg: d3.Selection<SVGSVGElement, undefined, null, undefined> = window.d3
+  const svg = (
+    window.d3 as d3.Selection<SVGSVGElement, unknown, null, undefined>
+  )
     .select("body")
     .append("svg")
     .attr("width", width)
@@ -106,35 +109,18 @@ function buildChart(data: { letter: string; frequency: number }[]) {
   // Add a rect for each bar.
   svg
     .append("g")
-    .attr("fill", "steelblue")
     .selectAll()
     .data(data)
     .join("rect")
-    .attr("x", (d) => x(d.letter))
-    .attr("y", (d) => y(d.frequency))
-    .attr("height", (d) => y(0) - y(d.frequency))
-    .attr("width", x.bandwidth());
-
-  // Add the x-axis and label.
-  svg
-    .append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0));
-
-  // Add the y-axis and label, and remove the domain line.
-  svg
-    .append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y).tickFormat((y) => ((y as number) * 100).toFixed()))
-    .call((g) => g.select(".domain").remove())
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", -marginLeft)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start")
-        .text("↑ Frequency (%)"),
+    .attr("x", (d) => x(d[keyField] as string))
+    .attr("y", (d) => y(d[valueField] as number))
+    .attr("height", (d) => y(0) - y(d[valueField] as number))
+    .attr("width", x.bandwidth())
+    .attr("data-i", (_, i) => i)
+    .attr("class", (d) =>
+      classnames("chart-cell", "chart-cell-series-0", {
+        "chart-cell__highlight": d[keyField] === highlightKey,
+      }),
     );
 
   // Return the SVG element.
@@ -146,3 +132,22 @@ app.http("verticalBarChart", {
   authLevel: "anonymous",
   handler: verticalBarChart,
 });
+
+interface VerticalBarChartPayload
+  extends Pick<ChartOptions<unknown>, "data">,
+    Partial<
+      Pick<ChartOptions<unknown>, "height" | "highlightKey" | "sort" | "width">
+    > {
+  keyField: string;
+  valueField: string;
+}
+
+interface ChartOptions<T> {
+  data: T[];
+  height: number;
+  highlightKey?: string | undefined;
+  keyField: keyof T;
+  sort?: "asc" | "desc" | undefined;
+  valueField: keyof T;
+  width: number;
+}
