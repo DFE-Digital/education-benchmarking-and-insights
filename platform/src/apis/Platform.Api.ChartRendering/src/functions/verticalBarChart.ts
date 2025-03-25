392e81f4feb9dc386a20b62a3c2ada8b3bda4533
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-25 12:15:33 +0000

    feat: Sample vertical bar chart rendered with d3 via jsdom

--- platform/src/apis/Platform.Api.ChartRendering/src/functions/verticalBarChart.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/functions/verticalBarChart.ts
@@ -4,6 +4,8 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
+import * as d3 from "d3";
+import { JSDOM } from "jsdom";
 
 export async function verticalBarChart(
   request: HttpRequest,
@@ -11,15 +13,132 @@ export async function verticalBarChart(
 ): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
-  // const posted = (await request.json()) || null;
-  const element = "<svg />";
+  let data = (await request.json()) as {
+    letter: string;
+    frequency: number;
+  }[];
+
+  if (!data || !Array.isArray(data)) {
+    data = [
+      { letter: "A", frequency: 0.08167 },
+      { letter: "B", frequency: 0.01492 },
+      { letter: "C", frequency: 0.02782 },
+      { letter: "D", frequency: 0.04253 },
+      { letter: "E", frequency: 0.12702 },
+      { letter: "F", frequency: 0.02288 },
+      { letter: "G", frequency: 0.02015 },
+      { letter: "H", frequency: 0.06094 },
+      { letter: "I", frequency: 0.06966 },
+      { letter: "J", frequency: 0.00153 },
+      { letter: "K", frequency: 0.00772 },
+      { letter: "L", frequency: 0.04025 },
+      { letter: "M", frequency: 0.02406 },
+      { letter: "N", frequency: 0.06749 },
+      { letter: "O", frequency: 0.07507 },
+      { letter: "P", frequency: 0.01929 },
+      { letter: "Q", frequency: 0.00095 },
+      { letter: "R", frequency: 0.05987 },
+      { letter: "S", frequency: 0.06327 },
+      { letter: "T", frequency: 0.09056 },
+      { letter: "U", frequency: 0.02758 },
+      { letter: "V", frequency: 0.00978 },
+      { letter: "W", frequency: 0.0236 },
+      { letter: "X", frequency: 0.0015 },
+      { letter: "Y", frequency: 0.01974 },
+      { letter: "Z", frequency: 0.00074 },
+    ];
+  }
+
+  const chart = buildChart(data);
 
   return {
-    body: element,
+    body: chart?.outerHTML,
     headers: {
       "Content-Type": "text/html",
     },
   };
+}
+
+// https://observablehq.com/@d3/bar-chart/2
+function buildChart(data: { letter: string; frequency: number }[]) {
+  const window = new JSDOM(`<html><head></head><body></body></html>`, {
+    pretendToBeVisual: true,
+  }).window;
+
+  window.d3 = d3.select(window.document); // get d3 into the dom
+
+  // Declare the chart dimensions and margins.
+  const width = 928;
+  const height = 500;
+  const marginTop = 30;
+  const marginRight = 0;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  // Declare the x (horizontal position) scale.
+  const x = d3
+    .scaleBand()
+    .domain(
+      d3.groupSort(
+        data,
+        ([d]) => -d.frequency,
+        (d) => d.letter,
+      ),
+    ) // descending frequency
+    .range([marginLeft, width - marginRight])
+    .padding(0.1);
+
+  // Declare the y (vertical position) scale.
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.frequency)])
+    .range([height - marginBottom, marginTop]);
+
+  // Create the SVG container.
+  const svg: d3.Selection<SVGSVGElement, undefined, null, undefined> = window.d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("style", "max-width: 100%; height: auto;");
+
+  // Add a rect for each bar.
+  svg
+    .append("g")
+    .attr("fill", "steelblue")
+    .selectAll()
+    .data(data)
+    .join("rect")
+    .attr("x", (d) => x(d.letter))
+    .attr("y", (d) => y(d.frequency))
+    .attr("height", (d) => y(0) - y(d.frequency))
+    .attr("width", x.bandwidth());
+
+  // Add the x-axis and label.
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+
+  // Add the y-axis and label, and remove the domain line.
+  svg
+    .append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y).tickFormat((y) => ((y as number) * 100).toFixed()))
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g
+        .append("text")
+        .attr("x", -marginLeft)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Frequency (%)"),
+    );
+
+  // Return the SVG element.
+  return svg.node();
 }
 
 app.http("verticalBarChart", {
