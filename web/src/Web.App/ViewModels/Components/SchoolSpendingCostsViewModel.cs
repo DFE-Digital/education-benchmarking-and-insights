8146b22265e9c1abcdb407b60e1ba4b370b5bfb1
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-26 15:02:13 +0000

    feat: Switched out single SSR chart resolution with multiple charts per request (albeit with one request per priority level)

---
+++ web/src/Web.App/ViewModels/Components/SchoolSpendingCostsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Web.App.Domain;
+
+namespace Web.App.ViewModels.Components;
+
+public class SchoolSpendingCostsViewModel(
+    string? id,
+    string urn,
+    bool isPartOfTrust,
+    bool isCustomData,
+    bool hasIncompleteData,
+    List<SchoolSpendingCostsViewModelCostCategory> categories)
+{
+    public List<SchoolSpendingCostsViewModelCostCategory> Costs => categories;
+    public string? Id => id;
+    public string? Urn => urn;
+    public bool HasIncompleteData => hasIncompleteData;
+    public bool IsCustomData => isCustomData;
+    public bool IsPartOfTrust => isPartOfTrust;
+    public CostCodes CostCodes => new(IsPartOfTrust);
+}
+
+public class SchoolSpendingCostsViewModelCostCategory
+{
+    public string? Uuid { get; set; }
+    public CostCategory? Category { get; set; }
+    public string? ChartSvg { get; set; }
+    public bool HasNegativeOrZeroValues { get; set; }
+}
