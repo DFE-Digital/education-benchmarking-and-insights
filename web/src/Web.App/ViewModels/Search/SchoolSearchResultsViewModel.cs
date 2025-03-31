d97f555e1826fe4d55df58599869d6ff1d34f69a
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-31 14:31:24 +0100

    feat: Populating search results view

---
+++ web/src/Web.App/ViewModels/Search/SchoolSearchResultsViewModel.cs
@@ -0,0 +1,15 @@
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+namespace Web.App.ViewModels.Search;
+
+public class SchoolSearchResultsViewModel : SchoolSearchViewModel
+{
+    public int TotalResults { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public Dictionary<string, IList<SearchResultFacetViewModel>> Facets { get; set; } = new();
+
+    public SearchResultFacetViewModel[] OverallPhaseFacets => Facets
+        .Where(f => f.Key == "OverallPhase")
+        .SelectMany(f => f.Value)
+        .ToArray();
+}
