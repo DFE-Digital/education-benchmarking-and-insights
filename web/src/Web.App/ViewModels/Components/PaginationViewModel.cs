e32ff8b387d448c44cbc5001186b540d147cc8a2
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-03-31 11:32:10 +0100

    feat: Pagination view component

---
+++ web/src/Web.App/ViewModels/Components/PaginationViewModel.cs
@@ -0,0 +1,37 @@
+using System.Text.RegularExpressions;
+
+namespace Web.App.ViewModels.Components;
+
+public class PaginationViewModel(int totalResults, int pageNumber, int pageSize, string pageQuery, string path, string query)
+{
+    public bool Visible => totalResults > pageSize;
+    public int CurrentPage => pageNumber;
+    public int FirstPage => 1;
+    public int LastPage => Convert.ToInt32(Math.Ceiling(totalResults * 1m / pageSize * 1m));
+    public int[] MidPages
+    {
+        get
+        {
+            var pages = new List<int>();
+            for (var i = CurrentPage - 1; i < CurrentPage + 2; i++)
+            {
+                if (i > FirstPage && i < LastPage)
+                {
+                    pages.Add(i);
+                }
+            }
+
+            return pages.ToArray();
+        }
+    }
+    public bool SkipBeforeMidPages => MidPages.FirstOrDefault(FirstPage) > FirstPage + 1;
+    public bool SkipAfterMidPages => MidPages.LastOrDefault(LastPage) < LastPage - 1;
+    public bool HasPreviousPage => CurrentPage > FirstPage;
+    public bool HasNextPage => CurrentPage * pageSize < totalResults;
+
+    public string BuildPageUrl(int page)
+    {
+        var q = Regex.Replace(query, "&{0,1}" + pageQuery + "=\\d+", string.Empty).Replace("?&", "?").TrimEnd('?');
+        return $"{path}{q}{(q.Length <= 1 ? "?" : "&")}{pageQuery}={page}";
+    }
+}
