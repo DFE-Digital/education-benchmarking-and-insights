9473e960ec90b098200baf40c258eb32354ef4ce
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-02 12:52:00 +0100

    feat: Shared JSDOM from worker instead of within builder

--- platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
@@ -2,16 +2,21 @@ import { workerData, parentPort } from "worker_threads";
 import { v4 as uuidv4 } from "uuid";
 import VerticalBarChartBuilder from "../builders/verticalBarChartBuilder.js";
 import { ChartDefinition } from "../functions";
+import { JSDOM } from "jsdom";
 
 const builder = new VerticalBarChartBuilder();
 const definitions = workerData.definitions as ChartDefinition[];
+const jsDom = new JSDOM(`<html><head></head><body></body></html>`, {
+  pretendToBeVisual: true,
+});
 
-const charts = definitions.map(
+const buildChartPromises = definitions.map(
   ({ data, height, id, keyField, valueField, width, ...rest }) =>
     builder.buildChart({
       data,
       height: height || 500,
       id: id || uuidv4(),
+      jsDom,
       keyField: keyField as never,
       valueField: valueField as never,
       width: width || 928,
@@ -19,4 +24,6 @@ const charts = definitions.map(
     }),
 );
 
-parentPort.postMessage(charts);
+Promise.all(buildChartPromises).then((charts) => {
+  parentPort.postMessage(charts);
+});
