f4c9ec1203a80de2ba3c85d2a1c264b200ff95da
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-02 14:40:09 +0100

    feat: New worker per chart per request

--- platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
@@ -1,27 +1,20 @@
 import { v4 as uuidv4 } from "uuid";
 import VerticalBarChartBuilder from "../builders/verticalBarChartBuilder.js";
 import { ChartDefinition } from "../functions";
-import { JSDOM } from "jsdom";
 
 const builder = new VerticalBarChartBuilder();
-const jsDom = new JSDOM(`<html><head></head><body></body></html>`, {
-  pretendToBeVisual: true,
-});
-
-export default function ({ definitions }: { definitions: ChartDefinition[] }) {
-  const buildChartPromises = definitions.map(
-    ({ data, height, id, keyField, valueField, width, ...rest }) =>
-      builder.buildChart({
-        data,
-        height: height || 500,
-        id: id || uuidv4(),
-        jsDom,
-        keyField: keyField as never,
-        valueField: valueField as never,
-        width: width || 928,
-        ...rest,
-      }),
-  );
-
-  return Promise.all(buildChartPromises);
+export default function ({
+  definition: { data, height, id, keyField, valueField, width, ...rest },
+}: {
+  definition: ChartDefinition;
+}) {
+  return builder.buildChart({
+    data,
+    height: height || 500,
+    id: id || uuidv4(),
+    keyField: keyField as never,
+    valueField: valueField as never,
+    width: width || 928,
+    ...rest,
+  });
 }
