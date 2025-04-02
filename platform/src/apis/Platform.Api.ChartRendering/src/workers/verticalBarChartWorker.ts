9664645d9e699dc95568366ca3fb0201be693004
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-02 13:59:34 +0100

    feat: Worker pool using Piscina

--- platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
@@ -1,29 +1,27 @@
-import { workerData, parentPort } from "worker_threads";
 import { v4 as uuidv4 } from "uuid";
 import VerticalBarChartBuilder from "../builders/verticalBarChartBuilder.js";
 import { ChartDefinition } from "../functions";
 import { JSDOM } from "jsdom";
 
 const builder = new VerticalBarChartBuilder();
-const definitions = workerData.definitions as ChartDefinition[];
 const jsDom = new JSDOM(`<html><head></head><body></body></html>`, {
   pretendToBeVisual: true,
 });
 
-const buildChartPromises = definitions.map(
-  ({ data, height, id, keyField, valueField, width, ...rest }) =>
-    builder.buildChart({
-      data,
-      height: height || 500,
-      id: id || uuidv4(),
-      jsDom,
-      keyField: keyField as never,
-      valueField: valueField as never,
-      width: width || 928,
-      ...rest,
-    }),
-);
+export default function ({ definitions }: { definitions: ChartDefinition[] }) {
+  const buildChartPromises = definitions.map(
+    ({ data, height, id, keyField, valueField, width, ...rest }) =>
+      builder.buildChart({
+        data,
+        height: height || 500,
+        id: id || uuidv4(),
+        jsDom,
+        keyField: keyField as never,
+        valueField: valueField as never,
+        width: width || 928,
+        ...rest,
+      }),
+  );
 
-Promise.all(buildChartPromises).then((charts) => {
-  parentPort.postMessage(charts);
-});
+  return Promise.all(buildChartPromises);
+}
