b2c1431d127a2036b1e8a716e875b78a2fb8a37d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-02 15:34:36 +0100

    feat: Added App Insights dependency tracking with Piscina `runTime`

--- platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
@@ -1,8 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 import VerticalBarChartBuilder from "../builders/verticalBarChartBuilder.js";
 import { ChartDefinition } from "../functions";
+import { JSDOM } from "jsdom";
 
 const builder = new VerticalBarChartBuilder();
+const jsDom = new JSDOM(`<html><head></head><body></body></html>`, {
+  pretendToBeVisual: true,
+});
+
 export default function ({
   definition: { data, height, id, keyField, valueField, width, ...rest },
 }: {
@@ -12,6 +17,7 @@ export default function ({
     data,
     height: height || 500,
     id: id || uuidv4(),
+    jsDom,
     keyField: keyField as never,
     valueField: valueField as never,
     width: width || 928,
