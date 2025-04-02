4e56c3f1e4305f57ef9e23b5db9e3c8492c58e3a
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-02 12:24:03 +0100

    feat: Implemented `worker_threads` for incoming chart requests

---
+++ platform/src/apis/Platform.Api.ChartRendering/src/workers/verticalBarChartWorker.ts
@@ -0,0 +1,22 @@
+import { workerData, parentPort } from "worker_threads";
+import { v4 as uuidv4 } from "uuid";
+import VerticalBarChartBuilder from "../builders/verticalBarChartBuilder.js";
+import { ChartDefinition } from "../functions";
+
+const builder = new VerticalBarChartBuilder();
+const definitions = workerData.definitions as ChartDefinition[];
+
+const charts = definitions.map(
+  ({ data, height, id, keyField, valueField, width, ...rest }) =>
+    builder.buildChart({
+      data,
+      height: height || 500,
+      id: id || uuidv4(),
+      keyField: keyField as never,
+      valueField: valueField as never,
+      width: width || 928,
+      ...rest,
+    }),
+);
+
+parentPort.postMessage(charts);
