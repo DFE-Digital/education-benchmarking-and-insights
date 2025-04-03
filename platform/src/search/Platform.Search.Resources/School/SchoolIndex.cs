10398b483e0cc7bf02a57c470029c1e3297f0180
Josh Langford <josh.langford@hippodigital.co.uk>
2025-04-03 12:10:24 +0100

    feat: updates to `SchoolIndex` to support faceted school search

--- platform/src/search/Platform.Search.Resources/School/SchoolIndex.cs
+++ platform/src/search/Platform.Search.Resources/School/SchoolIndex.cs
@@ -4,27 +4,30 @@ namespace Platform.Search.Resources.School;
 
 public class SchoolIndex
 {
-    [SimpleField(IsKey = true, IsFilterable = true, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsKey = true, IsFilterable = true, IsSortable = false, IsFacetable = false)]
     public string? URN { get; set; }
 
-    [SimpleField(IsFilterable = true, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = false)]
     public string? SchoolName { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressStreet { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressLocality { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressLine3 { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressTown { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressCounty { get; set; }
 
-    [SimpleField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
+    [SearchableField(IsFilterable = false, IsSortable = false, IsFacetable = false)]
     public string? AddressPostcode { get; set; }
+    
+    [SimpleField(IsFilterable = true, IsSortable = false, IsFacetable = true)]
+    public string? OverallPhase { get; set; }
 }
