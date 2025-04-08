1067f977bcfbe1b46b35680fa149fac2221f8dce
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-08 07:16:02 +0100

    feat: Included total pupils and education phase in results listing

--- web/src/Web.App/ViewModels/Search/SchoolSearchResultViewModel.cs
+++ web/src/Web.App/ViewModels/Search/SchoolSearchResultViewModel.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Web.App.Domain;
+
 // ReSharper disable MemberCanBePrivate.Global
 
 namespace Web.App.ViewModels.Search;
@@ -13,11 +15,35 @@ public record SchoolSearchResultViewModel
     public string? AddressTown { get; init; }
     public string? AddressCounty { get; init; }
     public string? AddressPostcode { get; init; }
+    public string? Address { get; init; }
+    public string? OverallPhase { get; init; }
     public int? PeriodCoveredByReturn { get; init; }
     public double? TotalPupils { get; init; }
 
     public static SchoolSearchResultViewModel Create(SchoolSummary school)
     {
+        var components = new[]
+        {
+            school.AddressStreet?.Replace(school.SchoolName!, string.Empty).TrimStart(',').Trim(),
+            school.AddressLocality?.Replace(school.SchoolName!, string.Empty).TrimStart(',').Trim(),
+            school.AddressLine3?.Replace(school.SchoolName!, string.Empty).TrimStart(',').Trim(),
+            school.AddressTown,
+            school.AddressCounty,
+            school.AddressPostcode
+        }.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        var address = new StringBuilder(components.First());
+        for (var i = 1; i < components.Length; i++)
+        {
+            var component = components.ElementAt(i);
+            if (component == components.ElementAt(i - 1))
+            {
+                continue;
+            }
+
+            address.Append(", ");
+            address.Append(component);
+        }
+
         return new SchoolSearchResultViewModel
         {
             URN = school.URN,
@@ -28,6 +54,8 @@ public record SchoolSearchResultViewModel
             AddressTown = school.AddressTown,
             AddressCounty = school.AddressCounty,
             AddressPostcode = school.AddressPostcode,
+            Address = address.ToString(),
+            OverallPhase = school.OverallPhase,
             PeriodCoveredByReturn = school.PeriodCoveredByReturn,
             TotalPupils = school.TotalPupils
         };
--- web/tests/Web.Tests/ViewModels/Search/GivenASchoolSearchResultViewModel.cs
+++ web/tests/Web.Tests/ViewModels/Search/GivenASchoolSearchResultViewModel.cs
@@ -12,7 +12,14 @@ public class GivenASchoolSearchResultViewModel
     [Fact]
     public void WhenCreateShouldMapFacetsIfSupplied()
     {
-        var school = _fixture.Create<SchoolSummary>();
+        var addressStreet = _fixture.Create<string>();
+        var school = _fixture
+            .Build<SchoolSummary>()
+            .With(s => s.SchoolName, "SchoolName")
+            .With(s => s.AddressStreet, $"SchoolName, {addressStreet}")
+            .With(s => s.AddressLocality, "AddressLocality")
+            .With(s => s.AddressLine3, "AddressLocality")
+            .Create();
 
         var actual = SchoolSearchResultViewModel.Create(school);
 
@@ -24,6 +31,8 @@ public class GivenASchoolSearchResultViewModel
         Assert.Equal(school.AddressTown, actual.AddressTown);
         Assert.Equal(school.AddressCounty, actual.AddressCounty);
         Assert.Equal(school.AddressPostcode, actual.AddressPostcode);
+        Assert.Equal($"{addressStreet}, {school.AddressLocality}, {school.AddressTown}, {school.AddressCounty}, {school.AddressPostcode}", actual.Address);
+        Assert.Equal(school.OverallPhase, actual.OverallPhase);
         Assert.Equal(school.PeriodCoveredByReturn, actual.PeriodCoveredByReturn);
         Assert.Equal(school.TotalPupils, actual.TotalPupils);
     }
