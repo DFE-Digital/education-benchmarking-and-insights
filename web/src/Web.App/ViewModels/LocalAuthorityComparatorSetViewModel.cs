f23fe989a3f02873cd8477c5111aaafe0818172e
Josh Langford <josh.langford@hippodigital.co.uk>
2025-04-10 07:43:47 +0100

    chore: remove unused view model

--- web/src/Web.App/ViewModels/LocalAuthorityComparatorSetViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Web.App.Domain;
-namespace Web.App.ViewModels;
-
-public class LocalAuthorityComparatorSetViewModel(LocalAuthority localAuthority, string[] strings)
-{
-    public string? Code => localAuthority.Code;
-    public string? Name => localAuthority.Name;
-
-}
