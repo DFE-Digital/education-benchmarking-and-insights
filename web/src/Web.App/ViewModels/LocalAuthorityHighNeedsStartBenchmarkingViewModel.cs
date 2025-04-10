c3992e215f099759bdd97a5836a486f010747171
Josh Langford <josh.langford@hippodigital.co.uk>
2025-04-10 07:43:32 +0100

    feat: wire up high needs benchmarking to use referrer

--- web/src/Web.App/ViewModels/LocalAuthorityHighNeedsStartBenchmarkingViewModel.cs
+++ web/src/Web.App/ViewModels/LocalAuthorityHighNeedsStartBenchmarkingViewModel.cs
@@ -2,10 +2,11 @@ using Web.App.Domain;
 
 namespace Web.App.ViewModels;
 
-public class LocalAuthorityHighNeedsStartBenchmarkingViewModel(LocalAuthorityStatisticalNeighbours localAuthority, string[] comparators)
+public class LocalAuthorityHighNeedsStartBenchmarkingViewModel(LocalAuthorityStatisticalNeighbours localAuthority, string[] comparators, string? referrer)
 {
     public string? Code => localAuthority.Code;
     public string? Name => localAuthority.Name;
+    public string? Referrer => referrer;
 
     public string[] StatisticalNeighbours => localAuthority.StatisticalNeighbours?
         .OrderBy(n => n.Position)
