e4583957946bc879b65bd54cd46136766a21abc9
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-04-25 10:37:21 +0100

    feat: Added SFB host name to permitted forwarded header values

--- web/src/Web.App/Program.cs
+++ web/src/Web.App/Program.cs
@@ -101,7 +101,8 @@ if (!builder.Environment.IsIntegration())
         options.AllowedHosts = new List<string>
         {
             "*.azurefd.net",
-            "financial-benchmarking-and-insights-tool.education.gov.uk"
+            "financial-benchmarking-and-insights-tool.education.gov.uk",
+            "schools-financial-benchmarking.service.gov.uk"
         };
     });
 
