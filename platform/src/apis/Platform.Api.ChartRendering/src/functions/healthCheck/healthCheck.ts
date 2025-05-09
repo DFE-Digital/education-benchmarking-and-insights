310e6a9d3cc12b09a39f8ffe9327ec1bdd90f68c
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-05-09 09:09:01 +0100

    feat: Added initial unit tests to Chart Rendering API

--- platform/src/apis/Platform.Api.ChartRendering/src/functions/healthCheck/healthCheck.ts
+++ platform/src/apis/Platform.Api.ChartRendering/src/functions/healthCheck/healthCheck.ts
@@ -1,11 +1,5 @@
-import { app, HttpResponseInit } from "@azure/functions";
+import { HttpResponseInit } from "@azure/functions";
 
 export async function healthCheck(): Promise<HttpResponseInit> {
   return { body: "Healthy" };
 }
-
-app.http("health", {
-  methods: ["GET"],
-  authLevel: "anonymous",
-  handler: healthCheck,
-});
