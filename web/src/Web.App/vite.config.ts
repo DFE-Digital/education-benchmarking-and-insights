4a1a4d0162ea45fff6e189eda661ff00a3cfc23b
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-05-20 12:15:50 +0100

    feat: Stripped back Vue scaffolding

--- web/src/Web.App/vite.config.ts
+++ web/src/Web.App/vite.config.ts
@@ -1,9 +1,11 @@
+import { dirname, resolve } from 'node:path'
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +14,25 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./ts", import.meta.url)),
     },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'ts/src/index.ts'),
+      name: 'main',
+      // the proper extensions will be added
+      fileName: 'main',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+    outDir: "dist/vite"
   },
 });
