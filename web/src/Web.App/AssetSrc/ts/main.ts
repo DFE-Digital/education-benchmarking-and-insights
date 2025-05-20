55c5e59c24f533cf4d183e52f7bab8de8051f29d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-05-20 14:37:08 +0100

    chore: Removed sample Vue components
    This commit may be referred back to when implementing real components

--- web/src/Web.App/AssetSrc/ts/main.ts
+++ web/src/Web.App/AssetSrc/ts/main.ts
@@ -1,6 +1,6 @@
+import { createApp } from "vue";
 import { ChartActions } from "./components/chart-actions/ChartActions.ts";
 import { Suggester } from "./components/suggester/Suggester.ts";
-import HelloWorld from "./components/HelloWorld.vue";
-import { createApp } from "vue";
 
-export { ChartActions, createApp, HelloWorld, Suggester };
+export { createApp };
+export { ChartActions, Suggester };
