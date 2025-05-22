b7fe9376ace3a21e9af4dbf58ae69b8ddb5faa03
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-05-22 06:34:39 +0100

    feat: Modal validation error

--- web/src/Web.App/AssetSrc/ts/components/page-actions/ElementSelector.vue
+++ web/src/Web.App/AssetSrc/ts/components/page-actions/ElementSelector.vue
@@ -12,8 +12,19 @@ onMounted(() => {
 });
 </script>
 
-<!-- todo: validation -->
 <template>
+  <div v-if="showValidationError" className="govuk-error-summary" data-module="govuk-error-summary">
+    <div role="alert">
+      <h2 className="govuk-error-summary__title">There is a problem</h2>
+      <div className="govuk-error-summary__body">
+        <ul className="govuk-list govuk-error-summary__list">
+          <li>
+            <a href="#elements-0">Select one or more items</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
   <div class="govuk-form-group">
     <fieldset className="govuk-fieldset">
       <div className="govuk-checkboxes govuk-checkboxes--elements" data-module="govuk-checkboxes">
