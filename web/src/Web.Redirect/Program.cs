5f386f78c489694cae89524ffcaa0885321f8b05
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-02 13:42:06 +0100

    chore: Log error if Redirect app service endpoint hit, instead of web.config redirect

--- web/src/Web.Redirect/Program.cs
+++ web/src/Web.Redirect/Program.cs
@@ -1,6 +1,6 @@
-// ReSharper disable ConvertToLambdaExpression
 // ReSharper disable RouteTemplates.RouteParameterIsNotPassedToMethod
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 app.UseRouting();
@@ -8,9 +8,8 @@ app.UseRouting();
 #pragma warning disable ASP0018
 app.MapGet("/{*pageRoute:nonfile}", () =>
 {
-    // web.config rewrite rules should intercept request before this point,
-    // but included as a 'catch all' route here as a fail-safe.
-    return Results.Redirect("https://financial-benchmarking-and-insights-tool.education.gov.uk/");
+    // web.config rewrite rules should intercept request before this catch-all endpoint
+    throw new ApplicationException("Unable to redirect to new service");
 });
 #pragma warning restore ASP0018
 
