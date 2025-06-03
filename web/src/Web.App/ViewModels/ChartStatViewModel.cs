110aa9c2c4bf8f2753b540d2ed3c4a1e2e91337e
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-03 09:17:04 +0100

    feat: Moved 'chart stat' to partial view and model

---
+++ web/src/Web.App/ViewModels/ChartStatViewModel.cs
@@ -0,0 +1,10 @@
+namespace Web.App.ViewModels;
+
+public class ChartStatViewModel
+{
+    public string? Label { get; init; }
+    public string? Value { get; init; }
+    public string? ValueSuffix { get; init; }
+    public string? Suffix { get; init; }
+    public bool? Highlight { get; init; }
+}
