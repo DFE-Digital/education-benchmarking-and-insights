1d8256ef6193cebcb15b46db646b7e445cbe5445
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-10 09:43:10 +0100

    feat: Added empty `Platform.Api.Content` function app and test project

---
+++ platform/src/apis/Platform.Api.Content/Constants.cs
@@ -0,0 +1,11 @@
+namespace Platform.Api.Content;
+
+public static class Constants
+{
+    public const string ApplicationName = "content-api";
+
+    public static class Features
+    {
+        public const string HealthCheck = "Health Check";
+    }
+}
