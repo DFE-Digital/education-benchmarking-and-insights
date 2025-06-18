d4bc9e6080fdcf23d03162890923b447cc1d3aac
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-18 12:54:53 +0100

    feat: Added `CarriedForwardBalance` value to Key Information card on High Needs dashboard

--- web/src/Web.App/ViewComponents/LocalAuthorityHighNeedsHeadlinesViewComponent.cs
+++ web/src/Web.App/ViewComponents/LocalAuthorityHighNeedsHeadlinesViewComponent.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Web.App.Domain.LocalAuthorities;
 using Web.App.Domain.NonFinancial;
 using Web.App.Infrastructure.Apis;
+using Web.App.Infrastructure.Apis.LocalAuthorities;
 using Web.App.Infrastructure.Apis.NonFinancial;
 using Web.App.Infrastructure.Extensions;
 using Web.App.ViewModels.Components;
@@ -9,6 +11,7 @@ namespace Web.App.ViewComponents;
 
 public class LocalAuthorityHighNeedsHeadlinesViewComponent(
     IEducationHealthCarePlansApi educationHealthCarePlansApi,
+    ILocalAuthoritiesApi localAuthoritiesApi,
     ILogger<LocalAuthorityHighNeedsHeadlinesViewComponent> logger) : ViewComponent
 {
     public async Task<IViewComponentResult> InvokeAsync(string identifier, string commentary, CancellationToken cancellationToken = default)
@@ -19,13 +22,18 @@ public class LocalAuthorityHighNeedsHeadlinesViewComponent(
                 .GetResultOrDefault<LocalAuthorityNumberOfPlans[]>() ?? [])
             .FirstOrDefault();
 
-        if (plans != null)
+        var highNeeds = (await localAuthoritiesApi
+            .GetHighNeeds(query, cancellationToken)
+            .GetResultOrDefault<LocalAuthority<HighNeeds>[]>() ?? [])
+            .FirstOrDefault();
+
+        if (plans != null && highNeeds != null)
         {
-            return View(new LocalAuthorityHighNeedsHeadlinesViewModel(plans, commentary));
+            return View(new LocalAuthorityHighNeedsHeadlinesViewModel(plans, highNeeds, commentary));
         }
 
-        logger.LogWarning("Local authority high needs headlines could not be displayed for {Code}", identifier);
-        return View("MissingData", "Headlines");
+        logger.LogWarning("Local authority high needs key information could not be displayed for {Code}", identifier);
+        return View("MissingData", "Key information");
     }
 
     private static ApiQuery BuildQuery(string code, string dimension)
--- web/src/Web.App/ViewModels/Components/LocalAuthorityHighNeedsHeadlinesViewModel.cs
+++ web/src/Web.App/ViewModels/Components/LocalAuthorityHighNeedsHeadlinesViewModel.cs
@@ -1,9 +1,11 @@
+using Web.App.Domain.LocalAuthorities;
 using Web.App.Domain.NonFinancial;
 
 namespace Web.App.ViewModels.Components;
 
-public class LocalAuthorityHighNeedsHeadlinesViewModel(LocalAuthorityNumberOfPlans plans, string? commentary)
+public class LocalAuthorityHighNeedsHeadlinesViewModel(LocalAuthorityNumberOfPlans plans, LocalAuthority<HighNeeds> highNeeds, string? commentary)
 {
     public decimal? TotalPlans => plans.Total;
+    public decimal? CarriedForwardBalance => highNeeds.CarriedForwardBalance;
     public string? Commentary => commentary;
 }
