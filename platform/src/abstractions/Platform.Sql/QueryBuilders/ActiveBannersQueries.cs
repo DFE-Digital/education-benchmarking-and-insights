73dadf709f10d568af358a00b604707e91b60d0c
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-26 17:11:13 +0100

    feat: Unit test coverage over `api/banner/{target}` endpoint and related dependencies

--- platform/src/abstractions/Platform.Sql/QueryBuilders/ActiveBannersQueries.cs
+++ platform/src/abstractions/Platform.Sql/QueryBuilders/ActiveBannersQueries.cs
@@ -1,6 +1,6 @@
 namespace Platform.Sql.QueryBuilders;
 
-public class BannersQuery() : PlatformQuery(Sql)
+public class ActiveBannersQuery() : PlatformQuery(Sql)
 {
     // see below for commentary on OPENJSON with CROSS APPLY when extracting from JSON arrays:
     // https://learn.microsoft.com/en-us/sql/t-sql/functions/openjson-transact-sql#example-3---join-rows-with-json-data-stored-in-table-cells-using-cross-apply
