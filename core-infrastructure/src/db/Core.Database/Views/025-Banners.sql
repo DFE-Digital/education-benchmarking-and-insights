faef14d8cbcf9e29fbf578957803eab23526e80f
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-26 09:03:24 +0100

    feat: New `Banner` table definition and 'active' rows view

---
+++ core-infrastructure/src/db/Core.Database/Views/025-Banners.sql
@@ -0,0 +1,10 @@
+DROP VIEW IF EXISTS VW_ActiveBanners
+GO
+
+CREATE VIEW VW_ActiveBanners
+AS
+  SELECT [Title], [Body], [Target]
+  FROM [dbo].[Banner]
+  WHERE [ValidFrom] < GETUTCDATE()
+    AND ([ValidTo] IS NULL OR [ValidTo] > GETUTCDATE())
+GO
