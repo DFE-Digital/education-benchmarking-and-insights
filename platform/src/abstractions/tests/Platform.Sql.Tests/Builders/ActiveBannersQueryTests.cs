a004e55f65b594ac4553185aadfd3c2dc02b6758
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-06-26 17:11:13 +0100

    feat: Unit test coverage over `api/banner/{target}` endpoint and related dependencies

---
+++ platform/src/abstractions/tests/Platform.Sql.Tests/Builders/ActiveBannersQueryTests.cs
@@ -0,0 +1,16 @@
+﻿using Platform.Sql.QueryBuilders;
+using Xunit;
+
+namespace Platform.Sql.Tests.Builders;
+
+public class ActiveBannersQueryTests
+{
+    [Fact]
+    public void ShouldReturnSql()
+    {
+        var builder = Create();
+        Assert.Equal("SELECT * FROM VW_ActiveBanners CROSS APPLY OPENJSON([Target], '$')  ", builder.QueryTemplate.RawSql);
+    }
+
+    private static ActiveBannersQuery Create() => new();
+}
---
+++ platform/tests/Platform.Content.Tests/Banners/Services/WhenBannersServiceQueriesAsync.cs
@@ -0,0 +1,53 @@
+﻿using AutoFixture;
+using Moq;
+using Platform.Api.Content.Features.Banners.Models;
+using Platform.Api.Content.Features.Banners.Services;
+using Platform.Sql;
+using Platform.Sql.QueryBuilders;
+using Xunit;
+
+namespace Platform.Content.Tests.Banners.Services;
+
+public class WhenBannersServiceQueriesAsync
+{
+    private readonly Mock<IDatabaseConnection> _connection;
+    private readonly Fixture _fixture = new();
+    private readonly BannersService _service;
+
+    public WhenBannersServiceQueriesAsync()
+    {
+        _connection = new Mock<IDatabaseConnection>();
+
+        var dbFactory = new Mock<IDatabaseFactory>();
+        dbFactory.Setup(d => d.GetConnection()).ReturnsAsync(_connection.Object);
+
+        _service = new BannersService(dbFactory.Object);
+    }
+
+    [Fact]
+    public async Task ShouldQueryAsync()
+    {
+        const string target = nameof(target);
+        var result = _fixture.Create<Banner>();
+        string? actualSql = null;
+        Dictionary<string, object>? actualParams = null;
+
+        _connection
+            .Setup(c => c.QueryFirstOrDefaultAsync<Banner>(It.IsAny<PlatformQuery>(), It.IsAny<CancellationToken>()))
+            .Callback<PlatformQuery, CancellationToken>((query, _) =>
+            {
+                actualSql = query.QueryTemplate.RawSql.Trim();
+                actualParams = query.QueryTemplate.Parameters?.GetTemplateParameters("Value");
+            })
+            .ReturnsAsync(result);
+
+        var actual = await _service.GetBannerOrDefault(target, CancellationToken.None);
+
+        Assert.Equal(result, actual);
+        Assert.Equal("SELECT * FROM VW_ActiveBanners CROSS APPLY OPENJSON([Target], '$') WHERE Value = @Value\n ORDER BY ValidFrom DESC", actualSql);
+        Assert.Equal(new Dictionary<string, object>
+        {
+            { "Value", target }
+        }, actualParams);
+    }
+}
