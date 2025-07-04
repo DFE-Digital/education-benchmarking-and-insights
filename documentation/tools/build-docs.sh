8df9931a658e515160364882e33839426ce9f583
Josh Langford <josh.langford@hippodigital.co.uk>
2025-07-04 07:49:02 +0100

    fix: disable Mermaid CLI sandbox in documentation pipeline
    In Ubuntu 24.04, AppArmor restrictions prevent Chromium from launching with its default sandbox, causing Mermaid CLI to fail when rendering diagrams. This change disables the sandbox via Puppeteer config. Diagrams are trusted and the environment is ephemeral.

--- documentation/tools/build-docs.sh
+++ documentation/tools/build-docs.sh
@@ -25,7 +25,15 @@ do
         mkdir -p work/$dir
     fi;
     echo $(pwd)/$fname
-    mmdc -e png -i $(pwd)/$fname -o work/$fname
+
+    # NOTE:
+    # In Ubuntu 24.04+ AppArmor restricts unprivileged user namespaces.
+    # This breaks Chromium's internal sandbox used by Mermaid CLI (via Puppeteer).
+    # To work around this we pass a Puppeteer config that disables the sandbox.
+    # For our use case diagrams are trusted and the environment is ephemeral.
+    # See: https://github.com/mermaid-js/mermaid-cli/blob/340561040b6b0621a486e3fc96723139e5718268/docs/linux-sandbox-issue.md
+    # https://github.com/mermaid-js/mermaid-cli/issues/730
+    mmdc -e png -i $(pwd)/$fname -o work/$fname -p "$SCRIPT_DIR/puppeteer-config.json"
 done 
 
 cp -a images/. work/images
