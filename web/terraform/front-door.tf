a2be20b34e45c7f3d5297fcf9d1fdf9aa101274f
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-07-08 14:15:05 +0100

    feat: Create new AFD WAF policy based on environment-specific SKU configuration

--- web/terraform/front-door.tf
+++ web/terraform/front-door.tf
@@ -1,5 +1,7 @@
 locals {
-  host_name = (lower(var.environment) == "production" ? azurerm_cdn_frontdoor_custom_domain.web-app-custom-domain[0].host_name : azurerm_cdn_frontdoor_endpoint.web-app-front-door-endpoint.host_name)
+  host_name = (lower(var.environment) == "production" ?
+    azurerm_cdn_frontdoor_custom_domain.web-app-custom-domain[0].host_name :
+  azurerm_cdn_frontdoor_endpoint.web-app-front-door-endpoint.host_name)
   custom-domain-ids = (lower(var.environment) == "production" ? [
     azurerm_cdn_frontdoor_custom_domain.web-app-custom-domain[0].id,
   ] : [])
@@ -10,7 +12,7 @@ resource "azurerm_cdn_frontdoor_profile" "web-app-front-door-profile" {
   resource_group_name = azurerm_resource_group.resource-group.name
   tags                = local.common-tags
 
-  sku_name = var.configuration[var.environment].front_door_sku_name
+  sku_name = var.configuration[var.environment].front_door_profile_sku_name
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "web-app-front-door-origin-group" {
@@ -126,7 +128,8 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "web-app-front-door-waf-policy"
   }
 
   dynamic "managed_rule" {
-    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ? ["apply"] : [])
+    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ?
+    ["apply"] : [])
     content {
       type    = "DefaultRuleSet"
       version = "1.0"
@@ -135,7 +138,85 @@ resource "azurerm_cdn_frontdoor_firewall_policy" "web-app-front-door-waf-policy"
   }
 
   dynamic "managed_rule" {
-    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ? ["apply"] : [])
+    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ?
+    ["apply"] : [])
+    content {
+      type    = "Microsoft_BotManagerRuleSet"
+      version = "1.0"
+      action  = "Log"
+    }
+  }
+}
+
+resource "azurerm_cdn_frontdoor_firewall_policy" "web-app-front-door-waf" {
+  name                = "${var.environment-prefix}waf"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  enabled             = true
+  tags                = local.common-tags
+
+  sku_name = var.configuration[var.environment].front_door_waf_policy_sku_name
+  mode     = var.configuration[var.environment].waf_mode
+
+  custom_rule {
+    name     = "blockrequestmethod"
+    action   = "Block"
+    priority = 100
+    type     = "MatchRule"
+
+    match_condition {
+      match_variable     = "RequestMethod"
+      operator           = "Equal"
+      negation_condition = true
+      match_values       = ["GET", "POST"]
+    }
+  }
+
+  custom_rule {
+    name     = "blockgeolocation"
+    action   = "Block"
+    priority = 200
+    type     = "MatchRule"
+
+    match_condition {
+      match_variable     = "SocketAddr"
+      operator           = "GeoMatch"
+      negation_condition = true
+      match_values       = ["GB"]
+    }
+  }
+
+  dynamic "custom_rule" {
+    for_each = var.environment == "test" ? ["apply"] : []
+
+    content {
+      name     = "allowsynthetictraffic"
+      action   = "Allow"
+      priority = 50
+      type     = "MatchRule"
+
+      match_condition {
+        match_variable     = "RequestHeader"
+        selector           = "x-synthetic-source"
+        operator           = "Equal"
+        negation_condition = false
+        match_values       = ["load-tests"]
+      }
+    }
+  }
+
+  dynamic "managed_rule" {
+    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ?
+    ["apply"] : [])
+    content {
+      type    = "DefaultRuleSet"
+      version = "1.0"
+      action  = "Block"
+    }
+  }
+
+  dynamic "managed_rule" {
+    for_each = (azurerm_cdn_frontdoor_profile.web-app-front-door-profile.sku_name == "Premium_AzureFrontDoor" ?
+    ["apply"] : [])
     content {
       type    = "Microsoft_BotManagerRuleSet"
       version = "1.0"
--- web/terraform/variables.tf
+++ web/terraform/variables.tf
@@ -5,12 +5,13 @@ variable "location" {}
 
 variable "configuration" {
   type = map(object({
-    sku_name               = string
-    zone_balancing_enabled = bool
-    worker_count           = number
-    front_door_sku_name    = string
-    waf_mode               = string
-    redirect_app_service   = bool
+    sku_name                       = string
+    zone_balancing_enabled         = bool
+    worker_count                   = number
+    front_door_profile_sku_name    = string
+    front_door_waf_policy_sku_name = string
+    waf_mode                       = string
+    redirect_app_service           = bool
     features = object({
       CurriculumFinancialPlanning          = bool
       CustomData                           = bool
@@ -44,12 +45,13 @@ variable "configuration" {
   }))
   default = {
     development = {
-      sku_name               = "B1"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Detection"
-      redirect_app_service   = false
+      sku_name                       = "B1"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Standard_AzureFrontDoor"
+      waf_mode                       = "Detection"
+      redirect_app_service           = false
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
@@ -82,12 +84,13 @@ variable "configuration" {
       }
     }
     automated-test = {
-      sku_name               = "B1"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Detection"
-      redirect_app_service   = false
+      sku_name                       = "B1"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Standard_AzureFrontDoor"
+      waf_mode                       = "Detection"
+      redirect_app_service           = false
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
@@ -120,12 +123,13 @@ variable "configuration" {
       }
     }
     test = {
-      sku_name               = "P0v3"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Prevention"
-      redirect_app_service   = false
+      sku_name                       = "P0v3"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Standard_AzureFrontDoor"
+      waf_mode                       = "Prevention"
+      redirect_app_service           = false
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
@@ -158,12 +162,13 @@ variable "configuration" {
       }
     }
     feature = {
-      sku_name               = "B1"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Detection"
-      redirect_app_service   = true
+      sku_name                       = "B1"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Standard_AzureFrontDoor"
+      waf_mode                       = "Detection"
+      redirect_app_service           = true
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
@@ -196,12 +201,13 @@ variable "configuration" {
       }
     }
     pre-production = {
-      sku_name               = "P0v3"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Prevention"
-      redirect_app_service   = false
+      sku_name                       = "P0v3"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Standard_AzureFrontDoor"
+      waf_mode                       = "Prevention"
+      redirect_app_service           = false
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
@@ -234,12 +240,13 @@ variable "configuration" {
       }
     }
     production = {
-      sku_name               = "P1v3"
-      zone_balancing_enabled = false
-      worker_count           = 1
-      front_door_sku_name    = "Standard_AzureFrontDoor"
-      waf_mode               = "Prevention"
-      redirect_app_service   = true
+      sku_name                       = "P1v3"
+      zone_balancing_enabled         = false
+      worker_count                   = 1
+      front_door_profile_sku_name    = "Standard_AzureFrontDoor"
+      front_door_waf_policy_sku_name = "Premium_AzureFrontDoor"
+      waf_mode                       = "Prevention"
+      redirect_app_service           = true
       features = {
         Trusts                               = true
         CurriculumFinancialPlanning          = true
