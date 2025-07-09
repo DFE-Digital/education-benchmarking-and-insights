5d32edee420d8b1efad27cf289c8c861e53cfbdc
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-07-09 12:01:22 +0100

    chore: Moved markdown configuration to its own file

--- shutter/src/gulpfile.js
+++ shutter/src/gulpfile.js
@@ -3,55 +3,20 @@ import { deleteAsync } from "del";
 import nunjucksRender from "gulp-nunjucks-render";
 import beautify from "gulp-beautify";
 import copy from "gulp-copy";
-import { use as useMarkdownExtension, parse as parseMarkdown } from "marked";
-import DOMPurify from "isomorphic-dompurify";
 import replace from "gulp-replace";
+import { options } from "./markdown.js";
+
+const assetsPath = process.env.ASSETS_PATH ?? "/assets";
+const markdownContent = process.env.MARKDOWN_CONTENT ?? undefined;
 const output = "dist";
 
 function clean() {
   return deleteAsync([output]);
 }
 
-const assetsPath = process.env.ASSETS_PATH ?? "/assets";
-const manageEnv = function (environment) {
-  environment.addGlobal("govukRebrand", false);
-  environment.addGlobal("assetPath", assetsPath);
-};
-
-// see https://github.com/markedjs/marked/blob/master/src/Renderer.ts for overridden functions
-const renderer = {
-  link({ href, tokens }) {
-    const text = this.parser.parseInline(tokens);
-    const cleanHref = encodeURI(href);
-    if (cleanHref === null) {
-      return text;
-    }
-    href = cleanHref;
-    return `<a class="govuk-link" href="${href}">${text}</a>`;
-  },
-  paragraph({ tokens }) {
-    return `<p class="govuk-body">${this.parser.parseInline(tokens)}</p>\n`;
-  },
-};
-
-useMarkdownExtension({ renderer });
-
-const data = {
-  markdown_content: process.env.MARKDOWN_CONTENT
-    ? DOMPurify.sanitize(parseMarkdown(process.env.MARKDOWN_CONTENT))
-    : undefined,
-};
-
 function html() {
   return src("views/*.+(njk)")
-    .pipe(
-      nunjucksRender({
-        path: ["node_modules/govuk-frontend/dist", "views"],
-        ext: ".html",
-        data,
-        manageEnv,
-      })
-    )
+    .pipe(nunjucksRender(options(markdownContent, assetsPath)))
     .pipe(beautify.html({ indent_size: 4, preserve_newlines: false }))
     .pipe(dest(output));
 }
@@ -75,7 +40,9 @@ function stylesheets() {
     .pipe(
       copy(`${output}/assets/styles`, {
         prefix: 4,
-      }).pipe(replace("url(/assets", `url(../../${assetsPath}`))
+      })
+        // update hard-coded asset paths in stylesheet when running in local dev mode
+        .pipe(replace("url(/assets", `url(../../${assetsPath}`))
     )
     .pipe(dest(`${output}/assets/styles`));
 }
