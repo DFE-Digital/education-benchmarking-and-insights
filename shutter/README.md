67e8232f61826b108fe5fafbeadfa579fe0b9fe1
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-07-09 12:01:22 +0100

    chore: Moved markdown configuration to its own file

--- shutter/README.md
+++ shutter/README.md
@@ -16,7 +16,13 @@ This project contains Nunjucks templates that may be used to build and deploy a 
 2. `npm i`
 3. `npm run build`
 
-Build output will be published to the `src/dist` folder, ready for deployment as a static website. This `build` script may be used by CI pipelines.
+Build output will be published to the `src/dist` folder by Gulp, ready for deployment as a static website. This `build` script may be used by CI pipelines, with the following environment variable(s) having been defined:
+
+| Variable           | Purpose                        | Default         |
+|--------------------|--------------------------------|-----------------|
+| `MARKDOWN_CONTENT` | Content to display on the page | Try again later |
+
+The Gulp script will parse as markdown and sanitize the content of the `MARKDOWN_CONTENT` before pushing into a Nunjucks variable.
 
 ### Local development
 
---
+++ shutter/src/markdown.js
@@ -0,0 +1,38 @@
+/**
+ * @import {RendererObject} from "marked"
+ */
+import { use, parse,  } from "marked";
+import DOMPurify from "isomorphic-dompurify";
+
+export const options = (markdown, assetsPath) => ({
+  path: ["node_modules/govuk-frontend/dist", "views"],
+  ext: ".html",
+  data: {
+    markdown_content: markdown
+      ? DOMPurify.sanitize(parse(markdown))
+      : undefined,
+  },
+  manageEnv: (environment) => {
+    environment.addGlobal("govukRebrand", false);
+    environment.addGlobal("assetPath", assetsPath);
+  },
+});
+
+// see https://github.com/markedjs/marked/blob/master/src/Renderer.ts for overridden functions
+/** @type {RendererObject} */
+const renderer = {
+  link({ href, tokens }) {
+    const text = this.parser.parseInline(tokens);
+    const cleanHref = encodeURI(href);
+    if (cleanHref === null) {
+      return text;
+    }
+    href = cleanHref;
+    return `<a class="govuk-link" href="${href}">${text}</a>`;
+  },
+  paragraph({ tokens }) {
+    return `<p class="govuk-body">${this.parser.parseInline(tokens)}</p>\n`;
+  },
+};
+
+use({ renderer });
