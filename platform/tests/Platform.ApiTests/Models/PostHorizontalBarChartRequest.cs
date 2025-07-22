0c57f261b1bd46fddf795c27d604774c4ee6fde2
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-07-22 10:18:40 +0100

    feat: Horizontal bar chart API tests

---
+++ platform/tests/Platform.ApiTests/Models/PostHorizontalBarChartRequest.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace Platform.ApiTests.Models;
+
+public record PostHorizontalBarChartRequest<T> : ChartRequest<T>
+{
+    public int? BarHeight { get; set; }
+    public string? LabelField { get; set; }
+    public string? LabelFormat { get; set; }
+    public string? LinkFormat { get; set; }
+    public string? ValueFormat { get; set; }
+    public string? XAxisLabel { get; set; }
+}
+
+public class PostHorizontalBarChartsRequest<T>(IEnumerable<PostHorizontalBarChartRequest<T>> collection) : List<PostHorizontalBarChartRequest<T>>(collection);
