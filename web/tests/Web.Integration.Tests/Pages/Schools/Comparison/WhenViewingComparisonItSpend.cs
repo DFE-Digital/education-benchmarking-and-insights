598664f882d4b4b4544961c83990bc59b894025d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-07-25 14:57:24 +0100

    feat: Integration test coverage over SSR charts on IT spend page

--- web/tests/Web.Integration.Tests/Pages/Schools/Comparison/WhenViewingComparisonItSpend.cs
+++ web/tests/Web.Integration.Tests/Pages/Schools/Comparison/WhenViewingComparisonItSpend.cs
@@ -2,6 +2,7 @@
 using AngleSharp.Html.Dom;
 using AutoFixture;
 using Web.App.Domain;
+using Web.App.Domain.Charts;
 using Xunit;
 
 namespace Web.Integration.Tests.Pages.Schools.Comparison;
@@ -14,6 +15,14 @@ public class WhenViewingComparisonItSpend(SchoolBenchmarkingWebAppClient client)
         var (page, school) = await SetupNavigateInitPage(EstablishmentTypes.Maintained);
 
         AssertPageLayout(page, school);
+    }
+
+    [Fact]
+    public async Task CanDisplayChartWarningForMaintainedSchoolWhenChartApiFails()
+    {
+        var (page, school) = await SetupNavigateInitPage(EstablishmentTypes.Maintained, true);
+
+        AssertPageLayout(page, school, true);
     }
 
     [Fact]
@@ -61,33 +70,41 @@ public class WhenViewingComparisonItSpend(SchoolBenchmarkingWebAppClient client)
             HttpStatusCode.InternalServerError);
     }
 
-    private async Task<(IHtmlDocument page, School school)> SetupNavigateInitPage(string financeType)
+    private async Task<(IHtmlDocument page, School school)> SetupNavigateInitPage(string financeType, bool chartApiException = false)
     {
         var school = Fixture.Build<School>()
             .With(x => x.URN, "123456")
             .With(x => x.FinanceType, financeType)
             .Create();
 
-        const int comparators = 3;
         var comparatorSet = Fixture.Build<SchoolComparatorSet>()
-            .With(c => c.Pupil, Fixture.CreateMany<string>(comparators).ToArray())
-            .With(c => c.Building, Fixture.CreateMany<string>(comparators).ToArray())
+            .With(c => c.Pupil, Fixture.CreateMany<string>().ToArray())
+            .Without(c => c.Building)
             .Create();
-        var characteristics = Fixture.Build<SchoolCharacteristic>()
-            .CreateMany(comparators)
+
+        var spend = Fixture.Build<SchoolItSpend>()
+            .CreateMany()
             .ToArray();
-        characteristics.First().URN = school.URN;
+        spend.ElementAt(0).URN = school.URN;
 
-        var page = await Client
+        var horizontalBarChart = new ChartResponse { Html = "<svg />" };
+
+        var client = Client
             .SetupEstablishment(school)
-            .SetupSchoolInsight(characteristics)
             .SetupComparatorSet(school, comparatorSet)
-            .Navigate(Paths.SchoolComparisonItSpend(school.URN));
+            .SetupItSpend(spend)
+            .SetupChartRendering<SchoolComparisonDatum>(horizontalBarChart);
 
+        if (chartApiException)
+        {
+            Client.SetupChartRenderingWithException<SchoolComparisonDatum>();
+        }
+
+        var page = await client.Navigate(Paths.SchoolComparisonItSpend(school.URN));
         return (page, school);
     }
 
-    private static void AssertPageLayout(IHtmlDocument page, School school)
+    private static void AssertPageLayout(IHtmlDocument page, School school, bool chartError = false)
     {
         var expectedBreadcrumbs = new[]
         {
@@ -99,5 +116,57 @@ public class WhenViewingComparisonItSpend(SchoolBenchmarkingWebAppClient client)
         DocumentAssert.Breadcrumbs(page, expectedBreadcrumbs);
         DocumentAssert.TitleAndH1(page, "Benchmark your IT spending - Financial Benchmarking and Insights Tool - GOV.UK",
             "Benchmark your IT spending");
+
+        var subCategorySections = page.QuerySelectorAll("section");
+        Assert.Equal(7, subCategorySections.Length);
+
+        for (var i = 0; i < subCategorySections.Length; i++)
+        {
+            var section = subCategorySections.ElementAt(i);
+            var sectionHeading = section.QuerySelector("h2")?.TextContent;
+            Assert.NotNull(sectionHeading);
+
+            switch (i)
+            {
+                case 0:
+                    Assert.Equal("Administration software and systems E20D", sectionHeading);
+                    break;
+                case 1:
+                    Assert.Equal("Connectivity E20A", sectionHeading);
+                    break;
+                case 2:
+                    Assert.Equal("IT learning resources E20C", sectionHeading);
+                    break;
+                case 3:
+                    Assert.Equal("IT support E20G", sectionHeading);
+                    break;
+                case 4:
+                    Assert.Equal("Laptops, desktops and tablets E20E ", sectionHeading);
+                    break;
+                case 5:
+                    Assert.Equal("Onsite servers E20B", sectionHeading);
+                    break;
+                case 6:
+                    Assert.Equal("Other hardware E20F", sectionHeading);
+                    break;
+            }
+
+            var chartSvg = section.QuerySelector(".ssr-chart");
+            var chartWarning = section.QuerySelector(".ssr-chart-warning");
+            var chartContainer = section.QuerySelector(".composed-container");
+
+            if (chartError)
+            {
+                Assert.NotNull(chartWarning);
+                Assert.Null(chartSvg);
+            }
+            else
+            {
+                Assert.NotNull(chartSvg);
+                Assert.Null(chartWarning);
+            }
+
+            Assert.Null(chartContainer);
+        }
     }
 }
