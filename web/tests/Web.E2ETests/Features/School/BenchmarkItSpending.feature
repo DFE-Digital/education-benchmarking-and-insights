512b89b56c0ca7c3b95f711f0aaaf10236814eb1
Josh Langford <josh.langford@hippodigital.co.uk>
2025-08-04 11:24:38 +0100

    chore: add e2e tests for benchmark IT spending page

---
+++ web/tests/Web.E2ETests/Features/School/BenchmarkItSpending.feature
@@ -0,0 +1,23 @@
+﻿Feature: School benchmark IT spending
+    
+    Scenario: Before interacting with filters all charts should be displayed
+        Given I am on it spend page for school with URN '777042'
+        Then I should see the following IT spend charts:
+          | Chart Title                              |
+          | Administration software and systems E20D |
+          | Connectivity E20A                        |
+          | IT learning resources E20C               |
+          | IT support E20G                          |
+          | Laptops, desktops and tablets E20E       |
+          | Onsite servers E20B                      |
+          | Other hardware E20F                      |
+
+    Scenario: Clicking school name in chart directs to homepage
+        Given I am on it spend page for school with URN '777042'
+        When I click on the school name on the chart
+        Then I am navigated to selected school home page
+        
+    Scenario: Pressing Enter on school name in chart directs to homepage
+        Given I am on it spend page for school with URN '777042'
+        When I enter on the school name on the chart
+        Then I am navigated to selected school home page
---
+++ web/tests/Web.E2ETests/Pages/School/BenchmarkItSpendingPage.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Playwright;
+using Xunit;
+
+namespace Web.E2ETests.Pages.School;
+
+public class BenchmarkItSpendPage(IPage page)
+{
+    private ILocator PageH1Heading => page.Locator(Selectors.H1);
+    private ILocator ChartContainers => page.Locator(Selectors.SsrChartContainer);
+    private ILocator ChartContainer(string chartName) => page.Locator($"[data-title=\"{chartName}\"]");
+    private ILocator SchoolLinksInCharts => page.Locator(Selectors.SsrSchoolNamesLinksInCharts);
+    private ILocator ComparatorSetDetails =>
+        page.Locator(Selectors.GovLink,
+            new PageLocatorOptions
+            {
+                HasText = "We've chosen 2 sets of similar schools"
+            });
+
+    public async Task IsDisplayed()
+    {
+        await PageH1Heading.ShouldBeVisible();
+        await ComparatorSetDetails.ShouldBeVisible();
+    }
+
+    public async Task<HomePage> ClickOnSchoolName()
+    {
+        await SchoolLinksInCharts.First.Click();
+        return new HomePage(page);
+    }
+
+    public async Task<HomePage> EnterOnSchoolName()
+    {
+        await SchoolLinksInCharts.First.FocusAsync();
+        await page.Keyboard.PressAsync("Enter");
+
+        return new HomePage(page);
+    }
+
+    public async Task AssertChartsVisible(IEnumerable<string> expectedTitles)
+    {
+        var titles = expectedTitles.ToArray();
+
+        await AssertChartCount(titles.Length);
+        await AssertVisibleCharts(titles);
+    }
+
+    private async Task AssertVisibleCharts(IEnumerable<string> expectedTitles)
+    {
+        foreach (var title in expectedTitles)
+        {
+            await ChartContainer(title).ShouldBeVisible();
+        }
+    }
+
+    private async Task AssertChartCount(int expectedCount)
+    {
+        var count = await ChartContainers.CountAsync();
+        Assert.Equal(expectedCount, count);
+    }
+}
