6a24fcdf6bcc532b587e156ccbfee4f4e306ae81
Josh Langford <josh.langford@hippodigital.co.uk>
2025-08-08 08:56:26 +0100

    chore: update platform api tests for SSR horizontal bar chart to assert whole response from expected test data files
    - add new methods to TestDataHelpers.TestDataProvider to read xml/svg and json objects and updates existing method name

--- platform/tests/Platform.ApiTests/TestDataHelpers/TestDataProvider.cs
+++ platform/tests/Platform.ApiTests/TestDataHelpers/TestDataProvider.cs
@@ -1,11 +1,12 @@
 using System.Reflection;
+using System.Xml.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace Platform.ApiTests.TestDataHelpers;
 
 public static class TestDataProvider
 {
-    public static JArray GetArrayData(string fileName)
+    public static JArray GetJsonArrayData(string fileName)
     {
         var assembly = Assembly.GetExecutingAssembly();
         var resourceName = $"Platform.ApiTests.Data.{fileName}";
@@ -18,5 +19,32 @@ public static class TestDataProvider
         var jsonString = reader.ReadToEnd();
 
         return JArray.Parse(jsonString);
+    }
+
+    public static JObject GetJsonObjectData(string fileName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"Platform.ApiTests.Data.{fileName}";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
+            throw new InvalidOperationException($"Embedded resource '{resourceName}' not found.");
+
+        using var reader = new StreamReader(stream);
+        var jsonString = reader.ReadToEnd();
+
+        return JObject.Parse(jsonString);
+    }
+
+    public static XDocument GetXmlData(string fileName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"Platform.ApiTests.Data.{fileName}";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
+            throw new InvalidOperationException($"Embedded resource '{resourceName}' not found.");
+
+        return XDocument.Load(stream);
     }
 }
