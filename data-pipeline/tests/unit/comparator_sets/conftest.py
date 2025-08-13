a66b81a5041f20eccbac5444984bc34fc4bbc98e
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2025-08-13 15:01:28 +0100

    removed unneeded fixture

--- data-pipeline/tests/unit/comparator_sets/conftest.py
+++ data-pipeline/tests/unit/comparator_sets/conftest.py
@@ -42,11 +42,3 @@ def select_top_set_urns_defaults() -> dict:
         "final_set_size": 3,
         "include": np.array([True] * 26),  # [True, True…]
     }
-
-
-@pytest.fixture(autouse=True)
-def mock_env_user(monkeypatch):
-    monkeypatch.setenv(
-        "STORAGE_CONNECTION_STRING",
-        "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;",
-    )
