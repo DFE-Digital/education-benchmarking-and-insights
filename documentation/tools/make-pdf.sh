eca46f956c63bd130a9b8ca9f44dc86ff0344650
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2025-08-15 16:12:43 +0100

    Test CICD environment works for pdf generation enhancements

--- documentation/tools/make-pdf.sh
+++ documentation/tools/make-pdf.sh
@@ -1,2 +1,30 @@
+#!/bin/bash
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-${SCRIPT_DIR}/build-docs.sh ${1} "${2}.pdf" ${3:-build} --strip-comments --toc -f markdown -t pdf --pdf-engine=/Library/TeX/texbin/pdflatex --wrap=preserve --data-dir=${SCRIPT_DIR} --template=pdf-template
+
+INPUT_FILE=${1}
+OUTPUT_FILE="${2}.pdf"
+BUILD_DIR=${3:-build}
+
+pandoc "${INPUT_FILE}" \
+  -o "${OUTPUT_FILE}" \
+  --from=markdown+pipe_tables+grid_tables+multiline_tables \
+  --to=pdf \
+  --pdf-engine=xelatex \
+  --variable=geometry:margin=2.5cm \
+  --variable=fontsize=10pt \
+  --variable=documentclass=article \
+  --variable=classoption=onecolumn \
+  --table-of-contents \
+  --toc-depth=3 \
+  --highlight-style=tango \
+  --strip-comments \
+  --standalone \
+  --filter pandoc-crossref \
+  --citeproc \
+  -V colorlinks=true \
+  -V linkcolor=blue \
+  -V urlcolor=blue \
+  -V toccolor=gray \
+  --data-dir="${SCRIPT_DIR}" \
+  --template=pdf-template
