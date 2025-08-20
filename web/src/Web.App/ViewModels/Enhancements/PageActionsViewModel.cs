792f273fffdfa0bee5e3b5f8ee8ee0f24bb4b354
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-08-20 11:56:09 +0100

    feat: Added 'Save chart images' button to filter panel for chart view only

--- web/src/Web.App/ViewModels/Enhancements/PageActionsViewModel.cs
+++ web/src/Web.App/ViewModels/Enhancements/PageActionsViewModel.cs
@@ -2,11 +2,12 @@ namespace Web.App.ViewModels.Enhancements;
 
 public record PageActionsViewModel
 {
+    public bool? All { get; init; }
+    public string? CostCodesAttr { get; init; }
     public string? ElementId { get; init; }
     public string? SaveClassName { get; init; }
     public string SaveEventId { get; init; } = "save-chart-as-image";
     public string? SaveFileName { get; init; }
     public string? SaveTitleAttr { get; init; }
-    public string? CostCodesAttr { get; init; }
-    public string? WaitForEventType { get; init; }
+    public bool? StartImmediately { get; init; }
 }
