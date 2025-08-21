7741777454e2507c45bb556a2f873685181ec342
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-08-21 13:45:02 +0100

    chore: Added checkov exclusions for `CKV_AZURE_33`

--- core-infrastructure/terraform/databases.tf
+++ core-infrastructure/terraform/databases.tf
@@ -207,6 +207,7 @@ resource "azurerm_storage_account" "sql-log-storage" {
   #checkov:skip=CKV_AZURE_59:See ADO backlog AB#206389
   #checkov:skip=CKV2_AZURE_50:potential false positive https://github.com/bridgecrewio/checkov/issues/6388
   #checkov:skip=CKV2_AZURE_40:See ADO backlog AB#222562:Managed identity is not supported by `threat_detection_policy`, so SAS must be used instead
+  #checkov:skip=CKV_AZURE_33:False positive on queue logging due to new azurerm_storage_account_queue_properties resource (https://github.com/bridgecrewio/checkov/issues/7174)
   name                            = "${var.environment-prefix}sqllog"
   location                        = azurerm_resource_group.resource-group.location
   resource_group_name             = azurerm_resource_group.resource-group.name
