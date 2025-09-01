9dbb9ab93fe00251e113acdcdc135bbda94508f5
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-01 14:31:29 +0100

    feat: Added `news` endpoint to Content API

--- platform/src/abstractions/tests/Platform.Sql.Tests/Builders/PublishedNewsQueryTests.cs
+++ platform/src/abstractions/tests/Platform.Sql.Tests/Builders/PublishedNewsQueryTests.cs
@@ -5,15 +5,22 @@ namespace Platform.Sql.Tests.Builders;
 
 public class PublishedNewsQueryTests
 {
-    [Fact]
-    public void ShouldReturnSql()
+    [Theory]
+    [InlineData(new string[]
     {
-        var builder = Create();
-        Assert.Equal("SELECT * FROM VW_PublishedNews  ", builder.QueryTemplate.RawSql);
+    }, "SELECT * FROM VW_PublishedNews")]
+    [InlineData(new[]
+    {
+        "Title"
+    }, "SELECT Title\n FROM VW_PublishedNews")]
+    public void ShouldReturnSql(string[] fields, string expected)
+    {
+        var builder = Create(fields);
+        Assert.Equal(expected, builder.QueryTemplate.RawSql.Trim());
     }
 
-    private static PublishedNewsQuery Create()
+    private static PublishedNewsQuery Create(params string[] fields)
     {
-        return new PublishedNewsQuery();
+        return new PublishedNewsQuery(fields);
     }
 }
---
+++ platform/src/apis/Platform.Api.Content/Features/News/Handlers/GetNewsV1Handler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Http;
+using Platform.Api.Content.Features.News.Services;
+using Platform.Functions;
+using Platform.Functions.Extensions;
+
+namespace Platform.Api.Content.Features.News.Handlers;
+
+public interface IGetNewsHandler : IVersionedHandler
+{
+    Task<HttpResponseData> HandleAsync(HttpRequestData request, CancellationToken cancellationToken);
+}
+
+public class GetNewsV1Handler(INewsService service) : IGetNewsHandler
+{
+    public string Version => "1.0";
+
+    public async Task<HttpResponseData> HandleAsync(HttpRequestData request, CancellationToken cancellationToken)
+    {
+        var result = await service.GetNews(cancellationToken);
+        return await request.CreateJsonResponseAsync(result, cancellationToken);
+    }
+}
