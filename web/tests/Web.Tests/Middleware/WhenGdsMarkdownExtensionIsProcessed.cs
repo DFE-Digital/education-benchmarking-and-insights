19ac733a5797b4a29ff8a417b17ee3ee3df86a2d
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-01 11:30:52 +0100

    feat: Added Heading support to GDS markdown middleware

--- web/tests/Web.Tests/Middleware/WhenGdsMarkdownExtensionIsProcessed.cs
+++ web/tests/Web.Tests/Middleware/WhenGdsMarkdownExtensionIsProcessed.cs
@@ -18,6 +18,17 @@ public class WhenGdsMarkdownExtensionIsProcessed
         _pipeline = pipeline.Build();
     }
 
+    [Theory]
+    [InlineData("# Heading 1", "<h1 class=\"govuk-heading-l\">Heading 1</h1>\n")]
+    [InlineData("## Heading 2", "<h2 class=\"govuk-heading-m\">Heading 2</h2>\n")]
+    [InlineData("### Heading 3", "<h3 class=\"govuk-heading-s\">Heading 3</h3>\n")]
+    public void ShouldRenderGdsHeading(string markdown, string expected)
+    {
+        var actual = Markdown.ToHtml(markdown, _pipeline);
+
+        Assert.Equal(expected, actual);
+    }
+
     [Fact]
     public void ShouldRenderGdsLink()
     {
@@ -27,13 +38,14 @@ public class WhenGdsMarkdownExtensionIsProcessed
         Assert.Equal("<p class=\"govuk-body\"><a href=\"https://example.com\" class=\"govuk-link\">Example link</a></p>\n", actual);
     }
 
-    [Fact]
-    public void ShouldRenderGdsList()
+    [Theory]
+    [InlineData("* Example item 1\n* Example item 2", "<ul class=\"govuk-list govuk-list--bullet\">\n<li>Example item 1</li>\n<li>Example item 2</li>\n</ul>\n")]
+    [InlineData("1. Example item 1\n1. Example item 2", "<ol class=\"govuk-list govuk-list--number\">\n<li>Example item 1</li>\n<li>Example item 2</li>\n</ol>\n")]
+    public void ShouldRenderGdsList(string markdown, string expected)
     {
-        const string markdown = "* Example item 1\n* Example item 2";
         var actual = Markdown.ToHtml(markdown, _pipeline);
 
-        Assert.Equal("<ul class=\"govuk-list govuk-list--bullet\">\n<li>Example item 1</li>\n<li>Example item 2</li>\n</ul>\n", actual);
+        Assert.Equal(expected, actual);
     }
 
     [Theory]
@@ -53,6 +65,8 @@ public class WhenGdsMarkdownExtensionIsProcessed
         const string markdown = "| Item | Value |\n| --- | --- |\n| 001 | 123 |\n| 002 | 456 |";
         var actual = Markdown.ToHtml(markdown, _pipeline);
 
-        Assert.Equal("<table class=\"govuk-table\">\n<thead>\n<tr class=\"govuk-table__row\">\n<th class=\"govuk-table__cell\">Item</th>\n<th class=\"govuk-table__cell\">Value</th>\n</tr>\n</thead>\n<tbody>\n<tr class=\"govuk-table__row\">\n<td class=\"govuk-table__cell\">001</td>\n<td class=\"govuk-table__cell\">123</td>\n</tr>\n<tr class=\"govuk-table__row\">\n<td class=\"govuk-table__cell\">002</td>\n<td class=\"govuk-table__cell\">456</td>\n</tr>\n</tbody>\n</table>\n", actual);
+        Assert.Equal(
+            "<table class=\"govuk-table\">\n<thead>\n<tr class=\"govuk-table__row\">\n<th class=\"govuk-table__cell\">Item</th>\n<th class=\"govuk-table__cell\">Value</th>\n</tr>\n</thead>\n<tbody>\n<tr class=\"govuk-table__row\">\n<td class=\"govuk-table__cell\">001</td>\n<td class=\"govuk-table__cell\">123</td>\n</tr>\n<tr class=\"govuk-table__row\">\n<td class=\"govuk-table__cell\">002</td>\n<td class=\"govuk-table__cell\">456</td>\n</tr>\n</tbody>\n</table>\n",
+            actual);
     }
 }
