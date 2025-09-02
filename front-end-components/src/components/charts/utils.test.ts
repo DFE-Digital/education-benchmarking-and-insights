daf6ddaeb5e11319b4b514318bfb17515d41e57f
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-02 10:07:25 +0100

    fix: Ensured currency formatted with SI units display a single decimal place (or no decimal places if `.0`)

--- front-end-components/src/components/charts/utils.test.ts
+++ front-end-components/src/components/charts/utils.test.ts
@@ -111,8 +111,10 @@ describe("Chart utils", () => {
         { input: 0.27, expected: "£0" },
         { input: 0.9, expected: "£1" },
         { input: 55.3, expected: "£55" },
-        { input: 12345.67, expected: "£12k" },
-        { input: 890123456, expected: "£890m" },
+        { input: 12345.67, expected: "£12.3k" },
+        { input: 890123456, expected: "£890.1m" },
+        { input: 8507, expected: "£8.5k" },
+        { input: 1001111, expected: "£1m" },
         { input: "not-a-number", expected: "not-a-number" },
       ];
 
@@ -205,8 +207,10 @@ describe("Chart utils", () => {
         { input: 0, expected: "£0" },
         { input: 1, expected: "£1" },
         { input: 2.3456789, expected: "£2" },
-        { input: 12345.67, expected: "£12k" },
-        { input: 890123456, expected: "£890m" },
+        { input: 12345.67, expected: "£12.3k" },
+        { input: 890123456, expected: "£890.1m" },
+        { input: 8507, expected: "£8.5k" },
+        { input: 1001111, expected: "£1m" },
         { input: "not-a-number", expected: "not-a-number" },
       ];
 
