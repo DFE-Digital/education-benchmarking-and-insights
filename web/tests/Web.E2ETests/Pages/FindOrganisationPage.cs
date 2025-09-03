69256bfea4e21134e75143e48d584ddfadf694f7
Josh Langford <josh.langford@hippodigital.co.uk>
2025-09-03 12:27:58 +0100

    chore: remove FindOrganisationEnabled trait e2e tests and drop CI filter
    These tests assumed FilteredSearch was disabled. CI already excluded them via `--filter Category!=FindOrganisationEnabled`

--- web/tests/Web.E2ETests/Pages/FindOrganisationPage.cs
+++ web/tests/Web.E2ETests/Pages/FindOrganisationPage.cs
@@ -6,15 +6,12 @@ namespace Web.E2ETests.Pages;
 public enum OrganisationTypes
 {
     School,
-    Trust
 }
 
 public class FindOrganisationPage(IPage page)
 {
     private ILocator PageH1Heading => page.Locator(Selectors.H1);
     private ILocator OrganisationsTypeRadios => page.Locator(Selectors.GovRadios);
-    private ILocator SchoolSearchInputField => page.Locator(Selectors.SchoolSearchInput);
-
     private ILocator ContinueButton =>
         page.Locator(Selectors.GovButton, new PageLocatorOptions
         {
@@ -22,32 +19,12 @@ public class FindOrganisationPage(IPage page)
         });
 
     private ILocator SchoolRadioButton => page.Locator(Selectors.SchoolRadio);
-    private ILocator SchoolSuggestionsDropdown => page.Locator(Selectors.SchoolSuggestDropdown);
 
     public async Task IsDisplayed()
     {
         await PageH1Heading.ShouldBeVisible();
         await OrganisationsTypeRadios.ShouldBeVisible();
         await ContinueButton.ShouldBeVisible().ShouldBeEnabled();
-    }
-
-    public async Task TypeIntoSchoolSearchBox(string text)
-    {
-        await SchoolSearchInputField.PressSequentially(text);
-        await SchoolSuggestionsDropdown.ShouldBeVisible();
-    }
-
-    public async Task SelectItemFromSuggester()
-    {
-        await SchoolSearchInputField.PressAsync(Keyboard.ArrowDownKey);
-        await page.Keyboard.PressAsync(Keyboard.EnterKey);
-    }
-
-    public async Task<School.HomePage> ClickContinueToSchool()
-    {
-        await ContinueButton.ClickAsync();
-        await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
-        return new School.HomePage(page);
     }
 
     public async Task<SearchPage> ClickContinueToSchoolSearch()
@@ -65,14 +42,5 @@ public class FindOrganisationPage(IPage page)
             _ => throw new ArgumentOutOfRangeException(nameof(type))
         };
         await radioButton.Check();
-    }
-
-    public async Task AssertSearchResults(string keyword)
-    {
-        var listItems = await SchoolSuggestionsDropdown.Locator("li").AllAsync();
-        foreach (var item in listItems)
-        {
-            await item.ShouldContainText(keyword);
-        }
     }
 }
