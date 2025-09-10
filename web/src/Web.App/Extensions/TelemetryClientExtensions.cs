c81692762d3b15495e61a35ef961fe8c42b1d47a
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-10 14:43:46 +0100

    feat: Replaced Claims dump to custom event properties with Organisation type/identifier only

--- web/src/Web.App/Extensions/TelemetryClientExtensions.cs
+++ web/src/Web.App/Extensions/TelemetryClientExtensions.cs
@@ -1,43 +1,51 @@
 using System.Security.Claims;
-using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Web.App.Identity.Models;
 using Web.App.Telemetry;
 
 namespace Web.App.Extensions;
 
 public static class TelemetryClientExtensions
 {
-    private static readonly string[] SensitiveClaims = [ClaimTypes.Email, ClaimTypes.GivenName, ClaimTypes.Surname, "sid", "nonce", "at_hash"];
-
-    public static void TrackUserSignedInEvent(this ITelemetryClientWrapper telemetry, TokenValidatedContext context)
+    public static void TrackUserSignedInEvent(this ITelemetryClientWrapper telemetry, TokenValidatedContext context, Organisation? organisation)
     {
         var user = string.Empty;
-        var claims = Array.Empty<string>();
         if (context.Principal?.Claims != null)
         {
             user = context.Principal.Claims
                 .Where(c => c.Type == ClaimTypes.NameIdentifier)
                 .Select(c => c.Value)
                 .SingleOrDefault() ?? string.Empty;
-
-            claims = context.Principal.Claims
-                .Select(c =>
-                {
-                    var value = c.Value;
-                    if (SensitiveClaims.Contains(c.Type))
-                    {
-                        value = new string('*', c.Value.Length);
-                    }
-
-                    return $"{c.Type}: {value}";
-                })
-                .ToArray();
         }
 
-        telemetry.TrackEvent(TrackedEvents.UserSignInSuccess.GetStringValue(), new Dictionary<string, string>
+        var organisationType = organisation?.Category?.Id switch
+        {
+            OrganisationCategories.SingleAcademyTrust or OrganisationCategories.MultiAcademyTrust => TrackedRequestEstablishmentType.Trust,
+            OrganisationCategories.LocalAuthority => TrackedRequestEstablishmentType.LocalAuthority,
+            null => (TrackedRequestEstablishmentType?)null,
+            _ => TrackedRequestEstablishmentType.School
+        };
+
+        var properties = new Dictionary<string, string>
         {
             { "User", user },
-            { "Claims", string.Join(Environment.NewLine, claims) }
-        });
+            { "Organisation Type", organisationType?.GetStringValue() ?? string.Empty }
+        };
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (organisationType)
+        {
+            case TrackedRequestEstablishmentType.Trust:
+                properties[nameof(TrackedRequestRouteParameters.CompanyNumber)] = organisation?.CompanyRegistrationNumber?.ToString("00000000") ?? string.Empty;
+                break;
+            case TrackedRequestEstablishmentType.LocalAuthority:
+                properties[nameof(TrackedRequestRouteParameters.Code)] = organisation?.EstablishmentNumber?.ToString("000") ?? string.Empty;
+                break;
+            case TrackedRequestEstablishmentType.School:
+                properties[nameof(TrackedRequestRouteParameters.Urn)] = organisation?.URN?.ToString("000000") ?? string.Empty;
+                break;
+        }
+
+        telemetry.TrackEvent(TrackedEvents.UserSignInSuccess.GetStringValue(), properties);
     }
 }
