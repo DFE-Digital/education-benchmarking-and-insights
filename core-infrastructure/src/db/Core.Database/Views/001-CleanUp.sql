ad2d94efd5053fc7df717be864a24b737f2375e0
Josh Langford <josh.langford@hippodigital.co.uk>
2025-09-15 07:51:11 +0100

    chore: add a number of redundant views to clean up script

--- core-infrastructure/src/db/Core.Database/Views/001-CleanUp.sql
+++ core-infrastructure/src/db/Core.Database/Views/001-CleanUp.sql
@@ -5,3 +5,54 @@ GO
 
 DROP VIEW IF EXISTS VW_TransparencyFilesCfr
 GO
+
+DROP VIEW IF EXISTS SchoolExpenditureHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditure
+    GO
+
+DROP VIEW IF EXISTS TrustExpenditureHistoric
+    GO
+
+DROP VIEW IF EXISTS TrustExpenditure
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureCustom
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureHistoricWithNulls
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditurePerUnitHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPerUnitHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgComparatorSet
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPerUnitComparatorSet
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditurePercentageOfExpenditureHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditurePercentageOfIncomeHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPercentageOfExpenditureHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPercentageOfIncomeHistoric
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPercentageOfExpenditureComparatorSet
+    GO
+
+DROP VIEW IF EXISTS SchoolExpenditureAvgPercentageOfIncomeComparatorSet
+    GO
