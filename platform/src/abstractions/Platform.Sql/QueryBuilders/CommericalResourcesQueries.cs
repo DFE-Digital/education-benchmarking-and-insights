cd590f7a67984346ff181a5c1350c0a6d257a43d
Josh Langford <josh.langford@hippodigital.co.uk>
2025-09-17 16:47:43 +0100

    feat: add new MonitorCommercialResourcesFunction to check response for each active commercial resources record and output logs and telemetry.
    - MonitorCommercialResourcesService with methods:
      - GetResourcesAsync: get Title and Url from db view
      - CheckResourceAsync: check result and return a CommercialResourceResult
    - adds a new QueryBuilder to facilitate the above.

--- platform/src/abstractions/Platform.Sql/QueryBuilders/CommericalResourcesQueries.cs
+++ platform/src/abstractions/Platform.Sql/QueryBuilders/CommericalResourcesQueries.cs
@@ -4,3 +4,8 @@ public class CommercialResourcesQuery() : PlatformQuery(Sql)
 {
     private const string Sql = "SELECT * FROM VW_CommercialResources";
 }
+
+public class MonitorCommercialResourcesQuery() : PlatformQuery(Sql)
+{
+    private const string Sql = "SELECT Title, Url FROM VW_CommercialResources";
+}
---
+++ platform/src/apis/Platform.MaintenanceTasks/Features/MonitorCommercialResources/Models/CommercialResourceResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace Platform.MaintenanceTasks.Features.MonitorCommercialResources.Models;
+
+public record CommercialResourceResult
+{
+    public required string Title { get; init; }
+    public required string Url { get; init; }
+    public bool Success { get; init; }
+    public HttpStatusCode? StatusCode { get; init; }
+    public string? RedirectLocation { get; init; }
+    public Exception? Exception { get; init; }
+}
