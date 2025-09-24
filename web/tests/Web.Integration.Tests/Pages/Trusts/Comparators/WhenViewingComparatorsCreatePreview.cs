863aa842ba4dcffdd915848035b4ab4cd0b4f1d2
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-24 16:06:03 +0100

    feat: Integration test coverage over optional `requestUri` in Trust comparators query string

--- web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsCreatePreview.cs
+++ web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsCreatePreview.cs
@@ -11,14 +11,16 @@ namespace Web.Integration.Tests.Pages.Trusts.Comparators;
 
 public class WhenViewingComparatorsCreatePreview(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
 {
-    [Fact]
-    public async Task CanDisplay()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("redirect-uri")]
+    public async Task CanDisplay(string? redirectUri)
     {
-        var (page, trust) = await SetupNavigateInitPage();
-        AssertPageLayout(page, trust);
+        var (page, trust) = await SetupNavigateInitPage(redirectUri);
+        AssertPageLayout(page, trust, redirectUri);
     }
 
-    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage()
+    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage(string? redirectUri = null)
     {
         var trust = Fixture.Build<Trust>()
             .With(x => x.CompanyNumber, "12345678")
@@ -36,29 +38,28 @@ public class WhenViewingComparatorsCreatePreview(SchoolBenchmarkingWebAppClient 
         };
 
         var page = await Client.SetupEstablishment(trust)
-            .SetupTrustInsightApi(new[]
-            {
+            .SetupTrustInsightApi([
                 new TrustCharacteristic
                 {
                     CompanyNumber = "114504",
                     TrustName = "Abbey Academies Trust"
                 }
-            })
+            ])
             .SetupHttpContextAccessor(sessionState)
-            .Navigate(Paths.TrustComparatorsCreatePreview(trust.CompanyNumber));
+            .Navigate(Paths.TrustComparatorsCreatePreview(trust.CompanyNumber, redirectUri));
 
         return (page, trust);
     }
 
-    private static void AssertPageLayout(IHtmlDocument page, Trust trust)
+    private static void AssertPageLayout(IHtmlDocument page, Trust trust, string? redirectUri = null)
     {
         DocumentAssert.BackLink(page, "Back", Paths.TrustComparatorsCreateBy(trust.CompanyNumber).ToAbsolute());
         DocumentAssert.TitleAndH1(page,
             "Trusts successfully matched - Financial Benchmarking and Insights Tool - GOV.UK",
             "Trusts successfully matched");
         var cta = page.QuerySelector("main .govuk-button");
-        DocumentAssert.PrimaryCta(cta, "Create a set using these trusts", Paths.TrustComparatorsCreateSubmit(trust.CompanyNumber));
+        DocumentAssert.PrimaryCta(cta, "Create a set using these trusts", Paths.TrustComparatorsCreateSubmit(trust.CompanyNumber, redirectUri));
         var change = page.QuerySelector("#change-set");
-        DocumentAssert.Link(change, "Change characteristics and create a new set", Paths.TrustComparatorsCreateByCharacteristic(trust.CompanyNumber).ToAbsolute());
+        DocumentAssert.Link(change, "Change characteristics and create a new set", Paths.TrustComparatorsCreateByCharacteristic(trust.CompanyNumber, redirectUri).ToAbsolute());
     }
 }
--- web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsRevert.cs
+++ web/tests/Web.Integration.Tests/Pages/Trusts/Comparators/WhenViewingComparatorsRevert.cs
@@ -11,25 +11,29 @@ namespace Web.Integration.Tests.Pages.Trusts.Comparators;
 
 public class WhenViewingComparatorsCreateRevert(SchoolBenchmarkingWebAppClient client) : PageBase<SchoolBenchmarkingWebAppClient>(client)
 {
-    [Fact]
-    public async Task CanDisplay()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("redirect-uri")]
+    public async Task CanDisplay(string? redirectUri)
     {
-        var (page, trust) = await SetupNavigateInitPage();
-        AssertPageLayout(page, trust);
+        var (page, trust) = await SetupNavigateInitPage(redirectUri: redirectUri);
+        AssertPageLayout(page, trust, redirectUri);
     }
 
-    [Fact]
-    public async Task CanRevert()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("redirect-uri")]
+    public async Task CanRevert(string? redirectUri)
     {
-        var (page, trust) = await SetupNavigateInitPage(true);
+        var (page, trust) = await SetupNavigateInitPage(true, redirectUri);
         var action = page.QuerySelector("main .govuk-button");
         Assert.NotNull(action);
 
         page = await Client.SubmitForm(page.Forms[0], action);
-        DocumentAssert.AssertPageUrl(page, Paths.TrustHome(trust.CompanyNumber).ToAbsolute() + "?comparator-reverted=true");
+        DocumentAssert.AssertPageUrl(page, $"{Paths.TrustHome(trust.CompanyNumber).ToAbsolute()}?comparator-reverted=true{(redirectUri == null ? string.Empty : $"&redirectUri={redirectUri}")}");
     }
 
-    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage(bool setupUserData = false)
+    private async Task<(IHtmlDocument page, Trust trust)> SetupNavigateInitPage(bool setupUserData = false, string? redirectUri = null)
     {
         var trust = Fixture.Build<Trust>()
             .With(x => x.CompanyNumber, "12345678")
@@ -63,11 +67,11 @@ public class WhenViewingComparatorsCreateRevert(SchoolBenchmarkingWebAppClient c
                 .SetupInsights();
         }
 
-        var page = await client.Navigate(Paths.TrustComparatorsRevert(trust.CompanyNumber));
+        var page = await client.Navigate(Paths.TrustComparatorsRevert(trust.CompanyNumber, redirectUri));
         return (page, trust);
     }
 
-    private static void AssertPageLayout(IHtmlDocument page, Trust trust)
+    private static void AssertPageLayout(IHtmlDocument page, Trust trust, string? redirectUri = null)
     {
         var expectedBreadcrumbs = new[]
         {
@@ -79,8 +83,8 @@ public class WhenViewingComparatorsCreateRevert(SchoolBenchmarkingWebAppClient c
             "Remove all your trusts? - Financial Benchmarking and Insights Tool - GOV.UK",
             "Remove all your trusts?");
         var cta = page.QuerySelector("main .govuk-button");
-        DocumentAssert.PrimaryCta(cta, "Remove all trusts", Paths.TrustComparatorsRevert(trust.CompanyNumber));
+        DocumentAssert.PrimaryCta(cta, "Remove all trusts", Paths.TrustComparatorsRevert(trust.CompanyNumber, redirectUri));
         var change = page.QuerySelector("#cancel-revert");
-        DocumentAssert.Link(change, "Cancel", Paths.TrustHome(trust.CompanyNumber).ToAbsolute());
+        DocumentAssert.Link(change, "Cancel", redirectUri == null ? Paths.TrustHome(trust.CompanyNumber).ToAbsolute() : $"{Paths.TrustComparators(trust.CompanyNumber).ToAbsolute()}/{redirectUri}");
     }
 }
