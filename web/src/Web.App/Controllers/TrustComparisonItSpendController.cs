aad9bead48a386bbe4faa4b9c0b796d7ceecdc5a
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-25 06:20:38 +0100

    feat: Created `BfrItSpendBreakdown` feature flag

---
+++ web/src/Web.App/Controllers/TrustComparisonItSpendController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using Web.App.Attributes;
+using Web.App.Attributes.RequestTelemetry;
+
+namespace Web.App.Controllers;
+
+[Controller]
+[Authorize]
+[Route("trust/{companyNumber}/benchmark-it-spending")]
+[ValidateCompanyNumber]
+[FeatureGate(FeatureFlags.BfrItSpendBreakdown)]
+public class TrustComparisonItSpendController : Controller
+{
+    [HttpGet]
+    [TrustRequestTelemetry(TrackedRequestFeature.BenchmarkItSpend)]
+    public IActionResult Index(string companyNumber)
+    {
+        return new NotFoundObjectResult(new { companyNumber });
+    }
+}
