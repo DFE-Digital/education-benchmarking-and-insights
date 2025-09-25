b75897ca1703ebd441cad14d01342510993eaec3
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-09-25 11:07:04 +0100

    feat: Enforced creation of comparator set before viewing Trust IT Spend
    Added view with link to modify comparator set

--- web/src/Web.App/Controllers/TrustComparisonItSpendController.cs
+++ web/src/Web.App/Controllers/TrustComparisonItSpendController.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.FeatureManagement.Mvc;
 using Web.App.Attributes;
 using Web.App.Attributes.RequestTelemetry;
+using Web.App.Domain;
+using Web.App.Infrastructure.Apis;
+using Web.App.Infrastructure.Apis.Benchmark;
+using Web.App.Infrastructure.Apis.Establishment;
+using Web.App.Infrastructure.Extensions;
+using Web.App.Services;
+using Web.App.ViewModels;
 
 namespace Web.App.Controllers;
 
@@ -11,12 +19,50 @@ namespace Web.App.Controllers;
 [Route("trust/{companyNumber}/benchmark-it-spending")]
 [ValidateCompanyNumber]
 [FeatureGate(FeatureFlags.TrustItSpendBreakdown)]
-public class TrustComparisonItSpendController : Controller
+public class TrustComparisonItSpendController(
+    ILogger<TrustComparisonItSpendController> logger,
+    IEstablishmentApi establishmentApi,
+    IComparatorSetApi comparatorSetApi,
+    IUserDataService userDataService) : Controller
 {
     [HttpGet]
     [TrustRequestTelemetry(TrackedRequestFeature.BenchmarkItSpend)]
-    public IActionResult Index(string companyNumber) => new NotFoundObjectResult(new
+    public async Task<IActionResult> Index(string companyNumber,
+        [FromQuery(Name = "comparator-generated")] bool? comparatorGenerated)
     {
-        companyNumber
-    });
+        using (logger.BeginScope(new
+        {
+            companyNumber
+        }))
+        {
+            try
+            {
+                var trust = await establishmentApi.GetTrust(companyNumber).GetResultOrThrow<Trust>();
+                var redirectUri = Url.Action("Index", new
+                {
+                    companyNumber
+                });
+                var userData = await userDataService.GetTrustDataAsync(User, companyNumber);
+                if (userData.ComparatorSet == null)
+                {
+                    return RedirectToAction("Index", "TrustComparatorsCreateBy", new
+                    {
+                        companyNumber,
+                        redirectUri
+                    });
+                }
+
+                var userDefinedSet = await comparatorSetApi.GetUserDefinedTrustAsync(companyNumber, userData.ComparatorSet)
+                    .GetResultOrDefault<UserDefinedSchoolComparatorSet>();
+
+                var viewModel = new TrustComparisonItSpendViewModel(trust, comparatorGenerated, redirectUri, userDefinedSet?.Set);
+                return View(viewModel);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error displaying trust IT spend landing: {DisplayUrl}", Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
 }
