5b3623ce7043842773454807b59d2d9d9af410b7
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-06 07:03:10 +0100

    feat: FBIS for Trust introductory text

---
+++ web/src/Web.App/Controllers/TrustFinancialBenchmarkingInsightsSummaryController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using Web.App.Attributes;
+using Web.App.Domain;
+using Web.App.Infrastructure.Apis;
+using Web.App.Infrastructure.Apis.Establishment;
+using Web.App.Infrastructure.Extensions;
+using Web.App.ViewModels;
+
+namespace Web.App.Controllers;
+
+[Controller]
+[Route("trust/{companyNumber}/summary")]
+[ValidateCompanyNumber]
+[FeatureGate(FeatureFlags.FbisForTrust)]
+public class TrustFinancialBenchmarkingInsightsSummaryController(
+    IEstablishmentApi establishmentApi,
+    ILogger<TrustFinancialBenchmarkingInsightsSummaryController> logger)
+    : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index(string companyNumber)
+    {
+        using (logger.BeginScope(new
+        {
+            companyNumber
+        }))
+        {
+            try
+            {
+                var trust = await establishmentApi.GetTrust(companyNumber).GetResultOrThrow<Trust>();
+
+                var viewModel = new TrustFinancialBenchmarkingInsightsSummaryViewModel(trust);
+                return View(viewModel);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error displaying financial benchmarking insights summary: {DisplayUrl}",
+                    Request.GetDisplayUrl());
+                return e is StatusCodeException s ? StatusCode((int)s.Status) : StatusCode(500);
+            }
+        }
+    }
+}
