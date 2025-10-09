b64725655e5643238ecec95ef6753b7e871ce381
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2025-10-09 10:56:34 +0100

    moca academies data for now, add IT spend rows

--- databricks/education_benchmarking_and_insights/src/education_benchmarking_and_insights/bfr/raw_tables.py
+++ databricks/education_benchmarking_and_insights/src/education_benchmarking_and_insights/bfr/raw_tables.py
@@ -1,6 +1,7 @@
 from pyspark import pipelines as dp
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from .bfr_pyspark_mocks import get_mock_academies_df
 
 BRONZE_BFR_CATALOG_PATH = "catalog_30_bronze.bfr"
 COPPER_FBIT_CATALOG_PATH = "catalog_40_copper_financial_benchmarking_insights.financial_benchmarking_insights"
@@ -27,14 +28,15 @@ def bfr_three_year_forecast_current_year():
     return dp.read(f"{COPPER_FBIT_CATALOG_PATH}.mv_BFR_Three_Year_Forecast_{current_year}")
 
 
-# @dp.view
-# def academies_current_year():
-#     """
-#     Loads the current year academies data.
-#     """
-#     spark = SparkSession.builder.getOrCreate()
-#     current_year = int(spark.conf.get("pipeline.year"))
-#     return dp.read(f"{BRONZE_AAR_CATALOG_PATH}.vw_academies_{current_year}")
+@dp.view
+def academies_current_year():
+    """
+    Loads the current year academies data. (TODO: get rid of the mock)
+    """
+    spark = SparkSession.builder.getOrCreate()
+    current_year = int(spark.conf.get("pipeline.year"))
+    return get_mock_academies_df(spark, current_year)
+    # return dp.read(f"{BRONZE_AAR_CATALOG_PATH}.vw_academies_{current_year}")
 
 
 @dp.view(
@@ -65,15 +67,16 @@ def bfr_sofa_historical_y2():
                 .select("TrustUPIN", "EFALineNo", "Y1P2", "Y2P2")
 
 
-# @dp.view
-# def academies_historical():
-#     """
-#     Loads historical academies data for a given year offset.
-#     """
-#     spark = SparkSession.builder.getOrCreate()
-#     current_year = int(spark.conf.get("pipeline.year"))
-#     historical_year = current_year - 1
-#     return dp.read(f"{BRONZE_AAR_CATALOG_PATH}.vw_academies_{historical_year}")
+@dp.view
+def academies_historical():
+    """
+    Loads historical academies data for a given year offset.
+    """
+    spark = SparkSession.builder.getOrCreate()
+    current_year = int(spark.conf.get("pipeline.year"))
+    historical_year = current_year - 1
+    return get_mock_academies_df(spark, historical_year)
+    # return dp.read(f"{BRONZE_AAR_CATALOG_PATH}.vw_academies_{historical_year}")
 
 
 # Example of how you would use the historical views in other dp tables
@@ -99,10 +102,10 @@ def bfr_sofa_y2_processed():
                 .filter(col("EFALineNo").isin(desired_efa_lines))
 
 
-# @dp.table
-# def academies_y1_processed():
-#     return dp.read("academies_historical", year_offset=1)
+@dp.table(comment="Mocked")
+def academies_y1_processed():
+    return dp.read("academies_historical")
 
-# @dp.table
-# def academies_y2_processed():
-#     return dp.read("academies_historical", year_offset=2)
+@dp.table(comment="Mocked")
+def academies_y2_processed():
+    return dp.read("academies_historical")
