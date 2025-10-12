c821e1428865b7a9be0bb354a46d554c1da6d6c6
Josh Langford <josh.langford@hippodigital.co.uk>
2025-10-13 00:57:51 +0100

    chore: a11y tests for fbis for trusts page

---
+++ web/tests/Web.A11yTests/Pages/Trust/WhenViewingFinancialBenchmarkingInsightsSummary.cs
@@ -0,0 +1,20 @@
+﻿using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Web.A11yTests.Pages.Trust;
+
+public class WhenViewingFinancialBenchmarkingInsightsSummary(
+    ITestOutputHelper testOutputHelper,
+    WebDriver webDriver)
+    : PageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/trust/{TestConfiguration.Trust}/summary";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssues()
+    {
+        await GoToPage();
+        await EvaluatePage();
+    }
+}
