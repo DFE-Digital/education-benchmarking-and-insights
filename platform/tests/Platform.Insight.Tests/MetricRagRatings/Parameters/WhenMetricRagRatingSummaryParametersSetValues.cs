5b7c8869683667590f5005df9d35b95bb83cf0b0
J05h-L <117924208+J05h-L@users.noreply.github.com>
2025-10-14 12:21:27 +0100

    feat: add new insight api endpoint for rag summary (#3105)

---
+++ platform/tests/Platform.Insight.Tests/MetricRagRatings/Parameters/WhenMetricRagRatingSummaryParametersSetValues.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Specialized;
+using Platform.Api.Insight.Features.MetricRagRatings.Parameters;
+using Xunit;
+
+namespace Platform.Insight.Tests.MetricRagRatings.Parameters;
+
+public class WhenMetricRagRatingSummaryParametersSetValues
+{
+    [Fact]
+    public void ShouldSetValuesFromQuery()
+    {
+        var values = new NameValueCollection
+        {
+            { "companyNumber", "12345678" },
+            { "urns", "123456" },
+            { "urns", "653456" },
+            { "laCode", "123" },
+            { "overallPhase", "Primary" },
+        };
+
+        var parameters = new MetricRagRatingSummaryParameters();
+        parameters.SetValues(values);
+
+        Assert.Equal("12345678", parameters.CompanyNumber);
+        Assert.Equal(["123456", "653456"], parameters.Urns);
+        Assert.Equal("123", parameters.LaCode);
+        Assert.Equal("Primary", parameters.OverallPhase);
+    }
+}
