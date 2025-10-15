d5c114a8ca06c9dd2776c6c2b902520e3d3f5f34
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-15 14:25:30 +0100

    feat: Part-implementation of LA school financials tab filter

--- web/src/Web.App/Domain/OverallPhaseTypes.cs
+++ web/src/Web.App/Domain/OverallPhaseTypes.cs
@@ -2,6 +2,19 @@ namespace Web.App.Domain;
 
 public static class OverallPhaseTypes
 {
+    public enum OverallPhaseTypeFilter
+    {
+        Primary = 0,
+        Secondary = 1,
+        Special = 2,
+        PupilReferralUnit = 3,
+        AllThrough = 4,
+        Nursery = 5,
+        PostSixteen = 6,
+        AlternativeProvision = 7,
+        UniversityTechnicalCollege = 8
+    }
+
     public const string Primary = "Primary";
     public const string Secondary = "Secondary";
     public const string Special = "Special";
@@ -11,6 +24,19 @@ public static class OverallPhaseTypes
     public const string PostSixteen = "Post-16";
     public const string AlternativeProvision = "Alternative Provision";
     public const string UniversityTechnicalCollege = "University Technical College";
+
+    public static readonly OverallPhaseTypeFilter[] AllFilters =
+    [
+        OverallPhaseTypeFilter.Primary,
+        OverallPhaseTypeFilter.Secondary,
+        OverallPhaseTypeFilter.Special,
+        OverallPhaseTypeFilter.PupilReferralUnit,
+        OverallPhaseTypeFilter.AllThrough,
+        OverallPhaseTypeFilter.Nursery,
+        OverallPhaseTypeFilter.PostSixteen,
+        OverallPhaseTypeFilter.AlternativeProvision,
+        OverallPhaseTypeFilter.UniversityTechnicalCollege
+    ];
 
     public static string[] All =>
     [
@@ -42,4 +68,18 @@ public static class OverallPhaseTypes
         PupilReferralUnit,
         AlternativeProvision
     ];
+
+    public static string GetFilterDescription(this OverallPhaseTypeFilter filter) => filter switch
+    {
+        OverallPhaseTypeFilter.Primary => Primary,
+        OverallPhaseTypeFilter.Secondary => Secondary,
+        OverallPhaseTypeFilter.Special => Special,
+        OverallPhaseTypeFilter.PupilReferralUnit => PupilReferralUnit,
+        OverallPhaseTypeFilter.AllThrough => AllThrough,
+        OverallPhaseTypeFilter.Nursery => Nursery,
+        OverallPhaseTypeFilter.PostSixteen => PostSixteen,
+        OverallPhaseTypeFilter.AlternativeProvision => AlternativeProvision,
+        OverallPhaseTypeFilter.UniversityTechnicalCollege => UniversityTechnicalCollege,
+        _ => throw new ArgumentException(nameof(filter))
+    };
 }
