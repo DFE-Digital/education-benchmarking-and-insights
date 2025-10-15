6dfbdc159ce6f9700a53450a5aaeb918f85f5990
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-15 14:57:08 +0100

    feat: Updated unit tests for modified view component and model

--- web/tests/Web.Tests/ViewComponents/GivenALocalAuthoritySchoolFinancialViewComponent.cs
+++ web/tests/Web.Tests/ViewComponents/GivenALocalAuthoritySchoolFinancialViewComponent.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Web.App.Domain;
+using Web.App.Domain.Charts;
 using Web.App.ViewComponents;
 using Web.App.ViewModels.Components;
 using Xunit;
@@ -10,20 +12,16 @@ namespace Web.Tests.ViewComponents;
 public class LocalAuthoritySchoolFinancialViewComponentTests
 {
     private readonly LocalAuthoritySchoolFinancialViewComponent _component;
+    private readonly HttpContext _httpContext;
     private readonly PathString _path = "/test/path";
 
     public LocalAuthoritySchoolFinancialViewComponentTests()
     {
-        var httpContext = new DefaultHttpContext
-        {
-            Request =
-            {
-                Path = _path
-            }
-        };
+        _httpContext = new DefaultHttpContext();
+        _httpContext.Request.Path = _path;
         var viewContext = new ViewContext
         {
-            HttpContext = httpContext
+            HttpContext = _httpContext
         };
         _component = new LocalAuthoritySchoolFinancialViewComponent
         {
@@ -35,18 +33,46 @@ public class LocalAuthoritySchoolFinancialViewComponentTests
     }
 
     [Fact]
-    public async Task ShouldReturnCode()
+    public async Task ShouldReturnCodeAndFormPrefix()
     {
         // arrange
         const string code = nameof(code);
+        const string formPrefix = nameof(formPrefix);
 
         // act
-        var result = await _component.InvokeAsync(code) as ViewViewComponentResult;
+        var result = await _component.InvokeAsync(code, formPrefix) as ViewViewComponentResult;
 
         // assert
         Assert.NotNull(result);
         var model = result.ViewData?.Model as LocalAuthoritySchoolFinancialViewModel;
         Assert.NotNull(model);
         Assert.Equal(code, model.Code);
+        Assert.Equal(formPrefix, model.FormPrefix);
+    }
+
+    [Theory]
+    [InlineData("f.", "", new OverallPhaseTypes.OverallPhaseTypeFilter[0], Dimensions.ResultAsOptions.SpendPerPupil)]
+    [InlineData("f.", "?f.as=0", new OverallPhaseTypes.OverallPhaseTypeFilter[0], Dimensions.ResultAsOptions.SpendPerPupil)]
+    [InlineData("f.", "?f.as=1", new OverallPhaseTypes.OverallPhaseTypeFilter[0], Dimensions.ResultAsOptions.Actuals)]
+    [InlineData("f.", "?f.as=2", new OverallPhaseTypes.OverallPhaseTypeFilter[0], Dimensions.ResultAsOptions.PercentExpenditure)]
+    [InlineData("f.", "?f.as=3", new OverallPhaseTypes.OverallPhaseTypeFilter[0], Dimensions.ResultAsOptions.PercentIncome)]
+    [InlineData("f.", "?f.phase=0", new[] { OverallPhaseTypes.OverallPhaseTypeFilter.Primary }, Dimensions.ResultAsOptions.SpendPerPupil)]
+    [InlineData("f.", "?f.phase=0&f.phase=1&f.phase=2", new[] { OverallPhaseTypes.OverallPhaseTypeFilter.Primary, OverallPhaseTypes.OverallPhaseTypeFilter.Secondary, OverallPhaseTypes.OverallPhaseTypeFilter.Special }, Dimensions.ResultAsOptions.SpendPerPupil)]
+    [InlineData("f.", "?f.phase=0&f.phase=1&f.as=1&other=value", new[] { OverallPhaseTypes.OverallPhaseTypeFilter.Primary, OverallPhaseTypes.OverallPhaseTypeFilter.Secondary }, Dimensions.ResultAsOptions.Actuals)]
+    public async Task ShouldReturnFormValuesFromQuery(string formPrefix, string query, OverallPhaseTypes.OverallPhaseTypeFilter[] expectedSelectedOverallPhases, Dimensions.ResultAsOptions expectedResultAs)
+    {
+        // arrange
+        const string code = nameof(code);
+        _httpContext.Request.QueryString = new QueryString(query);
+
+        // act
+        var result = await _component.InvokeAsync(code, formPrefix) as ViewViewComponentResult;
+
+        // assert
+        Assert.NotNull(result);
+        var model = result.ViewData?.Model as LocalAuthoritySchoolFinancialViewModel;
+        Assert.NotNull(model);
+        Assert.Equal(expectedSelectedOverallPhases, model.SelectedOverallPhases);
+        Assert.Equal(expectedResultAs, model.ResultAs);
     }
 }
