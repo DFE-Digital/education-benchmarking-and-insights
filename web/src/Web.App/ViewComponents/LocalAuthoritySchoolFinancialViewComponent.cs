d3780f55deceb08f25184a328986fea2fb208f6a
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-15 11:25:15 +0100

    feat: Added skeleton view component for LA school financials, within tabbed GDS component

---
+++ web/src/Web.App/ViewComponents/LocalAuthoritySchoolFinancialViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.App.Infrastructure.Apis.Insight;
+using Web.App.ViewModels.Components;
+
+namespace Web.App.ViewComponents;
+
+public class LocalAuthoritySchoolFinancialViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string code) => await Task.FromResult(View(new LocalAuthoritySchoolFinancialViewModel(code)));
+}
