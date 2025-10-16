e4cd5de078947861e705d5fdb37f5cce47d9db35
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-16 17:13:53 +0100

    feat: `<button>` alternative to `<a>` for dynamic column heading sort

--- web/src/Web.App/ViewModels/Components/SortableTableHeaderCellViewModel.cs
+++ web/src/Web.App/ViewModels/Components/SortableTableHeaderCellViewModel.cs
@@ -1,28 +1,55 @@
 namespace Web.App.ViewModels.Components;
 
-public class SortableTableHeaderCellViewModel(
+public class SortableTableHeaderCellLinkViewModel(
     string label,
     string sortField,
-    string sortFieldKey,
-    string sortOrderKey,
+    string sortKey,
+    string sortDelimeter,
     string? currentSortField,
     string? currentSortOrder,
     string baseUrl,
-    string? className)
+    string? className) : SortableTableHeaderCellViewModel(label, sortField, currentSortField, currentSortOrder, className)
 {
-    public string Label => label;
-    public string SortField => sortField;
-    public string ClassName => className ?? string.Empty;
-
     public string Href
     {
         get
         {
             var queryPrefix = baseUrl.Contains('?') ? "&" : "?";
             var newSort = currentSortField == sortField && currentSortOrder == "asc" ? "desc" : "asc";
-            return $"{baseUrl}{queryPrefix}{sortFieldKey}={sortField}&{sortOrderKey}={newSort}";
+            return $"{baseUrl}{queryPrefix}{sortKey}={sortField}{sortDelimeter}{newSort}";
         }
     }
+}
 
+public class SortableTableHeaderCellButtonViewModel(
+    string label,
+    string sortField,
+    string sortDelimeter,
+    string sortKey,
+    string? currentSortField,
+    string? currentSortOrder,
+    string? className) : SortableTableHeaderCellViewModel(label, sortField, currentSortField, currentSortOrder, className)
+{
+    public string SortValue => $"{sortField}{sortDelimeter}{(currentSortField == sortField && currentSortOrder == "asc" ? "desc" : "asc")}";
+    public string SortKey => sortKey;
+}
+
+public abstract class SortableTableHeaderCellViewModel(
+    string label,
+    string sortField,
+    string? currentSortField,
+    string? currentSortOrder,
+    string? className)
+{
+    public string Label => label;
+    public string SortField => sortField;
+    public string ClassName => className ?? string.Empty;
     public string? Sort => currentSortField == sortField ? currentSortOrder : null;
+
+    public string AriaSort => Sort switch
+    {
+        "asc" => "ascending",
+        "desc" => "descending",
+        _ => "none"
+    };
 }
