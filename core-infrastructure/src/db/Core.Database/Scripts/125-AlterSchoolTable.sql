e6c2b6c780e9e38a97fb1b41b17ec261783abd9f
Josh Langford <josh.langford@hippodigital.co.uk>
2025-10-16 16:50:49 +0100

    feat: add new columns for School table to persist gias school data

---
+++ core-infrastructure/src/db/Core.Database/Scripts/125-AlterSchoolTable.sql
@@ -0,0 +1,11 @@
+﻿IF EXISTS(SELECT 1
+          FROM INFORMATION_SCHEMA.TABLES
+          WHERE table_name = 'School')
+    BEGIN
+    ALTER TABLE dbo.School
+        ADD NurseryProvision nvarchar(50) NULL;
+    ALTER TABLE dbo.School
+        ADD SpecialClassProvision nvarchar(50) NULL;
+    ALTER TABLE dbo.School
+        ADD SixthFormProvision nvarchar(50) NULL;
+    END;
