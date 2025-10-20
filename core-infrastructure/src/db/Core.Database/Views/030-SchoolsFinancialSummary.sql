7fc0e991a37ed32c4666950601093d971e323c73
Josh Langford <josh.langford@hippodigital.co.uk>
2025-10-20 16:36:47 +0100

    feat: create schools financial summary views

---
+++ core-infrastructure/src/db/Core.Database/Views/030-SchoolsFinancialSummary.sql
@@ -0,0 +1,255 @@
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryActual
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryActual
+AS
+SELECT f.RunType,
+       f.RunId,
+       f.URN,
+       f.TotalExpenditure,
+       f.TotalIncome,
+       f.TotalPupils,
+       f.TotalTeachingSupportStaffCosts,
+       f.RevenueReserve,
+       f.FinanceType,
+       f.OverallPhase,
+       s.LACode,
+       s.TrustCompanyNumber,
+       s.NurseryProvision,
+       s.SpecialClassProvision,
+       s.SixthFormProvision
+FROM Financial f
+         LEFT JOIN School s on f.URN = s.URN
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryPerUnit
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryPerUnit
+AS
+SELECT RunType,
+       RunId,
+       URN,
+       TotalPupils,
+       IIF(TotalPupils > 0.0, TotalExpenditure / TotalPupils, NULL)                          AS TotalExpenditure,
+       IIF(TotalPupils > 0.0, TotalTeachingSupportStaffCosts / TotalPupils, NULL)            AS TotalTeachingSupportStaffCosts,
+       IIF(TotalPupils > 0.0, RevenueReserve / TotalPupils, NULL)                          AS RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryActual
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryPercentExpenditure
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryPercentExpenditure
+AS
+SELECT RunType,
+       RunId,
+       URN,
+       TotalPupils,
+       IIF(TotalExpenditure > 0.0, (TotalExpenditure / TotalExpenditure) * 100, NULL)                          AS TotalExpenditure,
+       IIF(TotalExpenditure > 0.0, (TotalTeachingSupportStaffCosts / TotalExpenditure) * 100, NULL)            AS TotalTeachingSupportStaffCosts,
+       IIF(TotalExpenditure > 0.0, (RevenueReserve / TotalExpenditure)  * 100, NULL)                          AS RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryActual
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryPercentIncome
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryPercentIncome
+AS
+SELECT RunType,
+       RunId,
+       URN,
+       TotalPupils,
+       IIF(TotalIncome > 0.0, (TotalExpenditure / TotalIncome) * 100, NULL)                          AS TotalExpenditure,
+       IIF(TotalIncome > 0.0, (TotalTeachingSupportStaffCosts / TotalIncome) * 100, NULL)            AS TotalTeachingSupportStaffCosts,
+       IIF(TotalIncome > 0.0, (RevenueReserve / TotalIncome)  * 100, NULL)                          AS RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryActual
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultActual
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultActual AS
+SELECT RunId,
+       URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryActual
+WHERE RunType = 'default'
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultPerUnit
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultPerUnit AS
+SELECT RunId,
+       URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryPerUnit
+WHERE RunType = 'default'
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultPercentExpenditure
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultPercentExpenditure AS
+SELECT RunId,
+       URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryPercentExpenditure
+WHERE RunType = 'default'
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultPercentIncome
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultPercentIncome AS
+SELECT RunId,
+       URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryPercentIncome
+WHERE RunType = 'default'
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultCurrentActual
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultCurrentActual AS
+SELECT URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryDefaultActual
+WHERE RunId = (SELECT Value FROM Parameters WHERE Name = 'CurrentYear')
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultCurrentPerUnit
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultCurrentPerUnit AS
+SELECT URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryDefaultPerUnit
+WHERE RunId = (SELECT Value FROM Parameters WHERE Name = 'CurrentYear')
+    GO
+
+DROP VIEW IF EXISTS VVW_SchoolsFinancialSummaryDefaultCurrentPercentExpenditure
+    GO
+
+CREATE VIEW VVW_SchoolsFinancialSummaryDefaultCurrentPercentExpenditure AS
+SELECT URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryDefaultPercentExpenditure
+WHERE RunId = (SELECT Value FROM Parameters WHERE Name = 'CurrentYear')
+    GO
+
+DROP VIEW IF EXISTS VW_SchoolsFinancialSummaryDefaultCurrentPercentIncome
+    GO
+
+CREATE VIEW VW_SchoolsFinancialSummaryDefaultCurrentPercentIncome AS
+SELECT URN,
+       TotalPupils,
+       TotalExpenditure,
+       TotalTeachingSupportStaffCosts,
+       RevenueReserve,
+       FinanceType,
+       OverallPhase,
+       LACode,
+       TrustCompanyNumber,
+       NurseryProvision,
+       SpecialClassProvision,
+       SixthFormProvision
+FROM VW_SchoolsFinancialSummaryDefaultPercentIncome
+WHERE RunId = (SELECT Value FROM Parameters WHERE Name = 'CurrentYear')
+    GO
+
+
