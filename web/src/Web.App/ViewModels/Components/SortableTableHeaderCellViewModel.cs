3187691cf7ab64556f8491bbffe43d27963839a3
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-10-21 06:10:48 +0100

    feat: Removed `<a>` rendering option for `SortableTableHeaderCellViewComponent`

--- web/src/Web.App/ViewModels/Components/SortableTableHeaderCellViewModel.cs
+++ web/src/Web.App/ViewModels/Components/SortableTableHeaderCellViewModel.cs
@@ -1,55 +1,20 @@
 namespace Web.App.ViewModels.Components;
 
-public class SortableTableHeaderCellLinkViewModel(
-    string label,
-    string sortField,
-    string sortKey,
-    string sortDelimeter,
-    string? currentSortField,
-    string? currentSortOrder,
-    string baseUrl,
-    string? className,
-    string? tableId) : SortableTableHeaderCellViewModel(label, sortField, sortDelimeter, currentSortField, currentSortOrder, className, tableId)
-{
-    public string Href
-    {
-        get
-        {
-            var queryPrefix = baseUrl.Contains('?') ? "&" : "?";
-            return $"{baseUrl}{queryPrefix}{sortKey}={SortValue}";
-        }
-    }
-}
-
-public class SortableTableHeaderCellButtonViewModel(
+public class SortableTableHeaderCellViewModel(
     string label,
     string sortField,
     string sortDelimeter,
     string sortKey,
-    string? currentSortField,
-    string? currentSortOrder,
-    string? className,
-    string? tableId) : SortableTableHeaderCellViewModel(label, sortField, sortDelimeter, currentSortField, currentSortOrder, className, tableId)
-{
-    public string AriaPressed => string.IsNullOrWhiteSpace(CurrentSortField)
-        ? "mixed"
-        : CurrentSortField == SortField
-            ? "true"
-            : "false";
-    public string SortKey => sortKey;
-}
-
-public abstract class SortableTableHeaderCellViewModel(
-    string label,
-    string sortField,
-    string sortDelimeter,
     string? currentSortField,
     string? currentSortOrder,
     string? className,
     string? tableId)
 {
-    protected string? CurrentSortField => currentSortField;
-
+    public string AriaPressed => string.IsNullOrWhiteSpace(currentSortField)
+        ? "mixed"
+        : currentSortField == SortField
+            ? "true"
+            : "false";
     public string AriaSort => Sort switch
     {
         "asc" => "ascending",
@@ -58,9 +23,10 @@ public abstract class SortableTableHeaderCellViewModel(
     };
     public string ClassName => className ?? string.Empty;
     public string Label => label;
-    public string? Sort => CurrentSortField == sortField ? currentSortOrder : null;
+    public string? Sort => currentSortField == sortField ? currentSortOrder : null;
     public string SortField => sortField;
     public string SortIntent => Sort == "asc" ? "desc" : "asc";
     public string SortValue => $"{SortField}{sortDelimeter}{SortIntent}";
     public string? TableId => tableId;
+    public string SortKey => sortKey;
 }
--- web/tests/Web.Tests/ViewComponents/GivenASortableTableHeaderCellViewComponent.cs
+++ web/tests/Web.Tests/ViewComponents/GivenASortableTableHeaderCellViewComponent.cs
@@ -32,7 +32,7 @@ public class SortableTableHeaderCellViewComponentTests
     }
 
     [Fact]
-    public void ShouldReturnPassThroughValuesForLink()
+    public void ShouldReturnPassThroughValues()
     {
         // arrange
         const string label = nameof(label);
@@ -45,120 +45,7 @@ public class SortableTableHeaderCellViewComponentTests
 
         // assert
         Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellLinkViewModel;
-        Assert.NotNull(model);
-        Assert.Equal(label, model.Label);
-        Assert.Equal(sortField, model.SortField);
-        Assert.Equal(className, model.ClassName);
-        Assert.Equal(tableId, model.TableId);
-    }
-
-    [Theory]
-    [InlineData("testField", "sortKey", "~", "testField", "asc", "other=value", "?other=value&sortKey=testField~desc")]
-    [InlineData("testField", "sortKey", "~", "testField", "desc", "other=value", "?other=value&sortKey=testField~asc")]
-    [InlineData("testField", "sortKey", "~", null, null, "other=value", "?other=value&sortKey=testField~asc")]
-    [InlineData("testField", "sortKey", "~", null, null, null, "?sortKey=testField~asc")]
-    [InlineData("testField", "sortKey", "~", "otherField", "asc", null, "?sortKey=testField~asc")]
-    public void ShouldGenerateHrefWithSortFieldAndSortOrderForLink(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? baseQuery, string expectedQueryString)
-    {
-        // arrange
-        const string label = nameof(label);
-        var queryValues = new Dictionary<string, StringValues>();
-        if (!string.IsNullOrWhiteSpace(baseQuery))
-        {
-            foreach (var kvp in baseQuery.Split("&"))
-            {
-                var parts = kvp.Split("=");
-                queryValues.Add(parts.First(), parts.Last());
-            }
-        }
-
-        if (!string.IsNullOrWhiteSpace(currentSortField) && !string.IsNullOrWhiteSpace(currentSortOrder))
-        {
-            queryValues.Add(sortKey, $"{currentSortField}{sortDelimeter}{currentSortOrder}");
-        }
-
-        _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
-
-        // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
-
-        // assert
-        Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellLinkViewModel;
-        Assert.NotNull(model);
-        Assert.Equal($"{_path}{expectedQueryString}", model.Href);
-    }
-
-    [Theory]
-    [InlineData("testField", "sortKey", "~", "testField", "asc", "asc")]
-    [InlineData("testField", "sortKey", "~", "testField", "desc", "desc")]
-    [InlineData("testField", "sortKey", "~", null, null, null)]
-    [InlineData("testField", "sortKey", "~", "otherField", "asc", null)]
-    public void ShouldReturnCurrentSortForFieldForLink(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedSort)
-    {
-        // arrange
-        const string label = nameof(label);
-        var queryValues = new Dictionary<string, StringValues>();
-        if (!string.IsNullOrWhiteSpace(currentSortField) && !string.IsNullOrWhiteSpace(currentSortOrder))
-        {
-            queryValues.Add(sortKey, $"{currentSortField}{sortDelimeter}{currentSortOrder}");
-        }
-
-        _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
-
-        // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
-
-        // assert
-        Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellLinkViewModel;
-        Assert.NotNull(model);
-        Assert.Equal(expectedSort, model.Sort);
-    }
-
-    [Theory]
-    [InlineData("testField", "sortKey", "~", "testField", "asc", "ascending")]
-    [InlineData("testField", "sortKey", "~", "testField", "desc", "descending")]
-    [InlineData("testField", "sortKey", "~", null, null, "none")]
-    [InlineData("testField", "sortKey", "~", "otherField", "asc", "none")]
-    public void ShouldReturnCurrentAriaSortForFieldForLink(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedAriaSort)
-    {
-        // arrange
-        const string label = nameof(label);
-        var queryValues = new Dictionary<string, StringValues>();
-        if (!string.IsNullOrWhiteSpace(currentSortField) && !string.IsNullOrWhiteSpace(currentSortOrder))
-        {
-            queryValues.Add(sortKey, $"{currentSortField}{sortDelimeter}{currentSortOrder}");
-        }
-
-        _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
-
-        // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
-
-        // assert
-        Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellLinkViewModel;
-        Assert.NotNull(model);
-        Assert.Equal(expectedAriaSort, model.AriaSort);
-    }
-
-    [Fact]
-    public void ShouldReturnPassThroughValuesForButton()
-    {
-        // arrange
-        const string label = nameof(label);
-        const string sortField = nameof(sortField);
-        const string className = nameof(className);
-        const string tableId = nameof(tableId);
-
-        // act
-        var result = _component.Invoke(label, sortField, button: true, className: className, tableId: tableId) as ViewViewComponentResult;
-
-        // assert
-        Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellButtonViewModel;
+        var model = result.ViewData?.Model as SortableTableHeaderCellViewModel;
         Assert.NotNull(model);
         Assert.Equal(label, model.Label);
         Assert.Equal(sortField, model.SortField);
@@ -172,7 +59,7 @@ public class SortableTableHeaderCellViewComponentTests
     [InlineData("testField", "sortKey", "~", null, null, "other=value", "testField~asc")]
     [InlineData("testField", "sortKey", "~", null, null, null, "testField~asc")]
     [InlineData("testField", "sortKey", "~", "otherField", "asc", null, "testField~asc")]
-    public void ShouldGenerateSortValueWithSortFieldAndSortOrderForButton(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? baseQuery, string expectedSortValue)
+    public void ShouldGenerateSortValueWithSortFieldAndSortOrderFor(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? baseQuery, string expectedSortValue)
     {
         // arrange
         const string label = nameof(label);
@@ -194,11 +81,11 @@ public class SortableTableHeaderCellViewComponentTests
         _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
 
         // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter, true) as ViewViewComponentResult;
+        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
 
         // assert
         Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellButtonViewModel;
+        var model = result.ViewData?.Model as SortableTableHeaderCellViewModel;
         Assert.NotNull(model);
         Assert.Equal(sortKey, model.SortKey);
         Assert.Equal(expectedSortValue, model.SortValue);
@@ -209,7 +96,7 @@ public class SortableTableHeaderCellViewComponentTests
     [InlineData("testField", "sortKey", "~", "testField", "desc", "desc")]
     [InlineData("testField", "sortKey", "~", null, null, null)]
     [InlineData("testField", "sortKey", "~", "otherField", "asc", null)]
-    public void ShouldReturnCurrentSortForFieldForButton(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedSort)
+    public void ShouldReturnCurrentSortForFieldFor(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedSort)
     {
         // arrange
         const string label = nameof(label);
@@ -222,11 +109,11 @@ public class SortableTableHeaderCellViewComponentTests
         _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
 
         // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter, true) as ViewViewComponentResult;
+        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
 
         // assert
         Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellButtonViewModel;
+        var model = result.ViewData?.Model as SortableTableHeaderCellViewModel;
         Assert.NotNull(model);
         Assert.Equal(expectedSort, model.Sort);
     }
@@ -236,7 +123,7 @@ public class SortableTableHeaderCellViewComponentTests
     [InlineData("testField", "sortKey", "~", "testField", "desc", "descending", "true")]
     [InlineData("testField", "sortKey", "~", null, null, "none", "mixed")]
     [InlineData("testField", "sortKey", "~", "otherField", "asc", "none", "false")]
-    public void ShouldReturnCurrentAriaValuesForFieldForButton(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedAriaSort, string? expectedAriaPressed)
+    public void ShouldReturnCurrentAriaValuesForFieldFor(string sortField, string sortKey, string sortDelimeter, string? currentSortField, string? currentSortOrder, string? expectedAriaSort, string? expectedAriaPressed)
     {
         // arrange
         const string label = nameof(label);
@@ -249,11 +136,11 @@ public class SortableTableHeaderCellViewComponentTests
         _httpContext.Request.QueryString = queryValues.Count > 0 ? QueryString.Create(queryValues) : QueryString.Empty;
 
         // act
-        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter, true) as ViewViewComponentResult;
+        var result = _component.Invoke(label, sortField, sortKey, sortDelimeter) as ViewViewComponentResult;
 
         // assert
         Assert.NotNull(result);
-        var model = result.ViewData?.Model as SortableTableHeaderCellButtonViewModel;
+        var model = result.ViewData?.Model as SortableTableHeaderCellViewModel;
         Assert.NotNull(model);
         Assert.Equal(expectedAriaSort, model.AriaSort);
         Assert.Equal(expectedAriaPressed, model.AriaPressed);
