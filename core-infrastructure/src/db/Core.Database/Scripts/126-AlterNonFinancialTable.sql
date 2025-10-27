0578b9a05b97d2a28b8c093309cbc58ff0e5ecd6
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2025-10-27 11:02:52 +0000

    check NonFinancial table exists before altering it

--- core-infrastructure/src/db/Core.Database/Scripts/126-AlterNonFinancialTable.sql
+++ core-infrastructure/src/db/Core.Database/Scripts/126-AlterNonFinancialTable.sql
@@ -1,2 +1,7 @@
-ALTER TABLE NonFinancial ADD EHCPlan smallint NULL;
-ALTER TABLE NonFinancial ADD SENSupport smallint NULL;
+IF EXISTS(SELECT 1
+          FROM INFORMATION_SCHEMA.TABLES
+          WHERE table_name = 'NonFinancial')
+    BEGIN
+    ALTER TABLE NonFinancial ADD EHCPlan smallint NULL;
+    ALTER TABLE NonFinancial ADD SENSupport smallint NULL;
+    END;
