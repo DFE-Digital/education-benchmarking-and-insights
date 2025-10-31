74edb2cf3bc7dc24c73cdaf7f17ade43ea9fb7fb
Josh Langford <josh.langford@hippodigital.co.uk>
2025-10-31 10:52:48 +0000

    chore: add platform api tests for schools workforce summary endpoint

--- platform/tests/Platform.ApiTests/Steps/LocalAuthoritiesSchoolsSteps.cs
+++ platform/tests/Platform.ApiTests/Steps/LocalAuthoritiesSchoolsSteps.cs
@@ -9,16 +9,28 @@ namespace Platform.ApiTests.Steps;
 [Scope(Feature = "Local authorities schools endpoints")]
 public class LocalAuthoritiesSchoolsSteps(LocalAuthorityFinancesApiDriver api)
 {
-    private const string SchoolsFinanceKey = "schools-finance-key";
+    private const string SchoolsKey = "schools-key";
 
-    [Given("a local authorities schools request with code '(.*)' and query parameters:")]
-    public void GivenALocalAuthoritiesSchoolsRequestWithCodeAndQueryParameters(string code, DataTable table)
+    [Given("a local authorities schools finance summary request with code '(.*)' and query parameters:")]
+    public void GivenALocalAuthoritiesSchoolsFinanceSummaryRequestWithCodeAndQueryParameters(string code, DataTable table)
     {
         var queryString = BuildQueryString(table);
 
-        api.CreateRequest(SchoolsFinanceKey, new HttpRequestMessage
+        api.CreateRequest(SchoolsKey, new HttpRequestMessage
         {
             RequestUri = new Uri($"/api/local-authorities/{code}/schools/finance{queryString}", UriKind.Relative),
+            Method = HttpMethod.Get
+        });
+    }
+
+    [Given("a local authorities schools workforce summary request with code '(.*)' and query parameters:")]
+    public void GivenALocalAuthoritiesSchoolsWorkforceSummaryRequestWithCodeAndQueryParameters(string code, DataTable table)
+    {
+        var queryString = BuildQueryString(table);
+
+        api.CreateRequest(SchoolsKey, new HttpRequestMessage
+        {
+            RequestUri = new Uri($"/api/local-authorities/{code}/schools/workforce{queryString}", UriKind.Relative),
             Method = HttpMethod.Get
         });
     }
@@ -32,7 +44,7 @@ public class LocalAuthoritiesSchoolsSteps(LocalAuthorityFinancesApiDriver api)
     [Then("the local authorities schools result should be ok and match the expected output of '(.*)'")]
     public async Task ThenTheLocalAuthoritiesSchoolsResultShouldBeOkAndMatchTheExpectedOutputOf(string testFile)
     {
-        var response = api[SchoolsFinanceKey].Response;
+        var response = api[SchoolsKey].Response;
         AssertHttpResponse.IsOk(response);
 
         var content = await response.Content.ReadAsStringAsync();
@@ -46,7 +58,7 @@ public class LocalAuthoritiesSchoolsSteps(LocalAuthorityFinancesApiDriver api)
     [Then("the local authorities schools result should be bad request and match the expected output in '(.*)'")]
     public async Task ThenTheLocalAuthoritiesSchoolsResultShouldBeBadRequestAndMatchTheExpectedOutputIn(string testFile)
     {
-        var response = api[SchoolsFinanceKey].Response;
+        var response = api[SchoolsKey].Response;
         AssertHttpResponse.IsBadRequest(response);
 
         var content = await response.Content.ReadAsStringAsync();
@@ -60,7 +72,7 @@ public class LocalAuthoritiesSchoolsSteps(LocalAuthorityFinancesApiDriver api)
     [Then("the local authorities schools result should be not found")]
     public void ThenTheLocalAuthoritiesSchoolsResultShouldBeNotFound()
     {
-        var response = api[SchoolsFinanceKey].Response;
+        var response = api[SchoolsKey].Response;
         AssertHttpResponse.IsNotFound(response);
     }
 
