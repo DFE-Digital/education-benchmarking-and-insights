774c0f94fde4675918d175217fcaeadd9f676a75
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-11-05 15:46:49 +0000

    feat: Added `KS4ProgressBanding` feature flag

--- web/src/Web.App/Constants/FeatureFlags.cs
+++ web/src/Web.App/Constants/FeatureFlags.cs
@@ -9,4 +9,5 @@ public static class FeatureFlags
     public const string TrustItSpendBreakdown = nameof(TrustItSpendBreakdown);
     public const string FbisForTrust = nameof(FbisForTrust);
     public const string LocalAuthorityHomepageV2 = nameof(LocalAuthorityHomepageV2);
+    public const string KS4ProgressBanding = nameof(KS4ProgressBanding);
 }
