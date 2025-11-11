86b8df4b26786ae69581ada2d4ed88e9778f2657
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-11-11 07:25:11 +0000

    feat: When resolving progress bandings for default comparators, include both Pupil and Building sets

--- web/src/Web.App/Domain/Benchmark/SchoolComparatorSet.cs
+++ web/src/Web.App/Domain/Benchmark/SchoolComparatorSet.cs
@@ -7,11 +7,12 @@ public record SchoolComparatorSet
 {
     public string[] Pupil { get; set; } = [];
     public string[] Building { get; set; } = [];
+    public string[] All => Pupil.Union(Building).ToArray();
 }
 
 public record UserDefinedSchoolComparatorSet
 {
     public string? RunId { get; set; }
     public long? TotalSchools { get; set; }
-    public string[] Set { get; set; } = Array.Empty<string>();
+    public string[] Set { get; set; } = [];
 }
