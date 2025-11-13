67c74d4469d103b4651ccb174dfc357eb704fde8
Josh Langford <josh.langford@hippodigital.co.uk>
2025-11-13 14:16:13 +0000

    feat: add new view to ComparatorSetDetailsViewComponent to display content as bulleted list

--- web/src/Web.App/ViewComponents/ComparatorSetDetailsViewComponent.cs
+++ web/src/Web.App/ViewComponents/ComparatorSetDetailsViewComponent.cs
@@ -5,6 +5,12 @@ namespace Web.App.ViewComponents;
 
 public class ComparatorSetDetailsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(string identifier, bool hasUserDefinedSet, bool hasCustomData, bool hasMissingComparatorSet = false, ComparatorSetType type = ComparatorSetType.Costs) =>
-        View(new ComparatorSetDetailsViewModel(identifier, hasUserDefinedSet, hasCustomData, hasMissingComparatorSet, type));
+    public IViewComponentResult Invoke(string identifier, bool hasUserDefinedSet, bool hasCustomData, bool hasMissingComparatorSet = false, ComparatorSetType type = ComparatorSetType.Costs, bool showAsBulletedList = false)
+    {
+        var model = new ComparatorSetDetailsViewModel(identifier, hasUserDefinedSet, hasCustomData, hasMissingComparatorSet, type);
+
+        return showAsBulletedList
+            ? View("BulletedList", model)
+            : View(model);
+    }
 }
--- web/tests/Web.Integration.Tests/Pages/Schools/Comparison/WhenViewingComparison.cs
+++ web/tests/Web.Integration.Tests/Pages/Schools/Comparison/WhenViewingComparison.cs
@@ -16,14 +16,60 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
     [InlineData(EstablishmentTypes.Academies, true, false)]
     [InlineData(EstablishmentTypes.Maintained, true, false)]
     [InlineData(EstablishmentTypes.Maintained, true, true)]
+    [InlineData(EstablishmentTypes.Academies, true, true)]
     [InlineData(EstablishmentTypes.Academies, false, false)]
     [InlineData(EstablishmentTypes.Maintained, false, false)]
     [InlineData(EstablishmentTypes.Maintained, false, true)]
+    [InlineData(EstablishmentTypes.Academies, false, true)]
     public async Task CanDisplay(string financeType, bool ks4ProgressBandingEnabled, bool hasProgressIndicators)
     {
         var (page, school) = await SetupNavigateInitPage(financeType, ks4ProgressBandingEnabled, hasProgressIndicators);
 
         AssertPageLayout(page, school, ks4ProgressBandingEnabled, hasProgressIndicators);
+    }
+
+    [Theory]
+    [InlineData(true, false, false)]
+    [InlineData(false, false, false)]
+    [InlineData(true, true, false)]
+    [InlineData(false, true, false)]
+    [InlineData(true, false, true)]
+    [InlineData(false, false, true)]
+    public async Task CanDisplayCorrectComparatorSetDetails(
+        bool ks4ProgressBandingEnabled,
+        bool withCustomUserData,
+        bool withUserDefinedUserData)
+    {
+        var (page, school) = await SetupNavigateInitPage(
+            EstablishmentTypes.Maintained,
+            ks4ProgressBandingEnabled,
+            true,
+            withCustomUserData,
+            withUserDefinedUserData);
+
+        AssertPageLayout(
+            page,
+            school,
+            ks4ProgressBandingEnabled,
+            true,
+            withCustomUserData,
+            withUserDefinedUserData);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task CanDisplayCorrectComparatorSetDetailsWhenMissingComparatorSet(bool ks4ProgressBandingEnabled)
+    {
+        var (page, school) = await SetupNavigateInitPage(
+            EstablishmentTypes.Maintained,
+            ks4ProgressBandingEnabled,
+            withEmptyComparatorSet: true);
+
+        var comparatorSetDetailsElement = page.QuerySelector("[data-testid=comparator-set-details]"); ;
+        Assert.NotNull(comparatorSetDetailsElement);
+
+        AssertMissingComparatorSetCreatedComparatorSetDetailsContent(comparatorSetDetailsElement, school, ks4ProgressBandingEnabled);
     }
 
     [Theory]
@@ -96,11 +142,18 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
             HttpStatusCode.InternalServerError);
     }
 
-    private async Task<(IHtmlDocument page, School school)> SetupNavigateInitPage(string financeType, bool ks4ProgressBandingEnabled = true, bool hasProgressIndicators = true)
+    private async Task<(IHtmlDocument page, School school)> SetupNavigateInitPage(
+        string financeType,
+        bool ks4ProgressBandingEnabled = true,
+        bool hasProgressIndicators = true,
+        bool withCustomUserData = false,
+        bool withUserDefinedUserData = false,
+        bool withEmptyComparatorSet = false)
     {
         var school = Fixture.Build<School>()
             .With(x => x.URN, "123456")
             .With(x => x.FinanceType, financeType)
+            .With(x => x.TrustCompanyNumber, financeType == EstablishmentTypes.Academies ? "87654321" : string.Empty)
             .Create();
 
         var characteristics = Fixture.Build<SchoolCharacteristic>()
@@ -112,6 +165,30 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
             .With(x => x.Building, ["building"])
             .Create();
 
+        var emptyComparatorSet = Fixture.Build<SchoolComparatorSet>()
+            .Without(x => x.Building)
+            .Without(x => x.Pupil)
+            .Create();
+
+        var customUserData = new[]
+        {
+            new UserData
+            {
+                Type = "custom-data",
+                Id = "123",
+                Status = "complete"
+            }
+        };
+
+        var userDefinedSetUserData = new[]
+        {
+            new UserData
+            {
+                Type = "comparator-set",
+                Id = "456"
+            }
+        };
+
         string[] features = ks4ProgressBandingEnabled ? [] : [FeatureFlags.KS4ProgressBanding];
         var page = await Client
             .SetupDisableFeatureFlags(features)
@@ -119,14 +196,23 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
             .SetupInsights()
             .SetupSchoolInsight(characteristics)
             .SetupExpenditure(school)
-            .SetupUserData()
-            .SetupComparatorSet(school, comparatorSet)
+            .SetupUserData(withUserDefinedUserData
+                ? userDefinedSetUserData
+                : withCustomUserData
+                    ? customUserData : null)
+            .SetupComparatorSet(school, withEmptyComparatorSet ? emptyComparatorSet : comparatorSet)
             .Navigate(Paths.SchoolComparison(school.URN));
 
         return (page, school);
     }
 
-    private static void AssertPageLayout(IHtmlDocument page, School school, bool ks4ProgressBandingEnabled = true, bool hasProgressIndicators = true)
+    private static void AssertPageLayout(
+        IHtmlDocument page,
+        School school,
+        bool ks4ProgressBandingEnabled = true,
+        bool hasProgressIndicators = true,
+        bool withCustomUserData = false,
+        bool withUserDefinedUserData = false)
     {
         var expectedBreadcrumbs = new[]
         {
@@ -141,8 +227,11 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
 
         var dataSourceElement = page.QuerySelector("main > div > div:nth-child(3) > div");
         Assert.NotNull(dataSourceElement);
-
         AssertDataSource(dataSourceElement, school, ks4ProgressBandingEnabled, hasProgressIndicators);
+
+        var comparatorSetDetailsElement = page.QuerySelector("[data-testid=comparator-set-details]");
+        Assert.NotNull(comparatorSetDetailsElement);
+        AssertComparatorSetDetails(comparatorSetDetailsElement, school, ks4ProgressBandingEnabled, withCustomUserData, withUserDefinedUserData, false);
 
         var comparisonComponent = page.GetElementById(ks4ProgressBandingEnabled ? "compare-your-costs-2" : "compare-your-costs");
         Assert.NotNull(comparisonComponent);
@@ -192,6 +281,171 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
         }
     }
 
+    private static void AssertComparatorSetDetails(IElement comparatorSetDetailsElement, School school, bool ks4ProgressBandingEnabled, bool withCustomUserData, bool withUserDefinedUserData, bool emptyComparatorSet)
+    {
+        if (withCustomUserData)
+        {
+            AssertCustomDataCreatedComparatorSetDetailsContent(comparatorSetDetailsElement, school, ks4ProgressBandingEnabled);
+        }
+        else if (withUserDefinedUserData)
+        {
+            AssertUserDefinedSetCreatedComparatorSetDetailsContent(comparatorSetDetailsElement, school, ks4ProgressBandingEnabled);
+        }
+        else
+        {
+            AssertComparatorSetDetailsDefaultContent(comparatorSetDetailsElement, school, ks4ProgressBandingEnabled);
+        }
+    }
+
+    private static void AssertComparatorSetDetailsDefaultContent(IElement comparatorSetDetailsElement, School school, bool ks4ProgressBandingEnabled)
+    {
+        if (ks4ProgressBandingEnabled)
+        {
+            var introParagraph = comparatorSetDetailsElement.QuerySelector("p.govuk-body");
+            Assert.NotNull(introParagraph);
+            Assert.Equal("You can:", introParagraph.TextContent.Trim());
+
+            var list = comparatorSetDetailsElement.QuerySelector("ul");
+            Assert.NotNull(list);
+            var listItems = list.QuerySelectorAll("li");
+            Assert.Equal(3, listItems.Length);
+
+            DocumentAssert.TextEqual(listItems[0], "view the 2 sets of similar schools we've chosen to benchmark this school's spending against", true);
+            DocumentAssert.Link(listItems[0].QuerySelector("a"), "view the 2 sets of similar schools we've chosen", Paths.SchoolComparators(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(listItems[1], "create or save your own set of schools to benchmark against", true);
+            DocumentAssert.Link(listItems[1].QuerySelector("a"), "create or save your own set of schools", Paths.SchoolComparatorsCreate(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(listItems[2], "Change the data for this school", true);
+            DocumentAssert.Link(listItems[2].QuerySelector("a"), "Change the data", Paths.SchoolCustomData(school.URN).ToAbsolute());
+        }
+        else
+        {
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll("p.govuk-body");
+            Assert.Equal(3, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "We've chosen 2 sets of similar schools to benchmark this school's spending against.", true);
+            DocumentAssert.Link(paragraphs[0].QuerySelector("a"), "We've chosen 2 sets of similar schools", Paths.SchoolComparators(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(paragraphs[1], "Create or save your own set of schools to benchmark against");
+            DocumentAssert.Link(paragraphs[1].QuerySelector("a"), "Create or save your own set of schools to benchmark against", Paths.SchoolComparatorsCreate(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(paragraphs[2], "Change the data for this school");
+            DocumentAssert.Link(paragraphs[2].QuerySelector("a"), "Change the data for this school", Paths.SchoolCustomData(school.URN).ToAbsolute());
+        }
+    }
+
+    private static void AssertMissingComparatorSetCreatedComparatorSetDetailsContent(IElement comparatorSetDetailsElement, School school, bool ks4ProgressBandingEnabled)
+    {
+        if (ks4ProgressBandingEnabled)
+        {
+            var warning = comparatorSetDetailsElement.QuerySelector("div.govuk-warning-text strong.govuk-warning-text__text");
+            Assert.NotNull(warning);
+            DocumentAssert.TextEqual(warning, "Warning There isn't enough information available to create a set of similar schools.", true);
+
+            var introParagraph = comparatorSetDetailsElement.QuerySelector("p.govuk-body");
+            Assert.NotNull(introParagraph);
+            DocumentAssert.TextEqual(introParagraph, "You can:");
+
+            var list = comparatorSetDetailsElement.QuerySelector("ul.govuk-list.govuk-list--bullet");
+            Assert.NotNull(list);
+            var listItems = list.QuerySelectorAll("li");
+            Assert.Equal(2, listItems.Length);
+
+            DocumentAssert.TextEqual(listItems[0], "create or save your own set of schools to benchmark against", true);
+            DocumentAssert.Link(listItems[0].QuerySelector("a"), "create or save your own set of schools", Paths.SchoolComparatorsCreate(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(listItems[1], "Change the data for this school", true);
+            DocumentAssert.Link(listItems[1].QuerySelector("a"), "Change the data", Paths.SchoolCustomData(school.URN).ToAbsolute());
+        }
+        else
+        {
+            var warning = comparatorSetDetailsElement.QuerySelector("div.govuk-warning-text strong.govuk-warning-text__text");
+            Assert.NotNull(warning);
+            DocumentAssert.TextEqual(warning, "Warning There isn't enough information available to create a set of similar schools.", true);
+
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll("p.govuk-body");
+            Assert.NotNull(paragraphs);
+            Assert.Equal(2, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "Create or save your own set of schools to benchmark against");
+            DocumentAssert.Link(paragraphs[0].QuerySelector("a"), "Create or save your own set of schools to benchmark against", Paths.SchoolComparatorsCreate(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(paragraphs[1], "Change the data for this school");
+            DocumentAssert.Link(paragraphs[1].QuerySelector("a"), "Change the data for this school", Paths.SchoolCustomData(school.URN).ToAbsolute());
+        }
+    }
+
+    private static void AssertUserDefinedSetCreatedComparatorSetDetailsContent(IElement comparatorSetDetailsElement, School school, bool ks4ProgressBandingEnabled)
+    {
+        if (ks4ProgressBandingEnabled)
+        {
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll(":scope > p.govuk-body");
+            Assert.NotNull(paragraphs);
+            Assert.Equal(2, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "You are now comparing with your chosen schools.");
+            DocumentAssert.TextEqual(paragraphs[1], "You can:");
+
+            var list = comparatorSetDetailsElement.QuerySelector("ul.govuk-list.govuk-list--bullet");
+            Assert.NotNull(list);
+            var listItems = list.QuerySelectorAll("li");
+            Assert.Equal(1, listItems.Length);
+
+            DocumentAssert.TextEqual(listItems[0], "Change your similar schools");
+            DocumentAssert.Link(listItems[0].QuerySelector("a"), "Change your similar schools", Paths.SchoolComparatorsUserDefined(school.URN).ToAbsolute());
+        }
+        else
+        {
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll("p.govuk-body");
+            Assert.NotNull(paragraphs);
+            Assert.Equal(2, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "You are now comparing with your chosen schools.");
+            DocumentAssert.TextEqual(paragraphs[1], "Change your similar schools");
+            DocumentAssert.Link(paragraphs[1].QuerySelector("a"), "Change your similar schools", Paths.SchoolComparatorsUserDefined(school.URN).ToAbsolute());
+        }
+    }
+
+    private static void AssertCustomDataCreatedComparatorSetDetailsContent(IElement comparatorSetDetailsElement, School school, bool ks4ProgressBandingEnabled)
+    {
+        if (ks4ProgressBandingEnabled)
+        {
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll(":scope > p.govuk-body");
+            Assert.NotNull(paragraphs);
+            Assert.Equal(2, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "The information displayed is the originally reported data, not the customised data that was provided.");
+            DocumentAssert.TextEqual(paragraphs[1], "You can:");
+
+            var list = comparatorSetDetailsElement.QuerySelector("ul.govuk-list.govuk-list--bullet");
+            Assert.NotNull(list);
+            var listItems = list.QuerySelectorAll("li");
+            Assert.Equal(2, listItems.Length);
+
+            DocumentAssert.TextEqual(listItems[0], "view the 2 sets of similar schools we've chosen to benchmark this school's spending against", true);
+            DocumentAssert.Link(listItems[0].QuerySelector("a"), "view the 2 sets of similar schools we've chosen", Paths.SchoolComparators(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(listItems[1], "View custom data set");
+            DocumentAssert.Link(listItems[1].QuerySelector("a"), "View custom data set", Paths.SchoolCustomisedData(school.URN).ToAbsolute());
+        }
+        else
+        {
+            var paragraphs = comparatorSetDetailsElement.QuerySelectorAll("p.govuk-body");
+            Assert.NotNull(paragraphs);
+            Assert.Equal(3, paragraphs.Length);
+
+            DocumentAssert.TextEqual(paragraphs[0], "We've chosen 2 sets of similar schools to benchmark this school's spending against.", true);
+            DocumentAssert.Link(paragraphs[0].QuerySelector("a"), "We've chosen 2 sets of similar schools", Paths.SchoolComparators(school.URN).ToAbsolute());
+
+            DocumentAssert.TextEqual(paragraphs[1], "The information displayed is the originally reported data, not the customised data that was provided.");
+
+            DocumentAssert.TextEqual(paragraphs[2], "View custom data set");
+            DocumentAssert.Link(paragraphs[2].QuerySelector("a"), "View custom data set", Paths.SchoolCustomisedData(school.URN).ToAbsolute());
+
+        }
+    }
+
     private static IHtmlCollection<IElement> GetInsetText(IElement element)
     {
         var inset = element.QuerySelector(".govuk-inset-text");
@@ -223,7 +477,7 @@ public class WhenViewingComparison(SchoolBenchmarkingWebAppClient client)
     {
         Assert.Equal(2, text.Length);
         DocumentAssert.TextEqual(text[0], "This school's data covers the financial year April 2020 to March 2021 consistent financial reporting return (CFR).");
-        DocumentAssert.TextEqual(text[1], "Progress 8 data uses the latest data available from the academic year 2023 to 2024 and is taken from Compare school and college performance in England");
+        DocumentAssert.TextEqual(text[1], "Progress 8 data uses the latest data available from the academic year 2023 to 2024 and is taken from Compare school and college performance in England", true);
 
         var link = text[1].QuerySelector("a");
         DocumentAssert.Link(link, "Compare school and college performance in England", $"https://www.compare-school-performance.service.gov.uk/school/{school.URN}");
