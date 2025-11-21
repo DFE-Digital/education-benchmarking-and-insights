00390f7308362360c1ebb0a7a3ad8b41b01d5d66
Marc Woolfson <marc.woolfson@education.gov.uk>
2025-11-21 15:15:30 +0000

    chore: Added `[Required]` annotations to `UriOptions` to throw `OptionsValidationException` if values missing

--- web/src/Web.App/Builders/UriOptions.cs
+++ web/src/Web.App/Builders/UriOptions.cs
@@ -1,10 +1,15 @@
 ﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable PropertyCanBeMadeInitOnly.Global
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.App.Builders;
 
 public record UriOptions
 {
-    public string? GiasBaseUrl { get; set; }
-    public string? CompareSchoolPerformanceBaseUrl { get; set; }
+    [Required]
+    public required string GiasBaseUrl { get; set; }
+
+    [Required]
+    public required string CompareSchoolPerformanceBaseUrl { get; set; }
 }
