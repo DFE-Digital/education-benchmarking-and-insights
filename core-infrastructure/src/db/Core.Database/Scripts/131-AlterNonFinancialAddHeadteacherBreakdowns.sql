4b379dfcd38fdbcbeea65a00345bf1b82a0011de
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2025-12-31 11:25:59 +0000

    specify decimal precision

--- core-infrastructure/src/db/Core.Database/Scripts/131-AlterNonFinancialAddHeadteacherBreakdowns.sql
+++ core-infrastructure/src/db/Core.Database/Scripts/131-AlterNonFinancialAddHeadteacherBreakdowns.sql
@@ -1,9 +1,9 @@
 ALTER TABLE dbo.NonFinancial
-    ADD HeadTeacherHeadcount            decimal NULL,
-        DeputyHeadTeacherHeadcount      decimal NULL,
-        AssistantHeadTeacherHeadcount   decimal NULL,
-        LeadershipNonTeacherHeadcount   decimal NULL,
-        HeadTeacherFTE                  decimal NULL,
-        DeputyHeadTeacherFTE            decimal NULL,
-        AssistantHeadTeacherFTE         decimal NULL,
-        LeadershipNonTeacherFTE         decimal NULL;
+    ADD HeadTeacherHeadcount            decimal(16,2) NULL,
+        DeputyHeadTeacherHeadcount      decimal(16,2) NULL,
+        AssistantHeadTeacherHeadcount   decimal(16,2) NULL,
+        LeadershipNonTeacherHeadcount   decimal(16,2) NULL,
+        HeadTeacherFTE                  decimal(16,2) NULL,
+        DeputyHeadTeacherFTE            decimal(16,2) NULL,
+        AssistantHeadTeacherFTE         decimal(16,2) NULL,
+        LeadershipNonTeacherFTE         decimal(16,2) NULL;
