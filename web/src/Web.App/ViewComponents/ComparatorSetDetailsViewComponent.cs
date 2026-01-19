10b5b77195405df808f6e556271fbfc47d87e9a8
Josh Langford <josh.langford@hippodigital.co.uk>
2026-01-19 14:23:33 +0000

    feat: update `ComparatorSetDetails` so that the display of the change custom data link is optional

--- web/src/Web.App/ViewComponents/ComparatorSetDetailsViewComponent.cs
+++ web/src/Web.App/ViewComponents/ComparatorSetDetailsViewComponent.cs
@@ -5,9 +5,16 @@ namespace Web.App.ViewComponents;
 
 public class ComparatorSetDetailsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(string identifier, bool hasUserDefinedSet, bool hasCustomData, bool hasMissingComparatorSet = false, ComparatorSetType type = ComparatorSetType.Costs, bool showAsBulletedList = false)
+    public IViewComponentResult Invoke(
+        string identifier,
+        bool hasUserDefinedSet,
+        bool hasCustomData,
+        bool hasMissingComparatorSet = false,
+        ComparatorSetType type = ComparatorSetType.Costs,
+        bool showAsBulletedList = false,
+        bool showCustomDataOption = true)
     {
-        var model = new ComparatorSetDetailsViewModel(identifier, hasUserDefinedSet, hasCustomData, hasMissingComparatorSet, type);
+        var model = new ComparatorSetDetailsViewModel(identifier, hasUserDefinedSet, hasCustomData, hasMissingComparatorSet, type, showCustomDataOption);
 
         return showAsBulletedList
             ? View("BulletedList", model)
--- web/src/Web.App/ViewModels/Components/ComparatorSetDetailsViewModel.cs
+++ web/src/Web.App/ViewModels/Components/ComparatorSetDetailsViewModel.cs
@@ -1,12 +1,13 @@
 ﻿namespace Web.App.ViewModels.Components;
 
-public class ComparatorSetDetailsViewModel(string identifier, bool hasUserDefinedSet, bool hasCustomData, bool hasMissingComparatorSet, ComparatorSetType type)
+public class ComparatorSetDetailsViewModel(string identifier, bool hasUserDefinedSet, bool hasCustomData, bool hasMissingComparatorSet, ComparatorSetType type, bool showCustomDataOption)
 {
     public string Identifier => identifier;
     public bool HasUserDefinedSet => hasUserDefinedSet;
     public bool HasCustomData => hasCustomData;
     public bool HasMissingComparatorSet => hasMissingComparatorSet;
     public ComparatorSetType Type => type;
+    public bool ShowCustomDataOption => showCustomDataOption;
 }
 
 public enum ComparatorSetType
