af3f3322eaa360ca7ce8243d7307759a6bdfea65
Faizan Ahmad <111439003+Faizan-ahmad00@users.noreply.github.com>
2026-01-22 14:28:23 +0000

    chore: senior leadership a11y tests (#3557)
    * added a11y tests
    
    * chore: update a11y tests
    
    ---------
    
    Co-authored-by: Faizan Ahmad <faizan.ahmad@education.gov.uk>
    Co-authored-by: Josh Langford <josh.langford@hippodigital.co.uk>

---
+++ web/tests/Web.A11yTests/Pages/Schools/WhenViewingBenchmarkSeniorLeadership.cs
@@ -0,0 +1,25 @@
+﻿using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Web.A11yTests.Pages.Schools;
+
+[Trait("Category", "SeniorLeadershipFlagEnabled")]
+public class WhenViewingBenchmarkSeniorLeadership(
+    ITestOutputHelper testOutputHelper,
+    WebDriver webDriver)
+    : PageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/school/{TestConfiguration.School}/census/senior-leadership";
+
+    [Theory]
+    [InlineData("Table")]
+    [InlineData("Chart")]
+    public async Task ThenThereAreNoAccessibilityIssues(string mode)
+    {
+        await GoToPage();
+        await Page.Locator($"#view-{mode}").ClickAsync();
+        await Page.Locator("form .actions-form__button button.govuk-button").ClickAsync();
+        await EvaluatePage();
+    }
+}
