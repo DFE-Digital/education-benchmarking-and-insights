d023604f2db873b481b1b962c06d1233a4c49a95
Matt Barton <matt.barton@hippodigital.co.uk>
2026-02-16 11:49:04 +0000

    feat: create data-normalised views for school balance, and add req'd fields to default views

--- core-infrastructure/src/db/Core.Database/Views/004-BalanceSchool.sql
+++ core-infrastructure/src/db/Core.Database/Views/004-BalanceSchool.sql
@@ -21,6 +21,8 @@ GO
 CREATE VIEW VW_BalanceSchoolDefaultActual AS
 SELECT RunId,
        URN,
+       OverallPhase,
+       FinanceType,
        InYearBalance,
        RevenueReserve
 FROM Financial
@@ -33,6 +35,8 @@ GO
 CREATE VIEW VW_BalanceSchoolDefaultPercentExpenditure AS
 SELECT RunId,
        URN,
+       OverallPhase,
+       FinanceType,
        IIF(TotalExpenditure != 0, (InYearBalance / TotalExpenditure) * 100, NULL) AS 'InYearBalance',
        IIF(TotalExpenditure != 0, (RevenueReserve / TotalExpenditure) * 100, NULL) AS 'RevenueReserve'
 FROM Financial
@@ -45,6 +49,8 @@ GO
 CREATE VIEW VW_BalanceSchoolDefaultPercentIncome AS
 SELECT RunId,
        URN,
+       OverallPhase,
+       FinanceType,
        IIF(TotalIncome != 0, (InYearBalance / TotalIncome) * 100, NULL) AS 'InYearBalance',
        IIF(TotalIncome != 0, (RevenueReserve / TotalIncome) * 100, NULL) AS 'RevenueReserve'
 FROM Financial
@@ -57,8 +63,62 @@ GO
 CREATE VIEW VW_BalanceSchoolDefaultPerUnit AS
 SELECT RunId,
        URN,
+       OverallPhase,
+       FinanceType,
        IIF(TotalPupils != 0, InYearBalance / TotalPupils, NULL) AS 'InYearBalance',
        IIF(TotalPupils != 0, RevenueReserve / TotalPupils, NULL) AS 'RevenueReserve'
 FROM Financial
 WHERE RunType = 'default'
+GO
+
+DROP VIEW IF EXISTS VW_BalanceSchoolDefaultNormalisedActual
+GO
+
+CREATE VIEW VW_BalanceSchoolDefaultNormalisedActual AS
+SELECT RunId,
+       URN,
+       OverallPhase,
+       FinanceType,
+       IIF (InYearBalance IS NULL OR InYearBalance <= 0, NULL, InYearBalance) AS 'InYearBalance',
+       IIF (RevenueReserve IS NULL OR RevenueReserve <= 0, NULL, RevenueReserve) AS 'RevenueReserve'
+FROM VW_BalanceSchoolDefaultActual
+GO
+
+DROP VIEW IF EXISTS VW_BalanceSchoolDefaultNormalisedPercentExpenditure
+GO
+
+CREATE VIEW VW_BalanceSchoolDefaultNormalisedPercentExpenditure AS
+SELECT RunId,
+       URN,
+       OverallPhase,
+       FinanceType,
+       IIF (InYearBalance IS NULL OR InYearBalance <= 0, NULL, InYearBalance) AS 'InYearBalance',
+       IIF (RevenueReserve IS NULL OR RevenueReserve <= 0, NULL, RevenueReserve) AS 'RevenueReserve'
+FROM VW_BalanceSchoolDefaultPercentExpenditure
+GO
+
+DROP VIEW IF EXISTS VW_BalanceSchoolDefaultNormalisedPercentIncome
+GO
+
+CREATE VIEW VW_BalanceSchoolDefaultNormalisedPercentIncome AS
+SELECT RunId,
+       URN,
+       OverallPhase,
+       FinanceType,
+       IIF (InYearBalance IS NULL OR InYearBalance <= 0, NULL, InYearBalance) AS 'InYearBalance',
+       IIF (RevenueReserve IS NULL OR RevenueReserve <= 0, NULL, RevenueReserve) AS 'RevenueReserve'
+FROM VW_BalanceSchoolDefaultPercentIncome
+GO
+
+DROP VIEW IF EXISTS VW_BalanceSchoolDefaultNormalisedPerUnit
+GO
+
+CREATE VIEW VW_BalanceSchoolDefaultNormalisedPerUnit AS
+SELECT RunId,
+       URN,
+       OverallPhase,
+       FinanceType,
+       IIF (InYearBalance IS NULL OR InYearBalance <= 0, NULL, InYearBalance) AS 'InYearBalance',
+       IIF (RevenueReserve IS NULL OR RevenueReserve <= 0, NULL, RevenueReserve) AS 'RevenueReserve'
+FROM VW_BalanceSchoolDefaultPerUnit
 GO
