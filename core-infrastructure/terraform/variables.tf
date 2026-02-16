9045c2a6ad547075d9c8a52a5e980ca023e25308
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-02-16 14:30:26 +0000

    feat: configure IAM for Databricks connectors

--- core-infrastructure/terraform/variables.tf
+++ core-infrastructure/terraform/variables.tf
@@ -14,6 +14,8 @@ variable "configuration" {
     development = {
       sql_db_sku_name    = "S0"
       sql_db_max_size_gb = 5
+      databricks_access_connector_name = "s101d01edapDatabricksConnector"
+      databricks_access_connector_rg = "s101d01-rg-edap"
     }
     automated-test = {
       sql_db_sku_name    = "S1"
@@ -22,6 +24,8 @@ variable "configuration" {
     test = {
       sql_db_sku_name    = "S1"
       sql_db_max_size_gb = 5
+      databricks_access_connector_name = "s101t01adaptDatabricksConnector"
+      databricks_access_connector_rg = "s101t01-rg-adapt"
     }
     feature = {
       sql_db_sku_name    = "S0"
@@ -30,10 +34,14 @@ variable "configuration" {
     pre-production = {
       sql_db_sku_name    = "S1"
       sql_db_max_size_gb = 5
+      databricks_access_connector_name = "s101p02adaptDatabricksConnector"
+      databricks_access_connector_rg = "s101p02-rg-edap"
     }
     production = {
       sql_db_sku_name    = "S2"
       sql_db_max_size_gb = 5
+      databricks_access_connector_name = "s101p01adaptDatabricksConnector"
+      databricks_access_connector_rg = "s101p01-rg-edap"
     }
   }
 }
