573444215fdcc83f836d01fa660041885dc0f9fc
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-02-17 11:49:56 +0000

    try hardcoding sub id

--- core-infrastructure/terraform/provider.tf
+++ core-infrastructure/terraform/provider.tf
@@ -29,7 +29,8 @@ provider "azurerm" {
 
 provider "azurerm" {
   alias           = "databricks_sub"
-  subscription_id = var.configuration[var.environment].databricks_subscription_id
+  # subscription_id = var.configuration[var.environment].databricks_subscription_id
+  subscription_id = "48ea0797-73c6-4202-bf90-b01c817058e9"
   features {}
 }
 
