f09ad882caa2720f4be5114cb4ab6b76ddba3255
Josh Langford <josh.langford@hippodigital.co.uk>
2026-02-26 14:25:14 +0000

    feat: do not omit selected establishment from sen charts even when missing data

--- front-end-components/src/composed/benchmark-chart-send-2/component.tsx
+++ front-end-components/src/composed/benchmark-chart-send-2/component.tsx
@@ -1,12 +1,15 @@
-import { useMemo } from "react";
+import { useMemo, useContext } from "react";
 import { BenchmarkChartSend2Props } from "src/composed/benchmark-chart-send-2";
 import { LocalAuthoritySend2Benchmark } from "src/services";
 import { LaChartData } from "src/components/charts/table-chart";
 import { HorizontalBarChartWrapper } from "../horizontal-bar-chart-wrapper";
+import { SelectedEstablishmentContext } from "src/contexts";
 
 export function BenchmarkChartSend2<
   TData extends LocalAuthoritySend2Benchmark,
 >({ chartTitle, data, valueField }: BenchmarkChartSend2Props<TData>) {
+  const selectedEstablishment = useContext(SelectedEstablishmentContext);
+
   const mergedData = useMemo(() => {
     const dataPoints: LaChartData[] = [];
 
@@ -14,14 +17,18 @@ export function BenchmarkChartSend2<
       data.forEach((s) => {
         const value = s && (s[valueField] as number);
 
-        if (value === undefined || value === null || isNaN(value)) {
+        const valueInvalid =
+          value === undefined || value === null || isNaN(value);
+        const isSelectedEstablishment = selectedEstablishment === s.code;
+
+        if (valueInvalid && !isSelectedEstablishment) {
           return;
         }
 
         dataPoints.push({
           laCode: s.code,
           laName: s.name,
-          value: value,
+          value: valueInvalid ? undefined : value,
           totalPupils: s.totalPupils,
         });
       });
@@ -31,7 +38,7 @@ export function BenchmarkChartSend2<
       tableHeadings: ["Local authority", "Amount", "Number of pupils"],
       dataPoints,
     };
-  }, [data, valueField]);
+  }, [data, valueField, selectedEstablishment]);
 
   return (
     <HorizontalBarChartWrapper
