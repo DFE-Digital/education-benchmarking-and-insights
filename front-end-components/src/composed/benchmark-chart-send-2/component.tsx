ac95350a90419cc6def12ab3e86f058f62382cf2
Josh Langford <josh.langford@hippodigital.co.uk>
2026-02-26 14:15:18 +0000

    feat: omit local authorities from sen charts if missing data and remove warning for missing data

--- front-end-components/src/composed/benchmark-chart-send-2/component.tsx
+++ front-end-components/src/composed/benchmark-chart-send-2/component.tsx
@@ -3,7 +3,6 @@ import { BenchmarkChartSend2Props } from "src/composed/benchmark-chart-send-2";
 import { LocalAuthoritySend2Benchmark } from "src/services";
 import { LaChartData } from "src/components/charts/table-chart";
 import { HorizontalBarChartWrapper } from "../horizontal-bar-chart-wrapper";
-import { DataWarning } from "src/components/charts/data-warning";
 
 export function BenchmarkChartSend2<
   TData extends LocalAuthoritySend2Benchmark,
@@ -15,13 +14,14 @@ export function BenchmarkChartSend2<
       data.forEach((s) => {
         const value = s && (s[valueField] as number);
 
+        if (value === undefined || value === null || isNaN(value)) {
+          return;
+        }
+
         dataPoints.push({
           laCode: s.code,
           laName: s.name,
-          value:
-            value === undefined || value === null || isNaN(value)
-              ? undefined
-              : value,
+          value: value,
           totalPupils: s.totalPupils,
         });
       });
@@ -33,28 +33,16 @@ export function BenchmarkChartSend2<
     };
   }, [data, valueField]);
 
-  const missingDataKeys = mergedData.dataPoints
-    .filter((d) => !d.value)
-    .map((d) => d.laCode);
-
   return (
     <HorizontalBarChartWrapper
       chartTitle={chartTitle}
       data={mergedData}
       xAxisLabel="Amount"
       localAuthority
-      missingDataKeys={missingDataKeys}
       showCopyImageButton
       valueUnit="amount"
     >
       <h3 className="govuk-heading-s govuk-!-margin-bottom-0">{chartTitle}</h3>
-      {missingDataKeys.length > 0 && (
-        <DataWarning className="govuk-!-margin-top-3">
-          {missingDataKeys.length > 1
-            ? "Comparator local authorities have missing data"
-            : "Comparator local authority has missing data"}
-        </DataWarning>
-      )}
     </HorizontalBarChartWrapper>
   );
 }
