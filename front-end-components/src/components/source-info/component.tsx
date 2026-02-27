4c4a914a69a7e6a16060acb607306ee20705d251
J05h-L <117924208+J05h-L@users.noreply.github.com>
2026-02-27 11:39:32 +0000

    fix: spacing and use secondary text colour for source text (#3753)

--- front-end-components/src/components/source-info/component.tsx
+++ front-end-components/src/components/source-info/component.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { SourceInfoContext } from "src/contexts";
 import { SourceInfoProps } from "src/components/source-info";
+import "./styles.scss";
 
 export const SourceInfo: React.FC<SourceInfoProps> = ({
   recoupment,
@@ -17,28 +18,32 @@ export const SourceInfo: React.FC<SourceInfoProps> = ({
 
   const lineLabel = `Line${lineCodes.length > 1 ? "s" : ""} ${lineCodes.join(", ")}`;
 
-  return recoupment ? (
-    <>
-      <p className="govuk-body-s">Sources:</p>
-      <ul className="govuk-list govuk-list--bullet">
-        <li className="govuk-body-s" data-test-id="line-code-source">
-          {yearsLabel}, {lineLabel}
-        </li>
-        <li className="govuk-body-s">
-          Place funding for academies. You can read about how we calculate this
-          in the{" "}
-          <a
-            href={glossaryUrl}
-            className="govuk-link govuk-link--no-visited-state"
-          >
-            glossary
-          </a>
-        </li>
-      </ul>
-    </>
-  ) : (
-    <p className="govuk-body-s" data-test-id="line-code-source">
-      Source: {yearsLabel}, {lineLabel}
-    </p>
+  return (
+    <div className="govuk-!-margin-bottom-6 app-source-info">
+      {recoupment ? (
+        <>
+          <p className="govuk-body-s govuk-!-margin-bottom-1">Sources:</p>
+          <ul className="govuk-list govuk-list--bullet">
+            <li className="govuk-body-s" data-test-id="line-code-source">
+              {yearsLabel}, {lineLabel}
+            </li>
+            <li className="govuk-body-s">
+              Place funding for academies. You can read about how we calculate
+              this in the{" "}
+              <a
+                href={glossaryUrl}
+                className="govuk-link govuk-link--no-visited-state"
+              >
+                glossary
+              </a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <p className="govuk-body-s" data-test-id="line-code-source">
+          Source: {yearsLabel}, {lineLabel}
+        </p>
+      )}
+    </div>
   );
 };
