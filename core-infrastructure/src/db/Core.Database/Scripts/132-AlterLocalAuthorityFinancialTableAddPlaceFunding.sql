bf00e7768504f4a0b56fbfd28c81f4fed20b6fc4
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-02-27 11:38:56 +0000

    update sql

--- core-infrastructure/src/db/Core.Database/Scripts/132-AlterLocalAuthorityFinancialTableAddPlaceFunding.sql
+++ core-infrastructure/src/db/Core.Database/Scripts/132-AlterLocalAuthorityFinancialTableAddPlaceFunding.sql
@@ -1,12 +1,13 @@
 ALTER TABLE dbo.LocalAuthorityFinancial
-    ADD PrimaryPlaces6000               decimal(16,2) NULL,
-        PrimaryPlaces10000              decimal(16,2) NULL,
-        SecondaryPlaces6000             decimal(16,2) NULL,
-        SecondaryPlaces10000            decimal(16,2) NULL,
-        DSGNurseryPlaceFunding             decimal(16,2) NULL,
-        DSGPrimaryAcademyPlaceFunding      decimal(16,2) NULL,
-        DSGSecondaryAcademyPlaceFunding    decimal(16,2) NULL,
-        DSGSENAcademyPlaceFunding          decimal(16,2) NULL,
-        DSGAPAcademyPlaceFunding           decimal(16,2) NULL,
-        DSGPost16PlaceFunding              decimal(16,2) NULL,
-        DSGHospitalPlaceFunding            decimal(16,2) NULL
+    ADD PrimaryPlaces6000                  DECIMAL(16,2) NULL,
+        PrimaryPlaces10000                 DECIMAL(16,2) NULL,
+        SecondaryPlaces6000                DECIMAL(16,2) NULL,
+        SecondaryPlaces10000               DECIMAL(16,2) NULL,
+        OutturnPlaceFundingPost16          DECIMAL(16,2) NULL
+        DSGNurseryPlaceFunding             DECIMAL(16,2) NULL,
+        DSGPrimaryAcademyPlaceFunding      DECIMAL(16,2) NULL,
+        DSGSecondaryAcademyPlaceFunding    DECIMAL(16,2) NULL,
+        DSGSENAcademyPlaceFunding          DECIMAL(16,2) NULL,
+        DSGAPAcademyPlaceFunding           DECIMAL(16,2) NULL,
+        DSGPost16PlaceFunding              DECIMAL(16,2) NULL,
+        DSGHospitalPlaceFunding            DECIMAL(16,2) NULL
