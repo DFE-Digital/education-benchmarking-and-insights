2da471c0a9f77b821ff452683c998bad350f0663
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-03-11 14:58:12 +0000

    refactor: remove unneeded terraform related to databricks access connectors

--- core-infrastructure/terraform/variables.tf
+++ core-infrastructure/terraform/variables.tf
@@ -9,45 +9,31 @@ variable "configuration" {
   type = map(object({
     sql_db_sku_name                  = string
     sql_db_max_size_gb               = number
-    databricks_access_connector_name = string
-    databricks_access_connector_rg   = string
   }))
   default = {
     development = {
       sql_db_sku_name                  = "S0"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = "s101d01edapDatabricksConnector"
-      databricks_access_connector_rg   = "s101d01-rg-edap"
     }
     automated-test = {
       sql_db_sku_name                  = "S1"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = ""
-      databricks_access_connector_rg   = ""
     }
     test = {
       sql_db_sku_name                  = "S1"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = "s101t01adaptDatabricksConnector"
-      databricks_access_connector_rg   = "s101t01-rg-adapt"
     }
     feature = {
       sql_db_sku_name                  = "S0"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = ""
-      databricks_access_connector_rg   = ""
     }
     pre-production = {
       sql_db_sku_name                  = "S1"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = "s101p02adaptDatabricksConnector"
-      databricks_access_connector_rg   = "s101p02-rg-edap"
     }
     production = {
       sql_db_sku_name                  = "S2"
       sql_db_max_size_gb               = 5
-      databricks_access_connector_name = "s101p01adaptDatabricksConnector"
-      databricks_access_connector_rg   = "s101p01-rg-edap"
     }
   }
 }
