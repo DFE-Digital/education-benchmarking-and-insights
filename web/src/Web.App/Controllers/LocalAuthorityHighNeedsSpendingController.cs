b3511a13747e91b129e44c16c8e071763c2e3d15
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-04-09 10:26:55 +0100

    feat: add new links to LA homepage, conditional based on feature flag

---
+++ web/src/Web.App/Controllers/LocalAuthorityHighNeedsSpendingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.App.Attributes;
+
+namespace Web.App.Controllers;
+
+[Controller]
+[Route("local-authority/{code}/high-needs-spending")]
+[ValidateLaCode]
+public class LocalAuthorityHighNeedsSpendingController 
+    : Controller
+{
+    [HttpGet]
+    public IActionResult Index(string code)
+    {
+        return new ContentResult();
+    }
+}
