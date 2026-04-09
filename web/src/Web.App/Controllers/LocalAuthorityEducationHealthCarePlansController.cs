d2c42f1e3699dee8a6fd8c34ffff3c5b9a99699f
Jon Abbott <jon.r.abbott@gmail.com>
2026-04-09 13:22:05 +0100

    feat: add new links to LA homepage, conditional based on feature flag (#4036)
    Co-authored-by: Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>

---
+++ web/src/Web.App/Controllers/LocalAuthorityEducationHealthCarePlansController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.App.Attributes;
+
+namespace Web.App.Controllers;
+
+[Controller]
+[Route("local-authority/{code}/education-health-care-plans")]
+[ValidateLaCode]
+public class LocalAuthorityEducationHealthCarePlansController
+    : Controller
+{
+    [HttpGet]
+    public IActionResult Index(string code)
+    {
+        return new ContentResult();
+    }
+}
