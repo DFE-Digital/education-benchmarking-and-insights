28d63d7e22070383a1ffc5085733328f3dff7f57
Faizan Ahmad <111439003+Faizan-ahmad00@users.noreply.github.com>
2026-04-24 09:11:04 +0100

    feat: add a11y tests (#4069)

---
+++ web/tests/Web.A11yTests/Pages/LocalAuthority/WhenViewingEducationHealthCareBenchmarking.cs
@@ -0,0 +1,23 @@
+﻿using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Web.A11yTests.Pages.LocalAuthority;
+
+[Trait("Category", "HighNeedsBenchmarkingFlagEnabled")]
+public class WhenViewingEducationHealthCareBenchmarking(
+    ITestOutputHelper testOutputHelper,
+    WebDriver webDriver) : PageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/local-authority/{TestConfiguration.LocalAuthority}/comparators?type=EducationHealthCarePlans";
+    [Theory]
+    [InlineData("view-Chart")]
+    [InlineData("view-Table")]
+    public async Task ThenThereAreNoAccessibilityIssues(string mode)
+    {
+        await GoToPage();
+        await Page.Locator("button[type=submit][name=action][value=continue]").ClickAsync();
+        await Page.Locator($"#{mode}").ClickAsync();
+        await EvaluatePage();
+    }
+}
