875f9eabfc1ba23925c4963bca9e51af841a6ecc
Josh Langford <josh.langford@hippodigital.co.uk>
2026-04-28 07:35:13 +0100

    feat: add new endpoint and handler/service support for high needs spending by transaction type

---
+++ platform/src/apis/Platform.Api.LocalAuthority/Features/Accounts/Models/HighNeedsFields.cs
@@ -0,0 +1,113 @@
+namespace Platform.Api.LocalAuthority.Features.Accounts.Models;
+
+public static class HighNeedsFields
+{
+    // LocalAuthorityBase
+    public const string LaCode = "LaCode";
+    public const string Name = "Name";
+    public const string Population2To18 = "Population2To18";
+    public const string TotalPupils = "TotalPupils";
+
+    // HighNeedsBase
+    public const string OutturnTotalHighNeeds = "OutturnTotalHighNeeds";
+    public const string BudgetTotalHighNeeds = "BudgetTotalHighNeeds";
+
+    // HighNeedsAmount
+    public const string OutturnTotalPlaceFunding = "OutturnTotalPlaceFunding";
+    public const string BudgetTotalPlaceFunding = "BudgetTotalPlaceFunding";
+
+    public const string OutturnTotalTopUpFundingMaintained = "OutturnTotalTopUpFundingMaintained";
+    public const string BudgetTotalTopUpFundingMaintained = "BudgetTotalTopUpFundingMaintained";
+
+    public const string OutturnTotalTopUpFundingNonMaintained = "OutturnTotalTopUpFundingNonMaintained";
+    public const string BudgetTotalTopUpFundingNonMaintained = "BudgetTotalTopUpFundingNonMaintained";
+
+    public const string OutturnTotalSenServices = "OutturnTotalSenServices";
+    public const string BudgetTotalSenServices = "BudgetTotalSenServices";
+
+    public const string OutturnTotalAlternativeProvisionServices = "OutturnTotalAlternativeProvisionServices";
+    public const string BudgetTotalAlternativeProvisionServices = "BudgetTotalAlternativeProvisionServices";
+
+    public const string OutturnTotalHospitalServices = "OutturnTotalHospitalServices";
+    public const string BudgetTotalHospitalServices = "BudgetTotalHospitalServices";
+
+    public const string OutturnTotalOtherHealthServices = "OutturnTotalOtherHealthServices";
+    public const string BudgetTotalOtherHealthServices = "BudgetTotalOtherHealthServices";
+
+    // TopFunding (Maintained)
+    public const string OutturnTopFundingMaintainedEarlyYears = "OutturnTopFundingMaintainedEarlyYears";
+    public const string BudgetTopFundingMaintainedEarlyYears = "BudgetTopFundingMaintainedEarlyYears";
+
+    public const string OutturnTopFundingMaintainedPrimary = "OutturnTopFundingMaintainedPrimary";
+    public const string BudgetTopFundingMaintainedPrimary = "BudgetTopFundingMaintainedPrimary";
+
+    public const string OutturnTopFundingMaintainedSecondary = "OutturnTopFundingMaintainedSecondary";
+    public const string BudgetTopFundingMaintainedSecondary = "BudgetTopFundingMaintainedSecondary";
+
+    public const string OutturnTopFundingMaintainedSpecial = "OutturnTopFundingMaintainedSpecial";
+    public const string BudgetTopFundingMaintainedSpecial = "BudgetTopFundingMaintainedSpecial";
+
+    public const string OutturnTopFundingMaintainedAlternativeProvision = "OutturnTopFundingMaintainedAlternativeProvision";
+    public const string BudgetTopFundingMaintainedAlternativeProvision = "BudgetTopFundingMaintainedAlternativeProvision";
+
+    public const string OutturnTopFundingMaintainedPostSchool = "OutturnTopFundingMaintainedPostSchool";
+    public const string BudgetTopFundingMaintainedPostSchool = "BudgetTopFundingMaintainedPostSchool";
+
+    public const string OutturnTopFundingMaintainedIncome = "OutturnTopFundingMaintainedIncome";
+    public const string BudgetTopFundingMaintainedIncome = "BudgetTopFundingMaintainedIncome";
+
+    // TopFunding (Non‑Maintained)
+    public const string OutturnTopFundingNonMaintainedEarlyYears = "OutturnTopFundingNonMaintainedEarlyYears";
+    public const string BudgetTopFundingNonMaintainedEarlyYears = "BudgetTopFundingNonMaintainedEarlyYears";
+
+    public const string OutturnTopFundingNonMaintainedPrimary = "OutturnTopFundingNonMaintainedPrimary";
+    public const string BudgetTopFundingNonMaintainedPrimary = "BudgetTopFundingNonMaintainedPrimary";
+
+    public const string OutturnTopFundingNonMaintainedSecondary = "OutturnTopFundingNonMaintainedSecondary";
+    public const string BudgetTopFundingNonMaintainedSecondary = "BudgetTopFundingNonMaintainedSecondary";
+
+    public const string OutturnTopFundingNonMaintainedSpecial = "OutturnTopFundingNonMaintainedSpecial";
+    public const string BudgetTopFundingNonMaintainedSpecial = "BudgetTopFundingNonMaintainedSpecial";
+
+    public const string OutturnTopFundingNonMaintainedAlternativeProvision = "OutturnTopFundingNonMaintainedAlternativeProvision";
+    public const string BudgetTopFundingNonMaintainedAlternativeProvision = "BudgetTopFundingNonMaintainedAlternativeProvision";
+
+    public const string OutturnTopFundingNonMaintainedPostSchool = "OutturnTopFundingNonMaintainedPostSchool";
+    public const string BudgetTopFundingNonMaintainedPostSchool = "BudgetTopFundingNonMaintainedPostSchool";
+
+    public const string OutturnTopFundingNonMaintainedIncome = "OutturnTopFundingNonMaintainedIncome";
+    public const string BudgetTopFundingNonMaintainedIncome = "BudgetTopFundingNonMaintainedIncome";
+
+    // PlaceFunding
+    public const string OutturnPlaceFundingPrimary = "OutturnPlaceFundingPrimary";
+    public const string BudgetPlaceFundingPrimary = "BudgetPlaceFundingPrimary";
+
+    public const string OutturnPlaceFundingSecondary = "OutturnPlaceFundingSecondary";
+    public const string BudgetPlaceFundingSecondary = "BudgetPlaceFundingSecondary";
+
+    public const string OutturnPlaceFundingSpecial = "OutturnPlaceFundingSpecial";
+    public const string BudgetPlaceFundingSpecial = "BudgetPlaceFundingSpecial";
+
+    public const string OutturnPlaceFundingAlternativeProvision = "OutturnPlaceFundingAlternativeProvision";
+    public const string BudgetPlaceFundingAlternativeProvision = "BudgetPlaceFundingAlternativeProvision";
+
+    // SENTransport
+    public const string OutturnSENTransportDSG = "OutturnSENTransportDSG";
+    public const string BudgetSENTransportDSG = "BudgetSENTransportDSG";
+
+    public const string OutturnHometoSchoolTransportPre16 = "OutturnHometoSchoolTransportPre16";
+    public const string BudgetHometoSchoolTransportPre16 = "BudgetHometoSchoolTransportPre16";
+
+    public const string OutturnHometoSchoolTransport1618 = "OutturnHometoSchoolTransport1618";
+    public const string BudgetHometoSchoolTransport1618 = "BudgetHometoSchoolTransport1618";
+
+    public const string OutturnHometoSchoolTransport1925 = "OutturnHometoSchoolTransport1925";
+    public const string BudgetHometoSchoolTransport1925 = "BudgetHometoSchoolTransport1925";
+
+    // CentralSenServices
+    public const string OutturnEdPsychologyService = "OutturnEdPsychologyService";
+    public const string BudgetEdPsychologyService = "BudgetEdPsychologyService";
+
+    public const string OutturnSENAdmin = "OutturnSENAdmin";
+    public const string BudgetSENAdmin = "BudgetSENAdmin";
+}
---
+++ platform/src/apis/Platform.Api.LocalAuthority/Features/Accounts/Parameters/HighNeedsParametersByTransactionType.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Specialized;
+using Platform.Domain;
+using Platform.Functions;
+using Platform.Functions.Extensions;
+
+namespace Platform.Api.LocalAuthority.Features.Accounts.Parameters;
+
+public record HighNeedsByTransactionTypeParameters : QueryParameters
+{
+    public string[] Codes { get; private set; } = [];
+    public string Dimension { get; private set; } = Dimensions.HighNeeds.PerPupil;
+    public string Type { get; private set; } = TransactionType.Budget;
+
+    public override void SetValues(NameValueCollection query)
+    {
+        Codes = query.ToStringArray("code");
+
+        if (query.TryGetValue("dimension", out var dimension))
+        {
+            Dimension = dimension;
+        }
+
+        if (query.TryGetValue("type", out var type))
+        {
+            Type = type;
+        }
+    }
+}
