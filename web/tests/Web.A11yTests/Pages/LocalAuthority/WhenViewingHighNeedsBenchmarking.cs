24294c3def579415f9dffddb9d8c87409a0acf88
Faizan Ahmad <111439003+Faizan-ahmad00@users.noreply.github.com>
2026-05-07 09:13:27 +0100

    feat: update a11y tests  (#4144)

--- web/tests/Web.A11yTests/Pages/LocalAuthority/WhenViewingHighNeedsBenchmarking.cs
+++ web/tests/Web.A11yTests/Pages/LocalAuthority/WhenViewingHighNeedsBenchmarking.cs
@@ -4,24 +4,27 @@ using Xunit.Abstractions;
 
 namespace Web.A11yTests.Pages.LocalAuthority;
 
-[Trait("Category", "HighNeedsFlagEnabled")]
+[Trait("Category", "HighNeedsBenchmarkingFlagEnabled")]
 public class WhenViewingHighNeedsBenchmarking(
     ITestOutputHelper testOutputHelper,
     WebDriver webDriver)
     : PageBase(testOutputHelper, webDriver)
 {
-    protected override string PageUrl => $"/local-authority/{TestConfiguration.LocalAuthority}/comparators";
-
-    [Theory]
-    [InlineData("mode-chart")]
-    [InlineData("mode-table")]
-    public async Task ThenThereAreNoAccessibilityIssues(string mode)
+    protected override string PageUrl => $"/local-authority/{TestConfiguration.LocalAuthority}/comparators?type=HighNeedsSpending";
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssuesInTheJourney()
     {
         await GoToPage();
+        await EvaluatePage();
         await Page.Locator("#LaInput").FillAsync("Hackney");
         await Page.Locator("button[type=submit][name=action][value=add]").ClickAsync();
+        await EvaluatePage();
         await Page.Locator("button[type=submit][name=action][value=continue]").ClickAsync();
-        await Page.Locator($"#{mode}").ClickAsync();
+        //evaluating the table view of the page
+        await EvaluatePage();
+        await Page.Locator("#view-Table").ClickAsync();
+        await Page.Locator("button:text(\"Apply\")").Nth(1).ClickAsync();
         await EvaluatePage();
     }
+
 }
