8dcbc861c0a2d37c878e15ca23d23f1302ea493d
Josh Langford <josh.langford@hippodigital.co.uk>
2026-05-11 13:03:37 +0100

    docs: update high needs endpoint with openapi examples

--- platform/src/apis/Platform.Api.LocalAuthority/Features/Accounts/OpenApiExamples.cs
+++ platform/src/apis/Platform.Api.LocalAuthority/Features/Accounts/OpenApiExamples.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics.CodeAnalysis;
-using Platform.Domain;
+using Platform.Api.LocalAuthority.Features.Accounts.Validators;
 using Platform.OpenApi.Attributes;
 
 namespace Platform.Api.LocalAuthority.Features.Accounts;
 
 [ExcludeFromCodeCoverage]
-[GenerateOpenApiExample(Name = "DimensionHighNeeds", SourceType = typeof(Dimensions.HighNeeds), SourceProperty = "All")]
+[GenerateOpenApiExample(Name = "DimensionHighNeeds", SourceType = typeof(HighNeedsValidatorV2Values), SourceProperty = "Dimensions")]
+[GenerateOpenApiExample(Name = "TypeHighNeeds", SourceType = typeof(HighNeedsValidatorV2Values), SourceProperty = "Types")]
 public partial class OpenApiExamples;
