1ee1f91f0400c9dc192b5175c58e3d4c3d2dbfff
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-05-13 18:18:10 +0100

    sort deps chain (+18 squashed commit)
    Squashed commit:
    
    [4883292c3] Add permissions
    
    [d8b9ff882] fixes
    
    [af235af9e] fixes
    
    [94ba0e641] fixing yaml
    
    [b24b5374d] use deployment storage account
    
    [be7485c6a] fix settings
    
    [444df5b41] trigger a key rotation
    
    [78aa2193b] add key rotation methods
    
    [e848b0134] remove prop
    
    [fcfad94b7] more fixes
    
    [31db2ff2b] Add missed service endpoints
    
    [57fa08448] more fixes
    
    [4ec3a7e7d] use correct subnet
    
    [69c73bd34] more fixes
    
    [b8f45a811] Adding in storage permission
    
    [7168b23cf] fix azapi body
    
    [23fc42dda] Use AzApi to set default key
    
    [7b54778f6] Add key vault keys

--- chart-renderer/terraform/variables.tf
+++ chart-renderer/terraform/variables.tf
@@ -2,8 +2,10 @@ variable "environment" {}
 variable "cip-environment" {}
 variable "environment-prefix" {}
 variable "location" {}
+variable "package_path" {}
 
 locals {
+  host_key_manual_rotation_trigger = "2"
   common-tags = {
     "Environment"      = var.cip-environment
     "Service Offering" = "DfE Financial Benchmarking service"
--- core-infrastructure/terraform/vnet.tf
+++ core-infrastructure/terraform/vnet.tf
@@ -89,8 +89,25 @@ resource "azurerm_subnet" "chart-renderer-subnet" {
   ]
 }
 
+resource "azurerm_subnet" "chart-renderer-endpoints-subnet" {
+  name                 = "${var.environment-prefix}-chart-renderer-endpoints-subnet"
+  resource_group_name  = azurerm_resource_group.resource-group.name
+  virtual_network_name = azurerm_virtual_network.app-service-network.name
+  address_prefixes     = ["10.0.5.0/24"]
+
+  service_endpoints = [
+    "Microsoft.Storage",
+    "Microsoft.KeyVault"
+  ]
+}
+
 resource "azurerm_subnet_network_security_group_association" "chart-renderer-subnet-nsg-association" {
   subnet_id                 = azurerm_subnet.chart-renderer-subnet.id
+  network_security_group_id = azurerm_network_security_group.network-security-group.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "chart-renderer-endpoints-subnet-nsg-association" {
+  subnet_id                 = azurerm_subnet.chart-renderer-endpoints-subnet.id
   network_security_group_id = azurerm_network_security_group.network-security-group.id
 }
 
@@ -117,5 +134,31 @@ resource "azurerm_private_dns_zone_virtual_network_link" "blob-storage-dns-zone-
   name                  = "${var.environment-prefix}-blob-storage-dns-zone-vnet-link"
   resource_group_name   = azurerm_resource_group.resource-group.name
   private_dns_zone_name = azurerm_private_dns_zone.blob-storage-dns-zone.name
+  virtual_network_id    = azurerm_virtual_network.app-service-network.id
+}
+
+resource "azurerm_private_dns_zone" "queue-storage-dns-zone" {
+  name                = "privatelink.queue.core.windows.net"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  tags                = local.common-tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "queue-storage-dns-zone-vnet-link" {
+  name                  = "${var.environment-prefix}-queue-storage-dns-zone-vnet-link"
+  resource_group_name   = azurerm_resource_group.resource-group.name
+  private_dns_zone_name = azurerm_private_dns_zone.queue-storage-dns-zone.name
+  virtual_network_id    = azurerm_virtual_network.app-service-network.id
+}
+
+resource "azurerm_private_dns_zone" "table-storage-dns-zone" {
+  name                = "privatelink.table.core.windows.net"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  tags                = local.common-tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "table-storage-dns-zone-vnet-link" {
+  name                  = "${var.environment-prefix}-table-storage-dns-zone-vnet-link"
+  resource_group_name   = azurerm_resource_group.resource-group.name
+  private_dns_zone_name = azurerm_private_dns_zone.table-storage-dns-zone.name
   virtual_network_id    = azurerm_virtual_network.app-service-network.id
 }
