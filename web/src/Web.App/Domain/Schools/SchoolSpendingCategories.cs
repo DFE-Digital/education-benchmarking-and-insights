b6f25ec51ebc5d087bed48ef7e014e392e1830a7
Matt Barton <162110610+mattb-hippo@users.noreply.github.com>
2026-06-03 07:57:45 +0100

    feat: ensure specific filter groups are open by default (#4274)

--- web/src/Web.App/Domain/Schools/SchoolSpendingCategories.cs
+++ web/src/Web.App/Domain/Schools/SchoolSpendingCategories.cs
@@ -207,6 +207,14 @@ public static class SchoolSpendingCategories
         _ => ComparatorSetTypes.Pupil
     };
 
+    public static bool GetOpenByDefault(this CategoryGroup group) => group switch
+    {
+        CategoryGroup.TotalExpenditure => true,
+        CategoryGroup.EducationalIct => true,
+        CategoryGroup.AdministrativeSupplies => true,
+        _ => false
+    };
+
     public static string GetHeading(this SubCategoryFilter filter) => filter switch
     {
         SubCategoryFilter.TotalExpenditure => "Total expenditure",
