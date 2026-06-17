da4a530b10ad331c08315763a2cc208d02bee15a
J05h-L <117924208+J05h-L@users.noreply.github.com>
2026-06-17 09:26:47 +0100

    chore: bump terraform dependencies (#4312)

--- web/terraform/provider.tf
+++ web/terraform/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.73.0"
+      version = "~> 4.77.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 2.9.0"
+      version = "~> 2.10.0"
     }
   }
   backend "azurerm" {}
