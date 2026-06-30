2a94115bfb82928d965e81ecc63fcfb12edec7fd
Matt Barton <matt.barton@hippodigital.co.uk>
2026-06-30 17:05:29 +0100

    chore: temporarily import keyvault secret into tf state

--- platform/terraform/modules/function_app/main.tf
+++ platform/terraform/modules/function_app/main.tf
@@ -112,6 +112,11 @@ resource "azurerm_key_vault_secret" "default-function-key" {
   ]
 }
 
+import {
+  to = azurerm_key_vault_secret.master-function-key
+  id = "/subscriptions/f155d7d4-a235-4563-b077-c19686c7c92a/resourceGroups/s198d14-ebis-platform/providers/Microsoft.KeyVault/vaults/s198d14-ebis-cnt-kv/secrets/host--masterKey--master"
+}
+
 # Add the master function key to the dedicated key vault
 resource "azurerm_key_vault_secret" "master-function-key" {
   name         = "host--masterKey--master"
@@ -121,6 +126,12 @@ resource "azurerm_key_vault_secret" "master-function-key" {
   depends_on = [
     azurerm_key_vault_access_policy.terraform-agent-kv-access
   ]
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
 
 # Add the master function key to the shared key vault
