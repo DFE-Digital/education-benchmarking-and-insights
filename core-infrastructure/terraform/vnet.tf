c70e1cd505567d398f0307a8b82c54c28f4a929e
Matt Barton <matt.barton@hippodigital.co.uk>
2026-07-21 10:56:30 +0100

    feat: add sql firewall rules for platform and orchestrator subnets

--- core-infrastructure/terraform/vnet.tf
+++ core-infrastructure/terraform/vnet.tf
@@ -98,3 +98,15 @@ resource "azurerm_subnet" "load-test-subnet" {
     "Microsoft.Web"
   ]
 }
+
+resource "azurerm_mssql_virtual_network_rule" "platform-subnet-sql-access" {
+  name      = "platform-subnet-sql-access-rule"
+  server_id = azurerm_mssql_server.sql-server.id
+  subnet_id = azurerm_subnet.platform-subnet.id
+}
+
+resource "azurerm_mssql_virtual_network_rule" "orchestrator-subnet-sql-access" {
+  name      = "orchestrator-subnet-sql-access-rule"
+  server_id = azurerm_mssql_server.sql-server.id
+  subnet_id = azurerm_subnet.orchestrator-subnet.id
+}
