8fe5758992e76f8b098c4a75dc23f9dca8b40057
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-08-04 16:28:09 +0100

    remove metrics references to minimise lines for PR

--- data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
+++ data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
@@ -11,8 +11,5 @@ def run_laa_risk_scores_pipeline(run_year: int, run_id: str):
     )
     cfr_with_all_extra_data = load_laa_risk_score_data(run_year)
     logger.info(
-        f"Loaded all preprocessed and ancillary LAA risk score data for year {run_year}."
-    )
-    logger.info(
         f"LAA risk scores data-loading pipeline completed for year {run_year} (RunId: {run_id})."
     )
