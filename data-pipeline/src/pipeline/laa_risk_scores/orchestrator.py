4585635511ae0f8791b4beaf845bcd0cec777f8c
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-08-04 16:18:48 +0100

    feat: introduce LAA risk scores derivations, metrics engine, and orchestration

--- data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
+++ data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
@@ -1,40 +1,25 @@
+import pandas as pd
+
+from pipeline.utils.database import insert_laa_risk_scores
 from pipeline.utils.log import setup_logger
 
-from .loader import load_laa_extra_ancillary_data, load_laa_risk_score_data
-from .preprocessing import preprocess_laa_data, preprocess_laa_extra_ancillary_data
+from .engine import derive_laa_risk_scores
+from .loader import load_laa_risk_score_data
 
 logger = setup_logger(__name__)
 
 
+def create_laa_risk_scores_download_file(headers_df: pd.DataFrame):
+    logger.info("Creating LAA risk scores download file...")
+    pass
+
+
 def run_laa_risk_scores_pipeline(run_year: int, run_id: str):
-    logger.info(
-        f"Starting LAA risk scores pipeline for year {run_year} (RunId: {run_id})..."
+    logger.info(f"Starting LAA risk scores pipeline for year {run_year} (RunId: {run_id})...")
+    cfr_with_all_extra_data = load_laa_risk_score_data(run_year)
+    indicators_df, headers_df = derive_laa_risk_scores(
+        cfr_with_all_extra_data, run_year=run_year, run_id=run_id
     )
-
-    raw_ancillary = load_laa_extra_ancillary_data(run_year)
-    absences, capacity, parental_preference = preprocess_laa_extra_ancillary_data(
-        absences_raw=raw_ancillary["absences_raw"],
-        capacity_raw=raw_ancillary["capacity_raw"],
-        capacity_special_raw=raw_ancillary["capacity_special_raw"],
-        parental_preference_raw=raw_ancillary["parental_preference_raw"],
-        run_year=run_year,
-    )
-
-    cfr_dfs = load_laa_risk_score_data(run_year)
-    cfr_with_all_extra_data = preprocess_laa_data(
-        cfr_data_this_year=cfr_dfs[0],
-        cfr_data_year_minus_one=cfr_dfs[1],
-        cfr_data_year_minus_two=cfr_dfs[2],
-        cfr_data_year_minus_three=cfr_dfs[3],
-        cfr_data_year_minus_four=cfr_dfs[4],
-        absences=absences,
-        capacity=capacity,
-        parental_preference=parental_preference,
-        run_year=run_year,
-    )
-
-    logger.info(
-        f"LAA risk scores data-loading pipeline completed for year {run_year} (RunId: {run_id})."
-    )
-
-    return cfr_with_all_extra_data
+    insert_laa_risk_scores(run_id, indicators_df, headers_df)
+    create_laa_risk_scores_download_file(headers_df)
+    logger.info(f"Completed LAA risk scores pipeline for year {run_year} (RunId: {run_id}).")
