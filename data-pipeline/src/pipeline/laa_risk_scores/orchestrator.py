2f3d436cd050b8e69464bb3371b9149173edad3a
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-08-05 14:36:09 +0100

    stash

--- data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
+++ data-pipeline/src/pipeline/laa_risk_scores/orchestrator.py
@@ -17,9 +17,9 @@ def create_laa_risk_scores_download_file(headers_df: pd.DataFrame):
 def run_laa_risk_scores_pipeline(run_year: int, run_id: str):
     logger.info(f"Starting LAA risk scores pipeline for year {run_year} (RunId: {run_id})...")
     cfr_with_all_extra_data = load_laa_risk_score_data(run_year)
-    indicators_df, headers_df = derive_laa_risk_scores(
+    risk_metrics_with_raw_data, melted_indicators_df, melted_headers_df = derive_laa_risk_scores(
         cfr_with_all_extra_data, run_year=run_year, run_id=run_id
     )
-    insert_laa_risk_scores(run_id, indicators_df, headers_df)
-    create_laa_risk_scores_download_file(headers_df)
+    insert_laa_risk_scores(run_id, melted_indicators_df, melted_headers_df)
+    create_laa_risk_scores_download_file(risk_metrics_with_raw_data)
     logger.info(f"Completed LAA risk scores pipeline for year {run_year} (RunId: {run_id}).")
