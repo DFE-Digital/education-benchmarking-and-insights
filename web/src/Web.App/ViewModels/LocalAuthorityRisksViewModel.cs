4eb3cb84dd1e6e11a0373dc786057367385b616a
J05h-L <117924208+J05h-L@users.noreply.github.com>
2026-08-06 12:33:20 +0100

    feat: create basic page layout for local authority risks overview (#4412)

---
+++ web/src/Web.App/ViewModels/LocalAuthorityRisksViewModel.cs
@@ -0,0 +1,10 @@
+using Web.App.Domain;
+
+namespace Web.App.ViewModels;
+
+public class LocalAuthorityRisksViewModel(LocalAuthority localAuthority, int cfrYear)
+{
+    public string? Code => localAuthority.Code;
+    public string? Name => localAuthority.Name;
+    public int CfrYear => cfrYear;
+}
