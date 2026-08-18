d5fb713db2a5407fda6a795efaf26cfbd20d178e
Josh Langford <josh.langford@hippodigital.co.uk>
2026-08-18 10:48:36 +0100

    feat: add OverallGrade to Web LocalAuthorityRiskIndicators and use to derive OverallGradeColour to display tag colour for la risks page

--- web/src/Web.App/Domain/LocalAuthorities/LocalAuthorityRiskIndicators.cs
+++ web/src/Web.App/Domain/LocalAuthorities/LocalAuthorityRiskIndicators.cs
@@ -7,9 +7,19 @@ public record LocalAuthorityRiskIndicators
 {
     public string Urn { get; init; } = string.Empty;
     public string SchoolName { get; init; } = string.Empty;
+    public string OverallGrade { get; init; } = string.Empty;
     public decimal Overall { get; init; }
-    public TagColour OverallRating { get; init; } = TagColour.Grey;
+    public string OverallGradeColour => MapGradeToTagColour(OverallGrade);
     public decimal Financial { get; init; }
     public decimal SchoolAndPupil { get; init; }
     public decimal EducationalPerformance { get; init; }
+
+    private static string MapGradeToTagColour(string? grade) =>
+        grade switch
+        {
+            "G" => "green",
+            "A" => "yellow",
+            "R" => "red",
+            _ => "grey"
+        };
 }
