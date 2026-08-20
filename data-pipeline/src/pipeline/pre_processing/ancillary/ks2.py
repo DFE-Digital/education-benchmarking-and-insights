2867574ffe485a70c415c1723386fc95add33a20
Murdo Moyse <murdo.moyse@hippodigital.co.uk>
2026-08-20 12:41:03 +0100

    Additional preprocessing for KS2 PTRWM_EXP field

--- data-pipeline/src/pipeline/pre_processing/ancillary/ks2.py
+++ data-pipeline/src/pipeline/pre_processing/ancillary/ks2.py
@@ -19,6 +19,9 @@ def prepare_ks2_data(ks2_path):
         ks2["READPROG"] = ks2["READPROG"].replace({"SUPP": "0", "LOWCOV": "0"})
         ks2["MATPROG"] = ks2["MATPROG"].replace({"SUPP": "0", "LOWCOV": "0"})
         ks2["WRITPROG"] = ks2["WRITPROG"].replace({"SUPP": "0", "LOWCOV": "0"})
+        ks2["PTRWM_EXP"] = ks2["PTRWM_EXP"].replace(
+            {"SUPP": "0", "LOWCOV": "0"}
+        ).str.replace("%", "").astype(float)
 
         ks2["Ks2Progress"] = (
             ks2["READPROG"].astype(float)
