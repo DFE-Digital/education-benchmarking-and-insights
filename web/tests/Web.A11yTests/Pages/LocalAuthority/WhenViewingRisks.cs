02cbbbfcace8da4379478fbca2d7efbfaeeff0d2
J05h-L <117924208+J05h-L@users.noreply.github.com>
2026-08-24 12:51:29 +0100

    test: add a11y tests for local authority risks page (#4445)

---
+++ web/tests/Web.A11yTests/Pages/LocalAuthority/WhenViewingRisks.cs
@@ -0,0 +1,31 @@
+using Web.A11yTests.Drivers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Web.A11yTests.Pages.LocalAuthority;
+
+[Trait("Category", "LocalAuthorityRisksEnabled")]
+public class WhenViewingLocalAuthorityRisks(
+    ITestOutputHelper testOutputHelper,
+    WebDriver webDriver)
+    : AuthPageBase(testOutputHelper, webDriver)
+{
+    protected override string PageUrl => $"/local-authority/{TestConfiguration.LocalAuthority}/risks";
+
+    [Fact]
+    public async Task ThenThereAreNoAccessibilityIssuesAcrossKeyInteractions()
+    {
+        // initial page
+        await GoToPage();
+        await EvaluatePage();
+
+        // apply a phase
+        await Page.Locator("#SelectedPhaseOption").SelectOptionAsync("Primary");
+        await Page.Locator("button:has-text(\"Apply\")").ClickAsync();
+        await EvaluatePage();
+
+        // apply a sort (clicking the header button)
+        await Page.Locator("th:has-text(\"URN\") button").ClickAsync();
+        await EvaluatePage();
+    }
+}
