253b00f4e9cc324c883740a7da9e89d7c1f0497c
murdo-hippo <murdo.moyse@hippodigital.co.uk>
2026-09-23 13:42:52 +0100

    feat: add 2026 schemas for BFR (#4518)

--- data-pipeline/src/pipeline/input_schemas/bfr.py
+++ data-pipeline/src/pipeline/input_schemas/bfr.py
@@ -1,24 +1,21 @@
+base_sofa_cols = {
+    "TrustUPIN": "Int64",
+    "Title": "string",
+    "EFALineNo": "Int64",
+    "Y1P1": "float",
+    "Y1P2": "float",
+    "Y2P1": "float",
+    "Y2P2": "float",
+}
+
+_2025_sofa_cols = base_sofa_cols.copy()
+_2025_sofa_cols["Y3P1"] = 'float'
+_2025_sofa_cols["Y3P2"] = 'float'
+
 bfr_sofa_cols = {
-    "default": {
-        "TrustUPIN": "Int64",
-        "Title": "string",
-        "EFALineNo": "Int64",
-        "Y1P1": "float",
-        "Y1P2": "float",
-        "Y2P1": "float",
-        "Y2P2": "float",
-    },
-    2025: {
-        "TrustUPIN": "Int64",
-        "Title": "string",
-        "EFALineNo": "Int64",
-        "Y1P1": "float",
-        "Y1P2": "float",
-        "Y2P1": "float",
-        "Y2P2": "float",
-        "Y3P1": "float",
-        "Y3P2": "float",
-    },
+    "default": base_sofa_cols,
+    2025: _2025_sofa_cols,
+    2026: _2025_sofa_cols,
 }
 
 bfr_3y_cols = {
